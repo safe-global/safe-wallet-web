db129062b9e97be0b50b19e51c96372050d2364a
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-13 21:24:35 +0200

    Refactor safe info service

--- services/useSafeInfo.ts
+++ services/useSafeInfo.ts
@@ -1,34 +1,74 @@
-import { useEffect } from 'react'
-import type { SafeInfo } from '@gnosis.pm/safe-react-gateway-sdk'
+import { useCallback, useEffect, useState } from 'react'
+import { getSafeInfo, SafeInfo } from '@gnosis.pm/safe-react-gateway-sdk'
 import { useAppDispatch } from 'store'
 import { setSafeInfo } from 'store/safeInfoSlice'
-import { initSafeInfoService } from 'services/safeInfoService'
 import useSafeAddress from './useSafeAddress'
+import { GATEWAY_URL, POLLING_INTERVAL } from 'config/constants'
+import useAsync from './useAsync'
+import { Errors, logError } from './exceptions/CodedException'
 
-const useSafeInfo = (): void => {
+const fetchSafeInfo = (chainId: string, address: string): Promise<SafeInfo> => {
+  return getSafeInfo(GATEWAY_URL, chainId, address)
+}
+
+// Fetch Safe Info every N seconds
+const usePolledSafeInfo = (
+  chainId: string,
+  address: string,
+): { safeInfo?: SafeInfo; loading: boolean; error?: Error } => {
+  const [fetchCount, setFetchCount] = useState<number>(0)
+
+  // Memoized function that loads safe info
+  const loadSafeInfo = useCallback(() => {
+    if (!chainId || !address) {
+      return Promise.resolve(undefined)
+    }
+    return fetchSafeInfo(chainId, address)
+  }, [address, chainId, fetchCount])
+
+  // Load safe info
+  const [data, error, loading] = useAsync<SafeInfo | undefined>(loadSafeInfo)
+
+  // Set a timer to re-fetch safe info after when data or error updates
+  useEffect(() => {
+    if (!data && !error) return
+
+    // Update the count so that loadSafeInfo is updated and called again
+    const timer = setTimeout(() => {
+      setFetchCount((prev: number) => prev + 1)
+    }, POLLING_INTERVAL)
+
+    // If the component is unmounted, the next poll won't happen
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [data, error, setFetchCount])
+
+  return { safeInfo: data, loading, error }
+}
+
+// Dispatch the Safe Info into the store
+const useSafeInfo = (): { loading: boolean; error?: Error } => {
   const { address, chainId } = useSafeAddress()
+  const { safeInfo, error, loading } = usePolledSafeInfo(chainId, address)
   const dispatch = useAppDispatch()
 
+  // Update the store when safe info is changed
   useEffect(() => {
-    if (!address || !chainId) return
+    if (!safeInfo || !address || !chainId) return
 
-    const { onSuccess, onError, unsubscribe } = initSafeInfoService(chainId, address)
+    // Check that the data is still relevant
+    if (safeInfo.chainId !== chainId || safeInfo.address.value !== address) return
 
-    const handleSuccess = (data: SafeInfo) => {
-      dispatch(setSafeInfo(data))
-    }
+    dispatch(setSafeInfo(safeInfo))
+  }, [dispatch, safeInfo, address, chainId])
 
-    const handleError = (error: Error) => {
-      console.error('Error loading Safe info', error)
-    }
+  // Log on error
+  useEffect(() => {
+    if (error) logError(Errors._605, error.message)
+  }, [error])
 
-    onSuccess(handleSuccess)
-    onError(handleError)
-
-    return () => {
-      unsubscribe()
-    }
-  }, [dispatch, address, chainId])
+  return { loading, error }
 }
 
 export default useSafeInfo
