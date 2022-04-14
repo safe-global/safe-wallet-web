0adc656bc4e8afaa7474a55fb0d548f7b689bf73
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-14 12:59:13 +0200

    Toggle sidebar on mobile

--- components/common/PageLayout/index.tsx
+++ components/common/PageLayout/index.tsx
@@ -1,11 +1,20 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Sidebar from '../Sidebar'
 import css from './styles.module.css'
 
 const PageLayout = ({ children }: { children: ReactElement }): ReactElement => {
+  const [sidebarExpanded, setSidebarExpanded] = useState<boolean>(false)
+
+  const onSidebarToggle = (e: React.SyntheticEvent) => {
+    e.stopPropagation()
+    setSidebarExpanded((prev: boolean) => !prev)
+  }
+
+  const sidebarClasses = [css.sidebar, sidebarExpanded ? css.sidebarExpanded : ''].join(' ')
+
   return (
-    <div className={css.container}>
-      <aside className={css.sidebar}>
+    <div className={css.container} onClick={() => setSidebarExpanded(false)}>
+      <aside className={sidebarClasses} onClick={onSidebarToggle}>
         <Sidebar />
       </aside>
 
