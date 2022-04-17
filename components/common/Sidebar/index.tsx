ef5f82c8feaae0bd19cdff6f31dc4a8d920538de
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-04-17 08:01:44 +0200

    feat: Add sidebar navigation (#15)
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- components/common/Sidebar/index.tsx
+++ components/common/Sidebar/index.tsx
@@ -1,23 +1,19 @@
 import { ReactElement, useState } from 'react'
 import { Button } from '@mui/material'
 
-import Link from 'next/link'
-import useSafeAddress from 'services/useSafeAddress'
 import { useAppSelector } from 'store'
 import { selectSafeInfo } from 'store/safeInfoSlice'
 import ChainIndicator from '../ChainIndicator'
 import SafeHeader from '../SafeHeader'
 import SafeList from '../SafeList'
 import ErrorToast from '../ErrorToast'
-import chains from 'config/chains'
 import css from './styles.module.css'
 import TxModal from 'components/tx/TxModal'
+import Navigation from 'components/common/Sidebar/Navigation'
 
 const Sidebar = (): ReactElement => {
   const [txOpen, setTxOpen] = useState<boolean>(false)
-  const { address, chainId } = useSafeAddress()
   const { error, loading } = useAppSelector(selectSafeInfo)
-  const shortName = Object.keys(chains).find((key) => chains[key] === chainId)
 
   return (
     <div className={css.container}>
@@ -33,25 +29,7 @@ const Sidebar = (): ReactElement => {
 
       {txOpen && <TxModal onClose={() => setTxOpen(false)} />}
 
-      <ul>
-        <li>
-          <Link href={`/${shortName}:${address}/balances`}>
-            <a>Balances</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href={`/${shortName}:${address}/transactions/history`}>
-            <a>History</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href={`/${shortName}:${address}/transactions/queue`}>
-            <a>Queue</a>
-          </Link>
-        </li>
-      </ul>
+      <Navigation />
 
       {!error && <SafeList />}
 
