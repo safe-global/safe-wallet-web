a3675e4ec14d0cce93eed16333a3e091fc5ed723
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-17 09:44:49 +0200

    Static routes

--- services/useSafeAddress.ts
+++ services/useSafeAddress.ts
@@ -3,9 +3,9 @@ import chains from 'config/chains'
 
 const useSafeAddress = (): { address: string; chainId: string } => {
   const router = useRouter()
-  let { safeAddress = '' } = router.query
-  if (Array.isArray(safeAddress)) safeAddress = safeAddress[0]
-  const [prefix, address] = safeAddress.split(':')
+  let { safe = '' } = router.query
+  if (Array.isArray(safe)) safe = safe[0]
+  const [prefix, address] = safe.split(':')
   const chainId = chains[prefix]
   return { address, chainId }
 }
