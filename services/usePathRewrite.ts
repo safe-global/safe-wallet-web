588fa12da64a4a5a6bfe8e91be85abb5d8488e94
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-22 10:09:17 +0200

    Fix: stop Safe polling when no safe address; add comments

--- services/usePathRewrite.ts
+++ services/usePathRewrite.ts
@@ -1,7 +1,15 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-// Rewrite the URL so that `/safe/` is replaced by the value in `?safe=`
+// Next.js needs to know all the static paths in advance when doing SSG (static site generation)
+// @see https://nextjs.org/docs/api-reference/next.config.js/runtime-config-static-paths
+// Since we cannot enumerate all the possible Safe addresses which are part of the path,
+// we need to use the `?safe=` query parameter to pass the Safe address.
+// However, the user still sees the `/safe/` path in the browser URL.
+// These two hooks make it work.
+
+// This hook takes the `?safe=` query parameter and rewrites the URL to put the Safe address into the path
+// Next.js doesn't care because dynamic path params are internally represented as query params anyway.
 const usePathRewrite = () => {
   const router = useRouter()
   let { safe = '' } = router.query
@@ -11,6 +19,7 @@ const usePathRewrite = () => {
     if (!safe) return
     const newPath = router.pathname.replace(/\/safe\//, `/${safe}/`)
     if (newPath !== router.pathname) {
+      // This just changes what you see in the URL bar w/o triggering any rendering or route change
       history.replaceState(history.state, '', newPath)
     }
   }, [safe, router.pathname])
@@ -18,7 +27,11 @@ const usePathRewrite = () => {
 
 export default usePathRewrite
 
-// Rewrite the URL back to `?safe=`
+// This hook is the opposite of the first one. It's called from the 404 page.
+// If we see that this URL wasn't found (i.e. the pass has a dynamic Safe address and no query),
+// we rewrite the URL to put the Safe address into the query.
+// This must and will cause a rendering and route change.
+// Thankfully, this will only happen if you reload the page or open this URL from an external link.
 export const useQueryRewrite = (): boolean => {
   const [redirecting, setRedirecting] = useState<boolean>(true)
   const router = useRouter()
--- services/useSafeInfo.ts
+++ services/useSafeInfo.ts
@@ -42,11 +42,18 @@ export const useInitSafeInfo = (): void => {
   )
 
   useEffect(() => {
-    if (!chainId || !address) return
-
     let isCurrent = true
     let timer: NodeJS.Timeout | null = null
 
+    // Stop polling on unmount
+    const onUnmount = () => {
+      isCurrent = false
+      timer && clearTimeout(timer)
+    }
+
+    if (!chainId || !address) return onUnmount
+
+    // Poll the Safe Info
     const loadSafe = async (isFirst = false) => {
       if (!isCurrent) return
 
@@ -68,13 +75,12 @@ export const useInitSafeInfo = (): void => {
       }
     }
 
-    // First load
+    // Start the loop.
+    // We pass true to indicate that this is the first load. Subsequent loads will pass false.
+    // This is important to ignore errors on subsequent polling requests.
     loadSafe(true)
 
-    return () => {
-      isCurrent = false
-      timer && clearTimeout(timer)
-    }
+    return onUnmount
   }, [chainId, address])
 }
 
