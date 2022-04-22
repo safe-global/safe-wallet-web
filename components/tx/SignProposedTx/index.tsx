dfbbe43012817f5b79dc689065cd5cbd736d3003
Diogo Soares <diogo.soares@gnosis.pm>
2022-04-22 13:20:51 +0200

    [wip] sign transactions

--- components/tx/SignProposedTx/index.tsx
+++ components/tx/SignProposedTx/index.tsx
@@ -1,9 +1,9 @@
 import { type SafeTransaction } from '@gnosis.pm/safe-core-sdk-types'
 import { getTransactionDetails, TransactionSummary } from '@gnosis.pm/safe-react-gateway-sdk'
-import { Button, Typography } from '@mui/material'
+import { Button, Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material'
 import ErrorToast from '@/components/common/ErrorToast'
 import { ReactElement, useState } from 'react'
-import { createTransaction, signTransaction } from '@/services/createTransaction'
+import { createTransaction, signTransaction, executeTransaction } from '@/services/createTransaction'
 import extractTxInfo from '@/services/extractTxInfo'
 import useSafeAddress from '@/services/useSafeAddress'
 import css from './styles.module.css'
@@ -21,8 +21,10 @@ const SignProposedTx = ({
 }): ReactElement => {
   const { chainId } = useSafeAddress()
   const [error, setError] = useState<Error>()
+  const [shouldExecute, setShouldExecute] = useState<boolean>(true)
 
   const onSign = async () => {
+    console.log('onSign')
     try {
       const txDetails = await getTxDetails(chainId, txSummary.id)
       const { txParams, signatures } = extractTxInfo(txSummary, txDetails)
@@ -33,7 +35,27 @@ const SignProposedTx = ({
       })
 
       const signedTx = await signTransaction(safeTx)
-      onSubmit(signedTx)
+      // await executeTransaction(safeTx)
+      // onSubmit(signedTx)
+    } catch (err) {
+      setError(err as Error)
+    }
+  }
+
+  const onSignAndExecute = async () => {
+    console.log('onSignAndExecute')
+    try {
+      const txDetails = await getTxDetails(chainId, txSummary.id)
+      const { txParams, signatures } = extractTxInfo(txSummary, txDetails)
+
+      const safeTx = await createTransaction(txParams)
+      Object.entries(signatures).forEach(([signer, data]) => {
+        safeTx.addSignature({ signer, data, staticPart: () => data, dynamicPart: () => '' })
+      })
+
+      const signedTx = await signTransaction(safeTx)
+      await executeTransaction(safeTx)
+      // onSubmit(signedTx)
     } catch (err) {
       setError(err as Error)
     }
@@ -43,11 +65,18 @@ const SignProposedTx = ({
     <div className={css.container}>
       <Typography variant="h6">Confirm transaction</Typography>
 
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={shouldExecute} onChange={(e) => setShouldExecute(e.target.checked)} />}
+          label="Execute Transaction"
+        />
+      </FormGroup>
+
       <div>Transaction id</div>
       <pre style={{ overflow: 'auto', width: '100%' }}>{txSummary.id}</pre>
 
       <div className={css.submit}>
-        <Button variant="contained" onClick={onSign}>
+        <Button variant="contained" onClick={shouldExecute ? onSignAndExecute : onSign}>
           Sign
         </Button>
       </div>
