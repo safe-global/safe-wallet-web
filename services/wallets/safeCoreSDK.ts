84ff72f088c1a84db64f94f2dc3fe9793de54991
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-24 13:45:20 +0200

    Code SDK: own web3

--- services/wallets/safeCoreSDK.ts
+++ services/wallets/safeCoreSDK.ts
@@ -1,14 +1,16 @@
+import { type EIP1193Provider } from '@web3-onboard/core'
+import { type provider } from 'web3-core'
 import Web3 from 'web3'
 import Safe from '@gnosis.pm/safe-core-sdk'
 import Web3Adapter from '@gnosis.pm/safe-web3-lib'
 import semverSatisfies from 'semver/functions/satisfies'
 import chains from '@/config/chains'
-import { getWeb3 } from './web3'
 
 const LEGACY_VERSION = '<1.3.0'
 
 // Safe Core SDK
 const initSafeSDK = async (
+  provider: EIP1193Provider,
   signerAddress: string,
   chainId: string,
   safeAddress: string,
@@ -16,7 +18,7 @@ const initSafeSDK = async (
 ): Promise<Safe> => {
   const ethAdapter = new Web3Adapter({
     signerAddress,
-    web3: getWeb3(),
+    web3: new Web3(provider as unknown as provider),
   })
 
   let isL1SafeMasterCopy = chainId === chains.eth
