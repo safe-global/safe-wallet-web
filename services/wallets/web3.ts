28904b807c7d1b02b812da83bae80c03dcadb045
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-04-24 12:21:11 +0200

    Put functions on top

--- services/wallets/web3.ts
+++ services/wallets/web3.ts
@@ -2,6 +2,16 @@ import Web3 from 'web3'
 import { RPC_AUTHENTICATION, type ChainInfo, type RpcUri } from '@gnosis.pm/safe-react-gateway-sdk'
 import { INFURA_TOKEN } from '@/config/constants'
 
+// RPC helpers
+const formatRpcServiceUrl = ({ authentication, value }: RpcUri, TOKEN: string): string => {
+  const needsToken = authentication === RPC_AUTHENTICATION.API_KEY_PATH
+  return needsToken ? `${value}${TOKEN}` : value
+}
+
+export const getRpcServiceUrl = (rpcUri: RpcUri): string => {
+  return formatRpcServiceUrl(rpcUri, INFURA_TOKEN)
+}
+
 // Web3 readonly
 let _web3ReadOnly: Web3
 
@@ -13,13 +23,4 @@ export const setWeb3ReadOnly = ({ rpcUri }: ChainInfo): void => {
       timeout: 10_000,
     }),
   )
-}
-
-const formatRpcServiceUrl = ({ authentication, value }: RpcUri, TOKEN: string): string => {
-  const needsToken = authentication === RPC_AUTHENTICATION.API_KEY_PATH
-  return needsToken ? `${value}${TOKEN}` : value
-}
-
-export const getRpcServiceUrl = (rpcUri: RpcUri): string => {
-  return formatRpcServiceUrl(rpcUri, INFURA_TOKEN)
 }
