bf987be6041da2f3f2e51c05535cdee605a0f6b5
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-05-03 15:34:09 +0200

    Move _document getInitialProps to an static prop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,38 @@ import { safeTheme } from '@gnosis.pm/safe-react-components'
 import createEmotionCache from '@/services/createEmotionCache'
 
 class SafeWebCoreDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage
+    const cache = createEmotionCache()
+    const { extractCriticalToChunks } = createEmotionServer(cache)
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) =>
+          function EnhanceApp(props) {
+            return <App emotionCache={cache} {...props} />
+          },
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    // This is important. It prevents emotion to render invalid HTML.
+    // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ))
+
+    return {
+      ...initialProps,
+      emotionStyleTags,
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -21,38 +53,6 @@ class SafeWebCoreDocument extends Document {
         </body>
       </Html>
     )
-  }
-}
-
-SafeWebCoreDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const originalRenderPage = ctx.renderPage
-  const cache = createEmotionCache()
-  const { extractCriticalToChunks } = createEmotionServer(cache)
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />
-        },
-    })
-
-  const initialProps = await Document.getInitialProps(ctx)
-  // This is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
-  const emotionStyles = extractCriticalToChunks(initialProps.html)
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ))
-
-  return {
-    ...initialProps,
-    emotionStyleTags,
   }
 }
 
