100e242fdbe2ff9657220c3828b3c4622a6d7917
iamacook <aaron.cook@gnosis.pm>
2022-05-06 13:14:57 +0200

    feat: add pending tx slice

---
+++ store/pendingTxsSlice.ts
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice, type PayloadAction } from '@reduxjs/toolkit'
+
+import type { RootState } from '@/store'
+import { getWeb3 } from '@/services/wallets/web3'
+
+interface PendingTxsState {
+  [chainId: string]: {
+    [txId: string]: {
+      txHash: string
+      block?: number
+    }
+  }
+}
+
+const initialState: PendingTxsState = {}
+
+export const pendingTxsSlice = createSlice({
+  name: 'pendingTxs',
+  initialState,
+  reducers: {
+    addPendingTx: (state, action: PayloadAction<{ chainId: string; txId: string; txHash: string; block?: number }>) => {
+      const { chainId, txId, txHash, block } = action.payload
+      return {
+        ...state,
+        [chainId]: {
+          ...state[chainId],
+          [txId]: {
+            txHash,
+            block,
+          },
+        },
+      }
+    },
+    removePendingTx: (state, action: PayloadAction<{ txId: string; chainId: string }>) => {
+      const { chainId, txId } = action.payload
+
+      // Omit txId from the pending txs on current chain
+      const { [txId]: _, ...newChainState } = state[chainId] || {}
+
+      if (Object.keys(newChainState || {}).length === 0) {
+        // Omit chainId from the pending txs if no pending txs on chain
+        const { [chainId]: _, ...newState } = state
+        return newState
+      }
+
+      return {
+        ...state,
+        [chainId]: newChainState,
+      }
+    },
+  },
+})
+
+export const { removePendingTx } = pendingTxsSlice.actions
+
+export const setPendingTx = createAsyncThunk<void, { chainId: string; txId: string; txHash: string }>(
+  `${pendingTxsSlice.name}/setPendingTx`,
+  async (details, { dispatch }) => {
+    let block: number | undefined
+
+    try {
+      block = await getWeb3().eth.getBlockNumber()
+    } catch {
+      // ignore
+    }
+
+    dispatch(
+      pendingTxsSlice.actions.addPendingTx({
+        ...details,
+        block,
+      }),
+    )
+  },
+)
+
+export const selectSafeInfo = (state: RootState): PendingTxsState => {
+  return state[pendingTxsSlice.name]
+}
