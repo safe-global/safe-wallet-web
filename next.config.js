7dc795bfd6875d578937d1fa4d2f36aed6aaba42
Usame Algan <usame.algan@gnosis.pm>
2022-05-08 19:02:40 +0200

    fix: Include bn.js only once in the bundle

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: false,
   eslint: {
     dirs: ['pages', 'services', 'store', 'components', 'config'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'bn.js': path.join(__dirname, 'node_modules/bn.js/lib/bn.js'),
+    }
+    return config
   },
 }
 
