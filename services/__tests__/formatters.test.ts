64526a65399f073b96a369d51fd1856778c44577
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-05-22 13:08:30 +0200

    Remove bignumber.js, use ethers BN

--- services/__tests__/formatters.test.ts
+++ services/__tests__/formatters.test.ts
@@ -1,4 +1,4 @@
-import BigNumber from 'bignumber.js'
+import { BigNumber } from 'ethers'
 import * as formatters from '../formatters'
 
 describe('shortenAddress', () => {
@@ -16,17 +16,21 @@ describe('formatDecimals', () => {
     expect(formatters.formatDecimals('100000000000000000')).toEqual('0.1')
   })
 
-  it('should format decimals with custom decimals', () => {
-    expect(formatters.formatDecimals('1000000000000000000', 6)).toEqual('1000000000000')
+  it('should format a big number with custom decimals', () => {
+    expect(formatters.formatDecimals('99999910000000000000000', 6)).toEqual('99,999,910,000,000,000')
+  })
+
+  it('should format a fractional number with custom decimals', () => {
+    expect(formatters.formatDecimals('12000001', 6)).toEqual('12.000001')
   })
 })
 
 describe('toDecimals', () => {
   it('should convert to decimals', () => {
-    expect(formatters.toDecimals('2.01')).toEqual(new BigNumber('2010000000000000000'))
+    expect(formatters.toDecimals('2.01')).toEqual(BigNumber.from('2010000000000000000'))
   })
 
   it('should convert to decimals with custom decimals', () => {
-    expect(formatters.toDecimals('3', 6)).toEqual(new BigNumber('3000000'))
+    expect(formatters.toDecimals('3', 6)).toEqual(BigNumber.from('3000000'))
   })
 })
--- services/formatters.ts
+++ services/formatters.ts
@@ -1,11 +1,18 @@
-import { BigNumber } from 'bignumber.js'
+import { type BigNumber } from 'ethers'
+import { formatUnits, parseUnits } from 'ethers/lib/utils'
+
+const formatter = new Intl.NumberFormat([], {
+  style: 'decimal',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 8,
+})
 
 export const formatDecimals = (value: string, decimals = 18): string => {
-  return new BigNumber(value).times(`1e-${decimals}`).toFixed()
+  return formatter.format(Number(formatUnits(value, decimals)))
 }
 
-export const toDecimals = (value: string, decimals = 18): BigNumber => {
-  return new BigNumber(value).div(`1e-${decimals}`)
+export const toDecimals = (value: string, decimals = 18): BigNumber  => {
+  return parseUnits(value, decimals)
 }
 
 export const shortenAddress = (address: string, length = 4): string => {
