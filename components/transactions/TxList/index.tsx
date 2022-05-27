d22523f52bb6835b1609fd6ff6e6e45242a5e325
iamacook <aaron.cook@gnosis.pm>
2022-05-27 18:26:44 +0200

    fix: don't manually render label component

--- components/transactions/TxList/index.tsx
+++ components/transactions/TxList/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo, type ReactElement } from 'react'
-import { type TransactionListPage } from '@gnosis.pm/safe-react-gateway-sdk'
+import { DateLabel, Transaction, type TransactionListPage } from '@gnosis.pm/safe-react-gateway-sdk'
 import TxListItem from '../TxListItem'
 import TxDateLabel from '../TxDateLabel'
 import { isDateLabel, isTransaction } from '../utils'
@@ -9,27 +9,27 @@ type TxListProps = {
 }
 
 const TxList = ({ items }: TxListProps): ReactElement => {
-  const shouldPrependDateLabel = useMemo(() => {
+  const list = useMemo(() => {
     const firstDateLabelIndex = items.findIndex(isDateLabel)
     const firstTxIndex = items.findIndex(isTransaction)
-    return firstDateLabelIndex === -1 || firstTxIndex === -1 || firstDateLabelIndex > firstTxIndex
-  }, [items])
+    const shouldPrependDateLabel =
+      firstDateLabelIndex === -1 || firstTxIndex === -1 || firstDateLabelIndex > firstTxIndex
 
-  const firstTx = useMemo(() => {
-    return items.find(isTransaction)
+    if (!shouldPrependDateLabel) {
+      return items
+    }
+
+    const dateLabel: DateLabel = {
+      type: 'DATE_LABEL',
+      timestamp: (items[firstTxIndex] as Transaction).transaction.timestamp,
+    }
+
+    return [dateLabel, ...items]
   }, [items])
 
   return (
     <div>
-      {shouldPrependDateLabel && firstTx && (
-        <TxDateLabel
-          item={{
-            type: 'DATE_LABEL',
-            timestamp: firstTx.transaction.timestamp,
-          }}
-        />
-      )}
-      {items.map((item, index) => (
+      {list.map((item, index) => (
         <TxListItem key={index} item={item} />
       ))}
     </div>
