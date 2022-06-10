4daeb485e048f2728df7c4bb477565249c8b0c6f
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-10 17:46:21 +0200

    Feat: add stubbed analytics

---
+++ components/common/Track/index.tsx
@@ -0,0 +1,48 @@
+import { ReactElement, Fragment, useEffect, useRef } from 'react'
+
+import { EventLabel, GTM_EVENT, trackEvent } from '@/services/analytics'
+
+type Props = {
+  children: ReactElement
+  as?: 'span' | 'div'
+  category: string
+  action: string
+  label?: EventLabel
+}
+
+const Track = ({ children, as: Wrapper = 'div', ...trackData }: Props): typeof children => {
+  const el = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!el.current) {
+      return
+    }
+
+    const trackEl = el.current
+
+    const handleClick = () => {
+      trackEvent({
+        event: GTM_EVENT.CLICK,
+        ...trackData,
+      })
+    }
+
+    // We cannot use onClick as events in children do not always bubble up
+    trackEl.addEventListener('click', handleClick)
+    return () => {
+      trackEl.removeEventListener('click', handleClick)
+    }
+  }, [el, trackData])
+
+  if (children.type === Fragment) {
+    throw new Error('Fragments cannot be tracked.')
+  }
+
+  return (
+    <Wrapper data-track={`${trackData.category}: ${trackData.action}`} ref={el}>
+      {children}
+    </Wrapper>
+  )
+}
+
+export default Track
