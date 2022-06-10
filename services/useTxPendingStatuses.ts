7dd7c93fecd79d3e12c22824f691d645f1f8a49c
iamacook <aaron.cook@gnosis.pm>
2022-06-10 11:39:34 +0200

    fix: add comment

--- services/useTxPendingStatuses.ts
+++ services/useTxPendingStatuses.ts
@@ -77,6 +77,7 @@ export const useMonitorTxs = (): void => {
         }
       }),
     )
+    // `provider` is updated when switching chains, re-running this effect
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [provider])
 }
