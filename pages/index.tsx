08efe60ecfec2e7ee7662a2a84bd76fc150b4ea0
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-13 18:41:03 +0200

    Refactor: move useLastSafe out of the slice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useLastSafeAddress } from '@/store/sessionSlice'
+import { selectLastSafeAddress } from '@/store/sessionSlice'
+import { useAppSelector } from '@/store'
+import useChainId from '@/services/useChainId'
+import chains from '@/config/chains'
+
+const useLastSafe = (): string | undefined => {
+  const chainId = useChainId()
+  const lastSafeAddress = useAppSelector((state) => selectLastSafeAddress(state, chainId))
+  const prefix = Object.keys(chains).find((prefix) => chains[prefix] === chainId)
+  return prefix && lastSafeAddress ? `${prefix}:${lastSafeAddress}` : undefined
+}
 
 const IndexPage: NextPage = () => {
   const router = useRouter()
   const { chain } = router.query
-  const lastSafeAddress = useLastSafeAddress()
+  const lastSafe = useLastSafe()
 
   useEffect(() => {
-    router.push(lastSafeAddress ? `/${lastSafeAddress}` : chain ? `/welcome?chain=${chain}` : `/welcome`)
-  }, [router, lastSafeAddress, chain])
+    router.push(lastSafe ? `/${lastSafe}` : chain ? `/welcome?chain=${chain}` : `/welcome`)
+  }, [router, lastSafe, chain])
 
   return <></>
 }
