971c295f0d04260cbc05afd6670e342894a53b27
iamacook <aaron.cook@gnosis.pm>
2022-06-13 14:13:57 +0200

    fix: remove unnecessary cache

--- services/wallets/useInitWeb3.ts
+++ services/wallets/useInitWeb3.ts
@@ -1,12 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { useCurrentChain } from '@/services/useChains'
 import useWallet from '@/services/wallets/useWallet'
 import { createWeb3, createWeb3ReadOnly, setWeb3, setWeb3ReadOnly } from '@/services/wallets/web3'
 
 export const useInitWeb3 = () => {
-  const [readOnlyChainId, setReadOnlyChainId] = useState<string>()
-
   const chain = useCurrentChain()
   const wallet = useWallet()
 
@@ -19,14 +17,11 @@ export const useInitWeb3 = () => {
   }, [chain, wallet])
 
   useEffect(() => {
-    if (!chain || readOnlyChainId === chain.chainId) {
+    if (!chain) {
       return
     }
     const web3ReadOnly = createWeb3ReadOnly(chain)
     setWeb3ReadOnly(web3ReadOnly)
-
-    // We cache the `chainId` to avoid calling `web3ReadOnly.getNetwork()` above
-    setReadOnlyChainId(chain.chainId)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chain])
+  }, [chain?.chainId])
 }
