e5f3aaf078ba4f191b8f3e686d3cf585a1d8880a
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-14 19:48:08 +0200

    Unit tests

--- services/useGasPrice.ts
+++ services/useGasPrice.ts
@@ -1,4 +1,4 @@
-import { BigNumber, FixedNumber } from 'ethers'
+import { BigNumber } from 'ethers'
 import { FeeData } from '@ethersproject/providers'
 import { GasPrice, GasPriceOracle, GAS_PRICE_TYPE } from '@gnosis.pm/safe-react-gateway-sdk'
 import useAsync from '@/services/useAsync'
@@ -13,25 +13,33 @@ const fetchGasOracle = async (gasPriceOracle: GasPriceOracle): Promise<BigNumber
   const { uri, gasParameter, gweiFactor } = gasPriceOracle
   const json = await fetch(uri).then((resp) => resp.json())
   const data = json.data || json.result || json
-  return BigNumber.from(gweiFactor).mul(FixedNumber.from(data[gasParameter]).toHexString())
+  return BigNumber.from(data[gasParameter] * Number(gweiFactor))
 }
 
 const getGasPrice = async (gasPriceConfigs: GasPrice): Promise<BigNumber | undefined> => {
+  let error: Error | undefined
+
   for (const config of gasPriceConfigs) {
+    if (config.type == GAS_PRICE_TYPE.FIXED) {
+      return BigNumber.from(config.weiValue)
+    }
+
     if (config.type == GAS_PRICE_TYPE.ORACLE) {
       try {
         return await fetchGasOracle(config)
-      } catch (error) {
-        console.error('Error fetching gas price from oracle', error)
+      } catch (err) {
+        error = err as Error
+        console.error('Error fetching gas price from oracle', err)
+        // Continue to the next oracle
         continue
       }
-    } else if (config.type == GAS_PRICE_TYPE.FIXED) {
-      return BigNumber.from(config.weiValue)
-    } else {
-      continue
     }
   }
-  return undefined
+
+  // If everything failed, throw the last error or return undefined
+  if (error) {
+    throw error
+  }
 }
 
 const useGasPrice = (): {
