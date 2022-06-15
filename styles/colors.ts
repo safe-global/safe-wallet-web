3fb1016b63031bcf418766a78052b1478a38a18d
iamacook <aaron.cook@gnosis.pm>
2022-06-15 18:36:22 +0200

    feat: theme

---
+++ styles/colors.ts
@@ -0,0 +1,55 @@
+export const primary = {
+  200: '#CBF1EB',
+  300: '#72B5A7',
+  400: '#008C73',
+  500: '#0E7361',
+  600: '#005546',
+}
+
+export const black = {
+  300: '#ACBAC3',
+  400: '#566976',
+  500: '#383E42',
+  600: '#001428',
+}
+
+export const gray = {
+  300: '#F6F7F8',
+  400: '#F0EFEE',
+  500: '#E2E3E3',
+}
+
+export const error = {
+  600: '#C31717',
+}
+
+export const red = {
+  200: '#FFC7D0',
+  300: '#FFC7D0',
+  400: '#F02525',
+}
+
+export const green = {
+  200: '#EFFAF1',
+  400: '#008C30',
+}
+
+export const warning = {
+  600: '#C97C05',
+}
+
+export const orange = {
+  200: '#FFF4E3',
+  300: '#FBE5C5',
+  400: '#FFC05F',
+  500: '#E8663D',
+}
+
+// Not listed in colour scheme but present in wireframes
+export const secondaryBlack = {
+  300: '#B2BBC0',
+}
+
+export const primaryBlack = {
+  500: '#162D45',
+}
