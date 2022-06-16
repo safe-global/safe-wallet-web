a9134a19b6f04fcd185121a4959ab5c84403998b
Diogo Soares <diogo.soares@gnosis.pm>
2022-06-16 12:02:37 +0200

    add more tx summary types support - address, hash, raw data

--- components/transactions/TxDetails/Summary/TxDataRow/index.tsx
+++ components/transactions/TxDetails/Summary/TxDataRow/index.tsx
@@ -1,3 +1,5 @@
+import { shortenAddress } from '@/services/formatters'
+import { hexDataLength } from 'ethers/lib/utils'
 import { ReactElement, ReactNode } from 'react'
 import css from './styles.module.css'
 
@@ -6,10 +8,47 @@ interface Props {
   inlineType?: 'hash' | 'rawData' | 'address' | 'bytes'
   hasExplorer?: boolean
   title: string
-  value?: string | null | ReactElement
+  value?: string | null
   isArray?: boolean
   method?: string
   paramType?: string
+}
+
+const generateInlineTypeValue = (
+  type: Props['inlineType'],
+  value?: string,
+  hasExplorer?: boolean,
+): ReactElement | null => {
+  if (!value) return null
+  switch (type) {
+    case 'address':
+      return (
+        <div className={css.inline}>
+          {/* TODO: missing the chain prefix */}
+          <p>{shortenAddress(value, 8)}</p>
+          {/* TODO: missing copy button */}
+          {/* TODO: missing block explorer button */}
+        </div>
+      )
+    case 'hash':
+      return (
+        <div className={css.inline}>
+          <div>{shortenAddress(value, 8)}</div>
+          {/* TODO: missing copy button */}
+          {/* TODO: missing block explorer button */}
+        </div>
+      )
+    case 'rawData':
+      return (
+        <div className={css.rawData}>
+          <p>{value ? hexDataLength(value) : 0} bytes</p>
+          {/* TODO: missing copy button */}
+        </div>
+      )
+    // case 'bytes':
+    //   return <HexEncodedData limit={60} hexData={value} />
+  }
+  return null
 }
 
 export const TxDataRow = ({
@@ -32,10 +71,8 @@ export const TxDataRow = ({
           {JSON.stringify(value)}
         </div>
       )}
-      {/* {!isArray && generateInlineTypeValue(inlineType, value, hasExplorer)} */}
-      {!isArray && value}
-      {/* missing logic here */}
-      {/* {!isArray && !inlineType && value && <span>{value}</span>} */}
+      {!isArray && generateInlineTypeValue(inlineType, value, hasExplorer)}
+      {!isArray && !inlineType && value && <span>{value}</span>}
       {children}
     </div>
   )
