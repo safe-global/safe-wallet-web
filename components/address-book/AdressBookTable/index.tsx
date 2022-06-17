684a487441f038f04ab7f3b6ff0bccd9c26521f2
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-17 15:38:42 +0200

    Feat: paginated table

--- components/address-book/AdressBookTable/index.tsx
+++ components/address-book/AdressBookTable/index.tsx
@@ -1,21 +1,30 @@
+import EnhancedTable from '@/components/common/EnhancedTable'
 import useAddressBook from '@/services/useAddressBook'
-import { DataGrid } from '@mui/x-data-grid'
 
-const columns = [
-  { field: 'name', headerName: 'Name', flex: 1 },
-  { field: 'id', headerName: 'Address', width: 400 },
-  { field: 'actions', headerName: '', flex: 0.5 },
+const headCells = [
+  { id: 'name', label: 'Name' },
+  { id: 'address', label: 'Address' },
+  { id: 'actions', label: '' },
 ]
 
 const AddressBookTable = () => {
   const addressBook = useAddressBook()
-  const rows = Object.entries(addressBook).map(([id, name]) => ({ id, name }))
+  const rows = Object.entries(addressBook).map(([address, name]) => ({
+    name: {
+      rawValue: name,
+      content: name,
+    },
+    address: {
+      rawValue: address,
+      content: address,
+    },
+    actions: {
+      rawValue: '',
+      content: '',
+    },
+  }))
 
-  return (
-    <div style={{ height: 631, width: '100%' }}>
-      <DataGrid columns={columns} rows={rows} pageSize={10} disableSelectionOnClick />
-    </div>
-  )
+  return <EnhancedTable rows={rows} headCells={headCells} />
 }
 
 export default AddressBookTable
