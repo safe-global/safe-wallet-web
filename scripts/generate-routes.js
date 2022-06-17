9e82c541b97088d21025dd2c637ca360bc3a6b9a
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-17 13:43:44 +0200

    Feat: generate a routes config from fs (#95)
    * Feat: generate a routes config from fs
    
    * Index route
    
    * Format camelCase
    
    * Reorder if else
    
    * Prettier

---
+++ scripts/generate-routes.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+const isFile = (item) => item.endsWith`.tsx`
+
+const iterate = (folderName, parentRoute, root) => {
+  const items = fs.readdirSync(folderName)
+
+  items
+    .sort((a, b) => (isFile(a) ? -1 : 1))
+    .forEach((item) => {
+      // Skip service files
+      if (/_app|_document|404/.test(item)) return
+
+      // A folder, continue iterating
+      if (!isFile(item)) {
+        root[item] = {}
+        iterate(`${folderName}/${item}`, `${parentRoute}/${item}`, root[item])
+        return
+      }
+
+      // A file
+      const name = item.split('.')[0]
+      const path = name === 'index' ? parentRoute : `${parentRoute}/${name}`
+      const key = name
+        .replace(/-\w/g, (match) => match.replace(/-/g, '').toUpperCase()) // spending-limit -> spendingLimit
+        .replace(/\W/g, '') // [txId] -> txId
+      root[key] = path || '/'
+    })
+
+  return root
+}
+
+const routes = iterate('pages', '', {})
+
+console.log(`export const AppRoutes = ${JSON.stringify(routes, null, 2)}`)
