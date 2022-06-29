4034125c9845c2a711b12f908918b4bd9a2003dc
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-29 11:38:44 +0200

    Try css cache again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import Head from 'next/head'
 import { Provider } from 'react-redux'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { setBaseUrl } from '@gnosis.pm/safe-react-gateway-sdk'
-import { StyledEngineProvider } from '@mui/material/styles'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
 import theme from '@/styles/theme'
-
 import '@/styles/globals.css'
 import { IS_PRODUCTION, STAGING_GATEWAY_URL } from '@/config/constants'
 import { store } from '@/store'
@@ -26,6 +26,11 @@ import useTxNotifications from '@/hooks/useTxNotifications'
 import useTxPendingStatuses, { useTxMonitor } from '@/hooks/useTxPendingStatuses'
 import { useInitSession } from '@/hooks/useInitSession'
 import Notifications from '@/components/common/Notifications'
+
+const cssCache = createCache({
+  key: 'css',
+  prepend: true,
+})
 
 const InitApp = (): null => {
   if (!IS_PRODUCTION) {
@@ -61,7 +66,7 @@ const SafeWebCore = ({ Component, pageProps }: AppProps): ReactElement => {
 
       {/* @ts-ignore - Temporary Fix */}
       <Sentry.ErrorBoundary showDialog fallback={({ error }) => <div>{error.message}</div>}>
-        <StyledEngineProvider injectFirst>
+        <CacheProvider value={cssCache}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <InitApp />
@@ -71,7 +76,7 @@ const SafeWebCore = ({ Component, pageProps }: AppProps): ReactElement => {
 
             <Notifications />
           </ThemeProvider>
-        </StyledEngineProvider>
+        </CacheProvider>
       </Sentry.ErrorBoundary>
     </Provider>
   )
