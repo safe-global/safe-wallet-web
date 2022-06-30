a462c3ef7392fd194e9f0a461ab0e6a2e88ef6c4
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-06-30 13:50:11 +0200

    Move constants to fns

--- utils/validation.ts
+++ utils/validation.ts
@@ -15,25 +15,24 @@ export const validateAddress = (value: string): string | undefined => {
   }
 }
 
-export const ADDRESS_REPEATED_ERROR = 'Address already added'
-
 export const uniqueAddress =
   (addresses: string[] = []) =>
   (address?: string): string | undefined => {
+    const ADDRESS_REPEATED_ERROR = 'Address already added'
     const addressExists = addresses.some((addressFromList) => sameAddress(addressFromList, address))
     return addressExists ? ADDRESS_REPEATED_ERROR : undefined
   }
 
-export const OWNER_ADDRESS_IS_SAFE_ADDRESS_ERROR = 'Cannot use Safe itself as owner.'
-
 export const addressIsNotCurrentSafe =
   (safeAddress: string) =>
-  (address?: string): string | undefined =>
-    sameAddress(safeAddress, address) ? OWNER_ADDRESS_IS_SAFE_ADDRESS_ERROR : undefined
+  (address?: string): string | undefined => {
+    const OWNER_ADDRESS_IS_SAFE_ADDRESS_ERROR = 'Cannot use Safe itself as owner.'
+    return sameAddress(safeAddress, address) ? OWNER_ADDRESS_IS_SAFE_ADDRESS_ERROR : undefined
+  }
 
 export const FLOAT_REGEX = /^[0-9]+([,.][0-9]+)?$/
 
-export const validateTokenAmount = (amount: string, token?: { balance: string, tokenInfo: TokenInfo }) => {
+export const validateTokenAmount = (amount: string, token?: { balance: string; tokenInfo: TokenInfo }) => {
   if (!token) return
 
   if (isNaN(Number(amount))) {
