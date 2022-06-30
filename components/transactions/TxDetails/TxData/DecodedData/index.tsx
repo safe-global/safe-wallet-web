35093108ae3ea5e7a2a57fa06358678d7c429bb7
Diogo Soares <diogo.soares@gnosis.pm>
2022-06-30 11:00:41 +0200

    rm some wip comments

--- components/transactions/TxDetails/TxData/DecodedData/index.tsx
+++ components/transactions/TxDetails/TxData/DecodedData/index.tsx
@@ -14,46 +14,12 @@ export const DecodedData = ({ txData, txInfo }: Props): ReactElement | null => {
     return null
   }
 
-  // // unknown tx information
-  // if (!txData.dataDecoded) {
-  //   // no hex data, nothing to render
-  //   if (!txData.hexData) {
-  //     return null
-  //   }
-
-  //   // we render the hex encoded data
-  //   return (
-  //     <DetailsWithTxInfo txData={txData} txInfo={txInfo}>
-  //       <HexEncodedData title="Data (hex encoded)" hexData={txData.hexData} />
-  //     </DetailsWithTxInfo>
-  //   )
-  // }
-
-  // // known data and particularly `multiSend` data type
-  // if (sameString(txData.dataDecoded.method, 'multiSend')) {
-  //   return <MultiSendDetails txData={txData} />
-  // }
-
   if (isMultisendTxInfo(txInfo)) {
     return <Multisend txData={txData} />
   }
-  // // FixMe: this way won't scale well
-  // if (isSetAllowance(txData.dataDecoded.method)) {
-  //   return <ModifySpendingLimitDetails txData={txData} txInfo={txInfo} />
-  // }
 
-  // // FixMe: this way won't scale well
-  // if (isDeleteAllowance(txData.dataDecoded.method)) {
-  //   return <DeleteSpendingLimitDetails txData={txData} txInfo={txInfo} />
-  // }
-
-  // we render the decoded data
-  return (
-    <></>
-    // <DetailsWithTxInfo txData={txData} txInfo={txInfo}>
-    //   <MethodDetails data={txData.dataDecoded} />
-    // </DetailsWithTxInfo>
-  )
+  // TODO: we render the decoded data
+  return <></>
 }
 
 export default DecodedData
