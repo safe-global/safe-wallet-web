b150cf5ad893ff2ba1d56f3e999763f10e52293a
Mikhail <mmvsha73@gmail.com>
2022-07-01 13:43:42 +0200

    use tl/react for testing hooks

--- hooks/__tests__/useAsync.test.ts
+++ hooks/__tests__/useAsync.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
 import useAsync from '@/hooks/useAsync'
 
 // Jest tests for the useAsync hook
--- hooks/__tests__/usePathRewrite.test.ts
+++ hooks/__tests__/usePathRewrite.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
 import { useRouter } from 'next/router'
 import usePathRewrite, { useQueryRewrite } from '@/hooks/usePathRewrite'
 
