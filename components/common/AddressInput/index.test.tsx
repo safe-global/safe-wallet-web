722f0afc7c1d7546551607fd2dcad517525fba61
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-07-02 09:41:18 +0200

    Test addresses

--- components/common/AddressInput/index.test.tsx
+++ components/common/AddressInput/index.test.tsx
@@ -67,28 +67,31 @@ const setup = (address: string, prefix?: string, validate?: AddressInputProps['v
   }
 }
 
+const TEST_ADDRESS_A = '0x0000000000000000000000000000000000000000'
+const TEST_ADDRESS_B = '0x0000000000000000000000000000000000000001'
+
 describe('AddressInput tests', () => {
   it('should render with a default address value', () => {
-    const { input } = setup('0x1234567890123456789012345678901234567890')
-    expect(input.value).toBe('0x1234567890123456789012345678901234567890')
+    const { input } = setup(TEST_ADDRESS_A)
+    expect(input.value).toBe(TEST_ADDRESS_A)
   })
 
   it('should render with a default prefixed address value', () => {
-    const { input } = setup('0x1234567890123456789012345678901234567890', 'eth')
-    expect(input.value).toBe('eth:0x1234567890123456789012345678901234567890')
+    const { input } = setup(TEST_ADDRESS_A, 'eth')
+    expect(input.value).toBe(`eth:${TEST_ADDRESS_A}`)
   })
 
   it('should validate the address on input', async () => {
     const { input, utils } = setup('')
 
-    fireEvent.change(input, { target: { value: 'xyz:0x1234567890123456789012345678901234567890' } })
+    fireEvent.change(input, { target: { value: `xyz:${TEST_ADDRESS_A}` } })
 
     utils.getByText('Submit').click()
     await act(() => Promise.resolve())
 
     expect(utils.getByLabelText('Invalid chain prefix "xyz"')).toBeDefined()
 
-    fireEvent.change(input, { target: { value: 'eth:0x1234567890123456789012345678901234567890' } })
+    fireEvent.change(input, { target: { value: `eth:${TEST_ADDRESS_A}` } })
     await act(() => Promise.resolve())
 
     expect(utils.getByLabelText(`"eth" doesn't match the current chain`)).toBeDefined()
@@ -102,33 +105,33 @@ describe('AddressInput tests', () => {
   it('should accept a custom validate function', async () => {
     const { input, utils } = setup('', undefined, (val) => `${val} is wrong`)
 
-    fireEvent.change(input, { target: { value: 'rin:0x1234567890123456789012345678901234567890' } })
+    fireEvent.change(input, { target: { value: `rin:${TEST_ADDRESS_A}` } })
 
     utils.getByText('Submit').click()
     await act(() => Promise.resolve())
 
-    expect(utils.getByLabelText('0x1234567890123456789012345678901234567890 is wrong')).toBeDefined()
+    expect(utils.getByLabelText(`${TEST_ADDRESS_A} is wrong`)).toBeDefined()
 
-    fireEvent.change(input, { target: { value: 'rin:0xa123456789012345678901234567890123456789' } })
+    fireEvent.change(input, { target: { value: `rin:${TEST_ADDRESS_B}` } })
     await act(() => Promise.resolve())
 
-    expect(utils.getByLabelText('0xa123456789012345678901234567890123456789 is wrong')).toBeDefined()
+    expect(utils.getByLabelText(`${TEST_ADDRESS_B} is wrong`)).toBeDefined()
   })
 
   it('should pass a parsed address on submit', async () => {
     const { input, utils } = setup('', undefined, (val) => `${val} is wrong`)
 
-    fireEvent.change(input, { target: { value: 'eth:0x1234567890123456789012345678901234567890' } })
+    fireEvent.change(input, { target: { value: `eth:${TEST_ADDRESS_A}` } })
 
     utils.getByText('Submit').click()
     await act(() => Promise.resolve())
 
     expect(utils.getByTestId('prefix')).toHaveTextContent('eth')
-    expect(utils.getByTestId('address')).toHaveTextContent('0x1234567890123456789012345678901234567890')
+    expect(utils.getByTestId('address')).toHaveTextContent(TEST_ADDRESS_A)
 
-    fireEvent.change(input, { target: { value: 'rin:0xa123456789012345678901234567890123456789' } })
+    fireEvent.change(input, { target: { value: `rin:${TEST_ADDRESS_B}` } })
     await act(() => Promise.resolve())
     expect(utils.getByTestId('prefix')).toHaveTextContent('rin')
-    expect(utils.getByTestId('address')).toHaveTextContent('0xa123456789012345678901234567890123456789')
+    expect(utils.getByTestId('address')).toHaveTextContent(TEST_ADDRESS_B)
   })
 })
