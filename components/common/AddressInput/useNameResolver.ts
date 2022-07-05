ae6f715c8d76e3b45518bd72c20000d5b04140b6
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-07-05 13:58:22 +0200

    Add a debounce

--- components/common/AddressInput/useNameResolver.ts
+++ components/common/AddressInput/useNameResolver.ts
@@ -1,16 +1,18 @@
 import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
 import useAsync from '@/hooks/useAsync'
 import { resolveName } from '@/services/domains'
+import useDebounce from '@/hooks/useDebounce'
 
 const useNameResolver = (value: string): { address: string | undefined; resolving: boolean } => {
   const ethersProvider = useWeb3ReadOnly()
+  const debouncedValue = useDebounce(value, 200)
 
   // Fetch an ENS resolution for the current address
   const [ens, , resolving] = useAsync<{ name: string; address: string } | undefined>(async () => {
     if (!ethersProvider) return
-    const address = await resolveName(ethersProvider, value)
-    return address ? { address, name: value } : undefined
-  }, [value, ethersProvider])
+    const address = await resolveName(ethersProvider, debouncedValue)
+    return address ? { address, name: debouncedValue } : undefined
+  }, [debouncedValue, ethersProvider])
 
   return {
     resolving: !!ethersProvider && resolving,
