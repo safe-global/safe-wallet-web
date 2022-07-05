caedbb5b712ebb14f68d5e83a7ad89a0df85d3fd
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-05 13:28:06 +0200

    add delegate warning

--- components/transactions/TxDetails/index.tsx
+++ components/transactions/TxDetails/index.tsx
@@ -1,5 +1,5 @@
 import { type ReactElement } from 'react'
-import { getTransactionDetails, TransactionSummary } from '@gnosis.pm/safe-react-gateway-sdk'
+import { getTransactionDetails, Operation, TransactionSummary } from '@gnosis.pm/safe-react-gateway-sdk'
 import TxSigners from '@/components/transactions/TxSigners'
 import Summary from '@/components/transactions/TxDetails/Summary'
 import TxData, { AddressInfo } from '@/components/transactions/TxDetails/TxData'
@@ -8,9 +8,35 @@ import useAsync from '@/hooks/useAsync'
 import { isModuleExecutionInfo, isMultisendTxInfo, isMultisigExecutionInfo } from '@/utils/transaction-guards'
 import { InfoDetails } from '@/components/transactions/TxDetails/TxData/SettingsChange'
 import { TxDataRow } from '@/components/transactions/TxDetails/Summary/TxDataRow'
+import { Alert, Tooltip } from '@mui/material'
 import css from './styles.module.css'
 
 export const NOT_AVAILABLE = 'n/a'
+
+const DelegateCallWarning = ({ showWarning }: { showWarning: boolean }): ReactElement => (
+  <Tooltip
+    title="This transaction calls a smart contract that will be able to modify your Safe."
+    placement="top-start"
+    arrow
+  >
+    <Alert
+      className={css.alert}
+      sx={({ palette }) => ({
+        color: palette.black[500],
+        backgroundColor: `${showWarning ? palette.orange[200] : palette.green[200]}`,
+        borderLeft: `3px solid ${showWarning ? palette.orange[500] : palette.green[400]}`,
+
+        '&.MuiAlert-standardInfo .MuiAlert-icon': {
+          marginRight: '8px',
+          color: `${showWarning ? palette.orange[500] : palette.green[400]}`,
+        },
+      })}
+      severity="info"
+    >
+      <b>{showWarning ? 'Unexpected Delegate Call' : 'Delegate Call'}</b>
+    </Alert>
+  </Tooltip>
+)
 
 const TxDetails = ({ txSummary }: { txSummary: TransactionSummary }): ReactElement => {
   const chainId = useChainId()
@@ -31,6 +57,11 @@ const TxDetails = ({ txSummary }: { txSummary: TransactionSummary }): ReactEleme
       <div className={`${css.details} ${!hasSigners ? css.noSigners : ''}`}>
         {isMultisendTxInfo(txDetails.txInfo) ? (
           <>
+            {txDetails.txData?.operation === Operation.DELEGATE && (
+              <div className={css.delegateCall}>
+                <DelegateCallWarning showWarning={!txDetails.txData.trustedDelegateCallTarget} />
+              </div>
+            )}
             <div className={css.multisendInfo}>
               <InfoDetails title="MultiSend contract:">
                 <AddressInfo address={txDetails.txInfo.to.value} />
