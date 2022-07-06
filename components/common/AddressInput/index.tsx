7fe592aa4a1bc0c8034770887b0a82c69671e151
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-07-06 10:14:13 +0200

    Fix: trigger address input validation after ENS resolution (#134)
    * Fix: trigger address input validation after ENS resolution
    
    * Update components/common/AddressInput/index.tsx
    
    Co-authored-by: Aaron Cook <aaron.cook@gnosis.pm>
    
    Co-authored-by: Aaron Cook <aaron.cook@gnosis.pm>

--- components/common/AddressInput/index.tsx
+++ components/common/AddressInput/index.tsx
@@ -11,6 +11,7 @@ const AddressInput = ({ name, validate, ...props }: AddressInputProps): ReactEle
   const {
     register,
     setValue,
+    trigger,
     watch,
     formState: { errors },
   } = useFormContext()
@@ -22,8 +23,10 @@ const AddressInput = ({ name, validate, ...props }: AddressInputProps): ReactEle
 
   // Update the input value with the resolved ENS name
   useEffect(() => {
-    if (address) setValue(name, address)
-  }, [address, name, setValue])
+    if (address) {
+      setValue(name, address, { shouldValidate: true })
+    }
+  }, [address, name, setValue, trigger])
 
   return (
     <TextField
