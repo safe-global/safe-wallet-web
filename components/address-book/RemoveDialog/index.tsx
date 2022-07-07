c6bc140b6ca666dab5a52934b773e5dc5c9691ea
Aaron Cook <aaron.cook@gnosis.pm>
2022-07-07 11:29:41 +0200

    feat: address book removal dialog (#145)

---
+++ components/address-book/RemoveDialog/index.tsx
@@ -0,0 +1,48 @@
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import type { ReactElement } from 'react'
+
+import ModalDialog from '@/components/common/ModalDialog'
+import { useAppDispatch } from '@/store'
+import { removeAddressBookEntry } from '@/store/addressBookSlice'
+import useChainId from '@/hooks/useChainId'
+import useAddressBook from '@/hooks/useAddressBook'
+
+const RemoveDialog = ({ handleClose, address }: { handleClose: () => void; address: string }): ReactElement => {
+  const dispatch = useAppDispatch()
+  const chainId = useChainId()
+  const addressBook = useAddressBook()
+
+  const name = addressBook?.[address]
+
+  const handleConfirm = () => {
+    dispatch(removeAddressBookEntry({ chainId, address }))
+    handleClose()
+  }
+
+  return (
+    <ModalDialog open onClose={handleClose} title="Delete entry">
+      <DialogContent sx={{ p: '24px !important' }}>
+        <Typography>
+          Are you sure you want to permanently delete <b>{name}</b> from your address book?
+        </Typography>
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button
+          onClick={handleConfirm}
+          variant="contained"
+          disableElevation
+          sx={({ palette }) => ({ backgroundColor: palette.error.main })}
+        >
+          Delete
+        </Button>
+      </DialogActions>
+    </ModalDialog>
+  )
+}
+
+export default RemoveDialog
