54246b232364f401b85e18cc7269e5642c5c4f33
Usame Algan <usame@safe.global>
2022-07-07 11:48:33 +0200

    chore: Disable image optimization in next.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   eslint: {
     dirs: ['pages', 'services', 'store', 'components', 'config'],
   },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 }
 
 module.exports = nextConfig
