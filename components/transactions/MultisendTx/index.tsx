97f603609105439986862091dd7a4462190e97d3
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-11 16:55:42 +0200

    mv MultisendTx to its own component

---
+++ components/transactions/MultisendTx/index.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement } from 'react'
+import { isMultisendTxInfo } from '@/utils/transaction-guards'
+import { Operation, TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
+import { InfoDetails } from '@/components/transactions/InfoDetails'
+import EthHashInfo from '@/components/common/EthHashInfo'
+import { TxDataRow } from '@/components/transactions/TxDetails/Summary/TxDataRow'
+import Summary from '@/components/transactions/TxDetails/Summary'
+import TxData from '@/components/transactions/TxDetails/TxData'
+import css from './styles.module.css'
+import { DelegateCallWarning } from '@/components/transactions/Warning'
+
+const MultiSendTx = ({ txDetails }: { txDetails: TransactionDetails }): ReactElement => {
+  const txInfo = isMultisendTxInfo(txDetails.txInfo) ? txDetails.txInfo : undefined
+
+  return (
+    <>
+      {txDetails.txData?.operation === Operation.DELEGATE && (
+        <div className={css.delegateCall}>
+          <DelegateCallWarning showWarning={!txDetails.txData.trustedDelegateCallTarget} />
+        </div>
+      )}
+
+      <div className={css.multisendInfo}>
+        <InfoDetails title="MultiSend contract:">
+          <EthHashInfo address={txInfo?.to.value || ''} />
+        </InfoDetails>
+        <TxDataRow title="Value:">{txInfo?.value}</TxDataRow>
+      </div>
+
+      <div className={`${css.txSummary}`}>
+        <Summary txDetails={txDetails} />
+      </div>
+
+      <div className={`${css.txData} ${css.noPadding}`}>
+        <TxData txDetails={txDetails} />
+      </div>
+    </>
+  )
+}
+
+export default MultiSendTx
