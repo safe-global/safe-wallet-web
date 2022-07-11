5d42194de66417fca943a8ea42397922f7e06c71
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-11 09:01:31 +0200

    extract TransferTxInfo to a component

--- components/transactions/TxDetails/TxData/index.tsx
+++ components/transactions/TxDetails/TxData/index.tsx
@@ -1,43 +1,21 @@
-import EthHashInfo from '@/components/common/EthHashInfo'
 import SettingsChangeTxInfo from '@/components/transactions/TxDetails/TxData/SettingsChange'
-import { TransferTx } from '@/components/transactions/TxInfo'
 import {
   isCancellationTxInfo,
   isMultisigExecutionDetails,
   isSettingsChangeTxInfo,
   isTransferTxInfo,
 } from '@/utils/transaction-guards'
-import { TransactionDetails, Transfer, TransferDirection } from '@gnosis.pm/safe-react-gateway-sdk'
+import { TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
 import { type ReactElement } from 'react'
 import RejectionTxInfo from '@/components/transactions/TxDetails/TxData/Rejection'
 import DecodedData from '@/components/transactions/TxDetails/TxData/DecodedData'
-
-const TransferTxInfoSummary = ({ txInfo }: { txInfo: Transfer }) => {
-  const { direction } = txInfo
-  return (
-    <span>
-      {/* TODO: copy should be 'Send' if is a queued tx */}
-      {direction === TransferDirection.INCOMING ? 'Received' : 'Sent'}{' '}
-      <b>
-        <TransferTx info={txInfo} withLogo={false} omitSign />
-      </b>
-      {direction === TransferDirection.INCOMING ? ' from:' : ' to:'}
-    </span>
-  )
-}
+import TransferTxInfo from '@/components/transactions/TxDetails/TxData/Transfer'
 
 const TxData = ({ txDetails }: { txDetails: TransactionDetails }): ReactElement => {
   const txInfo = txDetails.txInfo
 
   if (isTransferTxInfo(txInfo)) {
-    const address =
-      txInfo.direction.toUpperCase() === TransferDirection.INCOMING ? txInfo.sender.value : txInfo.recipient.value
-    return (
-      <div>
-        <TransferTxInfoSummary txInfo={txInfo} />
-        <EthHashInfo address={address} />
-      </div>
-    )
+    return <TransferTxInfo txInfo={txInfo} txStatus={txDetails.txStatus} />
   }
   if (isSettingsChangeTxInfo(txInfo)) {
     return <SettingsChangeTxInfo settingsInfo={txInfo.settingsInfo} />
