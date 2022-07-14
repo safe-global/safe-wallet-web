b8d414b895ab3e87738bb97a992f107ce02367fd
Diogo Soares <diogo.soares@gnosis.pm>
2022-07-14 16:34:13 +0200

    change copy button tooltip to give feedback

--- components/common/CopyAddressButton/index.tsx
+++ components/common/CopyAddressButton/index.tsx
@@ -2,19 +2,24 @@ import { useCurrentChain } from '@/hooks/useChains'
 import { useAppSelector } from '@/store'
 import { selectSettings } from '@/store/settingsSlice'
 import { IconButton, Tooltip } from '@mui/material'
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const CopyAddressButton = ({ address }: { address: string }) => {
+  const [tooltipText, setTooltipText] = useState('Copy to clipboard')
   const settings = useAppSelector(selectSettings)
   const chain = useCurrentChain()
 
   const handleCopy = useCallback(() => {
     const text = settings.shortName.copy && chain ? `${chain.shortName}:${address}` : address
-    navigator.clipboard.writeText(text)
+    navigator.clipboard.writeText(text).then(() => setTooltipText('Copied'))
   }, [address, chain, settings.shortName.copy])
 
+  const handleMouseLeave = () => {
+    setTimeout(() => setTooltipText('Copy to clipboard'), 500)
+  }
+
   return (
-    <Tooltip title="Copy to clipboard" placement="top">
+    <Tooltip title={tooltipText} placement="top" onMouseLeave={handleMouseLeave}>
       <IconButton onClick={handleCopy} sx={{ padding: 0, width: '24px', height: '24px' }}>
         <img src="/images/copy.svg" width={16} height={16} alt="Copy address" />
       </IconButton>
