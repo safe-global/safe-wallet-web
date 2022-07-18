c240134b5ecb5a5e9886ddfd06c86c9216038592
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-07-18 12:36:21 +0200

    TxList Accordion breaking lines (#177)
    * style: Adjust tx list accordion styles
    
    * style: TxList responsive styles
    
    * fix: Extend SvgIcon color palette in theme
    
    * refactor: Assign variables outside of render

--- components/transactions/TxSummary/index.tsx
+++ components/transactions/TxSummary/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from '@mui/material'
+import { Grid, Box, Typography } from '@mui/material'
 import { ReactElement } from 'react'
 import { TransactionStatus, type Transaction } from '@gnosis.pm/safe-react-gateway-sdk'
 
@@ -8,10 +8,11 @@ import SignTxButton from '@/components/transactions/SignTxButton'
 import ExecuteTxButton from '@/components/transactions/ExecuteTxButton'
 import css from './styles.module.css'
 import useWallet from '@/hooks/wallets/useWallet'
-import { isAwaitingExecution } from '@/utils/transaction-guards'
+import { isAwaitingExecution, isMultisigExecutionInfo } from '@/utils/transaction-guards'
 import RejectTxButton from '@/components/transactions/RejectTxButton'
 import { useTransactionStatus } from '@/hooks/useTransactionStatus'
 import TxType from '@/components/transactions/TxType'
+import GroupIcon from '@mui/icons-material/Group'
 
 type TxSummaryProps = {
   item: Transaction
@@ -28,31 +29,42 @@ const TxSummary = ({ item }: TxSummaryProps): ReactElement => {
   const txStatusLabel = useTransactionStatus(tx)
   const isQueue = tx.txStatus !== TransactionStatus.SUCCESS
   const awaitingExecution = isAwaitingExecution(item.transaction.txStatus)
-  const nonce = tx.executionInfo && 'nonce' in tx.executionInfo ? tx.executionInfo.nonce : ''
+  const nonce = isMultisigExecutionInfo(tx.executionInfo) ? tx.executionInfo.nonce : ''
+  const submittedConfirmations = isMultisigExecutionInfo(tx.executionInfo)
+    ? tx.executionInfo.confirmationsSubmitted
+    : ''
+  const requiredConfirmations = isMultisigExecutionInfo(tx.executionInfo) ? tx.executionInfo.confirmationsRequired : ''
 
   return (
-    <Grid container className={css.gridContainer} id={tx.id} gap={2} p={1}>
-      <Grid item md>
+    <Grid container className={css.gridContainer} id={tx.id} gap={[2, undefined, undefined, 0]}>
+      <Grid item xs={1}>
         {nonce}
       </Grid>
 
-      <Grid item md={4}>
+      <Grid item md={2} lg={3}>
         <TxType tx={tx} />
       </Grid>
 
-      <Grid item md={4}>
+      <Grid item md={2} lg={3}>
         <TxInfo info={tx.txInfo} />
       </Grid>
 
-      <Grid item xs sx={{ whiteSpace: 'nowrap' }}>
+      <Grid item lg={1} sx={{ whiteSpace: 'nowrap' }}>
         <DateTime value={tx.timestamp} options={dateOptions} />
       </Grid>
 
-      <Grid item>{txStatusLabel}</Grid>
+      {awaitingExecution && (
+        <Grid item lg={2} display="flex" alignItems="center" justifyContent="center" gap={1}>
+          <GroupIcon fontSize="small" color="border" />
+          <Typography variant="caption" fontWeight="bold" color="primary">
+            {submittedConfirmations} out of {requiredConfirmations}
+          </Typography>
+        </Grid>
+      )}
 
       {wallet && isQueue && (
-        <Grid item>
-          <Box display="flex" alignItems="center">
+        <Grid item lg={1}>
+          <Box display="flex" alignItems="center" justifyContent="flex-end">
             {awaitingExecution ? (
               <ExecuteTxButton txSummary={item.transaction} />
             ) : (
@@ -62,6 +74,16 @@ const TxSummary = ({ item }: TxSummaryProps): ReactElement => {
           </Box>
         </Grid>
       )}
+
+      <Grid item marginLeft="auto" marginRight={2}>
+        <Typography
+          variant="caption"
+          fontWeight="bold"
+          color={({ palette }) => (awaitingExecution ? palette.warning.dark : palette.primary.main)}
+        >
+          {txStatusLabel}
+        </Typography>
+      </Grid>
     </Grid>
   )
 }
