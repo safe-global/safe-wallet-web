8bc72aff44a55211857f76601a2fd3d9f3ab48c4
Mikhail <mmvsha73@gmail.com>
2022-07-20 14:57:57 +0200

    add custom app card

--- components/safe-apps/AddCustomAppCard.tsx
+++ components/safe-apps/AddCustomAppCard.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react'
 import { Button, Paper } from '@mui/material'
-import { useTheme } from '@mui/material'
 
 type Props = {}
 
 const AddCustomAppCard = ({}: Props) => {
-  const theme = useTheme()
-
   return (
     <Paper
       sx={({ palette }) => ({
@@ -16,6 +13,7 @@ const AddCustomAppCard = ({}: Props) => {
         alignItems: 'center',
         justifyContent: 'center',
         transition: 'background-color 0.3s ease-in-out, border 0.3s ease-in-out',
+        border: '2px solid transparent',
         '&:hover': {
           backgroundColor: palette.primary.background,
           border: `2px solid ${palette.primary.light}`,
@@ -23,7 +21,13 @@ const AddCustomAppCard = ({}: Props) => {
       })}
     >
       <img src="/images/add-custom-app.svg" alt="Add custom app icon" />
-      <Button variant="contained" size="small">
+      <Button
+        variant="contained"
+        size="small"
+        sx={{
+          mt: 1,
+        }}
+      >
         Add custom app
       </Button>
     </Paper>
