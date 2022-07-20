360328a5a80a01c8ff312e9f7a4d08ac4fb493b8
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-07-20 16:11:35 +0200

    feat: Estimate gas when creating a safe (#188)
    * feat: Estimate gas when creating a safe
    
    * fix: Adjust get deployment function
    
    * chore: gitignore autogenerated types
    
    * fix: Add EthHashInfo, fix typos
    
    * fix: Add tests

---
+++ components/create-safe/useEstimateSafeCreationGas.test.ts
@@ -0,0 +1,93 @@
+import { renderHook } from '@/tests/test-utils'
+import { useEstimateSafeCreationGas } from '@/components/create-safe/useEstimateSafeCreationGas'
+import { EMPTY_DATA, ZERO_ADDRESS } from '@gnosis.pm/safe-core-sdk/dist/src/utils/constants'
+import { ConnectedWallet } from '@/hooks/wallets/useOnboard'
+import * as chainIdModule from '@/hooks/useChainId'
+import * as store from '@/store'
+import * as safeContracts from '@/services/safeContracts'
+import * as sender from './sender'
+import * as wallet from '@/hooks/wallets/useWallet'
+import * as web3 from '@/hooks/wallets/web3'
+import { waitFor } from '@testing-library/react'
+import { EIP1193Provider } from '@web3-onboard/core'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { BigNumber } from 'ethers'
+import { Proxy_factory } from '@/types/contracts'
+
+const mockProps = {
+  owners: [],
+  threshold: 1,
+  saltNonce: 1,
+}
+
+describe('useEstimateSafeCreationGas', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    jest.spyOn(store, 'useAppSelector').mockReturnValue({})
+    jest.spyOn(chainIdModule, 'useChainId').mockReturnValue('4')
+    jest
+      .spyOn(safeContracts, 'getProxyFactoryContractInstance')
+      .mockReturnValue({ address: ZERO_ADDRESS } as unknown as Proxy_factory)
+    jest.spyOn(sender, 'getSafeCreationTx').mockReturnValue(EMPTY_DATA)
+    jest.spyOn(wallet, 'default').mockReturnValue({} as ConnectedWallet)
+  })
+
+  it('should return no gasLimit by default', () => {
+    const { result } = renderHook(() => useEstimateSafeCreationGas(mockProps))
+    expect(result.current.gasLimit).toBeUndefined()
+    expect(result.current.gasLimitLoading).toBe(true)
+  })
+
+  it('should estimate gas', async () => {
+    const mockProvider = new JsonRpcProvider()
+    mockProvider.estimateGas = jest.fn(() => {
+      return Promise.resolve(BigNumber.from('123'))
+    })
+    jest.spyOn(web3, 'useWeb3ReadOnly').mockReturnValue(mockProvider)
+    jest.spyOn(sender, 'getSafeCreationTx').mockReturnValue(EMPTY_DATA)
+    jest.spyOn(wallet, 'default').mockReturnValue({
+      label: 'MetaMask',
+      chainId: '4',
+      address: ZERO_ADDRESS,
+      provider: null as unknown as EIP1193Provider,
+    })
+
+    const { result } = renderHook(() => useEstimateSafeCreationGas(mockProps))
+
+    await waitFor(() => {
+      expect(result.current.gasLimit).toStrictEqual(BigNumber.from('123'))
+      expect(result.current.gasLimitLoading).toBe(false)
+    })
+  })
+
+  it('should not estimate gas if there is no wallet connected', async () => {
+    jest.spyOn(wallet, 'default').mockReturnValue(null)
+    const { result } = renderHook(() => useEstimateSafeCreationGas(mockProps))
+
+    await waitFor(() => {
+      expect(result.current.gasLimit).toBeUndefined()
+      expect(result.current.gasLimitLoading).toBe(true)
+    })
+  })
+
+  it('should not estimate gas if there is no encoded data', async () => {
+    jest.spyOn(sender, 'getSafeCreationTx').mockReturnValue(undefined)
+    const { result } = renderHook(() => useEstimateSafeCreationGas(mockProps))
+
+    await waitFor(() => {
+      expect(result.current.gasLimit).toBeUndefined()
+      expect(result.current.gasLimitLoading).toBe(true)
+    })
+  })
+
+  it('should not estimate gas if there is no provider', async () => {
+    jest.spyOn(web3, 'useWeb3ReadOnly').mockReturnValue(undefined)
+    const { result } = renderHook(() => useEstimateSafeCreationGas(mockProps))
+
+    await waitFor(() => {
+      expect(result.current.gasLimit).toBeUndefined()
+      expect(result.current.gasLimitLoading).toBe(true)
+    })
+  })
+})
