f6e14704ae0b1f612e0c80b96918e45192ea76cf
katspaugh <katspaugh@gmail.com>
2022-07-21 11:36:01 +0200

    Feat: max amount helper

---
+++ components/common/InputValueHelper/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, SyntheticEvent } from 'react'
+import { Link } from '@mui/material'
+
+type InputValueHelperProps = {
+  children: ReactNode
+  onClick: () => void
+  disabled?: boolean
+}
+
+const InputValueHelper = ({ children, onClick, disabled = false }: InputValueHelperProps) => {
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault()
+    onClick()
+  }
+
+  return (
+    <Link component="button" onClick={handleClick} sx={disabled ? { visibility: 'hidden' } : undefined}>
+      {children}
+    </Link>
+  )
+}
+
+export default InputValueHelper
