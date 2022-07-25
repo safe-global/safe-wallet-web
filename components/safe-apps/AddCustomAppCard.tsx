a939b2ab66c571412f1a1dacbb1501d1a4c57cda
Mikhail <mmvsha73@gmail.com>
2022-07-25 23:38:28 +0200

    Feature: Custom safe apps modal (#211)
    * wip
    
    * Add close button to the custom dialog
    
    * finish the modal, handling wip
    
    * form validation wip
    
    * fetching manifest
    
    * add chain indicator to the modal title
    
    * validation
    
    * Update utils/validation.ts
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>
    
    * Update services/safe-apps/manifest.ts
    
    Co-authored-by: Usame Algan <5880855+usame-algan@users.noreply.github.com>
    
    * remove redundant await, check if fetch response is ok
    
    * fix tests after fetch change
    
    * use appcard container
    
    * respect absolute urls as icons source, rename normalize url function
    
    * include chainindicator in modaldialog
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>
    Co-authored-by: Usame Algan <5880855+usame-algan@users.noreply.github.com>

--- components/safe-apps/AddCustomAppCard.tsx
+++ components/safe-apps/AddCustomAppCard.tsx
@@ -1,36 +1,49 @@
-import { ReactElement } from 'react'
+import * as React from 'react'
 import { useRouter } from 'next/router'
-import { SAFE_REACT_URL } from '@/config/constants'
-import { Box, Button } from '@mui/material'
+import { Button, Box } from '@mui/material'
+import { AddCustomAppModal } from '@/components/safe-apps/AddCustomAppModal'
 import { AppCardContainer } from './AppCard'
+import { IS_PRODUCTION, SAFE_REACT_URL } from '@/config/constants'
 
-const AddCustomAppCard = (): ReactElement => {
+type Props = {}
+
+const AddCustomAppCard = ({}: Props) => {
+  const [addCustomAppModalOpen, setAddCustomAppModalOpen] = React.useState(false)
   const router = useRouter()
   const url = `${SAFE_REACT_URL}/${router.query.safe}/apps`
 
   return (
-    <AppCardContainer url={url}>
-      <Box
-        sx={{
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <img src="/images/add-custom-app.svg" alt="Add custom app icon" />
-        <Button
-          variant="contained"
-          size="small"
+    <>
+      <AppCardContainer url={IS_PRODUCTION ? url : undefined}>
+        <Box
           sx={{
-            mt: 1,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          Add custom app
-        </Button>
-      </Box>
-    </AppCardContainer>
+          <img src="/images/add-custom-app.svg" alt="Add custom app icon" />
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              mt: 1,
+            }}
+            onClick={() => {
+              if (!IS_PRODUCTION) {
+                setAddCustomAppModalOpen(true)
+              }
+            }}
+          >
+            Add custom app
+          </Button>
+        </Box>
+      </AppCardContainer>
+
+      <AddCustomAppModal open={addCustomAppModalOpen} onClose={() => setAddCustomAppModalOpen(false)} />
+    </>
   )
 }
 
