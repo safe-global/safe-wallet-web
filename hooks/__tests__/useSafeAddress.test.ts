4f600077dc0b6b2723d366817ff4ab2d964aa734
katspaugh <katspaugh@gmail.com>
2022-07-25 11:45:07 +0200

    Fix test

--- hooks/__tests__/useSafeAddress.test.ts
+++ hooks/__tests__/useSafeAddress.test.ts
@@ -5,8 +5,9 @@ import useSafeAddress from '@/hooks/useSafeAddress'
 // Mock useRouter
 jest.mock('next/router', () => ({
   useRouter: jest.fn(() => ({
+    pathname: '/safe/home',
     query: {
-      safe: 'rin:0x0000000000000000000000000000000000000000',
+      safe: 'rin:0x0000000000000000000000000000000000000001',
     },
   })),
 }))
@@ -15,11 +16,12 @@ jest.mock('next/router', () => ({
 describe('useSafeAddress hook', () => {
   it('should return the safe address', () => {
     const { result } = renderHook(() => useSafeAddress())
-    expect(result.current).toBe('0x0000000000000000000000000000000000000000')
+    expect(result.current).toBe('0x0000000000000000000000000000000000000001')
   })
 
   it('should not return the safe address when it is not in the query', () => {
     ;(useRouter as any).mockImplementation(() => ({
+      pathname: '/',
       query: {
         safe: undefined,
       },
@@ -31,6 +33,7 @@ describe('useSafeAddress hook', () => {
 
   it('should cheksum the safe address', () => {
     ;(useRouter as any).mockImplementation(() => ({
+      pathname: '/safe/home',
       query: {
         safe: 'eth:0x220866b1a2219f40e72f5c628b65d54268ca3a9d',
       },
@@ -38,5 +41,15 @@ describe('useSafeAddress hook', () => {
 
     const { result } = renderHook(() => useSafeAddress())
     expect(result.current).toBe('0x220866B1A2219f40e72f5c628B65D54268cA3A9D')
+  })
+
+  it('should return zero address for safe routes w/o query', () => {
+    ;(useRouter as any).mockImplementation(() => ({
+      pathname: '/safe/home',
+      query: {},
+    }))
+
+    const { result } = renderHook(() => useSafeAddress())
+    expect(result.current).toBe('0x0000000000000000000000000000000000000000')
   })
 })
