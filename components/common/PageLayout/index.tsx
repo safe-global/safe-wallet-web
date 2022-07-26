269493dc0eb924ad4a7029a9a3e8cfb39a6995af
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-07-26 18:51:06 +0200

    Mobile: close hamburger menu on click (#213)
    * Mobile: close hamburger menu on click
    
    * Close menu on route change

--- components/common/PageLayout/index.tsx
+++ components/common/PageLayout/index.tsx
@@ -1,5 +1,6 @@
-import { useState, type ReactElement } from 'react'
+import { useEffect, useState, type ReactElement } from 'react'
 import { Drawer } from '@mui/material'
+import { useRouter } from 'next/router'
 
 import Sidebar from '@/components/sidebar/Sidebar'
 import Header from '@/components/common//Header'
@@ -8,6 +9,7 @@ import SafeLoadingError from '../SafeLoadingError'
 import Footer from '../Footer'
 
 const PageLayout = ({ children }: { children: ReactElement }): ReactElement => {
+  const router = useRouter()
   const [isMobileDrawerOpen, setIsMobileDrawerOpen] = useState<boolean>(false)
 
   const onMenuToggle = (): void => {
@@ -15,6 +17,10 @@ const PageLayout = ({ children }: { children: ReactElement }): ReactElement => {
   }
 
   const sidebar = <Sidebar />
+
+  useEffect(() => {
+    setIsMobileDrawerOpen(false)
+  }, [router.pathname, router.query.safe])
 
   return (
     <div className={css.container}>
