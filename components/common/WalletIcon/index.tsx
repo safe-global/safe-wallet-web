df7febad84edd0f7c9f23e7d2affc789d8c19740
Usame Algan <usame@safe.global>
2022-07-26 10:17:40 +0200

    fix: Use data uris and dynamic import icons

--- components/common/WalletIcon/index.tsx
+++ components/common/WalletIcon/index.tsx
@@ -1,14 +1,6 @@
-import Box from '@mui/material/Box'
 import { WALLET_KEYS } from '@/hooks/wallets/wallets'
-import metamaskIcon from '@web3-onboard/injected-wallets/dist/icons/metamask'
-import coinbaseIcon from '@web3-onboard/coinbase/dist/icon'
-import keystoneIcon from '@web3-onboard/keystone/dist/icon'
-import walletConnectIcon from '@web3-onboard/walletconnect/dist/icon'
-import trezorIcon from '@web3-onboard/trezor/dist/icon'
-import ledgerIcon from '@web3-onboard/ledger/dist/icon'
-import fortmaticIcon from '@web3-onboard/fortmatic/dist/icon'
-import portisIcon from '@web3-onboard/portis/dist/icon'
-import torusIcon from '@web3-onboard/torus/dist/icon'
+import useAsync from '@/hooks/useAsync'
+import { Skeleton } from '@mui/material'
 
 enum ADDITIONAL_KEYS {
   METAMASK = 'METAMASK',
@@ -16,26 +8,33 @@ enum ADDITIONAL_KEYS {
 
 export type ALL_WALLET_KEYS = typeof WALLET_KEYS & typeof ADDITIONAL_KEYS
 
-type Props = {
-  [k in keyof ALL_WALLET_KEYS]: string
-}
+const getWalletIcon = async (provider: keyof ALL_WALLET_KEYS): Promise<string> => {
+  const walletKeys = {
+    [ADDITIONAL_KEYS.METAMASK]: (await import('@web3-onboard/injected-wallets/dist/icons/metamask')).default,
+    [WALLET_KEYS.COINBASE]: (await import('@web3-onboard/coinbase/dist/icon')).default,
+    [WALLET_KEYS.INJECTED]: (await import('@web3-onboard/injected-wallets/dist/icons/metamask')).default,
+    [WALLET_KEYS.KEYSTONE]: (await import('@web3-onboard/keystone/dist/icon')).default,
+    [WALLET_KEYS.WALLETCONNECT]: (await import('@web3-onboard/walletconnect/dist/icon')).default,
+    [WALLET_KEYS.TREZOR]: (await import('@web3-onboard/trezor/dist/icon')).default,
+    [WALLET_KEYS.LEDGER]: (await import('@web3-onboard/ledger/dist/icon')).default,
+    [WALLET_KEYS.FORTMATIC]: (await import('@web3-onboard/fortmatic/dist/icon')).default,
+    [WALLET_KEYS.PORTIS]: (await import('@web3-onboard/portis/dist/icon')).default,
+    [WALLET_KEYS.TORUS]: (await import('@web3-onboard/torus/dist/icon')).default,
+  }
 
-const WALLET_ICONS: Props = {
-  [ADDITIONAL_KEYS.METAMASK]: metamaskIcon,
-  [WALLET_KEYS.COINBASE]: coinbaseIcon,
-  [WALLET_KEYS.INJECTED]: metamaskIcon,
-  [WALLET_KEYS.KEYSTONE]: keystoneIcon,
-  [WALLET_KEYS.WALLETCONNECT]: walletConnectIcon,
-  [WALLET_KEYS.TREZOR]: trezorIcon,
-  [WALLET_KEYS.LEDGER]: ledgerIcon,
-  [WALLET_KEYS.FORTMATIC]: fortmaticIcon,
-  [WALLET_KEYS.PORTIS]: portisIcon,
-  [WALLET_KEYS.TORUS]: torusIcon,
+  return walletKeys[provider]
 }
 
 const WalletIcon = ({ provider }: { provider: string }) => {
-  const icon = WALLET_ICONS[provider.toUpperCase() as keyof ALL_WALLET_KEYS]
-  return <Box dangerouslySetInnerHTML={{ __html: icon }} width={30} height={30} />
+  const [icon] = useAsync<string>(async () => {
+    return getWalletIcon(provider.toUpperCase() as keyof ALL_WALLET_KEYS)
+  }, [])
+
+  return icon ? (
+    <img width={30} height={30} src={`data:image/svg+xml;utf8,${encodeURIComponent(icon)}`} alt={`${provider} logo`} />
+  ) : (
+    <Skeleton variant="circular" width={30} height={30} />
+  )
 }
 
 export default WalletIcon
