65f6ec8a609fcdd774c02487c6700075d0287dbe
katspaugh <katspaugh@gmail.com>
2022-07-29 16:16:05 +0200

    Feat: paginated NFTs

--- pages/safe/balances/nfts.tsx
+++ pages/safe/balances/nfts.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
+import { Alert, AlertTitle, Box, CircularProgress, Paper, Typography } from '@mui/material'
 import useCollectibles from '@/hooks/useCollectibles'
 import { NftGrid } from '@/components/nfts'
 import { Breadcrumbs } from '@/components/common/Breadcrumbs'
 import AssetsIcon from '@/public/images/sidebar/assets.svg'
 import NavTabs from '@/components/common/NavTabs'
 import { balancesNavItems } from '@/components/sidebar/SidebarNavigation/config'
+import Pagination from '@/components/transactions/Pagination'
+import ErrorMessage from '@/components/tx/ErrorMessage'
 
 const NFTs: NextPage = () => {
-  const { collectibles } = useCollectibles()
+  const [pageUrl, setPageUrl] = useState<string | undefined>()
+  const [collectibles, error, loading] = useCollectibles(pageUrl)
 
   return (
     <main>
@@ -15,7 +20,35 @@ const NFTs: NextPage = () => {
 
       <NavTabs tabs={balancesNavItems} />
 
-      <NftGrid collectibles={collectibles} />
+      <Box py={3}>
+        <Alert severity="info" sx={{ marginBottom: 4 }}>
+          <AlertTitle>Use Safe Apps to view your NFT portfolio</AlertTitle>
+          Get the most optimal experience with Safe Apps. View your collections, buy or sell NFTs, and more.
+        </Alert>
+
+        {loading || collectibles?.next || collectibles?.previous ? (
+          <Box pb={4}>
+            <Pagination
+              page={pageUrl}
+              nextPage={collectibles?.next}
+              prevPage={collectibles?.previous}
+              onPageChange={setPageUrl}
+            />
+          </Box>
+        ) : null}
+
+        {loading ? (
+          <CircularProgress size={20} sx={{ marginTop: 2 }} />
+        ) : error ? (
+          <ErrorMessage error={error}>Failed to load NFTs</ErrorMessage>
+        ) : collectibles?.results.length ? (
+          <NftGrid collectibles={collectibles?.results || []} />
+        ) : (
+          <Paper sx={{ py: 9, textAlign: 'center' }}>
+            <Typography variant="h3">No NFTs available</Typography>
+          </Paper>
+        )}
+      </Box>
     </main>
   )
 }
