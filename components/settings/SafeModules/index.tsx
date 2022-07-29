1753090be821dc0befe926d818e68c9a5f50502e
Usame Algan <usame@safe.global>
2022-07-29 16:23:40 +0200

    feat: Add Remove module dialog

--- components/settings/SafeModules/index.tsx
+++ components/settings/SafeModules/index.tsx
@@ -3,6 +3,7 @@ import useSafeInfo from '@/hooks/useSafeInfo'
 import { Paper, Grid, Typography, Box, Link } from '@mui/material'
 
 import css from './styles.module.css'
+import { RemoveModule } from '@/components/settings/SafeModules/RemoveModule'
 
 const NoModules = () => {
   return (
@@ -14,8 +15,16 @@ const NoModules = () => {
 
 const ModuleDisplay = ({ moduleAddress, chainId }: { moduleAddress: string; chainId: string }) => {
   return (
-    <Box className={css.moduledisplay}>
-      <EthHashInfo shortAddress={false} address={moduleAddress} showCopyButton chainId={chainId} showAvatar={false} />
+    <Box className={css.container}>
+      <EthHashInfo
+        shortAddress={false}
+        address={moduleAddress}
+        showCopyButton
+        chainId={chainId}
+        showAvatar={false}
+        hasExplorer
+      />
+      <RemoveModule address={moduleAddress} />
     </Box>
   )
 }
