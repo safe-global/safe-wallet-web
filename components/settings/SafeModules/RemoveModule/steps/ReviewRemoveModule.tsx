9f63b1b1d969bafeaa9d501eac911ae13496d921
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-07-29 17:13:25 +0200

    feat: Add Remove module dialog (#243)
    * feat: Add Remove module dialog
    
    * fix: Add error log
    
    * fix: Use recommended nonce for tx

---
+++ components/settings/SafeModules/RemoveModule/steps/ReviewRemoveModule.tsx
@@ -0,0 +1,41 @@
+import useSafeInfo from '@/hooks/useSafeInfo'
+import useAsync from '@/hooks/useAsync'
+import { SafeTransaction } from '@gnosis.pm/safe-core-sdk-types'
+import { createTx } from '@/services/tx/txSender'
+import SignOrExecuteForm from '@/components/tx/SignOrExecuteForm'
+import { Typography } from '@mui/material'
+import EthHashInfo from '@/components/common/EthHashInfo'
+import { RemoveModuleData } from '@/components/settings/SafeModules/RemoveModule'
+import { useSafeSDK } from '@/hooks/coreSDK/safeCoreSDK'
+import { useEffect } from 'react'
+import { Errors, logError } from '@/services/exceptions'
+
+export const ReviewRemoveModule = ({ data, onSubmit }: { data: RemoveModuleData; onSubmit: (data: null) => void }) => {
+  const { safe } = useSafeInfo()
+  const sdk = useSafeSDK()
+
+  const [safeTx, safeTxError] = useAsync<SafeTransaction | undefined>(async () => {
+    if (!sdk) return
+
+    const tx = await sdk.getDisableModuleTx(data.address)
+
+    return createTx({ ...tx.data, nonce: undefined })
+  }, [sdk, data.address])
+
+  useEffect(() => {
+    if (safeTxError) {
+      logError(Errors._806, safeTxError.message)
+    }
+  }, [safeTxError])
+
+  return (
+    <SignOrExecuteForm safeTx={safeTx} isExecutable={safe.threshold === 1} onSubmit={onSubmit} error={safeTxError}>
+      <Typography sx={({ palette }) => ({ color: palette.secondary.light })}>Module</Typography>
+      <EthHashInfo address={data.address} showCopyButton hasExplorer shortAddress={false} />
+      <Typography my={2}>
+        After removing this module, any feature or app that uses this module might no longer work. If this Safe requires
+        more then one signature, the module removal will have to be confirmed by other owners as well.
+      </Typography>
+    </SignOrExecuteForm>
+  )
+}
