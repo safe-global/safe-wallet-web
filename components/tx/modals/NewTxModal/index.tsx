f26ab76f5ad6faf1efbddddec68128add629a1de
katspaugh <katspaugh@gmail.com>
2022-08-01 08:54:23 +0200

    Size attrs

--- components/tx/modals/NewTxModal/index.tsx
+++ components/tx/modals/NewTxModal/index.tsx
@@ -5,25 +5,7 @@ import TokenTransferModal from '../TokenTransferModal'
 import AssetsIcon from '@/public/images/sidebar/assets.svg'
 import NftIcon from '@/public/images/sidebar/nfts.svg'
 
-const TxButton = (props: ButtonProps & { iconSize?: number }) => (
-  <Button
-    variant="contained"
-    fullWidth
-    {...props}
-    sx={
-      props.iconSize
-        ? {
-            '& svg': {
-              height: `${props.iconSize}px`,
-              width: 'auto',
-              my: `${(24 - props.iconSize) / 2}px`,
-              ml: `${props.iconSize - 24}px`,
-            },
-          }
-        : undefined
-    }
-  />
-)
+const TxButton = (props: ButtonProps) => <Button variant="contained" fullWidth {...props} />
 
 const NewTxModal = ({ onClose }: { onClose: () => void }): ReactElement => {
   const [tokenModalOpen, setTokenModalOpen] = useState<boolean>(false)
@@ -33,12 +15,12 @@ const NewTxModal = ({ onClose }: { onClose: () => void }): ReactElement => {
     <>
       <ModalDialog open dialogTitle="New transaction" onClose={onClose}>
         <DialogContent>
-          <Box display="flex" flexDirection="column" alignItems="center" gap={2} pt={6} pb={4} width={240} m="auto">
-            <TxButton onClick={() => setTokenModalOpen(true)} startIcon={<AssetsIcon />} iconSize={24}>
+          <Box display="flex" flexDirection="column" alignItems="center" gap={2} pt={7} pb={4} width={240} m="auto">
+            <TxButton onClick={() => setTokenModalOpen(true)} startIcon={<AssetsIcon width={20} height={20} />}>
               Send tokens
             </TxButton>
 
-            <TxButton onClick={() => alert('Implement me')} startIcon={<NftIcon />} iconSize={18}>
+            <TxButton onClick={() => alert('Implement me')} startIcon={<NftIcon width={16} height={16} />}>
               Send NFTs
             </TxButton>
           </Box>
