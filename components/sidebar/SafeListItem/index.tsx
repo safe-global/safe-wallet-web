7e35c4840a4ceac6e88f4ed046ccd19c8806084a
katspaugh <katspaugh@gmail.com>
2022-08-02 15:16:01 +0200

    Fix: empty name/address in SafeList

--- components/sidebar/SafeListItem/index.tsx
+++ components/sidebar/SafeListItem/index.tsx
@@ -4,9 +4,10 @@ import ListItemText from '@mui/material/ListItemText'
 import ListItem from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import Link from 'next/link'
+import classnames from 'classnames'
 
+import css from './styles.module.css'
 import SafeIcon from '@/components/common/SafeIcon'
-import { shortenAddress } from '@/utils/formatters'
 import { useAppSelector } from '@/store'
 import useSafeAddress from '@/hooks/useSafeAddress'
 import { selectChainById } from '@/store/chainsSlice'
@@ -16,8 +17,8 @@ import { AppRoutes } from '@/config/routes'
 import SafeListContextMenu from '@/components/sidebar/SafeListContextMenu'
 import Box from '@mui/material/Box'
 import { selectAllAddressBooks } from '@/store/addressBookSlice'
-
-import css from './styles.module.css'
+import EthHashInfo from '@/components/common/EthHashInfo'
+import { sameAddress } from '@/utils/addresses'
 
 const SafeListItem = ({
   address,
@@ -37,28 +38,21 @@ const SafeListItem = ({
   const safeAddress = useSafeAddress()
   const chain = useAppSelector((state) => selectChainById(state, chainId))
   const allAddressBooks = useAppSelector(selectAllAddressBooks)
-
   const currChainId = useChainId()
   const isCurrentSafe = currChainId === currChainId && safeAddress.toLowerCase() === address.toLowerCase()
+  const name = allAddressBooks[chainId]?.[address]
+  const isOpen = sameAddress(address, safeAddress)
 
+  // Scroll to the current Safe
   useEffect(() => {
     if (isCurrentSafe && shouldScrollToSafe) {
       safeRef.current?.scrollIntoView({ block: 'center' })
     }
   }, [isCurrentSafe, shouldScrollToSafe])
 
-  const name = allAddressBooks[chainId]?.[address]
-
-  const isOpen = address.toLowerCase() === safeAddress.toLowerCase()
-
-  const formattedAddress = (
-    <>
-      <b>{chain?.shortName}</b>:{shortenAddress(address)}
-    </>
-  )
-
   return (
     <ListItem
+      className={css.container}
       disablePadding
       secondaryAction={
         <Box display="flex" alignItems="center">
@@ -66,28 +60,18 @@ const SafeListItem = ({
             chainId={chainId}
             address={address}
             onClick={closeDrawer}
-            href={`${AppRoutes.load.safe}?address=${address}`}
+            href={`${AppRoutes.load.safe}?safe=${chain?.shortName}:${address}`}
           />
           <SafeListContextMenu address={address} chainId={chainId} />
         </Box>
       }
-      sx={{ my: '8px', px: '8px' }}
     >
       <Link href={`${AppRoutes.safe.home}?safe=${chain?.shortName}:${address}`} passHref>
         <ListItemButton
           key={address}
           onClick={closeDrawer}
           selected={isOpen}
-          sx={({ palette }) => ({
-            borderLeft: isOpen ? `6px solid ${palette.primary.main}` : undefined,
-            '&.Mui-selected': {
-              backgroundColor: palette.border.background,
-            },
-            '&:hover': {
-              backgroundColor: palette.primary.background,
-            },
-          })}
-          className={css.safe}
+          className={classnames(css.safe, { [css.open]: isOpen })}
           ref={safeRef}
         >
           <ListItemIcon>
@@ -95,8 +79,8 @@ const SafeListItem = ({
           </ListItemIcon>
           <ListItemText
             primaryTypographyProps={{ variant: 'body2' }}
-            primary={name ?? formattedAddress}
-            secondary={name && formattedAddress}
+            primary={name || ''}
+            secondary={<EthHashInfo address={address} showAvatar={false} showName={false} />}
           />
         </ListItemButton>
       </Link>
