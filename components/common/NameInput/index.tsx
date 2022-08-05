42ca6a8d9db3d5a7dc56d69355f225bd3eda6106
Usame Algan <usame@safe.global>
2022-08-05 13:47:06 +0200

    fix: Add validation for owner name change modal

--- components/common/NameInput/index.tsx
+++ components/common/NameInput/index.tsx
@@ -1,11 +1,17 @@
 import { TextField, TextFieldProps } from '@mui/material'
 import get from 'lodash/get'
-import { FieldError, useFormContext } from 'react-hook-form'
+import { FieldError, useFormContext, Validate } from 'react-hook-form'
 
 const NameInput = ({
   name,
+  validate,
+  required = false,
   ...props
-}: Omit<TextFieldProps, 'helperText' | 'error' | 'variant' | 'ref' | 'fullWidth'> & { name: string }) => {
+}: Omit<TextFieldProps, 'helperText' | 'error' | 'variant' | 'ref' | 'fullWidth'> & {
+  name: string
+  validate?: Validate<string>
+  required?: boolean
+}) => {
   const { register, formState } = useFormContext() || {}
   // the name can be a path: e.g. "owner.3.name"
   const fieldError = get(formState.errors, name) as FieldError | undefined
@@ -17,7 +23,7 @@ const NameInput = ({
       error={Boolean(fieldError)}
       helperText={fieldError?.type === 'maxLength' ? 'Maximum 50 symbols' : fieldError?.message}
       fullWidth
-      {...register(name, { maxLength: 50 })}
+      {...register(name, { maxLength: 50, required })}
     />
   )
 }
