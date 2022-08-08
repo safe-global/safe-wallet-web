5439dd6a602f53f3c5f173fd2edba190d840ee9b
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-08-08 17:07:49 +0200

    fix: Display exact date of tx after a certain threshold (#294)
    * fix: Display exact date of tx after a certain threshold
    
    * refactor: Extract threshold as const

--- components/common/DateTime/index.tsx
+++ components/common/DateTime/index.tsx
@@ -2,10 +2,14 @@ import { ReactElement } from 'react'
 import { Tooltip } from '@mui/material'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
 
+const DAYS_THRESHOLD = 60
+
 const DateTime = ({ value }: { value: number }): ReactElement => {
+  const displayExactDate = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > DAYS_THRESHOLD
+
   return (
-    <Tooltip title={formatDateTime(value)} placement="top">
-      <span>{formatTimeInWords(value)}</span>
+    <Tooltip title={displayExactDate ? '' : formatDateTime(value)} placement="top">
+      <span>{displayExactDate ? formatDateTime(value) : formatTimeInWords(value)}</span>
     </Tooltip>
   )
 }
