bdd5aa5b097013274db68ba221f187b3ba966041
Usame Algan <usame@safe.global>
2022-08-08 16:37:05 +0200

    refactor: Extract threshold as const

--- components/common/DateTime/index.tsx
+++ components/common/DateTime/index.tsx
@@ -2,12 +2,14 @@ import { ReactElement } from 'react'
 import { Tooltip } from '@mui/material'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
 
+const DAYS_THRESHOLD = 60
+
 const DateTime = ({ value }: { value: number }): ReactElement => {
-  const olderThan60Days = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > 60
+  const displayExactDate = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > DAYS_THRESHOLD
 
   return (
-    <Tooltip title={olderThan60Days ? '' : formatDateTime(value)} placement="top">
-      <span>{olderThan60Days ? formatDateTime(value) : formatTimeInWords(value)}</span>
+    <Tooltip title={displayExactDate ? '' : formatDateTime(value)} placement="top">
+      <span>{displayExactDate ? formatDateTime(value) : formatTimeInWords(value)}</span>
     </Tooltip>
   )
 }
