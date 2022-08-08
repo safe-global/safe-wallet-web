4383dc0dd5e7c3ef7a6fbb20e0b256a3f46674de
Usame Algan <usame@safe.global>
2022-08-08 16:02:02 +0200

    fix: Display exact date of tx after a certain threshold

--- components/common/DateTime/index.tsx
+++ components/common/DateTime/index.tsx
@@ -3,9 +3,11 @@ import { Tooltip } from '@mui/material'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
 
 const DateTime = ({ value }: { value: number }): ReactElement => {
+  const olderThan60Days = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > 60
+
   return (
-    <Tooltip title={formatDateTime(value)} placement="top">
-      <span>{formatTimeInWords(value)}</span>
+    <Tooltip title={olderThan60Days ? '' : formatDateTime(value)} placement="top">
+      <span>{olderThan60Days ? formatDateTime(value) : formatTimeInWords(value)}</span>
     </Tooltip>
   )
 }
