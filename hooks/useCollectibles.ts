d9c38c9b002ff7be2ba5081660279ba9cd262562
katspaugh <katspaugh@gmail.com>
2022-08-08 12:59:24 +0200

    Load NFTs in parallel with Safe

--- hooks/useCollectibles.ts
+++ hooks/useCollectibles.ts
@@ -1,17 +1,17 @@
 import { useEffect } from 'react'
 import { getCollectiblesPage, type SafeCollectiblesPage } from '@gnosis.pm/safe-react-gateway-sdk'
 import useAsync, { type AsyncResult } from './useAsync'
-import useSafeInfo from './useSafeInfo'
 import { Errors, logError } from '@/services/exceptions'
+import useSafeAddress from './useSafeAddress'
+import useChainId from './useChainId'
 
 export const useCollectibles = (pageUrl?: string): AsyncResult<SafeCollectiblesPage> => {
-  const { safe, safeAddress, safeLoaded } = useSafeInfo()
-  const { chainId } = safe
+  const chainId = useChainId()
+  const safeAddress = useSafeAddress()
 
   const [data, error, loading] = useAsync<SafeCollectiblesPage | undefined>(async () => {
-    if (!safeLoaded) return
     return getCollectiblesPage(chainId, safeAddress, undefined, pageUrl)
-  }, [safeLoaded, safeAddress, chainId, pageUrl])
+  }, [safeAddress, chainId, pageUrl])
 
   // Log errors
   useEffect(() => {
