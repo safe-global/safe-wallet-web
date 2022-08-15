cb100773027564e5aba392941787e48f483962a0
iamacook <aaron.cook@gnosis.pm>
2022-08-15 11:43:57 +0100

    fix: remove

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,13 +1,11 @@
 import ChainSwitcher from '@/components/common/ChainSwitcher'
 import ConnectWallet from '@/components/common/ConnectWallet'
 import NetworkSelector from '@/components/common/NetworkSelector'
-import SafeTokenWidget, { getSafeTokenAddress } from '@/components/common/SafeTokenWidget'
+import SafeTokenWidget from '@/components/common/SafeTokenWidget'
 import NotificationCenter from '@/components/notification-center/NotificationCenter'
-import useChainId from '@/hooks/useChainId'
 import SafeLogo from '@/public/logo.svg'
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
-import classnames from 'classnames'
+import { IconButton, Paper } from '@mui/material'
 import { type ReactElement } from 'react'
 import css from './styles.module.css'
 
@@ -15,46 +13,34 @@ type HeaderProps = {
   onMenuToggle: () => void
 }
 
-const Header = ({ onMenuToggle }: HeaderProps): ReactElement => {
-  const chainId = useChainId()
+const Header = ({ onMenuToggle }: HeaderProps): ReactElement => (
+  <Paper className={css.container} elevation={1}>
+    <div className={css.menuButton}>
+      <IconButton onClick={onMenuToggle} size="large" edge="start" color="default" aria-label="menu" sx={{ mr: 2 }}>
+        <MenuIcon />
+      </IconButton>
+    </div>
 
-  return (
-    <AppBar component="header" sx={{ zIndex: ({ zIndex }) => zIndex.drawer + 1 }} className={css.container}>
-      <Toolbar className={css.header}>
-        <div className={css.menuButton}>
-          <IconButton onClick={onMenuToggle} size="large" edge="start" color="default" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-        </div>
+    <div className={css.logo}>
+      <SafeLogo alt="Safe Logo" height={29} />
+    </div>
 
-        <div className={css.logo}>
-          <SafeLogo alt="Safe Logo" height={29} className={css.logo} />
-        </div>
+    <div className={css.chainSwitcher}>
+      <ChainSwitcher />
+    </div>
 
-        <div className={css.chainSwitcher}>
-          <ChainSwitcher />
-        </div>
+    <div className={css.tokenWidget}>
+      <SafeTokenWidget />
+    </div>
 
-        {!!getSafeTokenAddress(chainId) && (
-          <div className={classnames(css.tokenWidget, css.element)}>
-            <SafeTokenWidget />
-          </div>
-        )}
+    <div className={css.notificationCenter}>
+      <NotificationCenter />
+    </div>
 
-        <div className={classnames(css.element, css.notificationCenter)}>
-          <NotificationCenter />
-        </div>
+    <ConnectWallet />
 
-        <div className={css.element}>
-          <ConnectWallet />
-        </div>
-
-        <Box className={css.element} sx={{ pr: '0 !important' }}>
-          <NetworkSelector />
-        </Box>
-      </Toolbar>
-    </AppBar>
-  )
-}
+    <NetworkSelector />
+  </Paper>
+)
 
 export default Header
