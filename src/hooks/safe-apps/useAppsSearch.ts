f42554040dd2644f7d6ab257a9361ec5de5aaaf7
Mikhail <mveehkim@gmail.com>
2022-08-18 15:29:38 +0200

    add fuzzy search

---
+++ src/hooks/safe-apps/useAppsSearch.ts
@@ -0,0 +1,25 @@
+import { useMemo } from 'react'
+import Fuse from 'fuse.js'
+import { SafeAppData } from '@gnosis.pm/safe-react-gateway-sdk'
+
+const useAppsSearch = (apps: SafeAppData[], query: string): SafeAppData[] => {
+  const fuse = useMemo(
+    () =>
+      new Fuse(apps, {
+        keys: ['name', 'description'],
+        // https://fusejs.io/api/options.html#threshold
+        // Very naive explanation: threshold represents how accurate the search results should be. The default is 0.6
+        // I tested it and found it to make the search results more accurate when the threshold is 0.2
+        // 0 - 1, where 0 is the exact match and 1 matches anything
+        threshold: 0.2,
+        findAllMatches: true,
+      }),
+    [apps],
+  )
+
+  const results = useMemo(() => (query ? fuse.search(query).map((result) => result.item) : apps), [fuse, apps, query])
+
+  return results
+}
+
+export { useAppsSearch }
--- src/pages/safe/apps.tsx
+++ src/pages/safe/apps.tsx
@@ -1,28 +1,24 @@
 import * as React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Grid from '@mui/material/Grid'
 import { useSafeApps } from '@/hooks/safe-apps/useSafeApps'
-import { AppCard } from '@/components/safe-apps/AppCard'
 import { SafeAppsHeader } from '@/components/safe-apps/SafeAppsHeader'
 import { IS_PRODUCTION } from '@/config/constants'
-import { AddCustomAppCard } from '@/components/safe-apps/AddCustomAppCard'
 import { Typography } from '@mui/material'
 import { SafeAppsList } from '@/components/safe-apps/SafeAppsList'
+import { useAppsSearch } from '@/hooks/safe-apps/useAppsSearch'
 
 const Apps: NextPage = () => {
   const [searchQuery, setSearchQuery] = React.useState('')
   const { allSafeApps, remoteSafeAppsLoading, customSafeAppsLoading, addCustomApp } = useSafeApps()
-  const filteredApps = React.useMemo(
-    () => allSafeApps.filter((app) => app.name.toLowerCase().includes(searchQuery.toLowerCase())),
-    [searchQuery, allSafeApps],
-  )
+  const filteredApps = useAppsSearch(allSafeApps, searchQuery)
 
   let pageBody = (
     <SafeAppsList
       loading={remoteSafeAppsLoading || customSafeAppsLoading}
       apps={filteredApps}
       hideAddCustomApp={searchQuery.length > 0}
+      onAddCustomApp={addCustomApp}
     />
   )
   if (searchQuery && filteredApps.length === 0) {
