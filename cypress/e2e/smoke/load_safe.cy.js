162045e8946c1764c3105c07fa298380da5d09ad
Franco Venica <francovenica@gmail.com>
2022-08-18 22:44:46 -0300

    commenting unused const

--- cypress/e2e/smoke/load_safe.cy.js
+++ cypress/e2e/smoke/load_safe.cy.js
@@ -7,8 +7,8 @@ const SAFE_QR_CODE_ADDRESS = '0x9913B9180C20C6b0F21B6480c84422F6ebc4B808'
 const NON_CONTRACT_ADDRESS = '0x61a0c717d18232711bC788F19C9Cd56a43cc8872'
 const INVALID_INPUT_ERROR_MSG = 'Invalid address format'
 const INVALID_ADDRESS_ERROR_MSG = 'Address given is not a valid Safe address'
-const OWNER_ENS_DEFAULT_NAME = 'francoledger.eth'
-const OWNER_ADDRESS = '0x6f965E48347AF3Df65c14CCc176A9CbeCEa0eDb5'
+// const OWNER_ENS_DEFAULT_NAME = 'francoledger.eth'
+// const OWNER_ADDRESS = '0x6f965E48347AF3Df65c14CCc176A9CbeCEa0eDb5'
 
 describe('Load Safe', () => {
   it('Should enter Add safe form', () => {
