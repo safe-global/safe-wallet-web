55f4dd591f74ad01198d433851224063b2e0855b
iamacook <aaron@safe.global>
2022-08-22 16:29:01 +0200

    fix: use `isSignableBy`

--- src/hooks/useTransactionStatus.ts
+++ src/hooks/useTransactionStatus.ts
@@ -1,28 +1,22 @@
 import { useAppSelector } from '@/store'
 import { PendingStatus, selectPendingTxById } from '@/store/pendingTxsSlice'
-import { sameAddress } from '@/utils/addresses'
-import { isMultisigExecutionInfo } from '@/utils/transaction-guards'
+import { isSignableBy } from '@/utils/transaction-guards'
 import { TransactionSummary, TransactionStatus } from '@gnosis.pm/safe-react-gateway-sdk'
 import { useMemo } from 'react'
 import useWallet from './wallets/useWallet'
 
 type TxLocalStatus = TransactionStatus | PendingStatus
 
-const hasSignature = (executionInfo: TransactionSummary['executionInfo'], address?: string) => {
-  return isMultisigExecutionInfo(executionInfo)
-    ? executionInfo.missingSigners?.some((signer) => sameAddress(signer.value, address))
-    : false
-}
+const useTransactionStatus = (txSummary: TransactionSummary): string => {
+  const { txStatus, id } = txSummary
 
-const useTransactionStatus = ({ txStatus, id, executionInfo }: TransactionSummary): string => {
   const wallet = useWallet()
   const pendingTx = useAppSelector((state) => selectPendingTxById(state, id))
 
   const STATUS_LABELS: Record<TxLocalStatus, string> = useMemo(
     () => ({
-      [TransactionStatus.AWAITING_CONFIRMATIONS]: hasSignature(executionInfo, wallet?.address)
-        ? 'Needs your confirmation'
-        : 'Needs confirmations',
+      [TransactionStatus.AWAITING_CONFIRMATIONS]:
+        wallet?.address && isSignableBy(txSummary, wallet.address) ? 'Needs your confirmation' : 'Needs confirmations',
       [TransactionStatus.AWAITING_EXECUTION]: 'Awaiting execution',
       [TransactionStatus.CANCELLED]: 'Cancelled',
       [TransactionStatus.FAILED]: 'Failed',
@@ -31,7 +25,7 @@ const useTransactionStatus = ({ txStatus, id, executionInfo }: TransactionSummar
       [PendingStatus.MINING]: 'Mining',
       [PendingStatus.INDEXING]: 'Indexing',
     }),
-    [executionInfo, wallet?.address],
+    [txSummary, wallet?.address],
   )
 
   return STATUS_LABELS[pendingTx?.status || txStatus] || ''
