ebde2655e4fb978963b20d82cae61a4ce42ffd8b
iamacook <aaron@safe.global>
2022-08-22 16:43:55 +0200

    fix: keep statuses static

--- src/hooks/useTransactionStatus.ts
+++ src/hooks/useTransactionStatus.ts
@@ -2,10 +2,25 @@ import { useAppSelector } from '@/store'
 import { PendingStatus, selectPendingTxById } from '@/store/pendingTxsSlice'
 import { isSignableBy } from '@/utils/transaction-guards'
 import { TransactionSummary, TransactionStatus } from '@gnosis.pm/safe-react-gateway-sdk'
-import { useMemo } from 'react'
 import useWallet from './wallets/useWallet'
 
 type TxLocalStatus = TransactionStatus | PendingStatus
+
+const STATUS_LABELS: Record<TxLocalStatus, string> = {
+  [TransactionStatus.AWAITING_CONFIRMATIONS]: 'Awaiting confirmations',
+  [TransactionStatus.AWAITING_EXECUTION]: 'Awaiting execution',
+  [TransactionStatus.CANCELLED]: 'Cancelled',
+  [TransactionStatus.FAILED]: 'Failed',
+  [TransactionStatus.SUCCESS]: 'Success',
+  [PendingStatus.SUBMITTING]: 'Submitting',
+  [PendingStatus.MINING]: 'Mining',
+  [PendingStatus.INDEXING]: 'Indexing',
+}
+
+const WALLET_STATUS_LABELS: Record<TxLocalStatus, string> = {
+  ...STATUS_LABELS,
+  [TransactionStatus.AWAITING_CONFIRMATIONS]: 'Needs your confirmation',
+}
 
 const useTransactionStatus = (txSummary: TransactionSummary): string => {
   const { txStatus, id } = txSummary
@@ -13,22 +28,9 @@ const useTransactionStatus = (txSummary: TransactionSummary): string => {
   const wallet = useWallet()
   const pendingTx = useAppSelector((state) => selectPendingTxById(state, id))
 
-  const STATUS_LABELS: Record<TxLocalStatus, string> = useMemo(
-    () => ({
-      [TransactionStatus.AWAITING_CONFIRMATIONS]:
-        wallet?.address && isSignableBy(txSummary, wallet.address) ? 'Needs your confirmation' : 'Needs confirmations',
-      [TransactionStatus.AWAITING_EXECUTION]: 'Awaiting execution',
-      [TransactionStatus.CANCELLED]: 'Cancelled',
-      [TransactionStatus.FAILED]: 'Failed',
-      [TransactionStatus.SUCCESS]: 'Success',
-      [PendingStatus.SUBMITTING]: 'Submitting',
-      [PendingStatus.MINING]: 'Mining',
-      [PendingStatus.INDEXING]: 'Indexing',
-    }),
-    [txSummary, wallet?.address],
-  )
+  const statuses = wallet?.address && isSignableBy(txSummary, wallet.address) ? WALLET_STATUS_LABELS : STATUS_LABELS
 
-  return STATUS_LABELS[pendingTx?.status || txStatus] || ''
+  return statuses[pendingTx?.status || txStatus] || ''
 }
 
 export default useTransactionStatus
