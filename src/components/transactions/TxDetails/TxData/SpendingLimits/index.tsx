420dc798ed646e3f8e7d359ff1855754bed90d6b
iamacook <aaron@safe.global>
2022-08-23 12:15:45 +0200

    fix: use diff. reset for Rinkeby spending limits

--- src/components/transactions/TxDetails/TxData/SpendingLimits/index.tsx
+++ src/components/transactions/TxDetails/TxData/SpendingLimits/index.tsx
@@ -11,6 +11,7 @@ import { sameAddress } from '@/utils/addresses'
 import { formatDecimals } from '@/utils/formatters'
 import { isSetAllowance, SpendingLimitMethods } from '@/utils/transaction-guards'
 import css from './styles.module.css'
+import chains from '@/config/chains'
 
 type SpendingLimitsProps = {
   txData?: TransactionData
@@ -108,9 +109,8 @@ const RINKEBY_RESET_TIME_OPTIONS = [
   { label: '1 hour', value: '60' },
 ]
 
-const getResetTimeOptions = (chainName = ''): { label: string; value: string }[] => {
-  const currentNetwork = chainName.toLowerCase()
-  return currentNetwork !== 'rinkeby' ? RESET_TIME_OPTIONS : RINKEBY_RESET_TIME_OPTIONS
+export const getResetTimeOptions = (chainId = ''): { label: string; value: string }[] => {
+  return chainId !== chains.rin ? RESET_TIME_OPTIONS : RINKEBY_RESET_TIME_OPTIONS
 }
 
 export default SpendingLimits
