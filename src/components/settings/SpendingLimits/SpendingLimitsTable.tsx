0dd784d6e84f7a3d7717bc9e7efe2cf489d4d940
Usame Algan <usame@safe.global>
2022-08-24 13:54:13 +0200

    fix: use safeFormatUnits for spending limit table

--- src/components/settings/SpendingLimits/SpendingLimitsTable.tsx
+++ src/components/settings/SpendingLimits/SpendingLimitsTable.tsx
@@ -1,7 +1,7 @@
 import EnhancedTable from '@/components/common/EnhancedTable'
 import useBalances from '@/hooks/useBalances'
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
-import { formatVisualAmount } from '@/utils/formatters'
+import { safeFormatUnits } from '@/utils/formatters'
 import { Box, IconButton } from '@mui/material'
 import { relativeTime } from '@/utils/date'
 import EthHashInfo from '@/components/common/EthHashInfo'
@@ -47,10 +47,10 @@ export const SpendingLimitsTable = ({ spendingLimits }: { spendingLimits: Spendi
       spendingLimits.map((spendingLimit) => {
         const token = balances.items.find((item) => item.tokenInfo.address === spendingLimit.token)
         const amount = BigNumber.from(spendingLimit.amount)
-        const formattedAmount = formatVisualAmount(amount, token?.tokenInfo.decimals)
+        const formattedAmount = safeFormatUnits(amount, token?.tokenInfo.decimals)
 
         const spent = BigNumber.from(spendingLimit.spent)
-        const formattedSpent = formatVisualAmount(spent, token?.tokenInfo.decimals)
+        const formattedSpent = safeFormatUnits(spent, token?.tokenInfo.decimals)
 
         return {
           beneficiary: {
