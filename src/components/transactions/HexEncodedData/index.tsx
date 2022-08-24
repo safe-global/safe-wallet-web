e24f4d345cdeaa04b7a911237e9a918d5b7dad88
iamacook <aaron@safe.global>
2022-08-24 13:08:06 +0200

    fix: lowercase

--- src/components/transactions/HexEncodedData/index.tsx
+++ src/components/transactions/HexEncodedData/index.tsx
@@ -31,7 +31,7 @@ export const HexEncodedData = ({ hexData, title, limit = 20 }: Props): ReactElem
         <>
           {showTxData ? hexData : shortenText(hexData, 25)}{' '}
           <Link component="button" onClick={toggleExpanded}>
-            Show {showTxData ? 'Less' : 'More'}
+            Show {showTxData ? 'less' : 'more'}
           </Link>
         </>
       ) : (
