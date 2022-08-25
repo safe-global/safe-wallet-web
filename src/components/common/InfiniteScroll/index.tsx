c917b0a7e5d4a9e5d531d63c9ae62d5843c7034f
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-08-25 09:24:22 +0200

    Feat: infinite scroll (#439)
    * Feat: infinite scroll
    
    * PR comments
    
    * It does indeed work w/o a height!

---
+++ src/components/common/InfiniteScroll/index.tsx
@@ -0,0 +1,40 @@
+import { type MutableRefObject, useEffect, useRef, useState, type ReactElement, useCallback } from 'react'
+
+const useIntersectionObserver = (element: MutableRefObject<HTMLElement | null>): boolean => {
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false)
+  const observer = useRef<IntersectionObserver | undefined>()
+
+  const callback = useCallback(([entry]: IntersectionObserverEntry[]) => {
+    setIsIntersecting(entry.isIntersecting)
+  }, [])
+
+  useEffect(() => {
+    if (element.current) {
+      observer.current = new IntersectionObserver(callback)
+
+      observer.current.observe(element.current)
+    }
+
+    return () => {
+      observer.current?.disconnect()
+      observer.current = undefined
+    }
+  }, [callback, element])
+
+  return isIntersecting
+}
+
+const InfiniteScroll = ({ onLoadMore }: { onLoadMore: () => void }): ReactElement => {
+  const elementRef = useRef<HTMLDivElement | null>(null)
+  const isIntersecting = useIntersectionObserver(elementRef)
+
+  useEffect(() => {
+    if (isIntersecting) {
+      onLoadMore()
+    }
+  }, [isIntersecting, onLoadMore])
+
+  return <div ref={elementRef} />
+}
+
+export default InfiniteScroll
