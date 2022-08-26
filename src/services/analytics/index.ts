2d69252ab50891d117169c47beb462ae9219484c
katspaugh <katspaugh@gmail.com>
2022-08-26 09:12:11 +0200

    Rm production check

--- src/services/analytics/index.ts
+++ src/services/analytics/index.ts
@@ -1,12 +1,5 @@
-import { IS_PRODUCTION } from '@/config/constants'
-import { AnalyticsEvent } from './types'
 import { gtmTrack } from './gtm'
 
-export const trackEvent = (eventData: AnalyticsEvent) => {
-  if (IS_PRODUCTION) {
-    gtmTrack(eventData)
-  }
-}
-
+export const trackEvent = gtmTrack
 export * from './types'
 export * from './events'
