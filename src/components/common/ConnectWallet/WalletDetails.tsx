5d35d78e7847472d60fb47fe0e92612530f53306
iamacook <aaron@safe.global>
2022-08-29 15:39:59 +0200

    fix: adjust styling of Safe creation

--- src/components/common/ConnectWallet/WalletDetails.tsx
+++ src/components/common/ConnectWallet/WalletDetails.tsx
@@ -4,14 +4,18 @@ import type { ReactElement } from 'react'
 import useOnboard from '@/hooks/wallets/useOnboard'
 import { logError, Errors } from '@/services/exceptions'
 import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
-import Track from '@/components/common/Track'
 import KeyholeIcon from '@/components/common/ConnectWallet/KeyholeIcon'
+import { trackEvent } from '@/services/analytics'
 
 const WalletDetails = ({ onConnect }: { onConnect?: () => void }): ReactElement => {
   const onboard = useOnboard()
 
   const handleConnect = async () => {
     if (!onboard) return
+
+    // We `trackEvent` instead of using `<Track>` as it impedes styling
+    trackEvent(OVERVIEW_EVENTS.OPEN_ONBOARD)
+
     onConnect?.()
     onboard.connectWallet().catch((e) => logError(Errors._302, (e as Error).message))
   }
@@ -22,11 +26,9 @@ const WalletDetails = ({ onConnect }: { onConnect?: () => void }): ReactElement 
 
       <KeyholeIcon height={40} width={40} />
 
-      <Track {...OVERVIEW_EVENTS.OPEN_ONBOARD}>
-        <Button onClick={handleConnect} variant="contained" size="small" disableElevation>
-          Connect
-        </Button>
-      </Track>
+      <Button onClick={handleConnect} variant="contained" size="small" disableElevation fullWidth>
+        Connect
+      </Button>
     </>
   )
 }
