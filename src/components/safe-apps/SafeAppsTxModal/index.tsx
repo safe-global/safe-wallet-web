3e2b25a5c8a6a8117f9b7d482660006c91452d90
Yago Pérez Vázquez <yago@safe.global>
2022-09-02 18:16:00 +0200

    Remove not required block and improve invalid transaction logic

--- src/components/safe-apps/SafeAppsTxModal/index.tsx
+++ src/components/safe-apps/SafeAppsTxModal/index.tsx
@@ -3,6 +3,23 @@ import { BaseTransaction, RequestId, SendTransactionRequestParams } from '@gnosi
 import { TxStepperProps } from '@/components/tx/TxStepper/useTxStepper'
 import TxModal, { TxModalProps } from '@/components/tx/TxModal'
 import ReviewSafeAppsTx from './ReviewSafeAppsTx'
+import InvalidTransaction from './InvalidTransaction'
+import { validateAddress } from '@/utils/validation'
+
+const isTxValid = (txs: BaseTransaction[]) => txs.length && txs.every((t) => validateTransaction(t))
+
+const validateTransaction = (t: BaseTransaction): boolean => {
+  if (!['string', 'number'].includes(typeof t.value)) {
+    return false
+  }
+
+  if (typeof t.value === 'string' && !/^(0x)?[0-9a-f]+$/i.test(t.value)) {
+    return false
+  }
+
+  const isAddressValid = validateAddress(t.to) === undefined
+  return isAddressValid && !!t.data && typeof t.data === 'string'
+}
 
 export type SafeAppsTxParams = {
   appId?: string
@@ -15,7 +32,13 @@ export type SafeAppsTxParams = {
 const SafeAppsTxSteps: TxStepperProps['steps'] = [
   {
     label: 'Safe Apps transaction',
-    render: (data, onSubmit) => <ReviewSafeAppsTx onSubmit={onSubmit} safeAppsTx={data as SafeAppsTxParams} />,
+    render: (data, onSubmit) => {
+      if (!isTxValid((data as SafeAppsTxParams).txs)) {
+        return <InvalidTransaction />
+      }
+
+      return <ReviewSafeAppsTx onSubmit={onSubmit} safeAppsTx={data as SafeAppsTxParams} />
+    },
   },
 ]
 
