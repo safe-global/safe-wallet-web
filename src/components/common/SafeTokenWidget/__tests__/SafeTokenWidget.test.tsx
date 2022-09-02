817240c21be5ec4d8146216870b0142df2b37418
schmanu <manu@safe.global>
2022-09-02 14:08:52 +0200

    change: adjust safe token widget - format amount using two decimals - link to safe app

--- src/components/common/SafeTokenWidget/__tests__/SafeTokenWidget.test.tsx
+++ src/components/common/SafeTokenWidget/__tests__/SafeTokenWidget.test.tsx
@@ -1,11 +1,16 @@
 import { SAFE_TOKEN_ADDRESSES } from '@/config/constants'
 import * as useBalances from '@/hooks/useBalances'
+import * as useChainId from '@/hooks/useChainId'
 import { render, waitFor } from '@/tests/test-utils'
 import { TokenType } from '@gnosis.pm/safe-react-gateway-sdk'
 import { ethers } from 'ethers'
 import SafeTokenWidget from '..'
 
 describe('SafeTokenWidget', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
   it('Should display skeleton if balance is loading', () => {
     jest.spyOn(useBalances, 'default').mockImplementation(() => ({
       balances: {
@@ -18,6 +23,22 @@ describe('SafeTokenWidget', () => {
 
     const result = render(<SafeTokenWidget />)
     expect(result.baseElement).not.toHaveTextContent('0')
+  })
+
+  it('Should render nothing for unsupported chains', () => {
+    jest.spyOn(useChainId, 'default').mockImplementation(() => '100')
+
+    jest.spyOn(useBalances, 'default').mockImplementation(() => ({
+      balances: {
+        fiatTotal: '0',
+        items: [],
+      },
+      loading: true,
+      error: undefined,
+    }))
+
+    const result = render(<SafeTokenWidget />)
+    expect(result.baseElement).toContainHTML('<body><div /></body>')
   })
 
   it('Should display 0 if balance has no safe token', async () => {
@@ -34,13 +55,13 @@ describe('SafeTokenWidget', () => {
     await waitFor(() => expect(result.baseElement).toHaveTextContent('0'))
   })
 
-  it('Should display the value rounded down if balance has safe token', async () => {
+  it('Should display the value formatted correctly', async () => {
     jest.spyOn(useBalances, 'default').mockImplementation(() => ({
       balances: {
         fiatTotal: '0',
         items: [
           {
-            balance: ethers.utils.parseEther('420.69').toString(),
+            balance: ethers.utils.parseEther('420000.691').toString(),
             fiatBalance: '0',
             fiatConversion: '1',
             tokenInfo: {
@@ -60,8 +81,38 @@ describe('SafeTokenWidget', () => {
 
     const result = render(<SafeTokenWidget />)
     await waitFor(() => {
-      expect(result.baseElement).toHaveTextContent('420')
-      expect(result.baseElement).not.toHaveTextContent('420.69')
+      expect(result.baseElement).toHaveTextContent('420,000.69')
+      expect(result.baseElement).not.toHaveTextContent('420,000.691')
+    })
+  })
+
+  it('Should render a link to the claiming app', async () => {
+    jest.spyOn(useBalances, 'default').mockImplementation(() => ({
+      balances: {
+        fiatTotal: '0',
+        items: [
+          {
+            balance: ethers.utils.parseEther('1').toString(),
+            fiatBalance: '0',
+            fiatConversion: '1',
+            tokenInfo: {
+              address: SAFE_TOKEN_ADDRESSES['4'],
+              decimals: 18,
+              logoUri: '',
+              name: 'SAFE Token',
+              symbol: 'SAFE',
+              type: TokenType.ERC20,
+            },
+          },
+        ],
+      },
+      loading: false,
+      error: undefined,
+    }))
+
+    const result = render(<SafeTokenWidget />)
+    await waitFor(() => {
+      expect(result.baseElement).toContainHTML(`appUrl=https://safe-claiming-app.pages.dev/`)
     })
   })
 })
--- src/components/common/SafeTokenWidget/index.tsx
+++ src/components/common/SafeTokenWidget/index.tsx
@@ -1,12 +1,18 @@
-import { SAFE_TOKEN_ADDRESSES } from '@/config/constants'
+import { SAFE_REACT_URL, SAFE_TOKEN_ADDRESSES } from '@/config/constants'
+import { AppRoutes } from '@/config/routes'
 import useBalances from '@/hooks/useBalances'
 import useChainId from '@/hooks/useChainId'
+import { formatAmountWithPrecision } from '@/utils/formatNumber'
 import { safeFormatUnits } from '@/utils/formatters'
 import { Box, ButtonBase, Skeleton, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
 
 import SafeTokenIcon from './safe_token.svg'
 
 import css from './styles.module.css'
+
+// TODO: once listed on safe apps list, get the url from there?
+const CLAIMING_APP_URL = 'https://safe-claiming-app.pages.dev/'
 
 export const getSafeTokenAddress = (chainId: string): string => {
   return SAFE_TOKEN_ADDRESSES[chainId]
@@ -15,24 +21,31 @@ export const getSafeTokenAddress = (chainId: string): string => {
 const SafeTokenWidget = () => {
   const balances = useBalances()
   const chainId = useChainId()
+  const router = useRouter()
 
   const tokenAddress = getSafeTokenAddress(chainId)
   if (!tokenAddress) {
     return null
   }
 
+  const shareUrl = `${SAFE_REACT_URL}/share/safe-app?appUrl=${CLAIMING_APP_URL}&chainId=${chainId}`
+  const url = router.query.safe
+    ? `${AppRoutes.safe.apps}?safe=${router.query.safe}&appUrl=${CLAIMING_APP_URL}`
+    : shareUrl
+
   const safeBalance = balances.balances.items.find((balanceItem) => balanceItem.tokenInfo.address === tokenAddress)
 
   const safeBalanceDecimals = Number(safeFormatUnits(safeBalance?.balance || 0, safeBalance?.tokenInfo.decimals))
-  const flooredSafeBalance = Math.floor(safeBalanceDecimals).toFixed(0)
+  const flooredSafeBalance = formatAmountWithPrecision(safeBalanceDecimals, 2)
 
   return (
     <Box className={css.buttonContainer}>
       <ButtonBase
-        disabled /* Until safe app is deployed! */
         aria-describedby={'safe-token-widget'}
         sx={{ alignSelf: 'stretch' }}
         className={css.tokenButton}
+        href={url}
+        rel="noref noopener"
       >
         <SafeTokenIcon />
 
