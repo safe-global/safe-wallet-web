6319378cf6be1f86d6dd35fa2de3e34102aceac2
iamacook <aaron@safe.global>
2022-09-05 13:46:31 +0200

    fix: do not pass version metadata to Core SDK

---
+++ src/hooks/coreSDK/__tests__/safeCoreSDK.test.ts
@@ -0,0 +1,26 @@
+import { isValidSafeVersion } from '../safeCoreSDK'
+
+describe('safeCoreSDK', () => {
+  describe('isValidSafeVersion', () => {
+    it('should return true for valid versions', () => {
+      expect(isValidSafeVersion('1.3.0')).toBe(true)
+
+      expect(isValidSafeVersion('1.2.0')).toBe(true)
+
+      expect(isValidSafeVersion('1.1.1')).toBe(true)
+
+      expect(isValidSafeVersion('1.3.0+L2')).toBe(true)
+    })
+    it('should return false for invalid versions', () => {
+      expect(isValidSafeVersion('1.3.1')).toBe(false)
+
+      expect(isValidSafeVersion('1.4.0')).toBe(false)
+
+      expect(isValidSafeVersion('1.0.0')).toBe(false)
+
+      expect(isValidSafeVersion('')).toBe(false)
+
+      expect(isValidSafeVersion()).toBe(false)
+    })
+  })
+})
