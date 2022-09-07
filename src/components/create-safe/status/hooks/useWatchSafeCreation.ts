e24ef33f0d17a58c5eca3c248add3abc8250a06d
Mikhail <mveehkim@gmail.com>
2022-09-07 12:23:26 +0200

    add apps event after safe creation

--- src/components/create-safe/status/hooks/useWatchSafeCreation.ts
+++ src/components/create-safe/status/hooks/useWatchSafeCreation.ts
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 import { pollSafeInfo } from '@/components/create-safe/status/usePendingSafeCreation'
 import { AppRoutes } from '@/config/routes'
 import { SafeCreationStatus } from '@/components/create-safe/status/useSafeCreation'
-import { trackEvent, CREATE_SAFE_EVENTS } from '@/services/analytics'
+import { trackEvent, CREATE_SAFE_EVENTS, SAFE_APPS_EVENTS } from '@/services/analytics'
 import { useAppSelector } from '@/store'
 import { selectChainById } from '@/store/chainsSlice'
 
@@ -48,6 +48,11 @@ const useWatchSafeCreation = ({
           // The URL for already existing query params
           const hasQueryParams = redirectUrl.includes('?')
           const appendChar = hasQueryParams ? '&' : '?'
+
+          if (redirectUrl.includes('apps')) {
+            trackEvent({ ...SAFE_APPS_EVENTS.SHARED_APP_OPEN_AFTER_SAFE_CREATION })
+          }
+
           router.push(redirectUrl + `${appendChar}safe=${address}`)
         } else {
           router.push({ pathname: AppRoutes.safe.home, query: { safe: address } })
