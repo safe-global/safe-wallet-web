96da29f2d84bce23e9831c1c3de532dd45354e76
katspaugh <katspaugh@gmail.com>
2022-09-15 15:14:24 +0200

    Fix: contrastive logo for the safe mobile wallet

--- src/services/pairing/connector.ts
+++ src/services/pairing/connector.ts
@@ -2,17 +2,22 @@ import WalletConnect from '@walletconnect/client'
 import bowser from 'bowser'
 
 import packageJson from '../../../package.json'
-import { IS_PRODUCTION, SAFE_REACT_URL } from '@/config/constants'
+import { IS_PRODUCTION } from '@/config/constants'
 import ExternalStore from '@/services/ExternalStore'
+import PairingIcon from '@/public/images/safe-mobile-connect.png'
 
 export const PAIRING_MODULE_STORAGE_ID = 'pairingConnector'
 
 export const getClientMeta = () => {
+  const host = location.origin
+
   const APP_META = {
     name: `Safe Web v${packageJson.version}`,
-    url: SAFE_REACT_URL,
-    icons: [`${SAFE_REACT_URL}/images/favicons/logo_120x120.png`],
+    url: host,
+    icons: [`${host}${PairingIcon.src}`],
   }
+
+  console.log('APP_META', APP_META)
 
   if (typeof window === 'undefined') {
     return {
