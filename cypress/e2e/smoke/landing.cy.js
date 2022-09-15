b6451b8e1d7d9e467d168de6a09a244ab4253eeb
Franco Venica <francovenica@gmail.com>
2022-09-15 05:00:48 +0300

    chore: edit selectors

---
+++ cypress/e2e/smoke/landing.cy.js
@@ -0,0 +1,34 @@
+describe('Landing page', () => {
+  it('redirects to welcome page', () => {
+    cy.visit('/')
+
+    cy.url().should('include', '/welcome')
+  })
+
+  it('features discoverability cookies', () => {
+    cy.visit('/')
+
+    let preferencesModal = cy.contains("We use cookies to provide").parent()
+
+    preferencesModal.contains('Accept selection').click()
+    preferencesModal.should('not.exist')
+
+    cy.contains("What's New").click()
+
+    //The modal was closed, so the element has to be set again
+    preferencesModal = cy.contains("We use cookies to provide").parent() 
+
+    preferencesModal.contains('accept the "Updates & Feedback"')
+
+    //TODO: understand why after every contains the next search is on the child instead of the current DOM object.
+    //Solved for now "going back" to the parent every time
+    preferencesModal.parent().contains('Updates (Beamer)').click()
+    preferencesModal.parent().contains('Accept selection').click()
+    preferencesModal.should('not.exist')
+
+    // Open the features discoverability section
+    // cy.wait(3000) // TODO: wait for Beamer cookies to be set
+    // cy.contains("What's New").click()
+    // cy.get('#beamerOverlay .iframeCointaner').should('exist')
+  })
+})
