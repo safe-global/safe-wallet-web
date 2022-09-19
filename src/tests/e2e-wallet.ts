05d82627e550446fbdb2643d60b71638e353c334
iamacook <aaron@safe.global>
2022-09-19 18:54:28 +0200

    fix: simplify module

--- src/tests/e2e-wallet.ts
+++ src/tests/e2e-wallet.ts
@@ -6,7 +6,7 @@ import { getRpcServiceUrl } from '@/hooks/wallets/web3'
 
 const WALLET_NAME = 'E2E Wallet'
 
-const e2eWallet = ({ chainId, rpcUri }: ChainInfo): WalletInit => {
+const e2eWallet = ({ rpcUri }: ChainInfo): WalletInit => {
   return () => {
     return {
       label: WALLET_NAME,
@@ -15,7 +15,6 @@ const e2eWallet = ({ chainId, rpcUri }: ChainInfo): WalletInit => {
         const { createEIP1193Provider } = await import('@web3-onboard/common')
 
         const { default: HDWalletProvider } = await import('@truffle/hdwallet-provider')
-        const { Web3Provider } = await import('@ethersproject/providers')
 
         const provider = new HDWalletProvider({
           mnemonic: CYPRESS_MNEMONIC,
@@ -23,9 +22,8 @@ const e2eWallet = ({ chainId, rpcUri }: ChainInfo): WalletInit => {
         })
 
         return {
-          provider: createEIP1193Provider(new Web3Provider(provider), {
+          provider: createEIP1193Provider(provider.engine, {
             eth_requestAccounts: async () => provider.getAddresses(),
-            eth_chainId: async () => `0x${parseInt(chainId).toString(16)}`,
           }),
         }
       },
