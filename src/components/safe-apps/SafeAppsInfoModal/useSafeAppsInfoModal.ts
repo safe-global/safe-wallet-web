a4c542daab0c53c5f4f27f49be0394c8f0f117dc
Yago Pérez Vázquez <yago@safe.global>
2022-09-23 10:56:41 +0200

    Make useSafeAppsInfoModal work by network

--- src/components/safe-apps/SafeAppsInfoModal/useSafeAppsInfoModal.ts
+++ src/components/safe-apps/SafeAppsInfoModal/useSafeAppsInfoModal.ts
@@ -1,16 +1,20 @@
 import { BrowserPermission } from '@/hooks/safe-apps/permissions'
 import useChainId from '@/hooks/useChainId'
 import useLocalStorage from '@/services/local-storage/useLocalStorage'
-import { useState, useEffect, useCallback, useMemo, useRef } from 'react'
+import { useEffect, useCallback, useMemo, useRef } from 'react'
 import { AllowedFeatures, PermissionStatus } from '../types'
 
-const APPS_SECURITY_FEEDBACK_MODAL = 'APPS_SECURITY_FEEDBACK_MODAL'
+const SAFE_APPS_INFO_MODAL = 'SAFE_APPS_INFO_MODAL'
 
 type useSafeAppsInfoModal = {
   url: string
   permissions: AllowedFeatures[]
   addPermissions: (origin: string, permissions: BrowserPermission[]) => void
   getPermissions: (origin: string) => BrowserPermission[]
+}
+
+type ModalInfoProps = {
+  [chainId: string]: { consentsAccepted?: boolean }
 }
 
 const useSafeAppsInfoModal = ({
@@ -26,25 +30,14 @@ const useSafeAppsInfoModal = ({
 } => {
   const didMount = useRef(false)
   const chainId = useChainId()
-  const [consentAccepted, setConsentAccepted] = useState<boolean>(false)
-  const [securityStepsStatus, setSecurityStepsStatus] = useLocalStorage(APPS_SECURITY_FEEDBACK_MODAL, {
-    consentAccepted: false,
-  })
-
-  useEffect(() => {
-    setConsentAccepted(securityStepsStatus.consentAccepted)
-  }, [securityStepsStatus])
+  const [modalInfo, setModalInfo] = useLocalStorage<ModalInfoProps>(SAFE_APPS_INFO_MODAL, {})
 
   useEffect(() => {
     if (!didMount.current) {
       didMount.current = true
       return
     }
-
-    setSecurityStepsStatus({
-      consentAccepted,
-    })
-  }, [consentAccepted, setSecurityStepsStatus])
+  }, [])
 
   const isPermissionsReviewCompleted = useMemo(() => {
     if (!url) return false
@@ -61,27 +54,32 @@ const useSafeAppsInfoModal = ({
 
   const isModalVisible = useMemo(() => {
     const isComponentReady = didMount.current
-    const shouldShowLegalDisclaimer = !consentAccepted
+    const shouldShowLegalDisclaimer = !modalInfo[chainId] || modalInfo[chainId].consentsAccepted === false
     const shouldShowAllowedFeatures = !isPermissionsReviewCompleted
 
     return isComponentReady && (shouldShowLegalDisclaimer || shouldShowAllowedFeatures)
-  }, [consentAccepted, isPermissionsReviewCompleted])
+  }, [chainId, isPermissionsReviewCompleted, modalInfo])
 
   const onComplete = useCallback(
     (browserPermissions: BrowserPermission[]) => {
-      setConsentAccepted(true)
+      setModalInfo({
+        ...modalInfo,
+        [chainId]: {
+          consentsAccepted: true,
+        },
+      })
 
       if (!isPermissionsReviewCompleted) {
         addPermissions(url, browserPermissions)
       }
     },
-    [addPermissions, isPermissionsReviewCompleted, url],
+    [addPermissions, chainId, isPermissionsReviewCompleted, modalInfo, setModalInfo, url],
   )
 
   return {
     isModalVisible,
     isPermissionsReviewCompleted,
-    isConsentAccepted: consentAccepted,
+    isConsentAccepted: !!modalInfo?.[chainId]?.consentsAccepted,
     onComplete,
   }
 }
