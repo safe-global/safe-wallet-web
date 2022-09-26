e78f866068bc0e9e3c0972c9bef74021086bf53d
Diogo Soares <diogo.lsoares@gmail.com>
2022-09-26 08:50:59 +0200

    test: add tests for error conditions

--- src/components/transactions/SingleTx/SingleTx.test.tsx
+++ src/components/transactions/SingleTx/SingleTx.test.tsx
@@ -3,14 +3,13 @@ import SingleTx from '@/pages/transactions/tx'
 import * as useSafeInfo from '@/hooks/useSafeInfo'
 import { SafeInfo, TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
 
+const MOCK_SAFE_ADDRESS = '0x0000000000000000000000000000000000005AFE'
 const SAFE_ADDRESS = 'rin:0x87a57cBf742CC1Fc702D0E9BF595b1E056693e2f'
 
 jest.mock('next/router', () => ({
   useRouter() {
     return {
-      query: {
-        id: 'multisig_0x87a57cBf742CC1Fc702D0E9BF595b1E056693e2f_0x236da79434c398bf98b204e6f3d93d',
-      },
+      query: '',
     }
   },
 }))
@@ -41,10 +40,67 @@ const txDetails = {
 
 describe('SingleTx', () => {
   it('renders <SingleTx />', async () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+    useRouter.mockImplementation(() => ({
+      query: {
+        id: 'multisig_0x87a57cBf742CC1Fc702D0E9BF595b1E056693e2f_0x236da79434c398bf98b204e6f3d93d',
+      },
+    }))
+
     const { getByTestId } = render(<SingleTx />)
 
     await act(() => Promise.resolve())
 
     expect(getByTestId('single-tx')).toBeInTheDocument()
+  })
+
+  it('renders <ErrorMessage /> when there is an error in the URL', async () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+
+    useRouter.mockImplementation(() => ({
+      query: {
+        foo: 'multisig_0x87a57cBf742CC1Fc702D0E9BF595b1E056693e2f_0x236da79434c398bf98b204e6f3d93d',
+      },
+    }))
+
+    const { queryByText } = render(<SingleTx />)
+
+    expect(queryByText('Failed to load transaction')).toBeInTheDocument()
+  })
+
+  it('renders <ErrorMessage /> when the transaction is not found', async () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+    useRouter.mockImplementation(() => ({
+      query: {
+        id: 'dummy',
+      },
+    }))
+
+    const { queryByText } = render(<SingleTx />)
+
+    expect(queryByText('Failed to load transaction')).toBeInTheDocument()
+  })
+
+  it('renders <ErrorMessage /> when transaction is not from the opened Safe', async () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+    useRouter.mockImplementation(() => ({
+      query: {
+        id: 'multisig_0x87a57cBf742CC1Fc702D0E9BF595b1E056693e2f_0x236da79434c398bf98b204e6f3d93d',
+      },
+    }))
+
+    jest.spyOn(useSafeInfo, 'default').mockImplementation(() => ({
+      safeAddress: MOCK_SAFE_ADDRESS,
+      safe: {} as SafeInfo,
+      safeError: undefined,
+      safeLoading: false,
+      safeLoaded: true,
+    }))
+
+    const { queryByText } = render(<SingleTx />)
+
+    await act(() => Promise.resolve())
+
+    expect(queryByText('Failed to load transaction')).toBeInTheDocument()
   })
 })
