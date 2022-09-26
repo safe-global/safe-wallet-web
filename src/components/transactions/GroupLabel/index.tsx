cd16e611d0911d2d8ebaf06689d2756de546302f
iamacook <aaron@safe.global>
2022-09-26 17:20:07 +0200

    fix: add nonce of transaction to execute to label

--- src/components/transactions/GroupLabel/index.tsx
+++ src/components/transactions/GroupLabel/index.tsx
@@ -1,9 +1,23 @@
 import { type ReactElement } from 'react'
-import { Label } from '@gnosis.pm/safe-react-gateway-sdk'
+import { Label, LabelValue } from '@gnosis.pm/safe-react-gateway-sdk'
 import css from './styles.module.css'
+import useTxQueue from '@/hooks/useTxQueue'
+import { isMultisigExecutionInfo, isTransactionListItem } from '@/utils/transaction-guards'
 
 const GroupLabel = ({ item }: { item: Label }): ReactElement => {
-  return <div className={css.container}>{item.label}</div>
+  let label: string | LabelValue = item.label
+
+  const { page } = useTxQueue()
+
+  if (label === LabelValue.Queued) {
+    const firstTx = page?.results.find(isTransactionListItem)
+
+    if (firstTx && isMultisigExecutionInfo(firstTx.transaction.executionInfo)) {
+      label = `${label} - transaction with nonce ${firstTx.transaction.executionInfo.nonce} needs to be executed first`
+    }
+  }
+
+  return <div className={css.container}>{label}</div>
 }
 
 export default GroupLabel
