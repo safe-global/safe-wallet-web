234be77ba751367167fa65c86f209f7c5a3d189f
iamacook <aaron@safe.global>
2022-09-26 16:55:23 +0200

    fix: custom NFT placeholder fallback

--- src/components/transactions/TxType/index.tsx
+++ src/components/transactions/TxType/index.tsx
@@ -1,6 +1,5 @@
 import ImageFallback from '@/components/common/ImageFallback'
 import { useTransactionType } from '@/hooks/useTransactionType'
-import { isTransferTxInfo, isERC721Transfer } from '@/utils/transaction-guards'
 import { TransactionSummary } from '@gnosis.pm/safe-react-gateway-sdk'
 import { Box } from '@mui/material'
 import css from './styles.module.css'
@@ -12,14 +11,9 @@ type TxTypeProps = {
 const TxType = ({ tx }: TxTypeProps) => {
   const type = useTransactionType(tx)
 
-  const fallbackSrc =
-    isTransferTxInfo(tx.txInfo) && isERC721Transfer(tx.txInfo?.transferInfo)
-      ? '/images/nft-placeholder.png'
-      : '/images/custom.svg'
-
   return (
     <Box className={css.txType}>
-      <ImageFallback src={type.icon} fallbackSrc={fallbackSrc} alt="Transaction type" width={16} height={16} />
+      <ImageFallback src={type.icon} fallbackSrc="/images/custom.svg" alt="Transaction type" width={16} height={16} />
       {type.text}
     </Box>
   )
