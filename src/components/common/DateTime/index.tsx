85bc9aa8b9aba8d9aa15f80fc69c824afb0caea2
iamacook <aaron@safe.global>
2022-09-26 18:26:43 +0200

    fix: don't adjust timestamp of transaction

--- src/components/common/DateTime/index.tsx
+++ src/components/common/DateTime/index.tsx
@@ -1,20 +1,15 @@
 import { ReactElement } from 'react'
 import { Tooltip } from '@mui/material'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
-import { isToday, startOfDay } from 'date-fns'
 
 const DAYS_THRESHOLD = 60
-
-const getRelevantTimestamp = (value: number) => {
-  return isToday(new Date(value)) ? value : startOfDay(value).getTime()
-}
 
 const DateTime = ({ value }: { value: number }): ReactElement => {
   const displayExactDate = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > DAYS_THRESHOLD
 
   return (
     <Tooltip title={displayExactDate ? '' : formatDateTime(value)} placement="top">
-      <span>{displayExactDate ? formatDateTime(value) : formatTimeInWords(getRelevantTimestamp(value))}</span>
+      <span>{displayExactDate ? formatDateTime(value) : formatTimeInWords(value)}</span>
     </Tooltip>
   )
 }
