b02e5e57d9776391ca43141b8eabc1da22dfacd6
iamacook <aaron@safe.global>
2022-09-26 11:32:48 +0200

    fix: display collection image

--- src/components/nfts/NftGrid/index.tsx
+++ src/components/nfts/NftGrid/index.tsx
@@ -1,6 +1,6 @@
 import { type ReactElement } from 'react'
 import groupBy from 'lodash/groupBy'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Divider, Grid, Typography } from '@mui/material'
 import { SafeCollectibleResponse } from '@gnosis.pm/safe-react-gateway-sdk'
 import NftCard from '../NftCard'
 
@@ -15,21 +15,36 @@ const NftGrid = ({
 
   return (
     <>
-      {Object.entries(collections).map(([address, nfts]) => (
-        <Box key={address} pb={4}>
-          <Typography variant="h6" mb={1}>
-            {nfts[0].tokenName}
-          </Typography>
-
-          <Grid container spacing={3}>
-            {nfts.map((nft) => (
-              <Grid item xs={12} md={4} lg={3} key={nft.address + nft.id}>
-                <NftCard nft={nft} onSendClick={onSendClick ? () => onSendClick(nft) : undefined} />
+      {Object.entries(collections).map(([address, nfts]) => {
+        const { logoUri, tokenName } = nfts[0]
+        return (
+          <Box key={address} pb={4}>
+            <Grid container alignItems="center" pb={2}>
+              <Grid item xs={1}>
+                {logoUri && (
+                  <img src={logoUri} alt={`${tokenName} collection icon`} style={{ height: '45px', width: '45px' }} />
+                )}
               </Grid>
-            ))}
-          </Grid>
-        </Box>
-      ))}
+              <Grid item xs={1}>
+                <Typography variant="h6" mb={1}>
+                  {tokenName}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <Divider flexItem />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={3}>
+              {nfts.map((nft) => (
+                <Grid item xs={12} md={4} lg={3} key={nft.address + nft.id}>
+                  <NftCard nft={nft} onSendClick={onSendClick ? () => onSendClick(nft) : undefined} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        )
+      })}
     </>
   )
 }
