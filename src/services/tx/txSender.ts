08b12e39aa509194fa9f0b90d3d397b04c188899
Diogo Soares <diogo.lsoares@gmail.com>
2022-09-27 13:00:42 +0200

    fix: handle the TRANSACTION_REPLACED error from ethers

--- src/services/tx/txSender.ts
+++ src/services/tx/txSender.ts
@@ -17,7 +17,7 @@ import extractTxInfo from '@/services/tx/extractTxInfo'
 import proposeTx from './proposeTransaction'
 import { txDispatch, TxEvent } from './txEvents'
 import { getSafeSDK } from '@/hooks/coreSDK/safeCoreSDK'
-import { didRevert } from '@/utils/ethers-utils'
+import { didRevert, EthersError } from '@/utils/ethers-utils'
 import Safe, { RemoveOwnerTxParams } from '@gnosis.pm/safe-core-sdk'
 import { AddOwnerTxParams, SwapOwnerTxParams } from '@gnosis.pm/safe-core-sdk/dist/src/Safe'
 import MultiSendCallOnlyEthersContract from '@gnosis.pm/safe-ethers-lib/dist/src/contracts/MultiSendCallOnly/MultiSendCallOnlyEthersContract'
@@ -26,6 +26,7 @@ import { ContractTransaction, ethers } from 'ethers'
 import { SpendingLimitTxParams } from '@/components/tx/modals/TokenTransferModal/ReviewSpendingLimitTx'
 import { getSpendingLimitContract } from '@/services/contracts/spendingLimitContracts'
 import EthersAdapter from '@gnosis.pm/safe-ethers-lib'
+import { ErrorCode } from '@ethersproject/logger'
 
 const getAndValidateSafeSDK = (): Safe => {
   const safeSDK = getSafeSDK()
@@ -262,7 +263,14 @@ export const dispatchTxExecution = async (
         txDispatch(TxEvent.PROCESSED, { txId, receipt })
       }
     })
-    .catch((error) => {
+    .catch((err) => {
+      const error = err as EthersError
+
+      // Meaning the transaction was "repriced" or "replaced"
+      if (error.code === ErrorCode.TRANSACTION_REPLACED && error.reason !== 'cancelled') {
+        // TxEvent.SUCCESS is handled by txHistoryMiddleware
+        return
+      }
       txDispatch(TxEvent.FAILED, { txId, error: error as Error })
     })
 
--- src/utils/ethers-utils.ts
+++ src/utils/ethers-utils.ts
@@ -1,4 +1,7 @@
 import type { TransactionReceipt } from '@ethersproject/abstract-provider/lib'
+import { ErrorCode } from '@ethersproject/logger'
+
+export type EthersError = Error & { code: ErrorCode; reason: string }
 
 export const didRevert = (receipt: TransactionReceipt): boolean => {
   return receipt.status === 0
