57cf0b1ba7f1a5a56ab35624dcadfb5534bf7115
iamacook <aaron@safe.global>
2022-09-27 09:45:48 +0200

    fix: show exact date on history

--- src/components/common/DateTime/index.tsx
+++ src/components/common/DateTime/index.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from 'react'
 import { Tooltip } from '@mui/material'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
+import { useRouter } from 'next/router'
+import { AppRoutes } from '@/config/routes'
 
 const DAYS_THRESHOLD = 60
 
 const DateTime = ({ value }: { value: number }): ReactElement => {
-  const displayExactDate = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > DAYS_THRESHOLD
+  const router = useRouter()
+  const isHistory = router.pathname.includes(AppRoutes.transactions.history)
+
+  const displayExactDate = Math.floor((Date.now() - value) / 1000 / 60 / 60 / 24) > DAYS_THRESHOLD || isHistory
 
   return (
     <Tooltip title={displayExactDate ? '' : formatDateTime(value)} placement="top">
