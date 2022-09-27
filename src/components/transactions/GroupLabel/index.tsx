37eaa6af29d2df4f05ef17e89c8a6f2ff2ff6da6
iamacook <aaron@safe.global>
2022-09-27 15:04:57 +0200

    fix: append when tx has out of order nonce

--- src/components/transactions/GroupLabel/index.tsx
+++ src/components/transactions/GroupLabel/index.tsx
@@ -3,23 +3,35 @@ import { Label, LabelValue } from '@gnosis.pm/safe-react-gateway-sdk'
 import css from './styles.module.css'
 import useTxQueue from '@/hooks/useTxQueue'
 import { isLabelListItem, isMultisigExecutionInfo, isTransactionListItem } from '@/utils/transaction-guards'
+import useSafeInfo from '@/hooks/useSafeInfo'
 
 const GroupLabel = ({ item }: { item: Label }): ReactElement => {
   let label: string | LabelValue = item.label
 
   const { page } = useTxQueue()
+  const { safe } = useSafeInfo()
 
-  // 'Queued' is returned when there is one tx in queue OR after the 'Next' tx when there is > 1 tx in queue
-  // We're dealing with 'Queued' txs that can be executed only after 'Next' txs
-  if (label === LabelValue.Queued) {
-    const hasNext = page?.results.some((tx) => isLabelListItem(tx) && tx !== item)
+  /**
+   * 'Next' label is returned when the first transaction has a correct nonce
+   * 'Queued' label is returned after 'Next' OR when the first transaction has an out of order nonce
+   *
+   * We want to append to the 'Queue' label if:
+   * - There is a 'Next' label on the first page
+   * - The first transaction has an out of order nonce
+   */
 
-    if (hasNext) {
-      const firstTx = page?.results.find(isTransactionListItem)
+  if (label === LabelValue.Queued && page) {
+    const hasNext = page.results.some((tx) => isLabelListItem(tx) && tx !== item)
+    const firstTx = page.results.find(isTransactionListItem)
 
-      if (firstTx && isMultisigExecutionInfo(firstTx.transaction.executionInfo)) {
-        label = `${label} - transaction with nonce ${firstTx.transaction.executionInfo.nonce} needs to be executed first`
-      }
+    if (
+      firstTx &&
+      isMultisigExecutionInfo(firstTx.transaction.executionInfo) &&
+      (hasNext || firstTx.transaction.executionInfo.nonce !== safe.nonce)
+    ) {
+      const nextNonce = hasNext ? firstTx.transaction.executionInfo.nonce : safe.nonce
+
+      label = `${label} - transaction with nonce ${nextNonce} needs to be executed first`
     }
   }
 
