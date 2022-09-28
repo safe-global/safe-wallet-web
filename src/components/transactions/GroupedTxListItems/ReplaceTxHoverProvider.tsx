f8f847027ab94d3e2fc9ae115ce2dfc7dacbe814
Aaron Cook <aaron@safe.global>
2022-09-28 18:10:29 +0200

    fix: strikethrough other transactions on hover (#716)
    * fix: strikethrough transactions when hovering
    
    * fix: adjust naming, status + use `onMouseEnter`
    
    * fix: rename setter, add var + adjust width

---
+++ src/components/transactions/GroupedTxListItems/ReplaceTxHoverProvider.tsx
@@ -0,0 +1,53 @@
+import { createContext, useMemo, useState, type Dispatch, type ReactElement, type SetStateAction } from 'react'
+import type { Transaction } from '@gnosis.pm/safe-react-gateway-sdk'
+
+import { useAppSelector } from '@/store'
+import { selectPendingTxs } from '@/store/pendingTxsSlice'
+
+export const ReplaceTxHoverContext = createContext<{
+  replacedTxIds: string[]
+  setSelectedTxId: Dispatch<SetStateAction<string | undefined>>
+}>({
+  replacedTxIds: [],
+  setSelectedTxId: () => {},
+})
+
+// Used for striking through transactions that will be replaced
+export const ReplaceTxHoverProvider = ({
+  groupedListItems,
+  children,
+}: {
+  groupedListItems: Transaction[]
+  children: ReactElement
+}): ReactElement => {
+  const [selectedTxId, setSelectedTxId] = useState<string>()
+  const pendingTxs = useAppSelector(selectPendingTxs)
+
+  const replacedTxIds = useMemo(() => {
+    const pendingTxInGroup = groupedListItems.find((item) => pendingTxs[item.transaction.id])
+
+    const disabledItems = groupedListItems
+      .filter((item) => {
+        const { id } = item.transaction
+
+        const willBeReplaced = selectedTxId && selectedTxId !== id
+        const isReplacing = pendingTxInGroup && id !== pendingTxInGroup.transaction.id
+
+        return willBeReplaced || isReplacing
+      })
+      .map((item) => item.transaction.id)
+
+    return disabledItems
+  }, [groupedListItems, pendingTxs, selectedTxId])
+
+  return (
+    <ReplaceTxHoverContext.Provider
+      value={{
+        replacedTxIds,
+        setSelectedTxId,
+      }}
+    >
+      {children}
+    </ReplaceTxHoverContext.Provider>
+  )
+}
