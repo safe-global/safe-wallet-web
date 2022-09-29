d97dbea738ce08644714734cad9653b0e4e78344
Aaron Cook <aaron@safe.global>
2022-09-29 09:52:17 +0200

    fix: token amount alignment (#729)
    * fix: standardise sidebar toolbar styles
    
    * fix: vertical alignment of token amount
    
    * chore: ugprade onboard (#689)
    
    * chore: ugprade onboard
    
    * fix: lock file
    
    * fix: vertical alignment of token amount

--- src/components/common/TokenAmount/index.tsx
+++ src/components/common/TokenAmount/index.tsx
@@ -3,6 +3,7 @@ import { TransferDirection } from '@gnosis.pm/safe-react-gateway-sdk'
 import css from './styles.module.css'
 import { formatVisualAmount } from '@/utils/formatters'
 import TokenIcon from '../TokenIcon'
+import classNames from 'classnames'
 
 const TokenAmount = ({
   value,
@@ -23,7 +24,7 @@ const TokenAmount = ({
   const amount = decimals ? formatVisualAmount(value, decimals) : value
 
   return (
-    <span className={css.container}>
+    <span className={classNames(css.container, { [css.verticalAlign]: logoUri })}>
       {logoUri && <TokenIcon logoUri={logoUri} tokenSymbol={tokenSymbol} fallbackSrc={fallbackSrc} />}
 
       <span>
