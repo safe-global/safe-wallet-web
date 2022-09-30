47be093a1f4c6cddb875c0f0f65ee724f1d377e0
iamacook <aaron@safe.global>
2022-09-30 17:17:08 +0200

    feat: sidebar upgrade badge

--- src/components/sidebar/SidebarNavigation/index.tsx
+++ src/components/sidebar/SidebarNavigation/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState, type ReactElement, useEffect } from 'react'
+import React, { type ReactElement } from 'react'
 import { useRouter } from 'next/router'
 import ListItem from '@mui/material/ListItem'
+import { ImplementationVersionState } from '@gnosis.pm/safe-react-gateway-sdk'
 
 import {
   SidebarList,
@@ -9,30 +10,32 @@ import {
   SidebarListItemText,
 } from '@/components/sidebar/SidebarList'
 import { navItems } from './config'
+import useSafeInfo from '@/hooks/useSafeInfo'
+import { AppRoutes } from '@/config/routes'
 
 const Navigation = (): ReactElement => {
   const router = useRouter()
-  const query = { safe: router.query.safe }
-  const [openHref, setOpenHref] = useState<string>(router.pathname)
+  const { safe } = useSafeInfo()
 
-  useEffect(() => {
-    setOpenHref(router.pathname)
-  }, [router.pathname, query.safe])
+  const needsUpdate = safe.implementationVersionState === ImplementationVersionState.OUTDATED
 
   return (
     <SidebarList>
-      {navItems.map((item) => (
-        <ListItem key={item.href} disablePadding selected={router.pathname === item.href}>
-          <SidebarListItemButton
-            onClick={() => setOpenHref(item.href)}
-            selected={openHref === item.href}
-            href={{ pathname: item.href, query }}
-          >
-            {item.icon && <SidebarListItemIcon>{item.icon}</SidebarListItemIcon>}
-            <SidebarListItemText bold>{item.label}</SidebarListItemText>
-          </SidebarListItemButton>
-        </ListItem>
-      ))}
+      {navItems.map((item) => {
+        const isSettings = item.href === AppRoutes.settings.setup
+
+        return (
+          <ListItem key={item.href} disablePadding selected={router.pathname === item.href}>
+            <SidebarListItemButton
+              selected={router.pathname === item.href}
+              href={{ pathname: item.href, query: { safe: router.query.safe } }}
+            >
+              {item.icon && <SidebarListItemIcon badge={isSettings && needsUpdate}>{item.icon}</SidebarListItemIcon>}
+              <SidebarListItemText bold>{item.label}</SidebarListItemText>
+            </SidebarListItemButton>
+          </ListItem>
+        )
+      })}
     </SidebarList>
   )
 }
