44d40cde15b0bff559ba54c7d689077a34d61204
Usame Algan <usame@safe.global>
2022-10-04 14:07:35 +0200

    fix: Display decoded actions without encoding a multisend call

--- src/components/transactions/TxDetails/TxData/DecodedData/Multisend/index.tsx
+++ src/components/transactions/TxDetails/TxData/DecodedData/Multisend/index.tsx
@@ -1,18 +1,8 @@
 import { HexEncodedData } from '@/components/transactions/HexEncodedData'
-import { MethodDetails } from '@/components/transactions/TxDetails/TxData/DecodedData/MethodDetails'
-import { useCurrentChain } from '@/hooks/useChains'
-import { formatVisualAmount } from '@/utils/formatters'
-import { Operation, TransactionData } from '@gnosis.pm/safe-react-gateway-sdk'
+import { TransactionData } from '@gnosis.pm/safe-react-gateway-sdk'
 import { ReactElement } from 'react'
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import css from '@/components/transactions/TxDetails/TxData/DecodedData/Multisend/styles.module.css'
-import CodeIcon from '@mui/icons-material/Code'
-import { DelegateCallWarning } from '@/components/transactions/Warning'
-import { InfoDetails } from '@/components/transactions/InfoDetails'
-import EthHashInfo from '@/components/common/EthHashInfo'
-import { isDeleteAllowance, isSetAllowance } from '@/utils/transaction-guards'
 import { AccordionProps } from '@mui/material/Accordion/Accordion'
+import SingleTxDecoded from '@/components/transactions/TxDetails/TxData/DecodedData/SingleTxDecoded'
 
 type MultisendProps = {
   txData?: TransactionData
@@ -25,8 +15,6 @@ export const Multisend = ({
   variant = 'elevation',
   showDelegateCallWarning = true,
 }: MultisendProps): ReactElement | null => {
-  const chain = useCurrentChain()
-
   if (!txData) return null
 
   // ? when can a multiSend call take no parameters?
@@ -41,57 +29,21 @@ export const Multisend = ({
   return (
     <>
       {txData.dataDecoded?.parameters[0].valueDecoded?.map(({ dataDecoded, data, value, to, operation }, index) => {
-        const actionTitle = `Action ${index + 1}`
-        const method = dataDecoded?.method || ''
-        const { decimals, symbol } = chain!.nativeCurrency
-        const amount = value ? formatVisualAmount(value, decimals) : 0
-
-        let details
-        if (dataDecoded) {
-          details = <MethodDetails data={dataDecoded} />
-        } else if (data) {
-          // If data is not decoded in the backend response
-          details = <HexEncodedData title="Data (hex encoded)" hexData={data} />
-        }
-
-        const addressInfo = txData.addressInfoIndex?.[to]
-        const name = addressInfo?.name
-        const avatarUrl = addressInfo?.logoUri
-
-        const title = `Interact with${Number(amount) !== 0 ? ` (and send ${amount} ${symbol} to)` : ''}:`
-        const isDelegateCall = operation === Operation.DELEGATE && showDelegateCallWarning
-        const isSpendingLimitMethod = isSetAllowance(dataDecoded?.method) || isDeleteAllowance(dataDecoded?.method)
-
         return (
-          <Accordion key={`${data ?? to}-${index}`} variant={variant} defaultExpanded={isDelegateCall}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <div className={css.summary}>
-                <CodeIcon />
-                <Typography>{actionTitle}</Typography>
-                <Typography ml="8px">
-                  <b>{method}</b>
-                </Typography>
-              </div>
-            </AccordionSummary>
-
-            <AccordionDetails>
-              {/* We always warn of nested delegate calls */}
-              {isDelegateCall && <DelegateCallWarning showWarning={!txData.trustedDelegateCallTarget} />}
-              {!isSpendingLimitMethod && (
-                <InfoDetails title={title}>
-                  <EthHashInfo
-                    address={to}
-                    name={name}
-                    customAvatar={avatarUrl}
-                    shortAddress={false}
-                    showCopyButton
-                    hasExplorer
-                  />
-                </InfoDetails>
-              )}
-              {details}
-            </AccordionDetails>
-          </Accordion>
+          <SingleTxDecoded
+            key={`${data ?? to}-${index}`}
+            tx={{
+              dataDecoded,
+              data,
+              value,
+              to,
+              operation,
+            }}
+            txData={txData}
+            showDelegateCallWarning={showDelegateCallWarning}
+            actionTitle={`Action ${index + 1}`}
+            variant={variant}
+          />
         )
       })}
     </>
--- src/components/tx/modals/BatchExecuteModal/DecodedTxs.tsx
+++ src/components/tx/modals/BatchExecuteModal/DecodedTxs.tsx
@@ -1,58 +1,55 @@
-import { DecodedDataResponse, getDecodedData, Operation, TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
-import { Box, Skeleton } from '@mui/material'
-import useAsync from '@/hooks/useAsync'
-import { OperationType, SafeTransaction } from '@gnosis.pm/safe-core-sdk-types'
-import useChainId from '@/hooks/useChainId'
+import { DataDecoded, TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
+import { Box } from '@mui/material'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import extractTxInfo from '@/services/tx/extractTxInfo'
-import { createMultiSendTx } from '@/services/tx/txSender'
-import { isEmptyHexData } from '@/utils/hex'
-import Multisend from '@/components/transactions/TxDetails/TxData/DecodedData/Multisend'
-import { standardizeMetaTransactionData } from '@gnosis.pm/safe-core-sdk/dist/src/utils/transactions/utils'
+import { isCustomTxInfo, isNativeTokenTransfer, isTransferTxInfo } from '@/utils/transaction-guards'
+import SingleTxDecoded from '@/components/transactions/TxDetails/TxData/DecodedData/SingleTxDecoded'
 
-const DecodedTxs = ({ txs, numberOfTxs }: { txs: TransactionDetails[] | undefined; numberOfTxs: number }) => {
-  const chainId = useChainId()
+const DecodedTxs = ({ txs }: { txs: TransactionDetails[] | undefined }) => {
   const { safeAddress } = useSafeInfo()
 
-  const [safeTx, , safeTxLoading] = useAsync<SafeTransaction | undefined>(async () => {
-    if (!txs) return
+  if (!txs) return null
 
-    const safeTxs = txs.map((txDetails) => extractTxInfo(txDetails, safeAddress).txParams)
-    const baseTxs = safeTxs.map((tx) => standardizeMetaTransactionData(tx))
-    return createMultiSendTx(baseTxs)
-  }, [txs, safeAddress])
-
-  const [decodedData, , decodedDataLoading] = useAsync<DecodedDataResponse | undefined>(async () => {
-    if (!safeTx || isEmptyHexData(safeTx.data.data)) return
-
-    return getDecodedData(chainId, safeTx.data.data)
-  }, [chainId, safeTx])
-
-  if (safeTxLoading || decodedDataLoading) {
-    return (
-      <Box display="flex" flexDirection="column" gap={1} my={1}>
-        {Array.from(Array(numberOfTxs)).map((_, i) => (
-          <Skeleton key={i} variant="rectangular" height={48} sx={{ borderRadius: 2 }} />
-        ))}
-      </Box>
-    )
-  }
-
-  return safeTx ? (
+  return (
     <Box mt={1} display="flex" flexDirection="column" gap={1}>
-      <Multisend
-        txData={{
-          dataDecoded: decodedData,
-          to: { value: safeTx.data.to },
-          value: safeTx.data.value,
-          operation: safeTx.data.operation === OperationType.Call ? Operation.CALL : Operation.DELEGATE,
-          trustedDelegateCallTarget: false,
-        }}
-        variant="outlined"
-        showDelegateCallWarning={false}
-      />
+      {txs.map((transaction, idx) => {
+        if (!transaction.txData) return null
+
+        const { txParams } = extractTxInfo(transaction, safeAddress)
+
+        let decodedDataParams: DataDecoded = {
+          method: '',
+          parameters: undefined,
+        }
+
+        if (isCustomTxInfo(transaction.txInfo) && transaction.txInfo.isCancellation) {
+          decodedDataParams.method = 'On-chain rejection'
+        }
+
+        if (isTransferTxInfo(transaction.txInfo) && isNativeTokenTransfer(transaction.txInfo.transferInfo)) {
+          decodedDataParams.method = 'transfer'
+        }
+
+        const dataDecoded = transaction.txData.dataDecoded || decodedDataParams
+
+        return (
+          <SingleTxDecoded
+            key={transaction.txId}
+            tx={{
+              dataDecoded,
+              data: txParams.data,
+              value: txParams.value,
+              to: txParams.to,
+              operation: 0,
+            }}
+            txData={transaction.txData}
+            actionTitle={`Action ${idx + 1}`}
+            showDelegateCallWarning={false}
+          />
+        )
+      })}
     </Box>
-  ) : null
+  )
 }
 
 export default DecodedTxs
