e322da6ffa911c6f6ce6ac7d87ba0f08c02b55b1
Yago Pérez Vázquez <yago@safe.global>
2022-10-04 09:19:26 +0200

    Remove cypress options

--- cypress.config.js
+++ cypress.config.js
@@ -13,8 +13,6 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3000',
-    chromeWebSecurity: false,
-    modifyObstructiveCode: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
