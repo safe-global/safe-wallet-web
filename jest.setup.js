3894143edf8acc207a82b81f0ca44c038c592881
katspaugh <katspaugh@gmail.com>
2022-10-05 09:28:50 +0200

    Fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -7,14 +7,12 @@ import '@testing-library/jest-dom/extend-expect'
 import 'whatwg-fetch'
 
 jest.mock('@web3-onboard/coinbase', () => jest.fn())
-jest.mock('@web3-onboard/fortmatic', () => jest.fn())
 jest.mock('@web3-onboard/injected-wallets', () => ({ ProviderLabel: { MetaMask: 'MetaMask' } }))
 jest.mock('@web3-onboard/keystone/dist/index', () => jest.fn())
 jest.mock('@web3-onboard/ledger', () => jest.fn())
-jest.mock('@web3-onboard/portis', () => jest.fn())
-jest.mock('@web3-onboard/torus', () => jest.fn())
 jest.mock('@web3-onboard/trezor', () => jest.fn())
 jest.mock('@web3-onboard/walletconnect', () => jest.fn())
+jest.mock('@web3-onboard/tallyho', () => jest.fn())
 
 jest.mock('@web3-onboard/injected-wallets/dist/icons/metamask', () => '')
 jest.mock('@web3-onboard/coinbase/dist/icon', () => '')
@@ -22,9 +20,7 @@ jest.mock('@web3-onboard/keystone/dist/icon', () => '')
 jest.mock('@web3-onboard/walletconnect/dist/icon', () => '')
 jest.mock('@web3-onboard/trezor/dist/icon', () => '')
 jest.mock('@web3-onboard/ledger/dist/icon', () => '')
-jest.mock('@web3-onboard/fortmatic/dist/icon', () => '')
-jest.mock('@web3-onboard/portis/dist/icon', () => '')
-jest.mock('@web3-onboard/torus/dist/icon', () => '')
+jest.mock('@web3-onboard/tallyho/dist/icon', () => '')
 
 const mockOnboardState = {
   chains: [],
