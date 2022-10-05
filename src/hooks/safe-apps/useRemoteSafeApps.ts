4fe588694ef96e977d09936d37c3fb07d5d48c85
iamacook <aaron@safe.global>
2022-10-05 14:56:35 +0200

    test: remove Safe apps cache

--- src/hooks/safe-apps/useRemoteSafeApps.ts
+++ src/hooks/safe-apps/useRemoteSafeApps.ts
@@ -6,27 +6,12 @@ import useChainId from '@/hooks/useChainId'
 import type { AsyncResult } from '../useAsync'
 import useAsync from '../useAsync'
 
-// To avoid multiple simultaneous requests (e.g. the Dashboard and the SAFE header widget),
-// cache the request promise for 100ms
-let cache: Promise<SafeAppsResponse> | undefined
-const cachedGetSafeApps = (chainId: string): ReturnType<typeof getSafeApps> => {
-  if (!cache) {
-    cache = getSafeApps(chainId, { client_url: window.location.origin })
-
-    // Clear the cache the promise resolves with a small delay
-    cache.finally(() => {
-      setTimeout(() => (cache = undefined), 100)
-    })
-  }
-  return cache
-}
-
 const useRemoteSafeApps = (): AsyncResult<SafeAppsResponse> => {
   const chainId = useChainId()
 
   const [remoteApps, error, loading] = useAsync(async () => {
     if (!chainId) return
-    return cachedGetSafeApps(chainId)
+    return getSafeApps(chainId, { client_url: window.location.origin })
   }, [chainId])
 
   useEffect(() => {
