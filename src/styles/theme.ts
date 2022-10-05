70a2765b133c1d26192b532f9692573af89e73d1
katspaugh <katspaugh@gmail.com>
2022-10-05 13:11:51 +0200

    Fix: tooltip colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -419,11 +419,11 @@ const initTheme = (darkMode: boolean) => {
         styleOverrides: {
           tooltip: ({ theme }) => ({
             ...theme.typography.body2,
-            color: darkMode ? darkPalette.background.main : palette.text.primary,
-            backgroundColor: darkMode ? darkPalette.text.primary : palette.background.main,
+            color: theme.palette.background.main,
+            backgroundColor: theme.palette.text.primary,
           }),
           arrow: {
-            color: darkMode ? darkPalette.text.primary : palette.background.main,
+            color: theme.palette.background.main,
           },
         },
       },
