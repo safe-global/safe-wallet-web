398e80e1e68718776af5dd4c958973f83332b3f3
Aaron Cook <aaron@safe.global>
2022-10-06 15:23:33 +0200

    fix: rename Safe Apps settings route (#845)
    * fix: rename Safe Apps settings route
    
    * fix: regenerate routes
    
    * fix: modify route generation script

--- src/components/sidebar/SidebarNavigation/config.tsx
+++ src/components/sidebar/SidebarNavigation/config.tsx
@@ -89,7 +89,7 @@ export const settingsNavItems = [
     href: AppRoutes.settings.spendingLimits,
   },
   {
-    label: 'Safe Apps Permissions',
-    href: AppRoutes.settings.safeAppsPermissions,
+    label: 'Safe Apps permissions',
+    href: AppRoutes.settings.safeApps.index,
   },
 ]
