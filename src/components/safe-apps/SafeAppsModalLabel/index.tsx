88d2b3b25b80cb58892a332b9baf4aac1119488f
Usame Algan <usame@safe.global>
2022-10-06 16:22:54 +0200

    fix: Use fallback image for safe app icons in tx modal

--- src/components/safe-apps/SafeAppsModalLabel/index.tsx
+++ src/components/safe-apps/SafeAppsModalLabel/index.tsx
@@ -2,6 +2,7 @@ import type { SafeAppData } from '@gnosis.pm/safe-react-gateway-sdk'
 import { Typography, Box } from '@mui/material'
 
 import css from './styles.module.css'
+import ImageFallback from '@/components/common/ImageFallback'
 
 const APP_LOGO_FALLBACK_IMAGE = '/images/apps/apps-icon.svg'
 
@@ -12,7 +13,14 @@ const SafeAppsModalLabel = ({ app }: { app?: SafeAppData }) => {
 
   return (
     <Box display="flex" alignItems="center">
-      <img src={app.iconUrl || APP_LOGO_FALLBACK_IMAGE} alt={app.name} className={css.modalLabel} />
+      <ImageFallback
+        src={app.iconUrl}
+        fallbackSrc={APP_LOGO_FALLBACK_IMAGE}
+        alt={app.name}
+        className={css.modalLabel}
+        width={24}
+        height={24}
+      />
       <Typography variant="h4">{app.name}</Typography>
     </Box>
   )
