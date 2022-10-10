ae992b4a34c36f01bae18338124795eb56850089
katspaugh <katspaugh@gmail.com>
2022-10-10 15:28:01 +0200

    Fix: auto-choose to WalletConnect on mobile

--- src/components/common/ConnectWallet/WalletDetails.tsx
+++ src/components/common/ConnectWallet/WalletDetails.tsx
@@ -1,13 +1,23 @@
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, useMediaQuery } from '@mui/material'
 import type { ReactElement } from 'react'
-
+import { useTheme } from '@mui/material/styles'
 import useOnboard, { connectWallet } from '@/hooks/wallets/useOnboard'
 import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
 import KeyholeIcon from '@/components/common/icons/KeyholeIcon'
 import { trackEvent } from '@/services/analytics'
+import { WalletNames } from '@/utils/wallets'
 
 const WalletDetails = ({ onConnect }: { onConnect?: () => void }): ReactElement => {
   const onboard = useOnboard()
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
+  // On mobile, automatically choose WalletConnect
+  const options = isMobile
+    ? {
+        autoSelect: WalletNames.WALLET_CONNECT,
+      }
+    : undefined
 
   const handleConnect = async () => {
     if (!onboard) return
@@ -16,7 +26,7 @@ const WalletDetails = ({ onConnect }: { onConnect?: () => void }): ReactElement 
     trackEvent(OVERVIEW_EVENTS.OPEN_ONBOARD)
 
     onConnect?.()
-    connectWallet(onboard)
+    connectWallet(onboard, options)
   }
 
   return (
