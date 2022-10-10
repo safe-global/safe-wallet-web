1661b8828ccffc0d11e5cf1d582a2d0e02018b0d
katspaugh <katspaugh@gmail.com>
2022-10-10 12:36:45 +0200

    Simplify queue label

--- src/components/transactions/GroupLabel/index.tsx
+++ src/components/transactions/GroupLabel/index.tsx
@@ -2,38 +2,12 @@ import type { ReactNode, ReactElement } from 'react'
 import type { Label } from '@gnosis.pm/safe-react-gateway-sdk'
 import { LabelValue } from '@gnosis.pm/safe-react-gateway-sdk'
 import css from './styles.module.css'
-import useTxQueue from '@/hooks/useTxQueue'
-import { isLabelListItem, isMultisigExecutionInfo, isTransactionListItem } from '@/utils/transaction-guards'
 import useSafeInfo from '@/hooks/useSafeInfo'
 
-export const useFutureNonceLabel = () => {
+export const useFutureNonceLabel = (label: LabelValue) => {
   const { safe } = useSafeInfo()
-  return `${LabelValue.Queued} - transaction with nonce ${safe.nonce} needs to be executed first`
-}
-
-export const _useGroupLabel = (label: LabelValue): string => {
-  const { page } = useTxQueue()
-  const { safe } = useSafeInfo()
-  const futureNonceLabel = useFutureNonceLabel()
-
-  if (label !== LabelValue.Queued || !page) {
-    return label
-  }
-
-  const firstTx = page.results.find(isTransactionListItem)
-
-  if (!firstTx || !isMultisigExecutionInfo(firstTx.transaction.executionInfo)) {
-    return label
-  }
-
-  const hasNextLabel = page.results.some((item) => isLabelListItem(item) && item.label !== label)
-  const nonceInFuture = firstTx.transaction.executionInfo.nonce !== safe.nonce
-
-  if (!hasNextLabel && !nonceInFuture) {
-    return label
-  }
-
-  return futureNonceLabel
+  if (label !== LabelValue.Queued) return label
+  return `${label} - transaction with nonce ${safe.nonce} needs to be executed first`
 }
 
 export const GroupLabelTypography = ({ children }: { children: ReactNode }) => {
@@ -41,8 +15,7 @@ export const GroupLabelTypography = ({ children }: { children: ReactNode }) => {
 }
 
 const GroupLabel = ({ item }: { item: Label }): ReactElement => {
-  const label = _useGroupLabel(item.label)
-
+  const label = useFutureNonceLabel(item.label)
   return <GroupLabelTypography>{label}</GroupLabelTypography>
 }
 
--- src/components/transactions/SingleTx/index.tsx
+++ src/components/transactions/SingleTx/index.tsx
@@ -4,32 +4,18 @@ import { useRouter } from 'next/router'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import useAsync from '@/hooks/useAsync'
 import type { Transaction, TransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
+import { LabelValue } from '@gnosis.pm/safe-react-gateway-sdk'
 import { getTransactionDetails } from '@gnosis.pm/safe-react-gateway-sdk'
 import { sameAddress } from '@/utils/addresses'
 import type { ReactElement } from 'react'
 import { makeTxFromDetails } from '@/utils/transactions'
 import { TxListGrid } from '@/components/transactions/TxList'
 import ExpandableTransactionItem from '@/components/transactions/TxListItem/ExpandableTransactionItem'
-import { isMultisigDetailedExecutionInfo } from '@/utils/transaction-guards'
 import { GroupLabelTypography, useFutureNonceLabel } from '../GroupLabel'
-
-// Display a warning if the transaction nonce is higher than the current safe nonce
-const useNonceWarning = (txDetails: TransactionDetails): string => {
-  const { safe } = useSafeInfo()
-  const futureNonceLabel = useFutureNonceLabel()
-
-  let nonceWarning = ''
-  const executionInfo = txDetails?.detailedExecutionInfo
-  if (isMultisigDetailedExecutionInfo(executionInfo) && executionInfo.nonce > safe.nonce) {
-    nonceWarning = futureNonceLabel
-  }
-
-  return nonceWarning
-}
 
 const SingleTxGrid = ({ txDetails }: { txDetails: TransactionDetails }): ReactElement => {
   const tx: Transaction = makeTxFromDetails(txDetails)
-  const nonceWarning = useNonceWarning(txDetails)
+  const nonceWarning = useFutureNonceLabel(LabelValue.Queued)
 
   return (
     <TxListGrid>
