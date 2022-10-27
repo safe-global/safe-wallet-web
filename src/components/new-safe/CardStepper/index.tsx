0b379c79a71886633b1a4ad879e35a2bf8f7e52a
Manuel Gellfart <manu@safe.global>
2022-10-27 18:22:11 +0200

    Safe creation stepper (#992)
    A new CardStepper component which controls the steps for new safe creation which can
    - go back and forth
    - jump to a specific step
    - set an initial step
    - set initial values
    - renders a progress bar
    - step data is stored in an object with a generic type
      - the update functions (onBack, onSubmit) use Partials of that type such that each step does not need to submit the full data
    
    change /demo route to /create-safe
    
    Co-authored-by: Usame Algan <usame@safe.global>

---
+++ src/components/new-safe/CardStepper/index.tsx
@@ -0,0 +1,30 @@
+import css from './styles.module.css'
+import { Card, LinearProgress, CardHeader, Avatar, Typography, CardContent } from '@mui/material'
+import type { TxStepperProps } from './useCardStepper'
+import { useCardStepper } from './useCardStepper'
+
+export function CardStepper<StepperData>(props: TxStepperProps<StepperData>) {
+  const { activeStep, onSubmit, onBack, stepData, setStep } = useCardStepper<StepperData>(props)
+  const { steps } = props
+  const currentStep = steps[activeStep]
+  const progress = (activeStep + 1 / steps.length) * 100
+
+  return (
+    <Card className={css.card}>
+      <LinearProgress color="secondary" variant="determinate" value={Math.min(progress, 100)} />
+      <CardHeader
+        title={currentStep.title}
+        subheader={currentStep.subtitle}
+        titleTypographyProps={{ variant: 'h4' }}
+        subheaderTypographyProps={{ variant: 'body2' }}
+        avatar={
+          <Avatar className={css.step}>
+            <Typography variant="body2">{activeStep + 1}</Typography>
+          </Avatar>
+        }
+        className={css.header}
+      />
+      <CardContent className={css.content}>{currentStep.render(stepData, onSubmit, onBack, setStep)}</CardContent>
+    </Card>
+  )
+}
--- src/components/new-safe/CreateSafe/index.tsx
+++ src/components/new-safe/CreateSafe/index.tsx
@@ -1,50 +1,97 @@
-import { Button, Typography, Grid } from '@mui/material'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import { useRouter } from 'next/router'
-
 import WalletInfo from '@/components/common/WalletInfo'
 import { useCurrentChain } from '@/hooks/useChains'
 import useWallet from '@/hooks/wallets/useWallet'
 import OverviewWidget from '../OverviewWidget'
+import type { NamedAddress } from '@/components/create-safe/types'
+import type { TxStepperProps } from '../CardStepper/useCardStepper'
 import CreateSafeStep1 from '../steps/Step1'
+import useAddressBook from '@/hooks/useAddressBook'
 import CreateSafeStep2 from '../steps/Step2'
+import { CardStepper } from '../CardStepper'
+import Grid from '@mui/material/Grid'
+import { Card, CardContent, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+import { AppRoutes } from '@/config/routes'
+import { CREATE_SAFE_CATEGORY } from '@/services/analytics'
+
+export type NewSafeFormData = {
+  name: string
+  threshold: number
+  owners: NamedAddress[]
+  mobileOwners: NamedAddress[]
+}
+
+export const CreateSafeSteps: TxStepperProps<NewSafeFormData>['steps'] = [
+  {
+    title: 'Select network and name Safe',
+    subtitle: 'Select the network on which to create your Safe',
+    render: (data, onSubmit, onBack) => <CreateSafeStep1 onSubmit={onSubmit} onBack={onBack} data={data} />,
+  },
+  {
+    title: 'Owners and confirmations',
+    subtitle:
+      'Here you can add owners to your Safe and determine how many owners need to confirm before making a successful transaction',
+    render: (data, onSubmit, onBack) => <CreateSafeStep2 onSubmit={onSubmit} onBack={onBack} data={data} />,
+  },
+]
 
 const CreateSafe = () => {
   const router = useRouter()
-
-  // TODO: These rows are just a demo
   const wallet = useWallet()
+  const addressBook = useAddressBook()
+  const defaultOwnerAddressBookName = wallet?.address ? addressBook[wallet.address] : undefined
+  const defaultOwner: NamedAddress = {
+    name: defaultOwnerAddressBookName || wallet?.ens || '',
+    address: wallet?.address || '',
+  }
+
+  const initialData: NewSafeFormData = {
+    name: '',
+    mobileOwners: [] as NamedAddress[],
+    owners: [defaultOwner],
+    threshold: 1,
+  }
+
+  const onClose = () => {
+    router.push(AppRoutes.welcome)
+  }
+
   const chain = useCurrentChain()
   const rows = [
     ...(wallet && chain ? [{ title: 'Wallet', component: <WalletInfo wallet={wallet} chain={chain} /> }] : []),
   ]
-
-  const onBack = () => {
-    router.back()
-
-    // Logic to be handled by stepper hook
-  }
 
   // TODO: Improve layout when other widget/responsive design is ready
   return (
     <Grid container spacing={3}>
       <Grid item xs={1} />
       <Grid item xs={11}>
-        <Button variant="text" startIcon={<ChevronLeftIcon />} onClick={onBack} sx={{ my: 4, mx: 0 }}>
-          Back
-        </Button>
         <Typography variant="h2" pb={2}>
           Create new Safe
         </Typography>
       </Grid>
 
       <Grid item xs={1} />
-      <Grid item xs={6}>
-        <CreateSafeStep1 />
-        <CreateSafeStep2 />
+      <Grid item xs={12} md={6}>
+        {wallet?.address ? (
+          <CardStepper
+            initialData={initialData}
+            onClose={onClose}
+            steps={CreateSafeSteps}
+            eventCategory={CREATE_SAFE_CATEGORY}
+          />
+        ) : (
+          <Card>
+            <CardContent>
+              <Typography variant="h3" fontWeight={700}>
+                You need to connect a wallet to create a new Safe.
+              </Typography>
+            </CardContent>
+          </Card>
+        )}
       </Grid>
-      <Grid item xs={4}>
-        <OverviewWidget rows={rows} />
+      <Grid item xs={12} md={4}>
+        {wallet?.address && <OverviewWidget rows={rows} />}
       </Grid>
       <Grid item xs={1} />
     </Grid>
