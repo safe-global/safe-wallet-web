57add59bb923f5ca6bf886d92af3602cf0c38315
schmanu <manu@safe.global>
2022-10-27 13:49:41 +0200

    add new safe creation stepper to wrap each step
    - Replaces StepCard with CardStepper
    - new useCardStepper hook with generic state type
    - adjust steps 1 and 2

--- src/components/new-safe/CreateSafe/index.tsx
+++ src/components/new-safe/CreateSafe/index.tsx
@@ -1,19 +1,56 @@
-import { Button, Typography, Grid } from '@mui/material'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import { useRouter } from 'next/router'
 
 import WalletInfo from '@/components/common/WalletInfo'
 import { useCurrentChain } from '@/hooks/useChains'
 import useWallet from '@/hooks/wallets/useWallet'
 import OverviewWidget from '../OverviewWidget'
+import type { NamedAddress } from '@/components/create-safe/types'
+import type { TxStepperProps } from '../CardStepper/useCardStepper'
 import CreateSafeStep1 from '../steps/Step1'
+import useAddressBook from '@/hooks/useAddressBook'
 import CreateSafeStep2 from '../steps/Step2'
+import { CardStepper } from '../CardStepper'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+
+export type NewSafeFormData = {
+  name: string
+  threshold: number
+  owners: NamedAddress[]
+  mobileOwners: NamedAddress[]
+}
+
+export const CreateSafeSteps: TxStepperProps<NewSafeFormData>['steps'] = [
+  {
+    title: 'Select network and name Safe',
+    subtitle: 'Select the network on which to create your Safe',
+    render: (data, onSubmit, onBack) => <CreateSafeStep1 onSubmit={onSubmit} onBack={onBack} data={data} />,
+  },
+  {
+    title: 'Owners and confirmations',
+    subtitle:
+      'Here you can add owners to your Safe and determine how many owners need to confirm before making a successful transaction',
+    render: (data, onSubmit, onBack) => <CreateSafeStep2 onSubmit={onSubmit} onBack={onBack} data={data} />,
+  },
+]
 
 const CreateSafe = () => {
   const router = useRouter()
 
-  // TODO: These rows are just a demo
   const wallet = useWallet()
+  const addressBook = useAddressBook()
+  const defaultOwnerAddressBookName = wallet?.address ? addressBook[wallet.address] : undefined
+  const defaultOwner: NamedAddress = {
+    name: defaultOwnerAddressBookName || wallet?.ens || '',
+    address: wallet?.address || '',
+  }
+
+  const initialData: NewSafeFormData = {
+    name: '',
+    mobileOwners: [] as NamedAddress[],
+    owners: [defaultOwner],
+    threshold: 1,
+  }
   const chain = useCurrentChain()
   const rows = [
     ...(wallet && chain ? [{ title: 'Wallet', component: <WalletInfo wallet={wallet} chain={chain} /> }] : []),
@@ -30,9 +67,6 @@ const CreateSafe = () => {
     <Grid container spacing={3}>
       <Grid item xs={1} />
       <Grid item xs={11}>
-        <Button variant="text" startIcon={<ChevronLeftIcon />} onClick={onBack} sx={{ my: 4, mx: 0 }}>
-          Back
-        </Button>
         <Typography variant="h2" pb={2}>
           Create new Safe
         </Typography>
@@ -40,8 +74,12 @@ const CreateSafe = () => {
 
       <Grid item xs={1} />
       <Grid item xs={6}>
-        <CreateSafeStep1 />
-        <CreateSafeStep2 />
+        <CardStepper
+          initialData={initialData}
+          onClose={() => {}}
+          steps={CreateSafeSteps}
+          eventCategory="NewSafeCreation"
+        />
       </Grid>
       <Grid item xs={4}>
         <OverviewWidget rows={rows} />
--- src/components/new-safe/steps/Step2/index.tsx
+++ src/components/new-safe/steps/Step2/index.tsx
@@ -1,23 +1,17 @@
-import { Button, Grid, SvgIcon, MenuItem, Select, Tooltip, Typography, Divider } from '@mui/material'
+import { Button, Grid, SvgIcon, MenuItem, Select, Tooltip, Typography, Divider, Box } from '@mui/material'
 import { FormProvider, useFieldArray, useForm } from 'react-hook-form'
 import type { ReactElement } from 'react'
 
-import useWallet from '@/hooks/wallets/useWallet'
 import AddIcon from '@/public/images/common/add.svg'
 import InfoIcon from '@/public/images/notifications/info.svg'
-import StepCard from '../../StepCard'
 import { OwnerRow } from './OwnerRow'
-import useAddressBook from '@/hooks/useAddressBook'
 import type { NamedAddress } from '@/components/create-safe/types'
-
-type Owner = {
-  name: string
-  address: string
-}
+import type { StepRenderProps } from '../../CardStepper/useCardStepper'
+import type { NewSafeFormData } from '../../CreateSafe'
 
 type CreateSafeStep2Form = {
-  owners: Owner[]
-  mobileOwners: Owner[]
+  owners: NamedAddress[]
+  mobileOwners: NamedAddress[]
   threshold: number
 }
 
@@ -29,26 +23,23 @@ enum CreateSafeStep2Fields {
 
 const STEP_2_FORM_ID = 'create-safe-step-2-form'
 
-const CreateSafeStep2 = (): ReactElement => {
-  const wallet = useWallet()
-  const addressBook = useAddressBook()
-
-  const defaultOwnerAddressBookName = wallet?.address ? addressBook[wallet.address] : undefined
-
-  const defaultOwner: NamedAddress = {
-    name: defaultOwnerAddressBookName || wallet?.ens || '',
-    address: wallet?.address || '',
-  }
+const CreateSafeStep2 = ({
+  onSubmit,
+  onBack,
+  data,
+}: Pick<StepRenderProps<NewSafeFormData>, 'onSubmit' | 'data' | 'onBack'>): ReactElement => {
   const formMethods = useForm<CreateSafeStep2Form>({
     mode: 'all',
     defaultValues: {
-      [CreateSafeStep2Fields.owners]: [defaultOwner],
-      [CreateSafeStep2Fields.mobileOwners]: [],
-      [CreateSafeStep2Fields.threshold]: 1,
+      [CreateSafeStep2Fields.owners]: data.owners,
+      [CreateSafeStep2Fields.mobileOwners]: data.mobileOwners,
+      [CreateSafeStep2Fields.threshold]: data.threshold,
     },
   })
 
-  const { register, handleSubmit, control } = formMethods
+  const { register, handleSubmit, control, watch } = formMethods
+
+  const allFormData = watch()
 
   const {
     fields: ownerFields,
@@ -64,103 +55,100 @@ const CreateSafeStep2 = (): ReactElement => {
 
   const allOwners = [...ownerFields, ...mobileOwnerFields]
 
-  const onSubmit = (data: CreateSafeStep2Form) => {
-    console.log(data)
+  const handleBack = () => {
+    onBack(allFormData)
   }
 
   return (
-    <StepCard
-      title="Owners and confirmations"
-      subheader="Here you can add owners to your Safe and determine how many owners need to confirm before making a successful transaction"
-      content={
-        <form onSubmit={handleSubmit(onSubmit)} id={STEP_2_FORM_ID}>
-          <FormProvider {...formMethods}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                {ownerFields.map((field, i) => (
-                  <OwnerRow
-                    key={field.id}
-                    index={i}
-                    removable={i > 0}
-                    groupName={CreateSafeStep2Fields.owners}
-                    remove={removeOwner}
-                  />
-                ))}
-                <Button
-                  variant="text"
-                  onClick={() => appendOwner({ name: '', address: '' }, { shouldFocus: true })}
-                  startIcon={<SvgIcon component={AddIcon} inheritViewBox fontSize="small" />}
-                >
-                  Add new owner
-                </Button>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="subtitle1" fontWeight={700} display="inline-flex" alignItems="center" gap={1}>
-                  Safe Mobile owner key (optional){' '}
-                  <Tooltip title="TODO: Add tooltip" arrow placement="top">
-                    <span style={{ display: 'flex' }}>
-                      <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
-                    </span>
-                  </Tooltip>
-                </Typography>
-                <Typography variant="body2">
-                  Add an extra layer of security and sign transactions with the Safe Mobile app.
-                </Typography>
-              </Grid>
+    <form onSubmit={handleSubmit(onSubmit)} id={STEP_2_FORM_ID}>
+      <FormProvider {...formMethods}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            {ownerFields.map((field, i) => (
+              <OwnerRow
+                key={field.id}
+                index={i}
+                removable={i > 0}
+                groupName={CreateSafeStep2Fields.owners}
+                remove={removeOwner}
+              />
+            ))}
+            <Button
+              variant="text"
+              onClick={() => appendOwner({ name: '', address: '' }, { shouldFocus: true })}
+              startIcon={<SvgIcon component={AddIcon} inheritViewBox fontSize="small" />}
+            >
+              Add new owner
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" fontWeight={700} display="inline-flex" alignItems="center" gap={1}>
+              Safe Mobile owner key (optional){' '}
+              <Tooltip title="TODO: Add tooltip" arrow placement="top">
+                <span style={{ display: 'flex' }}>
+                  <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
+                </span>
+              </Tooltip>
+            </Typography>
+            <Typography variant="body2">
+              Add an extra layer of security and sign transactions with the Safe Mobile app.
+            </Typography>
+          </Grid>
 
-              <Grid item xs={12}>
-                {mobileOwnerFields.map((field, i) => (
-                  <OwnerRow
-                    key={field.id}
-                    groupName={CreateSafeStep2Fields.mobileOwners}
-                    index={i}
-                    remove={removeMobileOwner}
-                  />
-                ))}
-                <Button
-                  variant="text"
-                  onClick={() => appendMobileOwner({ name: '', address: '' }, { shouldFocus: true })}
-                  startIcon={<SvgIcon component={AddIcon} inheritViewBox fontSize="small" />}
-                >
-                  Add mobile owner
-                </Button>
-              </Grid>
+          <Grid item xs={12}>
+            {mobileOwnerFields.map((field, i) => (
+              <OwnerRow
+                key={field.id}
+                groupName={CreateSafeStep2Fields.mobileOwners}
+                index={i}
+                remove={removeMobileOwner}
+              />
+            ))}
+            <Button
+              variant="text"
+              onClick={() => appendMobileOwner({ name: '', address: '' }, { shouldFocus: true })}
+              startIcon={<SvgIcon component={AddIcon} inheritViewBox fontSize="small" />}
+            >
+              Add mobile owner
+            </Button>
+          </Grid>
 
-              <Grid item xs={12}>
-                <Divider sx={{ ml: '-52px', mr: '-52px', mb: 4, mt: 3 }} />
-                <Typography variant="h4" fontWeight={700} display="inline-flex" alignItems="center" gap={1}>
-                  Threshold
-                  <Tooltip title="TODO: Add tooltip" arrow placement="top">
-                    <span style={{ display: 'flex' }}>
-                      <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
-                    </span>
-                  </Tooltip>
-                </Typography>
-                <Typography variant="body2" mb={2}>
-                  Any transaction requires the confirmation of:
-                </Typography>
-                <Select {...register(CreateSafeStep2Fields.threshold)} defaultValue={allOwners.length}>
-                  {allOwners.map((_, i) => (
-                    <MenuItem key={i} value={i + 1}>
-                      {i + 1}
-                    </MenuItem>
-                  ))}
-                </Select>{' '}
-                out of {allOwners.length} owner(s).
-              </Grid>
-            </Grid>
-          </FormProvider>
-        </form>
-      }
-      actions={
-        <>
-          <Button variant="contained" form={STEP_2_FORM_ID} type="submit">
-            Continue
-          </Button>
-          <Button variant="text">Cancel</Button>
-        </>
-      }
-    />
+          <Grid item xs={12}>
+            <Divider sx={{ ml: '-52px', mr: '-52px', mb: 4, mt: 3 }} />
+            <Typography variant="h4" fontWeight={700} display="inline-flex" alignItems="center" gap={1}>
+              Threshold
+              <Tooltip title="TODO: Add tooltip" arrow placement="top">
+                <span style={{ display: 'flex' }}>
+                  <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
+                </span>
+              </Tooltip>
+            </Typography>
+            <Typography variant="body2" mb={2}>
+              Any transaction requires the confirmation of:
+            </Typography>
+            <Select {...register(CreateSafeStep2Fields.threshold)} defaultValue={allOwners.length}>
+              {allOwners.map((_, i) => (
+                <MenuItem key={i} value={i + 1}>
+                  {i + 1}
+                </MenuItem>
+              ))}
+            </Select>{' '}
+            out of {allOwners.length} owner(s).
+          </Grid>
+          <Grid item xs={12}>
+            <Divider sx={{ ml: '-52px', mr: '-52px', mb: 4, mt: 3, alignSelf: 'normal' }} />
+            <Box display="flex" flexDirection="row" gap={3}>
+              <Button variant="outlined" onClick={handleBack}>
+                Back
+              </Button>
+              <Button type="submit" variant="contained">
+                Continue
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </FormProvider>
+    </form>
   )
 }
 
