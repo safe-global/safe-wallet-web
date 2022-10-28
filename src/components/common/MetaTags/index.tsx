c6b0bcc4a4c843905217240da97640c74385e430
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2022-10-28 14:41:24 +0200

    fix: favicons

--- src/components/common/MetaTags/index.tsx
+++ src/components/common/MetaTags/index.tsx
@@ -32,7 +32,6 @@ const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000" />
   </>
 )
 
