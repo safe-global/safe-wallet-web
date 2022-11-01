e66631e5d69ff1306c0c4633caf19bef48fa5203
Diogo Soares <diogo.lsoares@gmail.com>
2022-11-01 15:50:24 +0100

    restyle InfoWidget

--- src/components/create-safe/InfoWidget/index.tsx
+++ src/components/create-safe/InfoWidget/index.tsx
@@ -1,9 +1,18 @@
-import { Box, Button, Card, CardActions, CardContent, CardHeader, Collapse, SvgIcon, Typography } from '@mui/material'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  SvgIcon,
+  Typography,
+} from '@mui/material'
 import type { AlertColor } from '@mui/material'
-import { useEffect, useState, type ReactElement } from 'react'
+import type { ReactElement } from 'react'
 import LightbulbIcon from '@/public/images/common/lightbulb.svg'
-
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import css from './styles.module.css'
 
 type InfoWidgetProps = {
@@ -14,40 +23,16 @@ type InfoWidgetProps = {
 }
 
 const InfoWidget = ({ title, steps, variant, startExpanded = false }: InfoWidgetProps): ReactElement | null => {
-  const [activeStep, setActiveStep] = useState(0)
-  const [expanded, setExpanded] = useState(startExpanded)
-
-  const handleExpandClick = () => setExpanded(true)
-
-  const isFirst = activeStep === 0
-  const isLast = activeStep === steps.length - 1
   const isMultiStep = steps.length > 1
 
-  const onPrev = () => setActiveStep((prev) => prev - 1)
-  const handleNextClick = (isLast: boolean) => {
-    if (isLast) {
-      setExpanded(false)
-    } else {
-      setActiveStep((prev) => prev + 1)
-    }
-  }
-
-  useEffect(() => {
-    !startExpanded && setExpanded(false)
-    setActiveStep(0)
-  }, [startExpanded, steps])
-
-  if (steps[activeStep] == undefined) {
+  if (steps.length === 0) {
     return null
   }
-
-  console.log(steps[activeStep])
 
   return (
     <Card sx={{ backgroundColor: ({ palette }) => palette[variant]?.background }}>
       <CardHeader
-        onClick={handleExpandClick}
-        className={css.header}
+        sx={{ paddingBottom: '0px' }}
         title={
           <>
             <Box className={css.headerWrapper}>
@@ -61,46 +46,39 @@ const InfoWidget = ({ title, steps, variant, startExpanded = false }: InfoWidget
                 <SvgIcon component={LightbulbIcon} inheritViewBox fontSize="inherit" className={css.lightbulb} />
                 {title}
               </Typography>
-              {isMultiStep && (
-                <Typography variant="caption" className={css.count}>
-                  {activeStep + 1} of {steps.length}
-                </Typography>
-              )}
+              <Typography variant="caption" className={css.count}>
+                {steps.length} tip{isMultiStep && 's'}
+              </Typography>
             </Box>
-            {!expanded && (
-              <Box className={css.tipsTitles}>
-                {steps.map(({ title }) => (
-                  <Box className={css.listItem} key={title.replace(' ', '-')}>
-                    <Box
-                      className={css.dot}
-                      sx={{
-                        backgroundColor: ({ palette }) => palette[variant]?.main,
-                      }}
-                    />
-                    <Typography variant="body2">{title}</Typography>
-                  </Box>
-                ))}
-              </Box>
-            )}
           </>
         }
       />
-      <Collapse in={expanded} timeout={0} unmountOnExit>
-        <CardContent>
-          <Typography variant="h5">{steps[activeStep].title}</Typography>
-          <Typography variant="body2">{steps[activeStep].text}</Typography>
-        </CardContent>
-        <CardActions className={css.actions}>
-          {isMultiStep && !isFirst && (
-            <Button variant="contained" size="small" onClick={onPrev} startIcon={<ChevronLeftIcon />}>
-              Previous
-            </Button>
-          )}
-          <Button variant="contained" size="small" onClick={() => handleNextClick(isLast)}>
-            {isLast ? 'Got it' : 'Next'}
-          </Button>
-        </CardActions>
-      </Collapse>
+      <CardContent sx={{ padding: '0', '&.MuiCardContent-root': { paddingBottom: '0px' } }}>
+        {steps.map((step) => {
+          return (
+            <Accordion
+              key={title.replace(' ', '-')}
+              className={css.tipAccordion}
+              sx={{
+                '&:hover > .MuiAccordionSummary-root': {
+                  background: 'inherit',
+                },
+                '&.Mui-expanded > .MuiAccordionSummary-root': {
+                  background: 'inherit',
+                },
+              }}
+              defaultExpanded={startExpanded}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: ({ palette }) => palette[variant]?.main }} />}>
+                <Typography>{step.title}</Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ paddingTop: '0px' }}>
+                <Typography variant="body2">{step.text}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })}
+      </CardContent>
     </Card>
   )
 }
