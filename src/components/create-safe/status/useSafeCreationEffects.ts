6550a7e5cc1441bbab6993e973388b6955fc3dcd
Yago Pérez Vázquez <yago@safe.global>
2022-11-07 11:49:55 +0100

    feat(safe-apps): Analytics refactor (#1042)
    * Refactor analytics for using only the safeAppEvent
    
    * Add pageview tracking to Safe Apps iframe individual apps

--- src/components/create-safe/status/useSafeCreationEffects.ts
+++ src/components/create-safe/status/useSafeCreationEffects.ts
@@ -5,7 +5,7 @@ import { type UrlObject } from 'url'
 import { pollSafeInfo } from '@/components/create-safe/logic'
 import { AppRoutes } from '@/config/routes'
 import { SafeCreationStatus } from '@/components/create-safe/status/useSafeCreation'
-import { CREATE_SAFE_EVENTS, SAFE_APPS_EVENTS, trackEvent } from '@/services/analytics'
+import { CREATE_SAFE_EVENTS, SAFE_APPS_EVENTS, trackEvent, trackSafeAppEvent } from '@/services/analytics'
 import chains from '@/config/chains'
 import { updateAddressBook } from '../logic/address-book'
 import { useAppDispatch } from '@/store'
@@ -29,7 +29,7 @@ const getRedirect = (chainId: string, safeAddress: string, redirectQuery?: strin
 
   // Track the redirect to Safe App
   if (redirectUrl.includes('apps')) {
-    trackEvent(SAFE_APPS_EVENTS.SHARED_APP_OPEN_AFTER_SAFE_CREATION)
+    trackSafeAppEvent({ ...SAFE_APPS_EVENTS.SHARED_APP_OPEN_AFTER_SAFE_CREATION, label: redirectUrl })
   }
 
   // We're prepending the safe address directly here because the `router.push` doesn't parse
--- src/components/safe-apps/AppFrame/useAppCommunicator.ts
+++ src/components/safe-apps/AppFrame/useAppCommunicator.ts
@@ -26,7 +26,7 @@ import AppCommunicator from '@/services/safe-apps/AppCommunicator'
 import { Errors, logError } from '@/services/exceptions'
 import { createSafeAppsWeb3Provider } from '@/hooks/wallets/web3'
 import type { SafePermissionsRequest } from '@/hooks/safe-apps/permissions'
-import { gtmTrackSafeAppMessage } from '@/services/analytics/gtm'
+import { SAFE_APPS_EVENTS, trackSafeAppEvent } from '@/services/analytics'
 
 export enum CommunicatorMessages {
   REJECT_TRANSACTION_MESSAGE = 'Transaction was rejected',
@@ -77,12 +77,17 @@ const useAppCommunicator = (
     const initCommunicator = (iframeRef: MutableRefObject<HTMLIFrameElement>, app?: SafeAppData) => {
       communicatorInstance = new AppCommunicator(iframeRef, {
         onMessage: (msg) => {
-          gtmTrackSafeAppMessage({
-            app,
-            method: msg.data.method,
-            params: msg.data.params,
-            sdkVersion: msg.data.env.sdkVersion,
-          })
+          const isCustomApp = app && app.id < 1
+
+          trackSafeAppEvent(
+            { ...SAFE_APPS_EVENTS.SAFE_APP_SDK_METHOD_CALL },
+            isCustomApp ? app?.url : app?.name || '',
+            {
+              method: msg.data.method,
+              ethMethod: (msg.data.params as any)?.call,
+              version: msg.data.env.sdkVersion,
+            },
+          )
         },
         onError: (error, data) => {
           logError(Errors._901, error.message, {
