5f9fb26c90160014a5fea731ed2011acafd85128
Diogo Soares <diogo.lsoares@gmail.com>
2022-11-10 13:52:52 +0100

    use MUI Accordion for the collapsible section

--- src/components/dashboard/GovernanceSection/GovernanceSection.tsx
+++ src/components/dashboard/GovernanceSection/GovernanceSection.tsx
@@ -1,66 +1,51 @@
-import { Typography, Grid, Card, Box, IconButton } from '@mui/material'
-import { WidgetBody, WidgetContainer } from '@/components/dashboard/styled'
+import { Typography, Grid, Card, Box } from '@mui/material'
+import { WidgetBody } from '@/components/dashboard/styled'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import ExpandLessIcon from '@mui/icons-material/ExpandLess'
-import { useState } from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import css from './styles.module.css'
 
-const GovernanceSection = () => {
-  const [isExpanded, setIsExpanded] = useState(true)
-
-  const handleExpandClick = () => {
-    setIsExpanded((val) => !val)
-  }
-
-  return (
-    <Grid item xs={12} md>
-      <WidgetContainer>
-        <Box display="flex" justifyContent="space-between" position="relative">
+const GovernanceSection = () => (
+  <Grid item xs={12} md>
+    <Accordion className={css.accordion}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: ({ palette }) => palette.primary.light }} />}>
+        <div>
           <Typography component="h2" variant="subtitle1" fontWeight={700}>
             Governance
           </Typography>
+          <Typography variant="body2" mb={2} color="text.secondary">
+            Use your SAFE tokens to vote on important proposals or participate in forum discussions.
+          </Typography>
+        </div>
+      </AccordionSummary>
 
-          <IconButton
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-            }}
-            onClick={handleExpandClick}
-          >
-            {isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </IconButton>
-        </Box>
-        <Typography variant="body2" mb={2} color="text.secondary">
-          Use your SAFE tokens to vote on important proposals or participate in forum discussions.
-        </Typography>
-
-        {isExpanded && (
-          <WidgetBody>
-            <Grid spacing={3} container>
-              <Grid item xs={8}>
-                <Card>
-                  <Box m={2} sx={{ minHeight: '200px' }}>
-                    <Typography variant="h3">
-                      <strong>Snapshot</strong>
-                    </Typography>
-                  </Box>
-                </Card>
-              </Grid>
-              <Grid item xs={4}>
-                <Card>
-                  <Box m={2} sx={{ minHeight: '200px' }}>
-                    <Typography variant="h3">
-                      <strong>Claiming app</strong>
-                    </Typography>
-                  </Box>
-                </Card>
-              </Grid>
+      <AccordionDetails sx={({ spacing }) => ({ padding: `0 ${spacing(3)}` })}>
+        <WidgetBody>
+          <Grid spacing={3} container>
+            <Grid item xs={8}>
+              <Card>
+                <Box m={2} sx={{ minHeight: '200px' }}>
+                  <Typography variant="h3">
+                    <strong>Snapshot</strong>
+                  </Typography>
+                </Box>
+              </Card>
             </Grid>
-          </WidgetBody>
-        )}
-      </WidgetContainer>
-    </Grid>
-  )
-}
+            <Grid item xs={4}>
+              <Card>
+                <Box m={2} sx={{ minHeight: '200px' }}>
+                  <Typography variant="h3">
+                    <strong>Claiming app</strong>
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+          </Grid>
+        </WidgetBody>
+      </AccordionDetails>
+    </Accordion>
+  </Grid>
+)
 
 export default GovernanceSection
