5788ffae48f2e9576bdf08fc034f138e8c510d9c
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2022-11-10 17:29:35 +0100

    [Governance Visibility] Dashboard section (#1125)
    * dashboard section with placeholders
    
    * use MUI Accordion for the collapsible section
    
    * change expand icon color

--- src/components/dashboard/GovernanceSection/GovernanceSection.tsx
+++ src/components/dashboard/GovernanceSection/GovernanceSection.tsx
@@ -1,36 +1,50 @@
 import { Typography, Grid, Card, Box } from '@mui/material'
-import { WidgetBody, WidgetContainer } from '@/components/dashboard/styled'
+import { WidgetBody } from '@/components/dashboard/styled'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import css from './styles.module.css'
 
 const GovernanceSection = () => (
   <Grid item xs={12} md>
-    <WidgetContainer>
-      <Typography component="h2" variant="subtitle1" fontWeight={700} mb={2}>
-        Governance
-      </Typography>
+    <Accordion className={css.accordion}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon color="border" />}>
+        <div>
+          <Typography component="h2" variant="subtitle1" fontWeight={700}>
+            Governance
+          </Typography>
+          <Typography variant="body2" mb={2} color="text.secondary">
+            Use your SAFE tokens to vote on important proposals or participate in forum discussions.
+          </Typography>
+        </div>
+      </AccordionSummary>
 
-      <WidgetBody>
-        <Grid gap="24px" container>
-          <Grid minWidth="200px" item xs md>
-            <Card>
-              <Box m={2} sx={{ minHeight: '200px' }}>
-                <Typography variant="h3">
-                  <strong>Claiming app</strong>
-                </Typography>
-              </Box>
-            </Card>
+      <AccordionDetails sx={({ spacing }) => ({ padding: `0 ${spacing(3)}` })}>
+        <WidgetBody>
+          <Grid spacing={3} container>
+            <Grid item xs={8}>
+              <Card>
+                <Box m={2} sx={{ minHeight: '200px' }}>
+                  <Typography variant="h3">
+                    <strong>Snapshot</strong>
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card>
+                <Box m={2} sx={{ minHeight: '200px' }}>
+                  <Typography variant="h3">
+                    <strong>Claiming app</strong>
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid minWidth="200px" item xs md>
-            <Card>
-              <Box m={2} sx={{ minHeight: '200px' }}>
-                <Typography variant="h3">
-                  <strong>Snapshot</strong>
-                </Typography>
-              </Box>
-            </Card>
-          </Grid>
-        </Grid>
-      </WidgetBody>
-    </WidgetContainer>
+        </WidgetBody>
+      </AccordionDetails>
+    </Accordion>
   </Grid>
 )
 
