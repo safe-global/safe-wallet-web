d066defe3c20c6d7bbfa83a48b4c5f47b58c9932
Aaron Cook <aaron@safe.global>
2022-11-10 08:08:42 +0100

    feat: add `SIGNING` event + `safeTxHash` details (#1062)
    * feat: add `SIGNING` event + `safeTxHash` details
    
    * fix: add comment
    
    * fix: return `undefined`, add test + only defined

--- src/services/exceptions/ErrorCodes.ts
+++ src/services/exceptions/ErrorCodes.ts
@@ -39,6 +39,7 @@ enum ErrorCodes {
   _806 = '806: Failed to remove module',
   _807 = '807: Failed to remove guard',
   _808 = '808: Failed to get transaction origin',
+  _809 = '809: Error generating safeTxHash',
 
   _900 = '900: Error loading Safe App',
   _901 = '901: Error processing Safe Apps SDK request',
