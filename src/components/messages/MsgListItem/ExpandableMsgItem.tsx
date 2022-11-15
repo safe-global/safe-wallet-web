0353bbccb7c3cdec68498a451132f586b18e230a
iamacook <aaron@safe.global>
2022-11-15 16:35:36 +0100

    fix: remove single view

--- src/components/messages/MsgListItem/ExpandableMsgItem.tsx
+++ src/components/messages/MsgListItem/ExpandableMsgItem.tsx
@@ -1,23 +1,14 @@
 import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { useRouter } from 'next/router'
 import type { ReactElement } from 'react'
 
 import txListItemCss from '@/components/transactions/TxListItem/styles.module.css'
-import { AppRoutes } from '@/config/routes'
 import MsgSummary from '@/components/messages/MsgSummary'
 import type { Message } from '@/hooks/useMessages'
 
 const ExpandableMsgItem = ({ item }: { item: Message }): ReactElement => {
-  const { pathname } = useRouter()
-
   return (
-    <Accordion
-      disableGutters
-      elevation={0}
-      defaultExpanded={pathname === AppRoutes.transactions.msg}
-      className={txListItemCss.accordion}
-    >
+    <Accordion disableGutters elevation={0} className={txListItemCss.accordion}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ justifyContent: 'flex-start', overflowX: 'auto' }}>
         <MsgSummary item={item} />
       </AccordionSummary>
