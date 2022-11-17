9a1f5ded6889cb618162fe3097cd9876cbc93bc8
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-11-17 12:59:07 +0100

    Refactor sidebar & drawer (#1187)

--- src/components/common/PageLayout/index.tsx
+++ src/components/common/PageLayout/index.tsx
@@ -1,50 +1,28 @@
-import { useEffect, useState, type ReactElement } from 'react'
-import { useRouter } from 'next/router'
-import cn from 'classnames'
+import { useState, type ReactElement } from 'react'
+import classnames from 'classnames'
 
-import Sidebar from '@/components/sidebar/Sidebar'
 import Header from '@/components/common//Header'
 import css from './styles.module.css'
 import SafeLoadingError from '../SafeLoadingError'
 import Footer from '../Footer'
-import { AppRoutes } from '@/config/routes'
-import { useSafeAppUrl } from '@/hooks/safe-apps/useSafeAppUrl'
-import { useMediaQuery } from '@mui/material'
+import SideDrawer from './SideDrawer'
 
 const PageLayout = ({ children }: { children: ReactElement }): ReactElement => {
-  const router = useRouter()
-  const isMobile = useMediaQuery('(max-width: 900px)')
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true)
 
-  const [appUrl] = useSafeAppUrl()
-  const isSafeAppPage = !!appUrl && router.pathname === AppRoutes.apps
-  const isShareSafeAppPage = router.pathname === AppRoutes.share.safeApp
-
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState<boolean>(isMobile)
-
-  const collapseSidebar = () => setIsSidebarCollapsed((prev) => !prev)
-  const showCollapseSidebarButton = isSafeAppPage && !isMobile
-
-  const isSidebarHiddenDesktop = (isShareSafeAppPage || isSafeAppPage) && isSidebarCollapsed
-  const isSidebarHiddenMobile = isSidebarCollapsed
-  const isSidebarHidden = isMobile ? isSidebarHiddenMobile : isSidebarHiddenDesktop
-
-  useEffect(() => {
-    setIsSidebarCollapsed(true)
-  }, [router.pathname, router.query.safe, isMobile, isSafeAppPage])
+  const toggleSidebar = () => {
+    setSidebarOpen((prev) => !prev)
+  }
 
   return (
     <>
       <header className={css.header}>
-        <Header onMenuToggle={collapseSidebar} />
+        <Header onMenuToggle={toggleSidebar} />
       </header>
 
-      <Sidebar
-        isSidebarHidden={isSidebarHidden}
-        showCollapseSidebarButton={showCollapseSidebarButton}
-        collapseSidebar={collapseSidebar}
-      />
+      <SideDrawer isOpen={isSidebarOpen} onToggle={setSidebarOpen} />
 
-      <div className={cn(css.main, isSidebarHidden && css.mainNoSidebar)}>
+      <div className={classnames(css.main, !isSidebarOpen && css.mainNoSidebar)}>
         <div className={css.content}>
           <SafeLoadingError>{children}</SafeLoadingError>
         </div>
