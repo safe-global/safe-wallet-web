9c6a6ecc508db9415d01323db6d3fb4dc9e8a1bd
Diogo Soares <diogo.lsoares@gmail.com>
2022-11-22 11:12:41 +0100

    fetch the claiming app and display the widgets

--- src/components/dashboard/GovernanceSection/GovernanceSection.tsx
+++ src/components/dashboard/GovernanceSection/GovernanceSection.tsx
@@ -1,4 +1,4 @@
-import { Typography, Grid, Card } from '@mui/material'
+import { Typography, Grid, Card, Box, Paper } from '@mui/material'
 import { WidgetBody } from '@/components/dashboard/styled'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import Accordion from '@mui/material/Accordion'
@@ -9,27 +9,29 @@ import SafeAppsErrorBoundary from '@/components/safe-apps/SafeAppsErrorBoundary'
 import SafeAppsLoadError from '@/components/safe-apps/SafeAppsErrorBoundary/SafeAppsLoadError'
 import AppFrame from '@/components/safe-apps/AppFrame'
 import { useBrowserPermissions } from '@/hooks/safe-apps/permissions'
-
-export type WidgetType = {
-  appId: string
-  appUrl: string
-  widgetType?: 'iframe'
-}
-
-// TODO: use remote safe apps endpoint and the 'dashboard-mini-app' tag
-export const appWidgets: WidgetType[] = [
-  {
-    appId: '1',
-    appUrl: 'http://localhost:3001/safe-claiming-app#snapshot_widget',
-  },
-  {
-    appId: '2',
-    appUrl: 'http://localhost:3001/safe-claiming-app#widget',
-  },
-]
+import { useRemoteSafeApps } from '@/hooks/safe-apps/useRemoteSafeApps'
+import { SafeAppsTag } from '@/config/constants'
 
 const GovernanceSection = () => {
   const { getAllowedFeaturesList } = useBrowserPermissions()
+  const [claimingSafeApp, , loadingclaimingSafeApp] = useRemoteSafeApps(SafeAppsTag.SAFE_CLAIMING_APP)
+  const claimingApp = claimingSafeApp?.[0]
+
+  if (loadingclaimingSafeApp) {
+    return (
+      <Paper>
+        <Box sx={{ height: '300px' }} display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h1" color="text.secondary">
+            Loading Safe Claiming App...
+          </Typography>
+        </Box>
+      </Paper>
+    )
+  }
+
+  if (!claimingApp) {
+    return null
+  }
 
   return (
     <Grid item xs={12} md>
@@ -52,8 +54,8 @@ const GovernanceSection = () => {
                 <Card sx={{ height: '300px' }}>
                   <SafeAppsErrorBoundary render={() => <SafeAppsLoadError onBackToApps={() => {}} />}>
                     <AppFrame
-                      appUrl={appWidgets[0].appUrl}
-                      allowedFeaturesList={getAllowedFeaturesList(appWidgets[0].appUrl)}
+                      appUrl={`${claimingApp.url}#snapshot-widget`}
+                      allowedFeaturesList={getAllowedFeaturesList(claimingApp.url)}
                       isQueueBarDisabled
                     />
                   </SafeAppsErrorBoundary>
@@ -63,8 +65,8 @@ const GovernanceSection = () => {
                 <Card sx={{ height: '300px' }}>
                   <SafeAppsErrorBoundary render={() => <SafeAppsLoadError onBackToApps={() => {}} />}>
                     <AppFrame
-                      appUrl={appWidgets[1].appUrl}
-                      allowedFeaturesList={getAllowedFeaturesList(appWidgets[1].appUrl)}
+                      appUrl={`${claimingApp.url}#claiming-widget`}
+                      allowedFeaturesList={getAllowedFeaturesList(claimingApp.url)}
                       isQueueBarDisabled
                     />
                   </SafeAppsErrorBoundary>
