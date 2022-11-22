3007a6e629a8c68b4f30c307d8975b8b2244c0c1
iamacook <aaron@safe.global>
2022-11-22 17:37:21 +0100

    fix: add test template

---
+++ src/services/msg/__tests__/msgSender.test.ts
@@ -0,0 +1,19 @@
+describe('msgSender', () => {
+  describe('getMessageHash', () => {
+    it.todo('should return the hash of a string message')
+    it.todo('should return the hash of a typed message')
+  })
+
+  describe('dispatchMsgProposal', () => {
+    it.todo('should dispatch a message proposal')
+    it.todo('should dispatch a message proposal failure')
+  })
+
+  describe('dispatchMsgConfirmation', () => {
+    it.todo('should dispatch a message confirmation')
+    it.todo('should dispatch a message confirmation failure')
+  })
+})
+
+// TODO: Add tests. This appeases TypeScript for now.
+export {}
