6eb0acb1dbd582bc173cd6c32cdc98b23c7bb88f
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2022-11-28 09:01:29 +0100

    fix: Dislay an error when trying to transfer safe token (#1250)
    * fix: Dislay an error when trying to transfer safe token
    
    * fix: feedback

--- src/services/tx/safeUpdateParams.ts
+++ src/services/tx/safeUpdateParams.ts
@@ -4,6 +4,7 @@ import type { ChainInfo, SafeInfo } from '@gnosis.pm/safe-react-gateway-sdk'
 import { getFallbackHandlerContractInstance, getGnosisSafeContractInstance } from '@/services/contracts/safeContracts'
 import { LATEST_SAFE_VERSION } from '@/config/constants'
 
+// TODO: Check if these are still needed
 export const CHANGE_MASTER_COPY_ABI = 'function changeMasterCopy(address _masterCopy)'
 export const CHANGE_FALLBACK_HANDLER_ABI = 'function setFallbackHandler(address handler)'
 
