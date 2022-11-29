1131ee2b3f8617f28184d7857ec53da625bba2dc
iamacook <aaron@safe.global>
2022-11-29 11:40:58 +0100

    fix: poll on tag change

--- src/hooks/loadables/useLoadSafeMessages.ts
+++ src/hooks/loadables/useLoadSafeMessages.ts
@@ -6,23 +6,16 @@ import useAsync from '@/hooks/useAsync'
 import { logError, Errors } from '@/services/exceptions'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import type { AsyncResult } from '@/hooks/useAsync'
-import { POLLING_INTERVAL } from '@/config/constants'
-import useIntervalCounter from '@/hooks/useIntervalCounter'
 
 export const useLoadMessages = (): AsyncResult<SafeMessageListPage> => {
-  const { safe, safeAddress, safeLoaded } = useSafeInfo()
-  const [pollCount, resetPolling] = useIntervalCounter(POLLING_INTERVAL)
+  const { safe, safeAddress, safeLoaded, messagesTag } = useSafeInfo()
 
   const [data, error, loading] = useAsync<SafeMessageListPage>(() => {
     if (!safeLoaded) {
       return
     }
     return getSafeMessages(safe.chainId, safeAddress)
-  }, [safeLoaded, safe.chainId, safeAddress, pollCount])
-
-  useEffect(() => {
-    resetPolling()
-  }, [resetPolling, safe.chainId, safeAddress])
+  }, [safeLoaded, safe.chainId, safeAddress, messagesTag])
 
   useEffect(() => {
     if (error) {
