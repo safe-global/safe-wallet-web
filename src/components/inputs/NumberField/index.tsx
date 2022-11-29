dba456302e0583b2d936428290ddee79ff450f0e
iamacook <aaron@safe.global>
2022-11-29 21:35:40 +0100

    fix: use `inputMode` and `pattern`

--- src/components/inputs/NumberField/index.tsx
+++ src/components/inputs/NumberField/index.tsx
@@ -1,16 +1,19 @@
 import { TextField } from '@mui/material'
-import { useRef } from 'react'
+import { forwardRef } from 'react'
 import type { TextFieldProps } from '@mui/material'
 import type { ReactElement } from 'react'
 
-const NumberField = (props: TextFieldProps): ReactElement => {
-  const inputRef = useRef<HTMLInputElement>()
+const NumberField = forwardRef<HTMLInputElement, TextFieldProps>(({ inputProps, ...props }, ref): ReactElement => {
+  return (
+    <TextField
+      ref={ref}
+      autoComplete="off"
+      inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', ...inputProps }}
+      {...props}
+    />
+  )
+})
 
-  const handleBlur = () => {
-    inputRef.current?.blur()
-  }
-
-  return <TextField type="number" autoComplete="off" inputRef={inputRef} onWheel={handleBlur} {...props} />
-}
+NumberField.displayName = 'NumberField'
 
 export default NumberField
