cf7a037a65078251556630deccd0b8cb53a80dab
Usame Algan <usame@safe.global>
2022-11-29 17:12:07 +0100

    fix: Add tests for useInitSafeCoreSDK hook

--- src/hooks/coreSDK/useInitSafeCoreSDK.ts
+++ src/hooks/coreSDK/useInitSafeCoreSDK.ts
@@ -1,35 +1,25 @@
-import { useEffect, useState } from 'react'
-import useChainId from '@/hooks/useChainId'
+import { useEffect } from 'react'
 import useWallet from '../wallets/useWallet'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import { initSafeSDK, setSafeSDK } from '@/hooks/coreSDK/safeCoreSDK'
+import { trackError } from '@/services/exceptions'
+import ErrorCodes from '@/services/exceptions/ErrorCodes'
 
-export const useInitSafeCoreSDK = (): Error | null => {
-  const chainId = useChainId()
+export const useInitSafeCoreSDK = () => {
   const wallet = useWallet()
   const { safe, safeLoaded } = useSafeInfo()
 
-  const [error, setError] = useState<Error | null>(null)
-
   useEffect(() => {
-    if (
-      !safeLoaded ||
-      !wallet?.provider ||
-      !wallet.chainId ||
-      chainId !== wallet.chainId ||
-      safe.chainId !== wallet.chainId
-    ) {
+    if (!safeLoaded || !wallet?.provider || safe.chainId !== wallet.chainId) {
       return
     }
 
     initSafeSDK(wallet.provider, wallet.chainId, safe.address.value, safe.version)
       .then(setSafeSDK)
       .catch((e) => {
+        trackError(ErrorCodes._105, (e as Error).message)
         // If we don't reset the SDK, a previous Safe could remain in the store
         setSafeSDK(undefined)
-        setError(e)
       })
-  }, [chainId, wallet?.provider, wallet?.chainId, safe, safeLoaded])
-
-  return error
+  }, [wallet?.provider, wallet?.chainId, safe, safeLoaded])
 }
