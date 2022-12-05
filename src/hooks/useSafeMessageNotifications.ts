0dc153d289884afd75897c5cea20c8b4e62ef718
iamacook <aaron@safe.global>
2022-12-05 18:48:02 +0100

    feat: notification for messages w/o confirmation

--- src/hooks/useSafeMessageNotifications.ts
+++ src/hooks/useSafeMessageNotifications.ts
@@ -1,9 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+import { SafeMessageStatus } from '@gnosis.pm/safe-react-gateway-sdk'
 
 import { SafeMsgEvent, safeMsgSubscribe } from '@/services/safe-messages/safeMsgEvents'
-import { useAppDispatch } from '@/store'
-import { showNotification } from '@/store/notificationsSlice'
+import { useAppDispatch, useAppSelector } from '@/store'
+import { selectNotifications, showNotification } from '@/store/notificationsSlice'
 import { formatError } from '@/utils/formatters'
+import { isSafeMessageListItem } from '@/utils/safe-message-guards'
+import useSafeMessages from '@/hooks/useSafeMessages'
+import { selectPendinngSafeMessages } from '@/store/pendingSafeMessagesSlice'
+import useIsGranted from '@/hooks/useIsGranted'
+import { AppRoutes } from '@/config/routes'
+import useWallet from '@/hooks/wallets/useWallet'
+import { useCurrentChain } from '@/hooks/useChains'
+import useSafeAddress from '@/hooks/useSafeAddress'
 
 const SafeMessageNotifications: Partial<Record<SafeMsgEvent, string>> = {
   [SafeMsgEvent.PROPOSE]: 'You successfully signed the message.',
@@ -15,6 +24,10 @@ const SafeMessageNotifications: Partial<Record<SafeMsgEvent, string>> = {
 
 const useSafeMessageNotifications = () => {
   const dispatch = useAppDispatch()
+
+  /**
+   * Show notifications of a messages's lifecycle
+   */
 
   useEffect(() => {
     const entries = Object.entries(SafeMessageNotifications) as [keyof typeof SafeMessageNotifications, string][]
@@ -40,6 +53,55 @@ const useSafeMessageNotifications = () => {
       unsubFns.forEach((unsub) => unsub())
     }
   }, [dispatch])
+
+  /**
+   * If there's at least one message awaiting confirmations, show a notification for it
+   */
+
+  const { page } = useSafeMessages()
+  const pendingMsgs = useAppSelector(selectPendinngSafeMessages)
+  const wallet = useWallet()
+  const isGranted = useIsGranted()
+  const notifications = useAppSelector(selectNotifications)
+  const chain = useCurrentChain()
+  const safeAddress = useSafeAddress()
+
+  const msgsNeedingConfirmation = useMemo(() => {
+    if (!page?.results) {
+      return []
+    }
+
+    return page.results.filter(isSafeMessageListItem).filter((message) => {
+      const needsConfirmation = message.status === SafeMessageStatus.NEEDS_CONFIRMATION
+      const isPending = !!pendingMsgs[message.messageHash]
+      const canSign = message.confirmations.every(({ owner }) => owner.value !== wallet?.address)
+      return needsConfirmation && !isPending && canSign
+    })
+  }, [page?.results, pendingMsgs, wallet?.address])
+
+  useEffect(() => {
+    console.log({ isGranted, msgsNeedingConfirmation })
+    if (!isGranted || msgsNeedingConfirmation.length === 0) {
+      return
+    }
+
+    const messageHash = msgsNeedingConfirmation[0].messageHash
+    const hasNotified = notifications.some(({ groupKey }) => groupKey === messageHash)
+
+    if (!hasNotified) {
+      dispatch(
+        showNotification({
+          variant: 'info',
+          message: 'A message requires your confirmation.',
+          link: {
+            href: `${AppRoutes.transactions.messages}?safe=${chain?.shortName}:${safeAddress}`,
+            title: 'View messages',
+          },
+          groupKey: messageHash,
+        }),
+      )
+    }
+  }, [dispatch, isGranted, notifications, msgsNeedingConfirmation, chain?.shortName, safeAddress])
 }
 
 export default useSafeMessageNotifications
