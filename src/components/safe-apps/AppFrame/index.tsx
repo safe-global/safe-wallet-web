8644e6821cdd3feaa06a6d81b37e397c997858dd
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2022-12-09 13:59:54 +0100

    [Epic] Governance visibility (#1256)
    * create the Governance section on Dashboard
    
    * [Governance Visibility] Dashboard section (#1125)
    
    * dashboard section with placeholders
    
    * use MUI Accordion for the collapsible section
    
    * change expand icon color
    
    * feat: Dashboard governance widgets (#1149)
    
    * feat: load a mini safe app in dashboard's Governance section
    
    * fix card heights
    
    * display snapshot widget
    
    * fetch the claiming app and display the widgets
    
    * fix: accordion defaultExpanded and fix loading states flicker
    
    * responsive widgets grid
    
    * expandDefault true
    
    * no need to pass explicit boolean value. improve loading components
    
    * feat: pass dark mode in the widgets url (#1219)
    
    * expandDefault true
    
    * no need to pass explicit boolean value. improve loading components
    
    * feat: pass dark mode in the widgets url
    
    * pass preferred them in a search parameter
    
    * feat: One governance widget (#1237)
    
    * feat: call only one widget from the safe-claiming app
    
    * rm unused Grid
    
    * use widgetWrapper for the loading component
    
    * add a custom fallback component
    
    * ammend load error copy
    
    * use different color in the placeholder as per design
    
    * feat: dark flag in URL hash (#1251)
    
    * fix: governance widgets CSS (#1254)
    
    * fix: Accordion only clickable on expandIcon
    
    * adjust AppFrame height for widgets
    
    * change the widget load error component
    
    * fix code style
    
    * improve classnames params
    
    * fix: return `null` if no Claiming App on chain (#1284)
    
    * fix: return `null` if no Claiming App on chain
    
    * fix: reference deployed token address
    
    * fix: add address of token on Goerli
    
    * fix: add loading wrapper
    
    * fix: rename wrapper
    
    * Fix: use a non-tracking version of the AppFrame (#1307)
    
    * Fix: use a non-tracking version of the AppFrame
    
    * Clear communicator handlers
    
    * useAppIsLoading
    
    * Rename components
    
    * Extract getSafeInfo
    
    * AppFrameWrapperProps -> AppFrameProps
    
    * Fix: reload widget on safe change + catch errors (#1319)
    
    Co-authored-by: iamacook <aaron@safe.global>
    Co-authored-by: katspaugh <katspaugh@gmail.com>
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- src/components/safe-apps/AppFrame/index.tsx
+++ src/components/safe-apps/AppFrame/index.tsx
@@ -17,13 +17,11 @@ import { useSafeAppFromBackend } from '@/hooks/safe-apps/useSafeAppFromBackend'
 import useChainId from '@/hooks/useChainId'
 import useAddressBook from '@/hooks/useAddressBook'
 import { useSafePermissions } from '@/hooks/safe-apps/permissions'
-import useIsGranted from '@/hooks/useIsGranted'
 import { useCurrentChain } from '@/hooks/useChains'
 import { isSameUrl } from '@/utils/url'
 import { isMultisigDetailedExecutionInfo } from '@/utils/transaction-guards'
 import useTransactionQueueBarState from '@/components/safe-apps/AppFrame/useTransactionQueueBarState'
 import { gtmTrackPageview } from '@/services/analytics/gtm'
-import { getLegacyChainName } from '../utils'
 import useThirdPartyCookies from './useThirdPartyCookies'
 import useAnalyticsFromSafeApp from './useFromAppAnalytics'
 import useAppIsLoading from './useAppIsLoading'
@@ -38,6 +36,8 @@ import PermissionsPrompt from '../PermissionsPrompt'
 import { PermissionStatus } from '../types'
 
 import css from './styles.module.css'
+import SafeAppIframe from './SafeAppIframe'
+import useGetSafeInfo from './useGetSafeInfo'
 
 const UNKNOWN_APP_NAME = 'Unknown App'
 
@@ -46,10 +46,6 @@ type AppFrameProps = {
   allowedFeaturesList: string
 }
 
-// see sandbox mdn docs for more details https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attr-sandbox
-const IFRAME_SANDBOX_ALLOWED_FEATURES =
-  'allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-forms allow-downloads allow-orientation-lock'
-
 const AppFrame = ({ appUrl, allowedFeaturesList }: AppFrameProps): ReactElement => {
   const chainId = useChainId()
   const [txModalState, openTxModal, closeTxModal] = useTxModal()
@@ -57,7 +53,6 @@ const AppFrame = ({ appUrl, allowedFeaturesList }: AppFrameProps): ReactElement 
   const { safe, safeLoaded, safeAddress } = useSafeInfo()
   const addressBook = useAddressBook()
   const chain = useCurrentChain()
-  const granted = useIsGranted()
   const router = useRouter()
   const {
     expanded: queueBarExpanded,
@@ -75,6 +70,7 @@ const AppFrame = ({ appUrl, allowedFeaturesList }: AppFrameProps): ReactElement 
   const { getPermissions, hasPermission, permissionsRequest, setPermissionsRequest, confirmPermissionRequest } =
     useSafePermissions()
   const appName = useMemo(() => (remoteApp ? remoteApp.name : appUrl), [appUrl, remoteApp])
+
   const communicator = useAppCommunicator(iframeRef, remoteApp || safeAppFromManifest, chain, {
     onConfirmTransactions: openTxModal,
     onSignMessage: openSignMessageModal,
@@ -91,14 +87,7 @@ const AppFrame = ({ appUrl, allowedFeaturesList }: AppFrameProps): ReactElement 
     onGetEnvironmentInfo: () => ({
       origin: document.location.origin,
     }),
-    onGetSafeInfo: () => ({
-      safeAddress,
-      chainId: parseInt(chainId, 10),
-      owners: safe.owners.map((owner) => owner.value),
-      threshold: safe.threshold,
-      isReadOnly: !granted,
-      network: getLegacyChainName(chain?.chainName || '', chainId).toUpperCase(),
-    }),
+    onGetSafeInfo: useGetSafeInfo(),
     onGetSafeBalances: (currency) =>
       getBalances(chainId, safeAddress, currency, {
         exclude_spam: true,
@@ -216,24 +205,25 @@ const AppFrame = ({ appUrl, allowedFeaturesList }: AppFrameProps): ReactElement 
           </div>
         )}
 
-        <iframe
-          className={css.iframe}
-          id={`iframe-${appUrl}`}
-          ref={iframeRef}
-          src={appUrl}
-          title={safeAppFromManifest?.name}
-          onLoad={onIframeLoad}
-          sandbox={IFRAME_SANDBOX_ALLOWED_FEATURES}
-          allow={allowedFeaturesList}
+        <div
           style={{
+            height: '100%',
             display: appIsLoading ? 'none' : 'block',
             paddingBottom: queueBarVisible ? TRANSACTION_BAR_HEIGHT : 0,
           }}
-        />
+        >
+          <SafeAppIframe
+            appUrl={appUrl}
+            allowedFeaturesList={allowedFeaturesList}
+            iframeRef={iframeRef}
+            onLoad={onIframeLoad}
+            title={safeAppFromManifest?.name}
+          />
+        </div>
 
         <TransactionQueueBar
           expanded={queueBarExpanded}
-          visible={!queueBarDismissed}
+          visible={queueBarVisible && !queueBarDismissed}
           setExpanded={setExpanded}
           onDismiss={dismissQueueBar}
           transactions={transactions}
--- src/components/safe-apps/AppFrame/useAppCommunicator.ts
+++ src/components/safe-apps/AppFrame/useAppCommunicator.ts
@@ -43,7 +43,7 @@ type JsonRpcResponse = {
   error?: string
 }
 
-type UseAppCommunicatorHandlers = {
+export type UseAppCommunicatorHandlers = {
   onConfirmTransactions: (txs: BaseTransaction[], requestId: RequestId, params?: SendTransactionRequestParams) => void
   onSignMessage: (
     message: string | EIP712TypedData,
