260574beb4c5ed38183eca9f1712c44ec91adb68
iamacook <aaron@safe.global>
2022-12-20 14:41:00 +0100

    fix: poll for messages

--- src/hooks/loadables/useLoadSafeMessages.ts
+++ src/hooks/loadables/useLoadSafeMessages.ts
@@ -5,17 +5,37 @@ import type { SafeMessageListPage } from '@safe-global/safe-gateway-typescript-s
 import useAsync from '@/hooks/useAsync'
 import { logError, Errors } from '@/services/exceptions'
 import useSafeInfo from '@/hooks/useSafeInfo'
+import { POLLING_INTERVAL } from '@/config/constants'
+import useIntervalCounter from '@/hooks/useIntervalCounter'
 import type { AsyncResult } from '@/hooks/useAsync'
 
 export const useLoadSafeMessages = (): AsyncResult<SafeMessageListPage> => {
   const { safe, safeAddress, safeLoaded } = useSafeInfo()
 
-  const [data, error, loading] = useAsync<SafeMessageListPage>(() => {
-    if (!safeLoaded) {
-      return
-    }
-    return getSafeMessages(safe.chainId, safeAddress)
-  }, [safeLoaded, safe.chainId, safeAddress, safe.messagesTag])
+  // TODO: Remove manual polling when messagesTag is no longer cached on the backend
+  const [pollCount, resetPolling] = useIntervalCounter(POLLING_INTERVAL)
+
+  // Reset the counter when safe address/chainId changes
+  useEffect(() => {
+    resetPolling()
+  }, [resetPolling, safeAddress, safe.chainId])
+
+  const [data, error, loading] = useAsync<SafeMessageListPage>(
+    () => {
+      if (!safeLoaded) {
+        return
+      }
+      return getSafeMessages(safe.chainId, safeAddress)
+    },
+    [
+      safeLoaded,
+      safe.chainId,
+      safeAddress,
+      // safe.messagesTag,
+      pollCount,
+    ],
+    false,
+  )
 
   useEffect(() => {
     if (error) {
