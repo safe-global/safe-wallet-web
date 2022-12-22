87bb2d1f36dd4ae7e66b69338547162f3abaef34
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-12-22 13:31:16 +0100

    Feat: add PWA support (next-pwa) (#1401)
    * Feat: add PWA support (next-pwa)
    
    * Gitignore
    
    * Update TS
    
    * Runtime caching
    
    * Add offline page
    
    * Disable e2e service worker
    
    * Update offline text
    
    * Disable chrome security in e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -15,9 +15,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3000',
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
+
+  chromeWebSecurity: false,
 })
