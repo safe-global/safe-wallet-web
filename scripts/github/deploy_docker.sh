f742cf1f64852337318a62a1acc0e9c5e8b74c4e
Daniel Somoza <daniel.somoza@safe.global>
2022-12-22 17:58:05 +0100

    removed -f flag in the build script

--- scripts/github/deploy_docker.sh
+++ scripts/github/deploy_docker.sh
@@ -7,13 +7,13 @@ export DOCKER_BUILDKIT=1
 if [ "$1" = "dev" -o "$1" = "main" ]; then
     # If image does not exist, don't use cache
     docker pull safeglobal/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from safeglobal/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --build-arg BUILDKIT_INLINE_CACHE=1
+    docker build -t $DOCKERHUB_PROJECT . --cache-from safeglobal/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
 else
     # Building tag version from staging image (vX.X.X)
     docker pull safeglobal/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from safeglobal/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --build-arg BUILDKIT_INLINE_CACHE=1
+    docker build -t $DOCKERHUB_PROJECT . --cache-from safeglobal/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
     # Only push latest on release
     case $1 in v*)
         docker tag $DOCKERHUB_PROJECT safeglobal/$DOCKERHUB_PROJECT:latest
