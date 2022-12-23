58ad5069e08f471a6cde9978068bbc532cc1bbad
Mikhail <mveehkim@gmail.com>
2022-12-23 14:28:39 +0100

    i love boolean variables for request status

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -37,7 +37,7 @@ const Apps: NextPage = () => {
     permissions: safeApp?.safeAppsPermissions || [],
     addPermissions,
     getPermissions,
-    remoteSafeAppsLoading,
+    remoteSafeAppsLoading: remoteSafeAppsLoading || remoteSafeApps.length === 0,
   })
 
   if (!routerReady || isLoading) {
