e47990d1b062c321f1494c40509c5d7e2d8b5fed
katspaugh <katspaugh@gmail.com>
2022-12-27 14:36:07 +0100

    Perf: extract redirect logic into a static HTML page

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Custom404: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Safe – Page not found</title>
+      </Head>
+
+      <main>
+        <h1>404 - Page not found</h1>
+      </main>
+    </>
+  )
+}
+
+export default Custom404
