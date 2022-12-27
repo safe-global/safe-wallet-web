088fb501532a7b7d9e7047f820d99159c9d14b1f
Diogo Soares <diogo.lsoares@gmail.com>
2022-12-27 11:59:26 +0000

    fix: add what are signed messages link to page

--- src/pages/transactions/messages.tsx
+++ src/pages/transactions/messages.tsx
@@ -5,6 +5,21 @@ import PaginatedMsgs from '@/components/safe-messages/PaginatedMsgs'
 import TxHeader from '@/components/transactions/TxHeader'
 import NavTabs from '@/components/common/NavTabs'
 import { transactionNavItems } from '@/components/sidebar/SidebarNavigation/config'
+import { Box, SvgIcon, Typography } from '@mui/material'
+import InfoIcon from '@/public/images/notifications/info.svg'
+import ExternalLink from '@/components/common/ExternalLink'
+
+const SignedMessagesHelpLink = () => (
+  <Box display="flex" alignItems="center" gap={1}>
+    <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
+    {/* TODO: Add link to help article */}
+    <ExternalLink suppressIcon href="#">
+      <Typography variant="body2" fontWeight={700}>
+        What are signed messages?
+      </Typography>
+    </ExternalLink>
+  </Box>
+)
 
 const Messages: NextPage = () => {
   return (
@@ -13,7 +28,14 @@ const Messages: NextPage = () => {
         <title>Safe – Messages</title>
       </Head>
 
-      <TxHeader action={<NavTabs tabs={transactionNavItems} />} />
+      <TxHeader
+        action={
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <NavTabs tabs={transactionNavItems} />
+            <SignedMessagesHelpLink />
+          </Box>
+        }
+      />
 
       <main>
         <PaginatedMsgs />
