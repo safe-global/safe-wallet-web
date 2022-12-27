6dbb03ff4cda59d4ec4c446e3806126c14342e5a
Diogo Soares <diogo.lsoares@gmail.com>
2022-12-27 12:16:23 +0000

    show link only if there are messages

--- src/pages/transactions/messages.tsx
+++ src/pages/transactions/messages.tsx
@@ -8,18 +8,29 @@ import { transactionNavItems } from '@/components/sidebar/SidebarNavigation/conf
 import { Box, SvgIcon, Typography } from '@mui/material'
 import InfoIcon from '@/public/images/notifications/info.svg'
 import ExternalLink from '@/components/common/ExternalLink'
+import { useAppSelector } from '@/store'
+import { selectSafeMessages } from '@/store/safeMessagesSlice'
 
-const SignedMessagesHelpLink = () => (
-  <Box display="flex" alignItems="center" gap={1}>
-    <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
-    {/* TODO: Add link to help article */}
-    <ExternalLink suppressIcon href="#">
-      <Typography variant="body2" fontWeight={700}>
-        What are signed messages?
-      </Typography>
-    </ExternalLink>
-  </Box>
-)
+const SignedMessagesHelpLink = () => {
+  const safeMessages = useAppSelector(selectSafeMessages)
+  const safeMessagesCount = safeMessages.data?.results.length ?? 0
+
+  if (safeMessagesCount === 0) {
+    return null
+  }
+
+  return (
+    <Box display="flex" alignItems="center" gap={1}>
+      <SvgIcon component={InfoIcon} inheritViewBox color="border" fontSize="small" />
+      {/* TODO: Add link to help article */}
+      <ExternalLink suppressIcon href="#">
+        <Typography variant="body2" fontWeight={700}>
+          What are signed messages?
+        </Typography>
+      </ExternalLink>
+    </Box>
+  )
+}
 
 const Messages: NextPage = () => {
   return (
