223a23e82e870f19b3485c02f9924b2b86580fb5
katspaugh <katspaugh@gmail.com>
2023-01-02 15:42:26 +0100

    Upload index.html

--- scripts/github/s3_upload.sh
+++ scripts/github/s3_upload.sh
@@ -4,16 +4,19 @@ set -ev
 
 cd out
 
-# First, upload new files w/o deleting the old ones
+# Upload new files w/o deleting the old ones
 aws s3 sync . $BUCKET --exclude "*.html"
 
-# Second, upload them again but delete the old files this time
+# Upload them again but delete the old files this time
 # This allows for a no-downtime deployment
 aws s3 sync . $BUCKET --delete --exclude "*.html"
 
-# Finally, upload HTML files but w/o an extention so that URLs like /welcome open the right file
+# Upload HTML files but w/o an extention so that URLs like /welcome open the right file
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
     aws s3 cp ${file%} $BUCKET/${file%.*} --content-type "text/html"
 done
+
+# Upload index.html as the default file
+aws s3 cp index.html $BUCKET
 
 cd -
