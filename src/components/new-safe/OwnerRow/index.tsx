f4a5b68732176ae67616ee34d82673d789775fa2
Usame Algan <usame@safe.global>
2023-01-09 16:55:55 +0100

    fix: Increase bottom margin for owner row if there is a helper text

--- src/components/new-safe/OwnerRow/index.tsx
+++ src/components/new-safe/OwnerRow/index.tsx
@@ -11,6 +11,7 @@ import type { NamedAddress } from '@/components/new-safe/create/types'
 import useWallet from '@/hooks/wallets/useWallet'
 import { sameAddress } from '@/utils/addresses'
 import css from './styles.module.css'
+import classNames from 'classnames'
 
 export const OwnerRow = ({
   index,
@@ -63,8 +64,17 @@ export const OwnerRow = ({
     }
   }, [ens, setValue, getValues, name, fieldName])
 
+  const walletIsOwner = owner.address === wallet?.address
+
   return (
-    <Grid container spacing={3} alignItems="center" marginBottom={3} flexWrap={['wrap', undefined, 'nowrap']}>
+    <Grid
+      container
+      spacing={3}
+      alignItems="center"
+      marginBottom={3}
+      flexWrap={['wrap', undefined, 'nowrap']}
+      className={classNames({ [css.helper]: walletIsOwner })}
+    >
       <Grid item xs={12} md={readOnly ? 5 : 4}>
         <FormControl fullWidth>
           <NameInput
@@ -73,7 +83,7 @@ export const OwnerRow = ({
             label="Owner name"
             InputLabelProps={{ shrink: true }}
             placeholder={ens || `Owner ${index + 1}`}
-            helperText={owner.address === wallet?.address && 'Your connected wallet'}
+            helperText={walletIsOwner && 'Your connected wallet'}
             InputProps={{
               endAdornment: resolving ? (
                 <InputAdornment position="end">
