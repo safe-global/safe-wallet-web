eea34b3385b2c80e22d7a3060418a68ec6c7abab
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-01-10 10:08:56 +0100

    Feat: add a chain switch button in Safe creation (#1502)

--- src/components/new-safe/create/NetworkWarning/index.tsx
+++ src/components/new-safe/create/NetworkWarning/index.tsx
@@ -1,5 +1,6 @@
-import { Alert, AlertTitle } from '@mui/material'
+import { Alert, AlertTitle, Box } from '@mui/material'
 import { useCurrentChain } from '@/hooks/useChains'
+import ChainSwitcher from '@/components/common/ChainSwitcher'
 
 const NetworkWarning = () => {
   const chain = useCurrentChain()
@@ -7,9 +8,12 @@ const NetworkWarning = () => {
   if (!chain) return null
 
   return (
-    <Alert severity="info" sx={{ mt: 3 }}>
+    <Alert severity="warning" sx={{ mt: 3 }}>
       <AlertTitle sx={{ fontWeight: 700 }}>Change your wallet network</AlertTitle>
       You are trying to create a Safe on {chain.chainName}. Make sure that your wallet is set to the same network.
+      <Box mt={2}>
+        <ChainSwitcher />
+      </Box>
     </Alert>
   )
 }
