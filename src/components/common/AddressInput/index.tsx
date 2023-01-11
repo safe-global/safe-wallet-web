8b0f257d0eb230e8d65e88e929cf28d97b37dd60
Diogo Soares <diogo.lsoares@gmail.com>
2023-01-11 15:12:06 +0000

    fix: set a local loading state for when the input is filled in setValueAs

--- src/components/common/AddressInput/index.tsx
+++ src/components/common/AddressInput/index.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from 'react'
-import { useEffect, useCallback, useRef, useMemo } from 'react'
+import { useEffect, useCallback, useRef, useMemo, useState } from 'react'
 import { InputAdornment, TextField, type TextFieldProps, CircularProgress, Grid } from '@mui/material'
 import { useFormContext, useWatch, type Validate, get } from 'react-hook-form'
 import { FEATURES } from '@safe-global/safe-gateway-typescript-sdk'
@@ -25,6 +25,7 @@ const AddressInput = ({ name, validate, required = true, deps, ...props }: Addre
   const rawValueRef = useRef<string>('')
   const watchedValue = useWatch({ name, control })
   const currentShortName = currentChain?.shortName || ''
+  const [localLoading, setLocalLoading] = useState<boolean>(false)
 
   // Fetch an ENS resolution for the current address
   const isDomainLookupEnabled = !!currentChain && hasFeature(currentChain, FEATURES.DOMAIN_LOOKUP)
@@ -54,6 +55,10 @@ const AddressInput = ({ name, validate, required = true, deps, ...props }: Addre
     }
   }, [address, currentShortName, setAddressValue])
 
+  useEffect(() => {
+    setLocalLoading(false)
+  }, [isValidating])
+
   return (
     <Grid container alignItems="center" gap={1}>
       <Grid item flexGrow={1}>
@@ -72,7 +77,7 @@ const AddressInput = ({ name, validate, required = true, deps, ...props }: Addre
               <InputAdornment position="end">{currentShortName}:</InputAdornment>
             ),
 
-            endAdornment: (resolving || isValidating) && (
+            endAdornment: (resolving || isValidating || localLoading) && (
               <InputAdornment position="end">
                 <CircularProgress size={20} />
               </InputAdornment>
@@ -89,6 +94,7 @@ const AddressInput = ({ name, validate, required = true, deps, ...props }: Addre
             required,
 
             setValueAs: (value: string): string => {
+              setLocalLoading(true)
               rawValueRef.current = value
               // This also checksums the address
               return parsePrefixedAddress(value).address
