89a3af4908b31ffb4e0aa24ae30fca8a4b10b098
Yago Pérez Vázquez <yago@safe.global>
2023-01-12 13:58:02 +0100

    Add better close icon

--- src/components/new-safe-apps/SafeAppPreviewDrawer/SafeAppPreviewDrawer.tsx
+++ src/components/new-safe-apps/SafeAppPreviewDrawer/SafeAppPreviewDrawer.tsx
@@ -4,14 +4,15 @@ import Drawer from '@mui/material/Drawer'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import CloseIcon from '@mui/icons-material/Close'
-import { Chip, IconButton, Stack } from '@mui/material'
+import { Chip, IconButton, Stack, SvgIcon } from '@mui/material'
+import Tooltip from '@mui/material/Tooltip'
 import type { SafeAppData } from '@safe-global/safe-gateway-typescript-sdk'
 
 import { getSafeAppUrl } from '@/components/new-safe-apps/SafeAppCard/SafeAppCard'
 import ChainIndicator from '@/components/common/ChainIndicator'
 import SafeAppIconCard from '../SafeAppIconCard/SafeAppIconCard'
 import SafeAppActionButtons from '../SafeAppActionButtons/SafeAppActionButtons'
+import CloseIcon from '@/public/images/common/close.svg'
 
 import css from './styles.module.css'
 
@@ -25,41 +26,41 @@ const SafeAppPreviewDrawer = ({ isOpen, safeApp, onClose }: SafeAppPreviewDrawer
   const router = useRouter()
   const safeAppUrl = getSafeAppUrl(router, safeApp?.url || '')
 
-  if (!safeApp) {
-    return null
-  }
-
   return (
     <Drawer anchor="right" open={isOpen} onClose={onClose}>
       <Box className={css.drawerContainer}>
         {/* Toolbar */}
 
-        <Box display="flex" justifyContent="right">
-          <SafeAppActionButtons safeApp={safeApp} isBookmarked={false} onBookmarkSafeApp={() => {}} />
-
-          <IconButton
-            aria-label="close"
-            onClick={onClose}
-            size="small"
-            sx={{
-              color: 'border.main',
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Box>
+        {safeApp && (
+          <Box display="flex" justifyContent="right">
+            <SafeAppActionButtons safeApp={safeApp} isBookmarked={false} onBookmarkSafeApp={() => {}} />
+            <Tooltip title={`Close ${safeApp.name} info`} placement="top">
+              <IconButton
+                aria-label="close"
+                onClick={onClose}
+                size="small"
+                sx={{
+                  color: 'border.main',
+                  ml: 1,
+                }}
+              >
+                <SvgIcon component={CloseIcon} inheritViewBox color="border" fontSize="small" />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        )}
 
         {/* Safe App Info */}
         <Box sx={{ px: 1 }}>
-          <SafeAppIconCard src={safeApp.iconUrl || ''} alt={`${safeApp.name} logo`} width={90} height={90} />
+          <SafeAppIconCard src={safeApp?.iconUrl || ''} alt={`${safeApp?.name} logo`} width={90} height={90} />
         </Box>
 
         <Typography variant="h4" fontWeight={700} sx={{ mt: 2 }}>
-          {safeApp.name}
+          {safeApp?.name}
         </Typography>
 
         <Typography variant="body2" color="primary.light" sx={{ mt: 2 }}>
-          {safeApp.description}
+          {safeApp?.description}
         </Typography>
 
         {/* Tags */}
@@ -77,14 +78,14 @@ const SafeAppPreviewDrawer = ({ isOpen, safeApp, onClose }: SafeAppPreviewDrawer
         </Typography>
 
         <Box sx={{ display: 'flex', gap: 1, mt: 2, flexWrap: 'wrap' }}>
-          {safeApp.chainIds.map((chainId) => (
+          {safeApp?.chainIds.map((chainId) => (
             <ChainIndicator key={chainId} chainId={chainId} inline renderWhiteSpaceIfNoChain={false} />
           ))}
         </Box>
 
         {/* Open Safe App button */}
         <Link href={safeAppUrl} passHref>
-          <Button fullWidth variant="contained" color="primary" component={'a'} href={safeApp.url} sx={{ mt: 3 }}>
+          <Button fullWidth variant="contained" color="primary" component={'a'} href={safeApp?.url} sx={{ mt: 3 }}>
             Open App
           </Button>
         </Link>
