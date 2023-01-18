76d9b59d830616aba50700b7dec78e2a35ed28ff
Usame Algan <usame@safe.global>
2023-01-18 13:57:19 +0100

    fix: Adjust alert icon according to design

--- src/components/settings/EnvironmentVariables/EnvHintButton/index.tsx
+++ src/components/settings/EnvironmentVariables/EnvHintButton/index.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { SvgIcon, IconButton, Tooltip } from '@mui/material'
-import WarningAmberIcon from '@mui/icons-material/WarningAmber'
 import { AppRoutes } from '@/config/routes'
 import { useAppSelector } from '@/store'
 import { isEnvInitialState } from '@/store/settingsSlice'
+import css from './styles.module.css'
+import AlertIcon from '@/public/images/common/alert.svg'
 
 const EnvHintButton = () => {
   const router = useRouter()
@@ -17,8 +18,14 @@ const EnvHintButton = () => {
   return (
     <Link href={{ pathname: AppRoutes.settings.environmentVariables, query: router.query }} passHref>
       <Tooltip title="Default environment has been changed" placement="top" arrow>
-        <IconButton size="small" color="warning" sx={{ justifySelf: 'flex-end', marginLeft: 'auto' }} disableRipple>
-          <SvgIcon component={WarningAmberIcon} inheritViewBox />
+        <IconButton
+          className={css.button}
+          size="small"
+          color="warning"
+          sx={{ justifySelf: 'flex-end', marginLeft: { sm: '0', md: 'auto' } }}
+          disableRipple
+        >
+          <SvgIcon component={AlertIcon} inheritViewBox fontSize="small" />
         </IconButton>
       </Tooltip>
     </Link>
