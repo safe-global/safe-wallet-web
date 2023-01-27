17ee4f99a70703c9d28e10163bf8ae681af94918
sky cherfa <sherifcherfa@gmail.com>
2023-01-27 13:29:47 -0500

    add tx history to chat

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import useTxHistory from '@/hooks/useTxHistory'
 import useWallet from '@/hooks/wallets/useWallet'
+import TxListItem from '../transactions/TxListItem'
 import {
   getMessages,
   initCometChat,
@@ -17,11 +18,10 @@ const Chat = ({ user }) => {
   const { safe, safeAddress } = useSafeInfo()
   const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([])
+  const [chatData, setChatData] = useState<any[]>([])
   const [group, setGroup] = useState<any>()
   const wallet = useWallet()
   const txHistory = useTxHistory()
-
-  console.log(safe.owners, txHistory)
 
   const handleSubmit = async (e: any) => {
     e.preventDefault()
@@ -60,6 +60,41 @@ const Chat = ({ user }) => {
     }
     getM()
   }, [group, wallet, user])
+
+  useEffect(() => {
+    if (messages.length == 0) {
+      return
+    }
+    let allData: any[] = []
+    messages.forEach((message: any) => {
+      allData.push({
+        data: message,
+        timestamp: +message.sentAt * 1000,
+        type: 'message',
+      })
+    })
+    txHistory.page?.results.forEach((tx: any) => {
+      if (tx.type === 'DATE_LABEL') {
+        return
+      }
+      allData.push({
+        data: tx,
+        timestamp: tx.transaction.timestamp,
+        type: 'tx',
+      })
+    })
+    allData.sort(function (a, b) {
+      if (a['timestamp'] > b['timestamp']) {
+        return 1
+      } else if (a['timestamp'] < b['timestamp']) {
+        return -1
+      } else {
+        return 0
+      }
+    })
+    console.log(allData)
+    setChatData(allData)
+  }, [messages])
 
   const scrollToEnd = () => {
     const elmnt = document.getElementById('messages-container')
@@ -128,17 +163,20 @@ const Chat = ({ user }) => {
       <h4>Join the Live Chat</h4>
       <div>
         <div id="messages-container">
-          {messages.map((msg: any, i) => (
-            //@ts-ignore
-            <Message
-              isOwner={msg.sender.name === wallet?.address}
-              owner={msg.sender.uid}
-              msg={msg.text}
-              key={i}
-              data={msg}
-              timeStamp={msg.sentAt}
-            />
-          ))}
+          {chatData.map((item: any, i) =>
+            item.type === 'message' ? (
+              <Message
+                isOwner={item.data.sender.name === wallet?.address}
+                owner={item.data.sender.uid}
+                msg={item.data.text}
+                key={i}
+                data={item.data}
+                timeStamp={item.timeStamp}
+              />
+            ) : (
+              <TxListItem key={i} item={item.data} />
+            ),
+          )}
         </div>
 
         <form onSubmit={handleSubmit}>
