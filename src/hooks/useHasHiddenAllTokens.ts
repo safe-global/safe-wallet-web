a4a4eaa6811cdbfd735cf97e7dfa0b93a00f8465
iamacook <aaron@safe.global>
2023-01-30 10:04:04 +0100

    fix: higher no. of hidden tokens than current

--- src/hooks/useHasHiddenAllTokens.ts
+++ src/hooks/useHasHiddenAllTokens.ts
@@ -3,12 +3,12 @@ import { useVisibleBalances } from './useVisibleBalances'
 import useHiddenTokens from './useHiddenTokens'
 
 const useHasHiddenAllTokens = (): boolean => {
-  const { balances } = useVisibleBalances()
+  const { balances, loading } = useVisibleBalances()
   const hiddenTokens = useHiddenTokens()
 
   return useMemo(() => {
-    return balances.items.length > 0 && hiddenTokens.length > 0 && balances.items.length === hiddenTokens.length
-  }, [balances.items, hiddenTokens])
+    return !loading && hiddenTokens.length > 0 && hiddenTokens.length >= balances.items.length
+  }, [loading, balances.items, hiddenTokens])
 }
 
 export default useHasHiddenAllTokens
