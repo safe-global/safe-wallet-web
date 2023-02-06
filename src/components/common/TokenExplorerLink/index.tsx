1b4f92c908b732bbe95d289b51ef74cabfe8f41d
Yago Pérez Vázquez <yago@safe.global>
2023-02-06 12:25:26 +0100

    feat: Integrate safe react components (#1602)

--- src/components/common/TokenExplorerLink/index.tsx
+++ src/components/common/TokenExplorerLink/index.tsx
@@ -1,7 +1,7 @@
 import { type ReactElement } from 'react'
-import { IconButton, Tooltip, SvgIcon } from '@mui/material'
+import { ExplorerButton } from '@safe-global/safe-react-components'
+
 import { useCurrentChain } from '@/hooks/useChains'
-import LinkIcon from '@/public/images/common/link.svg'
 import { getBlockExplorerLink } from '@/utils/chains'
 
 const ExplorerLink = ({ address }: { address: string }): ReactElement | null => {
@@ -10,23 +10,7 @@ const ExplorerLink = ({ address }: { address: string }): ReactElement | null => 
 
   if (!link) return null
 
-  return (
-    <Tooltip title={link.title} placement="top">
-      <IconButton href={link.href} target="_blank" rel="noopener noreferrer" size="small">
-        <SvgIcon
-          component={LinkIcon}
-          inheritViewBox
-          color="primary"
-          sx={{
-            '& path': {
-              fill: ({ palette }) => palette.border.main,
-            },
-          }}
-          fontSize="small"
-        />
-      </IconButton>
-    </Tooltip>
-  )
+  return <ExplorerButton href={link.href} title={link.title} />
 }
 
 export default ExplorerLink
