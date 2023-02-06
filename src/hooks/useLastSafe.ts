d7b17135074143ec14092ef5538cbd0f08877950
Aaron Cook <aaron@safe.global>
2023-02-06 16:25:58 +0100

    refactor: prefix/chain id getters from one another (#1619)
    * fix: shown unknown when not found in config
    
    * fix: creation widget
    
    * fix: prefix displaying for unknown networks
    
    * fix: find prefix from chain config
    
    * fix: account center prefix
    
    * fix: remove unknown prefixes
    
    * Revert "fix: remove unknown prefixes"
    
    This reverts commit 39c5903c9df6ec62231fd1d12fe4c74fc6594507.
    
    * fix: move function to component
    
    * fix: remove function
    
    * refactor: prefix/chain id getters from one another
    
    * refactor: improve helpers

--- src/hooks/useLastSafe.ts
+++ src/hooks/useLastSafe.ts
@@ -1,12 +1,12 @@
 import useChainId from '@/hooks/useChainId'
 import { useAppSelector } from '@/store'
 import { selectLastSafeAddress } from '@/store/sessionSlice'
-import chains from '@/config/chains'
+import { getShortName } from '@/utils/chains'
 
 const useLastSafe = (): string | undefined => {
   const chainId = useChainId()
   const lastSafeAddress = useAppSelector((state) => selectLastSafeAddress(state, chainId))
-  const prefix = Object.keys(chains).find((prefix) => chains[prefix] === chainId)
+  const prefix = getShortName(chainId)
   return prefix && lastSafeAddress ? `${prefix}:${lastSafeAddress}` : undefined
 }
 
