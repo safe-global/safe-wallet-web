ed1823b0e3519075afb03112b3b0615aa8c4a7d8
schmanu <manu@safe.global>
2023-02-09 15:52:53 +0100

    feat: add onboarding tooltip for tokenlist toggle
    - link to actual help article

--- src/components/balances/TokenListSelect/index.tsx
+++ src/components/balances/TokenListSelect/index.tsx
@@ -6,6 +6,9 @@ import type { SelectChangeEvent } from '@mui/material'
 import { Box, SvgIcon, Tooltip, Typography, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 import InfoIcon from '@/public/images/notifications/info.svg'
 import ExternalLink from '@/components/common/ExternalLink'
+import { OnboardingTooltip } from '@/components/common/OnboardingTooltip'
+
+const LS_TOKENLIST_ONBOARDING = 'tokenlist_onboarding'
 
 const TokenListLabel = {
   [TOKEN_LISTS.TRUSTED]: 'Default tokens',
@@ -30,33 +33,41 @@ const TokenListSelect = () => {
   return (
     <FormControl size="small">
       <InputLabel id="tokenlist-select-label">Filter by</InputLabel>
-      <Select
-        labelId="tokenlist-select-label"
-        id="tokenlist-select"
-        value={settings.tokenList}
-        label="Tokenlist"
-        onChange={handleSelectTokenList}
-        renderValue={(value) => TokenListLabel[value]}
+      <OnboardingTooltip
+        widgetLocalStorageId={LS_TOKENLIST_ONBOARDING}
+        text="Missing tokens? Switch to “All tokens” to see more than the default tokens here."
       >
-        <MenuItem value={TOKEN_LISTS.TRUSTED}>
-          <Box display="flex" flexDirection="row" gap="4px" alignItems="center">
-            {TokenListLabel.TRUSTED}
-            <Tooltip
-              arrow
-              title={
-                <Typography>
-                  Learn more about <ExternalLink href="#">default tokens</ExternalLink>
-                </Typography>
-              }
-            >
-              <span>
-                <SvgIcon sx={{ display: 'block' }} color="border" fontSize="small" component={InfoIcon}></SvgIcon>
-              </span>
-            </Tooltip>
-          </Box>
-        </MenuItem>
-        <MenuItem value={TOKEN_LISTS.ALL}>{TokenListLabel.ALL}</MenuItem>
-      </Select>
+        <Select
+          labelId="tokenlist-select-label"
+          id="tokenlist-select"
+          value={settings.tokenList}
+          label="Tokenlist"
+          onChange={handleSelectTokenList}
+          renderValue={(value) => TokenListLabel[value]}
+        >
+          <MenuItem value={TOKEN_LISTS.TRUSTED}>
+            <Box display="flex" flexDirection="row" gap="4px" alignItems="center">
+              {TokenListLabel.TRUSTED}
+              <Tooltip
+                arrow
+                title={
+                  <Typography>
+                    Learn more about{' '}
+                    <ExternalLink href="https://help.safe.global/en/articles/6951406-default-token-list-local-hiding-of-spam-tokens">
+                      default tokens
+                    </ExternalLink>
+                  </Typography>
+                }
+              >
+                <span>
+                  <SvgIcon sx={{ display: 'block' }} color="border" fontSize="small" component={InfoIcon}></SvgIcon>
+                </span>
+              </Tooltip>
+            </Box>
+          </MenuItem>
+          <MenuItem value={TOKEN_LISTS.ALL}>{TokenListLabel.ALL}</MenuItem>
+        </Select>
+      </OnboardingTooltip>
     </FormControl>
   )
 }
