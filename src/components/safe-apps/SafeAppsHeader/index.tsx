b19ae281c30e4eb1a992d1ab4c5f58efdf255f57
iamacook <aaron@safe.global>
2023-02-16 12:50:38 +0100

    fix: change Safe Apps text

--- src/components/safe-apps/SafeAppsHeader/index.tsx
+++ src/components/safe-apps/SafeAppsHeader/index.tsx
@@ -12,13 +12,12 @@ const SafeAppsHeader = (): ReactElement => {
       <Box className={css.container}>
         {/* Safe Apps Title */}
         <Typography className={css.title} variant="h3">
-          Explore the Safe Ecosystem
+          Explore the Safe Apps ecosystem
         </Typography>
 
         {/* Safe Apps Subtitle */}
         <Typography className={css.subtitle}>
-          Connect to your favorite web3 applications with your Safe smart contract account. Safer and more efficient,
-          right from the interface.
+          Connect to your favourite web3 applications with your Safe wallet, securely and efficiently.
         </Typography>
       </Box>
 
