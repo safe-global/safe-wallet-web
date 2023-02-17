124a54bdcff3e0bbfd63e8d6460426d317cbcded
katspaugh <katspaugh@gmail.com>
2023-02-17 12:53:46 +0100

    Take chainId into account

--- src/hooks/useHasPendingTxs.ts
+++ src/hooks/useHasPendingTxs.ts
@@ -1,9 +1,15 @@
 import { useAppSelector } from '@/store'
 import { selectPendingTxs } from '@/store/pendingTxsSlice'
+import { useMemo } from 'react'
+import useChainId from './useChainId'
 
 const useHasPendingTxs = () => {
+  const chainId = useChainId()
   const pendingTxs = useAppSelector(selectPendingTxs)
-  return Object.keys(pendingTxs).length > 0
+
+  return useMemo(() => {
+    return Object.values(pendingTxs).some((tx) => tx.chainId === chainId)
+  }, [chainId, pendingTxs])
 }
 
 export default useHasPendingTxs
