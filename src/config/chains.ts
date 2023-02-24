710d9b190e8b5b46d15602b2e9ea32837aec1988
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-02-24 11:39:21 +0100

    Update src/config/chains.ts
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -12,9 +12,9 @@ import { networks } from '@safe-global/safe-core-sdk-utils/dist/src/eip-3770/con
  */
 type Chains = Record<string, string>
 
-const chains: Chains = networks.reduce((result, { shortName, chainId }) => {
+const chains = networks.reduce<Chains>((result, { shortName, chainId }) => {
   result[shortName] = chainId.toString()
   return result
-}, {} as Chains)
+}, {})
 
 export default chains
