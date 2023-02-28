a1614c7dbc8332b4bde83409119b0860a515b1fe
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-02-28 16:29:16 +0100

    Chore: remove an unnecessary deployment step (#1710)
    * Chore: remove unnecessary deployment step
    
    * Replace "ERROR" with an emoji to prevent automatic highlight in red

--- scripts/github/s3_upload.sh
+++ scripts/github/s3_upload.sh
@@ -8,14 +8,10 @@ fi
 
 cd out
 
-# First, upload the new files w/o deleting the old ones
-aws s3 sync . $BUCKET
-
-# Second, upload them again but delete the old files this time
-# This allows for a no-downtime deployment
+# Upload the build to S3
 aws s3 sync . $BUCKET --delete
 
-# Finally, upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
+# Upload all HTML files again but w/o an extention so that URLs like /welcome open the right page
 for file in $(find . -name '*.html' | sed 's|^\./||'); do
     aws s3 cp ${file%} $BUCKET/${file%.*} --content-type 'text/html'
 done
