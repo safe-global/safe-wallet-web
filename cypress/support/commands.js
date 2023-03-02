1002afb7674c89c7142e17121a1020d894fef126
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-03-02 10:00:54 +0100

    test: add pending actions E2E (#1688)
    * test: pending actions
    
    * change the connected wallet label
    
    * add missing tests steps
    
    * return MM icon instead of faking MetaMask connection
    
    * tests the queued txs and the signable txs
    
    * manually set the localStorage
    
    * preserve localStorage between tests
    
    * Add comments
    
    * Update queued txs size assertion
    
    Co-authored-by: Aaron Cook <aaron@safe.global>
    
    ---------
    
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,3 +4,17 @@ Cypress.Commands.add('connectE2EWallet', () => {
     window.localStorage.setItem('SAFE_v2__lastWallet', JSON.stringify('E2E Wallet'))
   })
 })
+
+let LOCAL_STORAGE_MEMORY = {}
+
+Cypress.Commands.add('saveLocalStorageCache', () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key]
+  })
+})
+
+Cypress.Commands.add('restoreLocalStorageCache', () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key])
+  })
+})
