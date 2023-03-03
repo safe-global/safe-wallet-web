025a91939acfc297d773ddeabeb26af2e996cb43
Diogo Soares <diogo.lsoares@gmail.com>
2023-03-03 11:40:03 +0100

    refactor hook

--- src/hooks/useRemainingRelays.ts
+++ src/hooks/useRemainingRelays.ts
@@ -6,23 +6,23 @@ import { Errors, logError } from '@/services/exceptions'
 // TODO: replace once the relay-service is deployed
 const RELAY_LIMIT_BASE_URL = 'http://localhost:3001/v1/relay'
 
+const fetchRemainingRelays = async (chainId: string, safeAddress: string): Promise<number | undefined> => {
+  const url = `${RELAY_LIMIT_BASE_URL}/${chainId}/${safeAddress}`
+
+  try {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data.remaining
+  } catch (error) {
+    logError(Errors._630, (error as Error).message)
+  }
+}
+
 const useRemainingRelays = () => {
   const chainId = useChainId()
   const safeAddress = useSafeAddress()
 
-  const url = `${RELAY_LIMIT_BASE_URL}/${chainId}/${safeAddress}`
-
-  const fetchRemainingRelays = async (): Promise<number | undefined> => {
-    try {
-      const res = await fetch(url)
-      const data = await res.json()
-      return data.remaining
-    } catch (error) {
-      logError(Errors._630, (error as Error).message)
-    }
-  }
-
-  return useAsync(fetchRemainingRelays, [chainId, safeAddress])
+  return useAsync(() => fetchRemainingRelays(chainId, safeAddress), [chainId, safeAddress])
 }
 
 export default useRemainingRelays
