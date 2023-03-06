eb28e00ffaa30cac12374fd8f5bbd24a76e89e2b
iamacook <aaron@safe.global>
2023-03-06 11:05:44 +0100

    fix: tests

--- src/hooks/coreSDK/__tests__/safeCoreSDK.test.ts
+++ src/hooks/coreSDK/__tests__/safeCoreSDK.test.ts
@@ -1,29 +1,16 @@
-import { Web3Provider } from '@ethersproject/providers'
 import {
   getProxyFactoryContract,
   getSafeContract,
 } from '@safe-global/safe-core-sdk/dist/src/contracts/safeDeploymentContracts'
 import type { SafeInfo } from '@safe-global/safe-gateway-typescript-sdk'
 import type { EIP1193Provider } from '@web3-onboard/core'
+import Safe from '@safe-global/safe-core-sdk'
 
-import * as coreSdk from '../safeCoreSDK'
+import { isValidSafeVersion, initSafeSDK } from '../safeCoreSDK'
 import { hexZeroPad } from 'ethers/lib/utils'
+import { ethers } from 'ethers'
 
-jest.mock('../safeCoreSDK', () => {
-  return {
-    __esModule: true,
-    ...jest.requireActual('../safeCoreSDK'),
-  }
-})
-
-jest.mock('@safe-global/safe-core-sdk/dist/src/contracts/safeDeploymentContracts', () => {
-  return {
-    __esModule: true,
-    ...jest.requireActual('@safe-global/safe-core-sdk/dist/src/contracts/safeDeploymentContracts'),
-    getProxyFactoryContract: jest.fn(),
-    getSafeContract: jest.fn(),
-  }
-})
+jest.mock('@safe-global/safe-core-sdk/dist/src/contracts/safeDeploymentContracts')
 
 jest.mock('@/types/contracts', () => {
   return {
@@ -35,32 +22,32 @@ jest.mock('@/types/contracts', () => {
 describe('safeCoreSDK', () => {
   describe('isValidSafeVersion', () => {
     it('should return true for valid versions', () => {
-      expect(coreSdk.isValidSafeVersion('1.3.0')).toBe(true)
+      expect(isValidSafeVersion('1.3.0')).toBe(true)
 
-      expect(coreSdk.isValidSafeVersion('1.2.0')).toBe(true)
+      expect(isValidSafeVersion('1.2.0')).toBe(true)
 
-      expect(coreSdk.isValidSafeVersion('1.1.1')).toBe(true)
+      expect(isValidSafeVersion('1.1.1')).toBe(true)
 
-      expect(coreSdk.isValidSafeVersion('1.3.0+L2')).toBe(true)
+      expect(isValidSafeVersion('1.3.0+L2')).toBe(true)
     })
     it('should return false for invalid versions', () => {
-      expect(coreSdk.isValidSafeVersion('1.3.1')).toBe(false)
+      expect(isValidSafeVersion('1.3.1')).toBe(false)
 
-      expect(coreSdk.isValidSafeVersion('1.4.0')).toBe(false)
+      expect(isValidSafeVersion('1.4.0')).toBe(false)
 
-      expect(coreSdk.isValidSafeVersion('1.0.0')).toBe(true)
+      expect(isValidSafeVersion('1.0.0')).toBe(true)
 
-      expect(coreSdk.isValidSafeVersion('0.0.1')).toBe(false)
+      expect(isValidSafeVersion('0.0.1')).toBe(false)
 
-      expect(coreSdk.isValidSafeVersion('')).toBe(false)
+      expect(isValidSafeVersion('')).toBe(false)
 
-      expect(coreSdk.isValidSafeVersion()).toBe(false)
+      expect(isValidSafeVersion()).toBe(false)
     })
   })
 
   describe('initSafeSDK', () => {
     const MAINNET_MASTER_COPY = '0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552' // L1
-    const GNOSIS_CHAIN_MASTER_COPY = '0x3E5c63644E683549055b9Be8653de26E0B4CD36E' // L2
+    const POLYGON_MASTER_COPY = '0x3E5c63644E683549055b9Be8653de26E0B4CD36E' // L2
 
     ;(getProxyFactoryContract as jest.Mock).mockImplementation(async () => {
       return await Promise.resolve({
@@ -92,107 +79,143 @@ describe('safeCoreSDK', () => {
       })
     })
 
-    it('should return an SDK instance', async () => {
-      const chainId = '1'
+    const getMockProvider = (chainId: string, version: string) => {
+      return jest.fn((method, params) => {
+        const VERSION_SIG_HASH = ethers.utils.id('VERSION()').slice(0, 10)
 
-      const provider = new Web3Provider(
-        jest.fn((method, params) => {
-          if (method === 'eth_chainId') {
-            return Promise.resolve({ chainId: +chainId })
-          }
+        if (method === 'eth_chainId') {
+          return Promise.resolve(+chainId)
+        }
 
-          return Promise.resolve()
-        }),
-      )
+        if (method === 'eth_call' && params[0].data.startsWith(VERSION_SIG_HASH)) {
+          const encodedVersion = ethers.utils.defaultAbiCoder.encode(['string'], [version])
 
-      const sdk = await coreSdk.initSafeSDK(
-        provider as unknown as EIP1193Provider,
-        {
+          return Promise.resolve(encodedVersion)
+        }
+
+        return Promise.resolve()
+      }) as unknown as EIP1193Provider
+    }
+
+    describe('Supported contracts', () => {
+      it('should return an SDK instance', async () => {
+        const chainId = '1'
+        const version = '1.3.0'
+
+        const mockProvider = getMockProvider(chainId, version)
+
+        const sdk = await initSafeSDK(mockProvider, {
           chainId,
           address: { value: hexZeroPad('0x1', 20) },
-          version: '1.3.0',
+          version,
           implementation: { value: MAINNET_MASTER_COPY },
-        } as SafeInfo,
-      )
+        } as SafeInfo)
 
-      expect(sdk).toBeDefined()
+        expect(sdk).toBeInstanceOf(Safe)
+      })
+
+      it('should return an L1 SDK instance for mainnet', async () => {
+        const chainId = '1'
+        const version = '1.3.0'
+
+        const mockProvider = getMockProvider(chainId, version)
+
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId,
+          address: { value: hexZeroPad('0x1', 20) },
+          version,
+          implementation: { value: MAINNET_MASTER_COPY },
+        } as SafeInfo)
+
+        expect(sdk).toBeInstanceOf(Safe)
+        expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
+      })
+
+      it('should return an L2 SDK instance for L2 chain', async () => {
+        const chainId = '137' // Polygon
+        const version = '1.3.0'
+
+        const mockProvider = getMockProvider(chainId, version)
+
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId,
+          address: { value: hexZeroPad('0x1', 20) },
+          version: `${version}+L2`,
+          implementation: { value: POLYGON_MASTER_COPY },
+        } as SafeInfo)
+
+        expect(sdk).toBeInstanceOf(Safe)
+        expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(false)
+      })
+
+      it('should return an L1 SDK instance for legacy Safes, regardless of chain', async () => {
+        const chainId = '137' // Polygon
+        const version = '1.0.0'
+
+        const mockProvider = getMockProvider(chainId, version)
+
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId,
+          address: { value: hexZeroPad('0x1', 20) },
+          version,
+          implementation: { value: POLYGON_MASTER_COPY },
+        } as SafeInfo)
+
+        expect(sdk).toBeInstanceOf(Safe)
+        expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
+      })
     })
 
-    // it('should retrieve the Safe version from the contract if not provided', async () => {
-    //   const versionSpy = jest.spyOn(types, 'Gnosis_safe__factory').mockImplementation(() => ({
-    //     connect: () => ({
-    //       VERSION: () => '1.3.0',
-    //     }),
-    //   }))
+    describe('Unsupported contracts', () => {
+      // Note: backend returns a null version for unsupported contracts
+      it('should retrieve the Safe version from the contract if not provided', async () => {
+        const chainId = '1'
+        const version = '1.3.0'
 
-    //   const sdk = await initSafeSDK(mockProvider, {
-    //     chainId: '1',
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: null,
-    //     implementation: { value: MAINNET_MASTER_COPY },
-    //   } as SafeInfo)
+        const mockProvider = getMockProvider(chainId, version)
 
-    //   expect(sdk).toBeDefined()
-    //   expect(versionSpy).toHaveBeenCalledTimes(1)
-    // })
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId: '1',
+          address: { value: hexZeroPad('0x1', 20) },
+          version: null, // Indexer returns null if unsupported contract version
+          implementation: { value: MAINNET_MASTER_COPY },
+        } as SafeInfo)
 
-    // it('should return an L1 SDK instance for mainnet', async () => {
-    //   const sdk = await initSafeSDK(mockProvider, {
-    //     chainId: '1',
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: '1.3.0',
-    //     implementation: { value: MAINNET_MASTER_COPY },
-    //   } as SafeInfo)
+        expect(sdk).toBeInstanceOf(Safe)
+      })
 
-    //   expect(sdk).toBeDefined()
-    //   expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
-    // })
+      it('should return an L1 SDK instance for L1 contracts not deployed on mainnet', async () => {
+        const chainId = '137' // Polygon
+        const version = '1.3.0'
 
-    // it('should return an L2 SDK instance for L2 chain', async () => {
-    //   const sdk = await initSafeSDK(mockProvider, {
-    //     chainId: '100', // Gnosis Chain
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: '1.3.0',
-    //     implementation: { value: GNOSIS_CHAIN_MASTER_COPY },
-    //   } as SafeInfo)
+        const mockProvider = getMockProvider(chainId, version)
 
-    //   expect(sdk).toBeDefined()
-    //   expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(false)
-    // })
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId,
+          address: { value: hexZeroPad('0x1', 20) },
+          version: null,
+          implementation: { value: MAINNET_MASTER_COPY },
+        } as SafeInfo)
 
-    // it('should return an L1 SDK instance for legacy Safes, regardless of chain', async () => {
-    //   const sdk = await initSafeSDK(mockProvider, {
-    //     chainId: '100', // Gnosis Chain
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: '1.0.0',
-    //     implementation: { value: GNOSIS_CHAIN_MASTER_COPY },
-    //   } as SafeInfo)
+        expect(sdk).toBeInstanceOf(Safe)
+        expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
+      })
 
-    //   expect(sdk).toBeDefined()
-    //   expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
-    // })
+      it('should return undefined for unsupported mastercopies', async () => {
+        const chainId = '1'
+        const version = '1.3.0'
 
-    // it('should return an L1 SDK instance for L1 contracts not deployed on mainnet', async () => {
-    //   const sdk = await initSafeSDK(mockProvider, {
-    //     chainId: '100',
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: '1.3.0',
-    //     implementation: { value: MAINNET_MASTER_COPY },
-    //   } as SafeInfo)
+        const mockProvider = getMockProvider(chainId, version)
 
-    //   expect(sdk).toBeDefined()
-    //   expect(sdk?.getContractManager().isL1SafeMasterCopy).toBe(true)
-    // })
+        const sdk = await initSafeSDK(mockProvider, {
+          chainId,
+          address: { value: hexZeroPad('0x1', 20) },
+          version: null,
+          implementation: { value: '0xinvalid' },
+        } as SafeInfo)
 
-    // it('should return undefined for unsupported mastercopies', async () => {
-    //   const safe = await initSafeSDK(mockProvider, {
-    //     chainId: '1',
-    //     address: { value: hexZeroPad('0x1', 20) },
-    //     version: '1.3.0',
-    //     implementation: { value: '0xinvalid' },
-    //   } as SafeInfo)
-
-    //   expect(safe).toBeUndefined()
-    // })
+        expect(sdk).toBeUndefined()
+      })
+    })
   })
 })
