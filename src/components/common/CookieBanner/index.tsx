d5759a3507440b5cfc28336d6098ce617d945778
Aaron Cook <aaron@safe.global>
2023-04-19 14:51:41 +0200

    Update src/components/common/CookieBanner/index.tsx
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- src/components/common/CookieBanner/index.tsx
+++ src/components/common/CookieBanner/index.tsx
@@ -113,7 +113,7 @@ const CookieBannerPopup = ({ warningKey }: { warningKey?: CookieType }): ReactEl
                   />
                   <br />
                   <Typography variant="body2" color="background.paper">
-                    Help us make the app better. We never track your Safe Account or wallet addresses, or any
+                    Help us make the app better. We never track your Safe Account address or wallet addresses, or any
                     transaction data.
                   </Typography>
                 </Box>
