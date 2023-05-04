592863bb71dcbc2bba980cedf1039b2e89b60f8b
katspaugh <katspaugh@gmail.com>
2023-05-04 14:58:47 +0200

    Blue background, no button

--- src/components/tx/WrongChainWarning/index.tsx
+++ src/components/tx/WrongChainWarning/index.tsx
@@ -1,12 +1,8 @@
 import type { ReactElement } from 'react'
-import { Box } from '@mui/material'
-
+import { Typography } from '@mui/material'
 import { useCurrentChain } from '@/hooks/useChains'
 import useIsWrongChain from '@/hooks/useIsWrongChain'
 import ErrorMessage from '@/components/tx/ErrorMessage'
-
-import css from './styles.module.css'
-import ChainSwitcher from '@/components/common/ChainSwitcher'
 
 export const WrongChainWarning = (): ReactElement | null => {
   const chain = useCurrentChain()
@@ -17,12 +13,9 @@ export const WrongChainWarning = (): ReactElement | null => {
   }
 
   return (
-    <ErrorMessage className={css.container}>
-      Your wallet is connected to the wrong chain. When you submit, you will first be asked to connect to{' '}
-      {chain.chainName}.
-      <Box mt={1.5} textAlign="center">
-        <ChainSwitcher />
-      </Box>
+    <ErrorMessage level="info">
+      <Typography fontWeight="bold">Wallet network switch</Typography>
+      When you submit the transaction, you will be asked to switch your wallet network to {chain.chainName}.
     </ErrorMessage>
   )
 }
