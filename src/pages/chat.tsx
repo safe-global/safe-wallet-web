75e396081cd0b3c3d08e04e22b6f2bd125447f6c
sky cherfa <sherifcherfa@gmail.com>
2023-05-05 10:33:32 -0400

    make smaller component

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -52,6 +52,7 @@ import React, { useEffect, useState, useRef, useCallback } from 'react'
 import ChatNotifications from '@/components/chat/chatNotifications'
 import NewTxButton from '@/components/chat/NewTxButton'
 import ConnectionCenter from '@/components/common/ConnectWallet/ConnectionCenter'
+import { DesktopChat } from '@/components/chat/desktopChat'
 
 const SendMessage = dynamic(() => import('@/components/chat/sendMessage'), { ssr: false })
 
@@ -614,127 +615,14 @@ const Chat = () => {
               </TabPanel>
             </Box>
           </Hidden>
-          <Hidden mdDown>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box sx={{ height: '80vh', overflowY: 'auto' }}>
-                <Box
-                  sx={{
-                    flex: '1 0 auto',
-                    display: 'flex',
-                    height: '100%',
-                    flexDirection: 'column',
-                    justifyContent: 'start',
-                    alignItems: 'start',
-                    gap: '16px',
-                    p: 3,
-                    bgcolor: 'background.default',
-                  }}
-                >
-                  <StyledAlert icon={false}>
-                    <Typography paragraph>This is the beginning of the timeline from this Safe</Typography>
-                    <Typography paragraph>
-                      The timeline shows all your chat, transactions and events in one place. Only members of this group
-                      can see the chat. Say hi!
-                    </Typography>
-                    <Typography sx={{ fontStyle: 'italic', fontSize: '12px' }} paragraph>
-                      Safe created on 5 March 2023 at 19:34:53 CET
-                    </Typography>
-                  </StyledAlert>
-                  <Typography sx={{ fontWeight: 500 }}>Thursday, 9 March 2023</Typography>
-                  <List>
-                    {chatData &&
-                      chatData.map((chat, index) => {
-                        if (chat.type === 'message' && chat?.data?.sender) {
-                          return (
-                            <ListItem
-                              sx={{ display: 'flex', alignItems: 'center' }}
-                              key={index}
-                              alignItems="flex-start"
-                              disableGutters
-                            >
-                              <ListItemAvatar sx={{ minWidth: 35, pr: '10px' }}>
-                                <Avatar sx={{ width: 32, height: 32 }} alt={chat?.data?.sender.uid || ''} />
-                              </ListItemAvatar>
-                              <ListItemText
-                                sx={{ bgcolor: 'background.paper', p: 2, width: '100%' }}
-                                style={{ borderRadius: '8px' }}
-                                primary={
-                                  <React.Fragment>
-                                    <Typography
-                                      sx={{ display: 'inline', pr: '12px', fontWeight: 600 }}
-                                      component="span"
-                                      variant="subtitle2"
-                                    >
-                                      {chat.data.sender.name === wallet?.address ? 'You' : chat?.data?.sender.uid}
-                                    </Typography>
-                                    <Typography sx={{ display: 'inline' }} component="span" variant="body2">
-                                      {chat.timeStamp}
-                                    </Typography>
-                                  </React.Fragment>
-                                }
-                                secondary={chat.data.text}
-                              />
-                            </ListItem>
-                          )
-                        } else {
-                          return (
-                            <ListItem key={index} alignItems="flex-start" disableGutters>
-                              <ListItemAvatar sx={{ minWidth: 35, pr: '10px' }}>
-                                <Avatar sx={{ width: 32, height: 32 }} alt={chat.name} />
-                              </ListItemAvatar>
-                              <TxListItem key={`${index}-tx`} item={chat?.data} />
-                              <ListItemText
-                                primary={
-                                  <React.Fragment>
-                                    <Typography
-                                      sx={{ display: 'inline', pr: '8px', fontWeight: 600 }}
-                                      component="span"
-                                      variant="subtitle2"
-                                    >
-                                      {chat.name}
-                                    </Typography>
-                                    <Typography sx={{ display: 'inline' }} component="span" variant="body2">
-                                      {chat.timeAgo}
-                                    </Typography>
-                                  </React.Fragment>
-                                }
-                                secondary={chat.message}
-                              />
-                            </ListItem>
-                          )
-                        }
-                      })}
-                    <Box ref={bottom} sx={{ height: 0 }} />
-                    {!chatData ? <ListItem>No Chat</ListItem> : ''}
-                  </List>
-                </Box>
-              </Box>
-              <Box
-                sx={{
-                  height: '12vh',
-                  flexShrink: 0,
-                  bgcolor: 'background.default',
-                }}
-              >
-                <Divider />
-                <Box sx={{ width: '100%', display: 'flex', gap: '16px', p: 3 }}>
-                  <TextField
-                    sx={{ flexGrow: 1 }}
-                    label="Type Something"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                  />
-                  <SendMessage
-                    message={message}
-                    safeAddress={safeAddress}
-                    setMessages={setMessages}
-                    setMessage={setMessage}
-                    prevState={messages}
-                  />
-                </Box>
-              </Box>
-            </Box>
-          </Hidden>
+          <DesktopChat
+            message={message}
+            setMessage={setMessage}
+            messages={messages}
+            setMessages={setMessages}
+            bottom={bottom}
+            chatData={chatData}
+          />
         </Main>
         <Hidden mdDown>
           <Drawer
