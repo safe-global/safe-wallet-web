54abffc8630d7b3314a8387e29585cede7738bca
Diogo Soares <diogo.lsoares@gmail.com>
2023-05-08 17:09:35 +0200

    filter out the Messages tab when no EIP1271 messages are present

--- src/components/transactions/TxNavigation/index.tsx
+++ src/components/transactions/TxNavigation/index.tsx
@@ -1,15 +1,20 @@
 import NavTabs from '@/components/common/NavTabs'
 import { transactionNavItems } from '@/components/sidebar/SidebarNavigation/config'
 import { useCurrentChain } from '@/hooks/useChains'
+import useSafeMessages from '@/hooks/useSafeMessages'
 import { hasFeature, FEATURES } from '@/utils/chains'
 
 const TxNavigation = () => {
   const chain = useCurrentChain()
-  const isEIP1271 = chain && hasFeature(chain, FEATURES.EIP1271)
+  const { page } = useSafeMessages()
 
-  return (
-    <NavTabs tabs={isEIP1271 ? transactionNavItems : transactionNavItems.filter((item) => item.label !== 'Messages')} />
-  )
+  const isEIP1271 = chain && hasFeature(chain, FEATURES.EIP1271)
+  const hasMessages = page && page.results.length > 0
+  const showMessages = isEIP1271 && hasMessages
+
+  const navItems = showMessages ? transactionNavItems : transactionNavItems.filter((item) => item.label !== 'Messages')
+
+  return <NavTabs tabs={navItems} />
 }
 
 export default TxNavigation
