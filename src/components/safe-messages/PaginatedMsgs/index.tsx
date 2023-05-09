dc37f19872e31859f2d9b48e9e371067a6604b23
iamacook <aaron@safe.global>
2023-05-09 17:56:27 +0200

    fix: clear next pages when switching Safe

--- src/components/safe-messages/PaginatedMsgs/index.tsx
+++ src/components/safe-messages/PaginatedMsgs/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import { Typography, Link, SvgIcon } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { ReactElement } from 'react'
 
 import ErrorMessage from '@/components/tx/ErrorMessage'
@@ -12,6 +12,7 @@ import PagePlaceholder from '@/components/common/PagePlaceholder'
 import MsgList from '@/components/safe-messages/MsgList'
 import SkeletonTxList from '@/components/common/PaginatedTxns/SkeletonTxList'
 import { SIGNED_MESSAFES_HELP_LINK } from '@/components/transactions/SignedMessagesHelpLink'
+import useSafeInfo from '@/hooks/useSafeInfo'
 
 const NoMessages = (): ReactElement => {
   return (
@@ -62,6 +63,12 @@ const MsgPage = ({
 
 const PaginatedMsgs = (): ReactElement => {
   const [pages, setPages] = useState<string[]>([''])
+  const { safeAddress, safe } = useSafeInfo()
+
+  // Reset the pages when the Safe changes
+  useEffect(() => {
+    setPages([''])
+  }, [safeAddress, safe.chainId])
 
   // Trigger the next page load
   const onNextPage = (pageUrl: string) => {
@@ -69,7 +76,7 @@ const PaginatedMsgs = (): ReactElement => {
   }
 
   return (
-    <Box mb={4} position="relative">
+    <Box position="relative">
       {pages.map((pageUrl, index) => (
         <MsgPage key={pageUrl} pageUrl={pageUrl} onNextPage={index === pages.length - 1 ? onNextPage : undefined} />
       ))}
