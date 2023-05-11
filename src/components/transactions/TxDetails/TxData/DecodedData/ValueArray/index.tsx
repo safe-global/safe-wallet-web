dd573750baaab75d166c425245947f0b2a1877a6
Aaron Cook <aaron@safe.global>
2023-05-11 09:40:12 +0200

    fix: improve array decoding (#1965)
    * fix: improve array decoding
    
    * fix: update type + add test

--- src/components/transactions/TxDetails/TxData/DecodedData/ValueArray/index.tsx
+++ src/components/transactions/TxDetails/TxData/DecodedData/ValueArray/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import type { ReactElement } from 'react'
 import { Typography } from '@mui/material'
 import { isAddress, isArrayParameter } from '@/utils/transaction-guards'
@@ -12,13 +13,30 @@ type ValueArrayProps = {
   key?: string
 }
 
-export const Value = ({ type, ...props }: ValueArrayProps): ReactElement => {
-  if (isArrayParameter(type) && isAddress(type)) {
+// Sometime DApps return stringified arrays, e.g. "["hello","world"]"
+const parseValue = (value: ValueArrayProps['value']) => {
+  if (Array.isArray(value)) {
+    return value
+  }
+
+  try {
+    return JSON.parse(value)
+  } catch {
+    return value
+  }
+}
+
+export const Value = ({ type, value, ...props }: ValueArrayProps): ReactElement => {
+  const parsedValue = useMemo(() => {
+    return parseValue(value)
+  }, [value])
+
+  if (isArrayParameter(type) && isAddress(type) && Array.isArray(parsedValue)) {
     return (
       <Typography component="div" variant="body2">
         [
         <div className={css.nestedWrapper}>
-          {(props.value as string[]).map((address, index) => {
+          {parsedValue.map((address, index) => {
             const key = `${props.key || props.method}-${index}`
             if (Array.isArray(address)) {
               const newProps = {
@@ -45,34 +63,36 @@ export const Value = ({ type, ...props }: ValueArrayProps): ReactElement => {
     )
   }
 
-  return <GenericValue type={type} {...props} />
+  return <GenericValue value={parsedValue} {...props} />
 }
 
-const GenericValue = ({ method, type, value }: ValueArrayProps): React.ReactElement => {
-  const getTextValue = (value: string, key?: string) => <HexEncodedData limit={60} hexData={value} key={key} />
+const getTextValue = (value: string, key?: string) => {
+  return <HexEncodedData limit={60} hexData={value} key={key} />
+}
 
-  const getArrayValue = (parentId: string, value: string[] | string, separator?: boolean) => (
-    <Typography component="div" variant="body2">
-      [
-      <div className={css.nestedWrapper}>
-        {(value as string[]).map((currentValue, index, values) => {
-          const key = `${parentId}-value-${index}`
-          const hasSeparator = index < values.length - 1
+const getArrayValue = (parentId: string, value: string[], separator?: boolean) => (
+  <Typography component="div" variant="body2">
+    [
+    <div className={css.nestedWrapper}>
+      {value.map((currentValue, index, values) => {
+        const key = `${parentId}-value-${index}`
+        const hasSeparator = index < values.length - 1
 
-          return Array.isArray(currentValue) ? (
-            <div key={key}>{getArrayValue(key, currentValue, hasSeparator)}</div>
-          ) : (
-            getTextValue(currentValue, key)
-          )
-        })}
-      </div>
-      ]{separator ? ',' : null}
-    </Typography>
-  )
+        return Array.isArray(currentValue) ? (
+          <div key={key}>{getArrayValue(key, currentValue, hasSeparator)}</div>
+        ) : (
+          getTextValue(currentValue, key)
+        )
+      })}
+    </div>
+    ]{separator ? ',' : null}
+  </Typography>
+)
 
-  if (isArrayParameter(type) || Array.isArray(value)) {
+const GenericValue = ({ method, value }: Omit<ValueArrayProps, 'type'>): React.ReactElement => {
+  if (Array.isArray(value)) {
     return getArrayValue(method, value)
   }
 
-  return getTextValue(value as string)
+  return getTextValue(value)
 }
