b1b5dee6f81aa6d770b36fa6de48f7829ce74ecc
Aaron Cook <aaron@safe.global>
2023-05-11 10:36:28 +0200

    fix: redirect non-existing "Messages" tab (#1973)

--- src/pages/transactions/messages.tsx
+++ src/pages/transactions/messages.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
+import { FEATURES } from '@safe-global/safe-gateway-typescript-sdk'
+import { useRouter } from 'next/router'
 import type { NextPage } from 'next'
 
 import PaginatedMsgs from '@/components/safe-messages/PaginatedMsgs'
 import TxHeader from '@/components/transactions/TxHeader'
 import SignedMessagesHelpLink from '@/components/transactions/SignedMessagesHelpLink'
+import { AppRoutes } from '@/config/routes'
+import { useCurrentChain } from '@/hooks/useChains'
+import { hasFeature } from '@/utils/chains'
 
 const Messages: NextPage = () => {
+  const chain = useCurrentChain()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!chain || hasFeature(chain, FEATURES.EIP1271)) {
+      return
+    }
+
+    router.replace({ ...router, pathname: AppRoutes.transactions.history })
+  }, [router, chain])
+
   return (
     <>
       <Head>
