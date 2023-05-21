e173186581ad44b6baebf48fd5898b4bb468ac82
sky cherfa <sherifcherfa@gmail.com>
2023-05-21 08:10:16 -0400

    trigger re-deploy

--- src/pages/api/moralis/[...moralis].ts
+++ src/pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next'
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY!,
   authentication: {
-    domain: 'app.decentra.so',
+    domain: process.env.APP_DOMAIN || 'app.decentra.so',
     uri: process.env.NEXTAUTH_URL!,
     timeout: 120,
   },
