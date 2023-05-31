63879929b430c07e813506adebd08d5663fbd625
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-05-31 12:38:56 +0200

    Refactor: reactive recommended nonce (#2050)
    * Refactor: reactive recommended nonce
    
    Reduce re-renders
    
    Nonce -1
    
    * Use "nonceReadyOnly" instead of -1
    
    * Split sign and execute into two components

--- src/components/tx/SignOrExecuteForm/index.tsx
+++ src/components/tx/SignOrExecuteForm/index.tsx
@@ -1,29 +1,20 @@
-import { type ReactElement, type ReactNode, type SyntheticEvent, useEffect, useState } from 'react'
-import { Box, Button, DialogContent, Typography } from '@mui/material'
+import { type ReactElement, type ReactNode, useState, useEffect } from 'react'
+import { DialogContent } from '@mui/material'
 import type { SafeTransaction } from '@safe-global/safe-core-sdk-types'
 
-import useGasLimit from '@/hooks/useGasLimit'
-import ErrorMessage from '@/components/tx/ErrorMessage'
-import AdvancedParams, { type AdvancedParameters, useAdvancedParams } from '@/components/tx/AdvancedParams'
 import DecodedTx from '../DecodedTx'
 import ExecuteCheckbox from '../ExecuteCheckbox'
-import { logError, Errors } from '@/services/exceptions'
-import { useCurrentChain } from '@/hooks/useChains'
-import { getTxOptions } from '@/utils/transactions'
-import { TxSimulation } from '@/components/tx/TxSimulation'
-import useIsSafeOwner from '@/hooks/useIsSafeOwner'
-import useIsValidExecution from '@/hooks/useIsValidExecution'
-import { createTx } from '@/services/tx/tx-sender'
-import CheckWallet from '@/components/common/CheckWallet'
 import { WrongChainWarning } from '../WrongChainWarning'
-import { useImmediatelyExecutable, useIsExecutionLoop, useTxActions, useValidateNonce } from './hooks'
-import UnknownContractError from './UnknownContractError'
-import { useRelaysBySafe } from '@/hooks/useRemainingRelays'
-import useWalletCanRelay from '@/hooks/useWalletCanRelay'
-import { ExecutionMethod, ExecutionMethodSelector } from '../ExecutionMethodSelector'
-import { hasRemainingRelays } from '@/utils/relaying'
+import { useImmediatelyExecutable, useRecommendedNonce, useValidateNonce } from './hooks'
+import ExecuteForm from './ExecuteForm'
+import SignForm from './SignForm'
+import AdvancedParams, { useAdvancedParams } from '../AdvancedParams'
+import { TxSimulation } from '../TxSimulation'
+import { createTx } from '@/services/tx/tx-sender'
+import { Errors, logError } from '@/services/exceptions'
+import useGasLimit from '@/hooks/useGasLimit'
 
-type SignOrExecuteProps = {
+export type SignOrExecuteProps = {
   safeTx?: SafeTransaction
   txId?: string
   onSubmit: () => void
@@ -36,218 +27,85 @@ type SignOrExecuteProps = {
   origin?: string
 }
 
-const SignOrExecuteForm = ({
-  safeTx,
-  txId,
-  onSubmit,
-  children,
-  onlyExecute = false,
-  isExecutable = false,
-  isRejection = false,
-  disableSubmit = false,
-  origin,
-  ...props
-}: SignOrExecuteProps): ReactElement => {
-  //
-  // Hooks & variables
-  //
+const SignOrExecuteForm = (props: SignOrExecuteProps): ReactElement => {
   const [shouldExecute, setShouldExecute] = useState<boolean>(true)
-  const [isSubmittable, setIsSubmittable] = useState<boolean>(true)
-  const [tx, setTx] = useState<SafeTransaction | undefined>(safeTx)
-  const [submitError, setSubmitError] = useState<Error | undefined>()
-
-  // Hooks
-  const isOwner = useIsSafeOwner()
-  const currentChain = useCurrentChain()
-  const { signTx, executeTx } = useTxActions()
-  const [relays] = useRelaysBySafe()
-
-  // Check that the transaction is executable
-  const isCreation = !txId
+  const isCreation = !props.txId
   const isNewExecutableTx = useImmediatelyExecutable() && isCreation
+  const [tx, setTx] = useState<SafeTransaction | undefined>(props.safeTx)
   const isCorrectNonce = useValidateNonce(tx)
-  const isExecutionLoop = useIsExecutionLoop()
-  const canExecute = isCorrectNonce && (isExecutable || isNewExecutableTx)
+
+  // Nonce cannot be edited if the tx is already proposed, or signed, or it's a rejection
+  const nonceReadonly = !isCreation || !!tx?.signatures.size || !!props.isRejection
 
   // If checkbox is checked and the transaction is executable, execute it, otherwise sign it
-  const willExecute = (onlyExecute || shouldExecute) && canExecute
+  const canExecute = isCorrectNonce && (props.isExecutable || isNewExecutableTx)
+  const willExecute = (props.onlyExecute || shouldExecute) && canExecute
 
-  // We default to relay, but the option is only shown if we canRelay
-  const [executionMethod, setExecutionMethod] = useState(ExecutionMethod.RELAY)
-
-  // SC wallets can relay fully signed transactions
-  const [walletCanRelay] = useWalletCanRelay(tx)
-
-  // The transaction can/will be relayed
-  const canRelay = hasRemainingRelays(relays) && !!walletCanRelay && willExecute
-  const willRelay = canRelay && executionMethod === ExecutionMethod.RELAY
-
-  // Synchronize the tx with the safeTx
-  useEffect(() => setTx(safeTx), [safeTx])
-
+  // Recommended nonce and safeTxGas
+  const recommendedParams = useRecommendedNonce(tx)
   // Estimate gas limit
-  const { gasLimit, gasLimitError, gasLimitLoading } = useGasLimit(willExecute ? tx : undefined)
+  const { gasLimit, gasLimitError } = useGasLimit(tx)
 
-  const [advancedParams, setAdvancedParams] = useAdvancedParams({
-    nonce: tx?.data.nonce,
+  const error = props.error || gasLimitError
+
+  const [advancedParams, setAdvancedParams] = useAdvancedParams(
     gasLimit,
-    safeTxGas: tx?.data.safeTxGas,
-  })
-
-  // Check if transaction will fail
-  const { executionValidationError, isValidExecutionLoading } = useIsValidExecution(
-    willExecute ? tx : undefined,
-    advancedParams.gasLimit,
+    // Initial nonce or a recommended one
+    nonceReadonly ? props.safeTx?.data.nonce : recommendedParams?.nonce,
+    // Initial safeTxGas or a recommended one
+    nonceReadonly ? props.safeTx?.data.safeTxGas : recommendedParams?.safeTxGas,
   )
 
-  // Estimating gas
-  const isEstimating = willExecute && gasLimitLoading
-  // Nonce cannot be edited if the tx is already proposed, or signed, or it's a rejection
-  const nonceReadonly = !isCreation || !!tx?.signatures.size || isRejection
+  // Synchronize the tx with the safeTx
+  useEffect(() => setTx(props.safeTx), [props.safeTx])
 
-  // Sign transaction
-  const onSign = async (): Promise<string | undefined> => {
-    return await signTx(tx, txId, origin)
-  }
+  // Update the tx when the advancedParams change
+  useEffect(() => {
+    if (nonceReadonly || !tx?.data) return
+    if (tx.data.nonce === advancedParams.nonce && tx.data.safeTxGas === advancedParams.safeTxGas) return
 
-  // Execute transaction
-  const onExecute = async (): Promise<string | undefined> => {
-    const txOptions = getTxOptions(advancedParams, currentChain)
-    return await executeTx(txOptions, tx, txId, origin, willRelay)
-  }
-
-  // On modal submit
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault()
-    setIsSubmittable(false)
-    setSubmitError(undefined)
-
-    try {
-      await (willExecute ? onExecute() : onSign())
-    } catch (err) {
-      logError(Errors._804, (err as Error).message)
-      setIsSubmittable(true)
-      setSubmitError(err as Error)
-      return
-    }
-
-    onSubmit()
-  }
-
-  // On advanced params submit (nonce, gas limit, price, etc), recreate the transaction
-  const onAdvancedSubmit = async (data: AdvancedParameters) => {
-    // If nonce was edited, create a new tx with that nonce
-    if (tx && (data.nonce !== tx.data.nonce || data.safeTxGas !== tx.data.safeTxGas)) {
-      try {
-        setTx(await createTx({ ...tx.data, safeTxGas: data.safeTxGas }, data.nonce))
-      } catch (err) {
-        logError(Errors._103, (err as Error).message)
-        return
-      }
-    }
-
-    setAdvancedParams(data)
-  }
-
-  const cannotPropose = !isOwner && !onlyExecute // Can't sign or create a tx if not an owner
-  const submitDisabled =
-    !isSubmittable ||
-    isEstimating ||
-    !tx ||
-    disableSubmit ||
-    cannotPropose ||
-    isValidExecutionLoading ||
-    (willExecute && isExecutionLoop)
-
-  const error = props.error || (willExecute ? gasLimitError || executionValidationError : undefined)
+    createTx({ ...tx.data, safeTxGas: advancedParams.safeTxGas }, advancedParams.nonce)
+      .then(setTx)
+      .catch((err) => logError(Errors._103, (err as Error).message))
+  }, [nonceReadonly, tx?.data, advancedParams.nonce, advancedParams.safeTxGas])
 
   return (
-    <form onSubmit={handleSubmit}>
-      <DialogContent>
-        {children}
+    <DialogContent>
+      {props.children}
 
-        <DecodedTx tx={tx} txId={txId} />
+      <TxSimulation
+        canExecute
+        gasLimit={advancedParams.gasLimit?.toNumber()}
+        transactions={tx}
+        disabled={!!gasLimitError}
+      />
 
-        {canExecute && <ExecuteCheckbox checked={shouldExecute} onChange={setShouldExecute} disabled={onlyExecute} />}
+      <DecodedTx tx={props.safeTx} txId={props.txId} />
 
-        <AdvancedParams
-          params={advancedParams}
-          recommendedGasLimit={gasLimit}
-          recommendedNonce={safeTx?.data.nonce}
-          willExecute={willExecute}
-          nonceReadonly={nonceReadonly}
-          onFormSubmit={onAdvancedSubmit}
-          gasLimitError={gasLimitError}
-          willRelay={willRelay}
-        />
+      {canExecute && (
+        <ExecuteCheckbox checked={shouldExecute} onChange={setShouldExecute} disabled={props.onlyExecute} />
+      )}
 
-        {canRelay && (
-          <Box
-            sx={{
-              '& > div': {
-                marginTop: '-1px',
-                borderTopLeftRadius: 0,
-                borderTopRightRadius: 0,
-              },
-            }}
-          >
-            <ExecutionMethodSelector
-              executionMethod={executionMethod}
-              setExecutionMethod={setExecutionMethod}
-              relays={relays}
-            />
-          </Box>
-        )}
+      <AdvancedParams
+        params={advancedParams}
+        recommendedGasLimit={gasLimit}
+        recommendedNonce={recommendedParams?.nonce}
+        willExecute={willExecute}
+        nonceReadonly={nonceReadonly}
+        onFormSubmit={setAdvancedParams}
+        gasLimitError={gasLimitError}
+        willRelay={false /* FIXME */}
+      />
 
-        <TxSimulation
-          gasLimit={advancedParams.gasLimit?.toNumber()}
-          transactions={tx}
-          canExecute={canExecute}
-          disabled={submitDisabled}
-        />
+      {/* Warning message and switch button */}
+      <WrongChainWarning />
 
-        {/* Warning message and switch button */}
-        <WrongChainWarning />
-
-        {/* Error messages */}
-        {isSubmittable && cannotPropose ? (
-          <ErrorMessage>
-            You are currently not an owner of this Safe Account and won&apos;t be able to submit this transaction.
-          </ErrorMessage>
-        ) : willExecute && isExecutionLoop ? (
-          <ErrorMessage>
-            Cannot execute a transaction from the Safe Account itself, please connect a different account.
-          </ErrorMessage>
-        ) : error ? (
-          <ErrorMessage error={error}>
-            This transaction will most likely fail.{' '}
-            {isNewExecutableTx
-              ? 'To save gas costs, avoid creating the transaction.'
-              : 'To save gas costs, reject this transaction.'}
-          </ErrorMessage>
-        ) : submitError ? (
-          <ErrorMessage error={submitError}>Error submitting the transaction. Please try again.</ErrorMessage>
-        ) : (
-          willExecute && <UnknownContractError />
-        )}
-
-        {/* Info text */}
-        <Typography variant="body2" color="border.main" textAlign="center" mt={3}>
-          You&apos;re about to {txId ? '' : 'create and '}
-          {willExecute ? 'execute' : 'sign'} a transaction and will need to confirm it with your currently connected
-          wallet.
-        </Typography>
-
-        {/* Submit button */}
-        <CheckWallet allowNonOwner={willExecute}>
-          {(isOk) => (
-            <Button variant="contained" type="submit" disabled={!isOk || submitDisabled}>
-              {isEstimating ? 'Estimating...' : 'Submit'}
-            </Button>
-          )}
-        </CheckWallet>
-      </DialogContent>
-    </form>
+      {willExecute ? (
+        <ExecuteForm {...props} safeTx={tx} advancedParams={advancedParams} error={error} />
+      ) : (
+        <SignForm {...props} safeTx={tx} error={error} />
+      )}
+    </DialogContent>
   )
 }
 
