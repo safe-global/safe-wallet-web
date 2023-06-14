99cdacc286a7f5a9311aa9c02d0d224e34f3f69e
Aaron Cook <aaron@safe.global>
2023-06-14 16:58:46 +0200

    feat: remove spending limit flow (#2098)

---
+++ src/components/tx-flow/flows/RemoveSpendingLimit/RemoveSpendingLimit.tsx
@@ -0,0 +1,71 @@
+import SignOrExecuteForm from '@/components/tx/SignOrExecuteForm'
+import { getSpendingLimitInterface, getSpendingLimitModuleAddress } from '@/services/contracts/spendingLimitContracts'
+import useChainId from '@/hooks/useChainId'
+import { useContext, useEffect } from 'react'
+import { SafeTxContext } from '../../SafeTxProvider'
+import EthHashInfo from '@/components/common/EthHashInfo'
+import { DialogContent, Typography } from '@mui/material'
+import type { SpendingLimitState } from '@/store/spendingLimitsSlice'
+import { relativeTime } from '@/utils/date'
+import { trackEvent, SETTINGS_EVENTS } from '@/services/analytics'
+import useBalances from '@/hooks/useBalances'
+import { TokenTransferReview } from '@/components/tx/modals/TokenTransferModal/ReviewTokenTx'
+import { safeFormatUnits } from '@/utils/formatters'
+import SpendingLimitLabel from '@/components/common/SpendingLimitLabel'
+import { createTx } from '@/services/tx/tx-sender'
+
+export const RemoveSpendingLimit = ({ params }: { params: SpendingLimitState }) => {
+  const { setSafeTx, setSafeTxError } = useContext(SafeTxContext)
+  const chainId = useChainId()
+  const { balances } = useBalances()
+  const token = balances.items.find((item) => item.tokenInfo.address === params.token.address)
+
+  useEffect(() => {
+    const spendingLimitAddress = getSpendingLimitModuleAddress(chainId)
+
+    if (!spendingLimitAddress) {
+      return
+    }
+
+    const spendingLimitInterface = getSpendingLimitInterface()
+    const txData = spendingLimitInterface.encodeFunctionData('deleteAllowance', [
+      params.beneficiary,
+      params.token.address,
+    ])
+
+    const txParams = {
+      to: spendingLimitAddress,
+      value: '0',
+      data: txData,
+    }
+
+    createTx(txParams).then(setSafeTx).catch(setSafeTxError)
+  }, [chainId, params.beneficiary, params.token, setSafeTx, setSafeTxError])
+
+  const onFormSubmit = () => {
+    trackEvent(SETTINGS_EVENTS.SPENDING_LIMIT.LIMIT_REMOVED)
+  }
+
+  return (
+    <DialogContent>
+      <SignOrExecuteForm onSubmit={onFormSubmit}>
+        {token && (
+          <TokenTransferReview
+            amount={safeFormatUnits(params.amount, token.tokenInfo.decimals)}
+            tokenInfo={token.tokenInfo}
+          />
+        )}
+        <Typography sx={({ palette }) => ({ color: palette.primary.light })}>Beneficiary</Typography>
+        <EthHashInfo address={params.beneficiary} showCopyButton hasExplorer shortAddress={false} />
+        <Typography mt={2} sx={({ palette }) => ({ color: palette.primary.light })}>
+          Reset time
+        </Typography>
+        <SpendingLimitLabel
+          label={relativeTime(params.lastResetMin, params.resetTimeMin)}
+          mb={2}
+          isOneTime={params.resetTimeMin === '0'}
+        />
+      </SignOrExecuteForm>
+    </DialogContent>
+  )
+}
