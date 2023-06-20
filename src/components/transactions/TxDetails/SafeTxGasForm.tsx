b43c415b87820fce01527d57f1ce8e12aec97919
katspaugh <katspaugh@gmail.com>
2023-06-20 13:07:20 +0200

    PR comments

--- src/components/transactions/TxDetails/SafeTxGasForm.tsx
+++ src/components/transactions/TxDetails/SafeTxGasForm.tsx
@@ -1,21 +1,26 @@
 import { useContext, useState } from 'react'
-import { Link, TextField, Box, Paper, Button } from '@mui/material'
-import { FormProvider, useForm } from 'react-hook-form'
+import { Link, Box, Paper, Button } from '@mui/material'
+import { useForm } from 'react-hook-form'
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
+import NumberField from '@/components/common/NumberField'
+
+type FormFields = {
+  safeTxGas: number
+}
 
 const SafeTxGasForm = () => {
   const { safeTx, safeTxGas, setSafeTxGas } = useContext(SafeTxContext)
   const isEditable = safeTx?.signatures.size === 0
   const [editing, setEditing] = useState(false)
 
-  const formMethods = useForm<{ safeTxGas: number }>({
+  const formMethods = useForm<FormFields>({
     defaultValues: {
       safeTxGas,
     },
     mode: 'onChange',
   })
 
-  const onSubmit = (values: { safeTxGas: number }) => {
+  const onSubmit = (values: FormFields) => {
     setSafeTxGas(values.safeTxGas || 0)
     setEditing(false)
   }
@@ -33,27 +38,24 @@ const SafeTxGasForm = () => {
       </>
 
       {editing && (
-        <Paper sx={{ position: 'absolute', zIndex: 2, p: 1, ml: -3 }} elevation={3}>
-          <FormProvider {...formMethods}>
-            <form onSubmit={formMethods.handleSubmit(onSubmit)} style={{ display: 'flex' }}>
-              <TextField
-                size="small"
-                autoComplete="off"
-                autoFocus
-                type="number"
-                error={!!formMethods.formState.errors.safeTxGas}
-                sx={{ width: '7em' }}
-                {...formMethods.register('safeTxGas', {
-                  valueAsNumber: true,
-                  min: 0,
-                  setValueAs: Math.round,
-                })}
-              />
-              <Button type="submit" size="small" variant="contained" sx={{ ml: 1 }}>
-                Save
-              </Button>
-            </form>
-          </FormProvider>
+        <Paper sx={{ position: 'absolute', zIndex: 2, p: 1, ml: '-22px' }} elevation={2}>
+          <form onSubmit={formMethods.handleSubmit(onSubmit)} style={{ display: 'flex' }}>
+            <NumberField
+              size="small"
+              autoFocus
+              type="number"
+              error={!!formMethods.formState.errors.safeTxGas}
+              sx={{ width: '7em' }}
+              {...formMethods.register('safeTxGas', {
+                valueAsNumber: true,
+                min: 0,
+                setValueAs: Math.round,
+              })}
+            />
+            <Button type="submit" size="small" variant="contained" sx={{ ml: 1 }}>
+              Save
+            </Button>
+          </form>
         </Paper>
       )}
     </Box>
