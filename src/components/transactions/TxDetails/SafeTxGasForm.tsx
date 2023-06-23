376b813daf58c20bb269c1a33987d9e534e3603f
katspaugh <katspaugh@gmail.com>
2023-06-23 16:26:04 +0200

    Fix: hide Edit for safeTxGas when 0 in 1.3.0+

--- src/components/transactions/TxDetails/SafeTxGasForm.tsx
+++ src/components/transactions/TxDetails/SafeTxGasForm.tsx
@@ -3,15 +3,15 @@ import { Link, Box, Paper, Button } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 import NumberField from '@/components/common/NumberField'
+import useSafeInfo from '@/hooks/useSafeInfo'
+import { isLegacyVersion } from '@/hooks/coreSDK/safeCoreSDK'
 
 type FormFields = {
   safeTxGas: number
 }
 
-const SafeTxGasForm = () => {
-  const { safeTx, safeTxGas = 0, setSafeTxGas } = useContext(SafeTxContext)
-  const isEditable = safeTx?.signatures.size === 0
-  const [editing, setEditing] = useState(false)
+const Form = ({ onSubmit }: { onSubmit: () => void }) => {
+  const { safeTxGas = 0, setSafeTxGas } = useContext(SafeTxContext)
 
   const formMethods = useForm<FormFields>({
     defaultValues: {
@@ -20,52 +20,60 @@ const SafeTxGasForm = () => {
     mode: 'onChange',
   })
 
-  const onSubmit = (values: FormFields) => {
+  const onFormSubmit = (values: FormFields) => {
     setSafeTxGas(values.safeTxGas || 0)
-    setEditing(false)
+    onSubmit()
   }
 
   const onBlur = () => {
     setTimeout(() => {
-      setEditing(false)
       formMethods.setValue('safeTxGas', safeTxGas)
+      onSubmit()
     }, 100)
   }
 
   return (
+    <Paper sx={{ position: 'absolute', zIndex: 2, p: 1, ml: '-22px' }} elevation={2}>
+      <form onSubmit={formMethods.handleSubmit(onFormSubmit)} style={{ display: 'flex' }}>
+        <NumberField
+          size="small"
+          autoFocus
+          type="number"
+          error={!!formMethods.formState.errors.safeTxGas}
+          sx={{ width: '7em' }}
+          {...formMethods.register('safeTxGas', {
+            valueAsNumber: true,
+            min: 0,
+            setValueAs: Math.round,
+            onBlur,
+          })}
+        />
+        <Button type="submit" size="small" variant="contained" sx={{ ml: 1 }}>
+          Save
+        </Button>
+      </form>
+    </Paper>
+  )
+}
+
+const SafeTxGasForm = () => {
+  const { safeTx, safeTxGas = 0 } = useContext(SafeTxContext)
+  const { safe } = useSafeInfo()
+  const isOldSafe = safe.version && isLegacyVersion(safe.version)
+  const isEditable = safeTx?.signatures.size === 0 && (safeTxGas > 0 || isOldSafe)
+  const [editing, setEditing] = useState(false)
+
+  return (
     <Box display="flex" alignItems="center" gap={1} position="relative">
-      <>
-        {safeTxGas}
+      {safeTxGas}
 
-        {isEditable && (
-          <Link component="button" onClick={() => setEditing(true)} fontSize="small">
-            Edit
-          </Link>
-        )}
-      </>
-
-      {editing && (
-        <Paper sx={{ position: 'absolute', zIndex: 2, p: 1, ml: '-22px' }} elevation={2}>
-          <form onSubmit={formMethods.handleSubmit(onSubmit)} style={{ display: 'flex' }}>
-            <NumberField
-              size="small"
-              autoFocus
-              type="number"
-              error={!!formMethods.formState.errors.safeTxGas}
-              sx={{ width: '7em' }}
-              {...formMethods.register('safeTxGas', {
-                valueAsNumber: true,
-                min: 0,
-                setValueAs: Math.round,
-                onBlur,
-              })}
-            />
-            <Button type="submit" size="small" variant="contained" sx={{ ml: 1 }}>
-              Save
-            </Button>
-          </form>
-        </Paper>
+      {isEditable && (
+        <Link component="button" onClick={() => setEditing(true)} fontSize="small">
+          Edit
+        </Link>
       )}
+
+      {editing && <Form onSubmit={() => setEditing(false)} />}
     </Box>
   )
 }
