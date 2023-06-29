b9f6262d8529728cac3a2855fb671f7865c021e7
Usame Algan <usame@safe.global>
2023-06-29 15:47:25 +0200

    Fix zIndex issue

--- src/components/common/Sticky/index.tsx
+++ src/components/common/Sticky/index.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from 'react'
 
 const stickyTop = { xs: '103px', md: '111px' }
 export const Sticky = ({ children }: { children: ReactElement }): ReactElement => (
-  <Box position="sticky" zIndex="2" top={stickyTop} py={1} bgcolor="background.main" mt={-1} mb={1}>
+  <Box position="sticky" zIndex="1" top={stickyTop} py={1} bgcolor="background.main" mt={-1} mb={1}>
     {children}
   </Box>
 )
