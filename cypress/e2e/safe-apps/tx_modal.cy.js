b00401c30d880dca34ace3aa28808643f6248cdf
Usame Algan <usame@safe.global>
2023-06-30 10:20:59 +0200

    Fix failing e2e test

--- cypress/e2e/safe-apps/tx_modal.cy.js
+++ cypress/e2e/safe-apps/tx_modal.cy.js
@@ -1,7 +1,7 @@
 const appUrl = 'https://safe-test-app.com'
 
 describe('The transaction modal', () => {
-  before(() => {
+  beforeEach(() => {
     cy.fixture('safe-app').then((html) => {
       cy.intercept('GET', `${appUrl}/*`, html)
       cy.intercept('GET', `*/manifest.json`, {
@@ -10,14 +10,13 @@ describe('The transaction modal', () => {
         icons: [{ src: 'logo.svg', sizes: 'any', type: 'image/svg+xml' }],
       })
     })
-
-    cy.visitSafeApp(`${appUrl}/dummy`)
-
-    cy.findByText(/accept selection/i).click()
   })
 
   describe('When sending a transaction from an app', () => {
     it('should show the transaction popup', { defaultCommandTimeout: 12000 }, () => {
+      cy.visitSafeApp(`${appUrl}/dummy`)
+
+      cy.findByText(/accept selection/i).click()
       cy.findByRole('dialog').within(() => {
         cy.findByText(/Cypress Test App/i)
       })
