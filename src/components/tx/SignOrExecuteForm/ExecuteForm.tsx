794448eb9ecca2350ae4ed59330f83bd9adda16e
Usame Algan <usame@safe.global>
2023-07-05 10:12:05 +0200

    fix: Pass classname to ErrorMessage

--- src/components/tx/SignOrExecuteForm/ExecuteForm.tsx
+++ src/components/tx/SignOrExecuteForm/ExecuteForm.tsx
@@ -96,7 +96,6 @@ const ExecuteForm = ({
   }
 
   const submitDisabled = !safeTx || !isSubmittable || disableSubmit || isValidExecutionLoading || isExecutionLoop
-  const hasError = isExecutionLoop || executionValidationError || gasLimitError || submitError
 
   return (
     <>
@@ -123,25 +122,23 @@ const ExecuteForm = ({
         )}
 
         {/* Error messages */}
-        {hasError && (
-          <div className={css.errorWrapper}>
-            {isExecutionLoop ? (
-              <ErrorMessage>
-                Cannot execute a transaction from the Safe Account itself, please connect a different account.
-              </ErrorMessage>
-            ) : executionValidationError || gasLimitError ? (
-              <ErrorMessage error={executionValidationError || gasLimitError}>
-                This transaction will most likely fail.{' '}
-                {isNewExecutableTx
-                  ? 'To save gas costs, avoid creating the transaction.'
-                  : 'To save gas costs, reject this transaction.'}
-              </ErrorMessage>
-            ) : (
-              submitError && (
-                <ErrorMessage error={submitError}>Error submitting the transaction. Please try again.</ErrorMessage>
-              )
-            )}
-          </div>
+        {isExecutionLoop ? (
+          <ErrorMessage className={css.errorWrapper}>
+            Cannot execute a transaction from the Safe Account itself, please connect a different account.
+          </ErrorMessage>
+        ) : executionValidationError || gasLimitError ? (
+          <ErrorMessage error={executionValidationError || gasLimitError} className={css.errorWrapper}>
+            This transaction will most likely fail.{' '}
+            {isNewExecutableTx
+              ? 'To save gas costs, avoid creating the transaction.'
+              : 'To save gas costs, reject this transaction.'}
+          </ErrorMessage>
+        ) : (
+          submitError && (
+            <ErrorMessage error={submitError} className={css.errorWrapper}>
+              Error submitting the transaction. Please try again.
+            </ErrorMessage>
+          )
         )}
 
         <Divider className={commonCss.nestedDivider} sx={{ pt: 3 }} />
