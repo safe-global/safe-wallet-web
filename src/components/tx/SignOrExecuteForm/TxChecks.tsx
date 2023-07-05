cdc4420b1d7279292ef9bba4bbc0ec2be09a4b50
schmanu <manu@safe.global>
2023-07-05 15:59:51 +0200

    feat: show redefine warnings on mobile
    - adds RedefineMessage to TxChecks for small screens
    - changes order of Redefine scan results and Tenderly Simulation

--- src/components/tx/SignOrExecuteForm/TxChecks.tsx
+++ src/components/tx/SignOrExecuteForm/TxChecks.tsx
@@ -1,8 +1,10 @@
 import { type ReactElement, useContext } from 'react'
 import { TxSimulation } from '@/components/tx/security/tenderly'
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
-import { Typography } from '@mui/material'
-import { Redefine } from '@/components/tx/security/redefine'
+import { Box, Typography } from '@mui/material'
+import { Redefine, RedefineMessage } from '@/components/tx/security/redefine'
+
+import css from './styles.module.css'
 
 const TxChecks = (): ReactElement => {
   const { safeTx } = useContext(SafeTxContext)
@@ -11,9 +13,13 @@ const TxChecks = (): ReactElement => {
     <>
       <Typography variant="h5">Transaction checks</Typography>
 
+      <TxSimulation disabled={false} transactions={safeTx} />
+
       <Redefine />
 
-      <TxSimulation disabled={false} transactions={safeTx} />
+      <Box className={css.mobileRedefineMessages}>
+        <RedefineMessage />
+      </Box>
     </>
   )
 }
