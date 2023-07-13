0c9d269d399a8b068055c8caf68ca847fb7aaf4b
iamacook <aaron@safe.global>
2023-07-13 10:33:32 +0200

    fix: update date

--- src/components/privacy/index.tsx
+++ src/components/privacy/index.tsx
@@ -6,7 +6,7 @@ const SafePrivacyPolicy = () => {
   return (
     <div>
       <h1>Privacy Policy</h1>
-      <p>Last updated on May&nbsp;2023.</p>
+      <p>Last updated on July&nbsp;2023.</p>
       <p>
         Your privacy is important to us. It is our policy to respect your privacy and comply with any applicable law and
         regulation regarding any personal information we may collect about you, including across our website,{' '}
