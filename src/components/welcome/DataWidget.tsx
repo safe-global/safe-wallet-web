423fbacace7b9ab7fe780e82e03e1fab53e20476
katspaugh <katspaugh@gmail.com>
2023-07-21 18:39:43 +0200

    Feat: import/export in the Safe List sidebar

--- src/components/welcome/DataWidget.tsx
+++ src/components/welcome/DataWidget.tsx
@@ -44,6 +44,7 @@ export const DataWidget = (): ReactElement => {
             <SvgIcon component={FileIcon} inheritViewBox fontSize="small" sx={{ fill: 'none' }} />
           </Box>
         }
+        className={css.cardHeader}
         title={<b>{hasData ? 'Work with your data' : 'Already have a Safe Account?'}</b>}
         subheader={hasData ? 'Export or import your data' : 'Import your data'}
       />
