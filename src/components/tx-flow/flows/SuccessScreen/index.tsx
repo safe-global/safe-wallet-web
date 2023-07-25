07b9a6e40c919b6c491e70edf4164e123a336c3e
schmanu <manu@safe.global>
2023-07-25 12:13:18 +0200

    fix: show error for reverted txs

--- src/components/tx-flow/flows/SuccessScreen/index.tsx
+++ src/components/tx-flow/flows/SuccessScreen/index.tsx
@@ -29,11 +29,19 @@ export const SuccessScreen = ({ txId }: { txId: string }) => {
   }, [txHash])
 
   useEffect(() => {
-    const unsubscribe = txSubscribe(TxEvent.FAILED, (detail) => {
-      if (detail.txId === txId) setError(detail.error)
-    })
+    const unsubFns: Array<() => void> = []
+    unsubFns.push(
+      txSubscribe(TxEvent.FAILED, (detail) => {
+        if (detail.txId === txId) setError(detail.error)
+      }),
+    )
+    unsubFns.push(
+      txSubscribe(TxEvent.REVERTED, (detail) => {
+        if (detail.txId === txId) setError(detail.error)
+      }),
+    )
 
-    return unsubscribe
+    return () => unsubFns.forEach((unsubscribe) => unsubscribe())
   }, [txId])
 
   const homeLink: UrlObject = {
