7dccfd4f17c12ca5d5fc369d6aef18bec3a519e0
katspaugh <katspaugh@gmail.com>
2023-08-01 15:37:10 +0200

    Extract static fn part

--- src/components/common/EthHashInfo/AddressEmoji.tsx
+++ src/components/common/EthHashInfo/AddressEmoji.tsx
@@ -3,23 +3,23 @@ import { useAppSelector } from '@/store'
 import { selectSettings } from '@/store/settingsSlice'
 import css from './styles.module.css'
 
+// Define the Unicode ranges for animal, fruit, and vegetable emojis
+const unicodeRanges = [
+  [0x1f400, 0x1f43f],
+  [0x1f980, 0x1f994],
+  [0x1f345, 0x1f35f],
+  [0x1f950, 0x1f96b],
+]
+
+// Calculate the total number of emojis
+let totalEmojis = 0
+for (let range of unicodeRanges) {
+  totalEmojis += range[1] - range[0] + 1
+}
+
 function ethereumAddressToEmoji(address: string): string {
   // Convert the Ethereum address from hexadecimal to decimal
   const decimal = BigInt(address.slice(0, 6))
-
-  // Define the Unicode ranges for animal, fruit, and vegetable emojis
-  const unicodeRanges = [
-    [0x1f400, 0x1f43f],
-    [0x1f980, 0x1f994],
-    [0x1f345, 0x1f35f],
-    [0x1f950, 0x1f96b],
-  ]
-
-  // Calculate the total number of emojis
-  let totalEmojis = 0
-  for (let range of unicodeRanges) {
-    totalEmojis += range[1] - range[0] + 1
-  }
 
   // Calculate the index by taking the modulo of the decimal by the number of emojis
   let index = Number(decimal % BigInt(totalEmojis))
