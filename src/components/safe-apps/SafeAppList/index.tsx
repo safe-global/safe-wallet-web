26581664043cac77f40d201e8a596869e2eb8900
Aaron Cook <aaron@safe.global>
2023-08-02 08:21:19 +0100

    feat: Safe App info button (#1726)
    * feat: Safe App info button
    
    * fix: return type
    
    * fix: mark apps agnostic of chain
    
    * Revert "fix: mark apps agnostic of chain"
    
    This reverts commit 35e7917d26b87f09e13a710f1d73da8efe26d54f.

--- src/components/safe-apps/SafeAppList/index.tsx
+++ src/components/safe-apps/SafeAppList/index.tsx
@@ -106,6 +106,7 @@ const SafeAppList = ({
               onBookmarkSafeApp={onBookmarkSafeApp}
               removeCustomApp={removeCustomApp}
               onClickSafeApp={handleSafeAppClick(safeApp)}
+              openPreviewDrawer={openPreviewDrawer}
             />
           </li>
         ))}
