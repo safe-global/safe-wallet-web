bba709a46116593a0912c0af489fa24dab63ce13
iamacook <aaron@safe.global>
2023-08-10 18:29:18 +0200

    fix: add more test coverage

---
+++ src/components/settings/Notifications/logic.test.ts
@@ -0,0 +1,563 @@
+import * as firebase from 'firebase/messaging'
+import { hexZeroPad } from 'ethers/lib/utils'
+import { Web3Provider } from '@ethersproject/providers'
+import type { JsonRpcSigner } from '@ethersproject/providers'
+import type { SafeInfo } from '@safe-global/safe-gateway-typescript-sdk'
+
+import * as logic from './logic'
+import packageJson from '../../../../package.json'
+
+jest.mock('firebase/messaging')
+
+Object.defineProperty(globalThis, 'crypto', {
+  value: {
+    randomUUID: () => Math.random().toString(),
+  },
+})
+
+Object.defineProperty(globalThis, 'navigator', {
+  value: {
+    serviceWorker: {
+      getRegistration: jest.fn(),
+    },
+  },
+})
+
+Object.defineProperty(globalThis, 'location', {
+  value: {
+    origin: 'https://app.safe.global',
+  },
+})
+
+const mockProvider = new Web3Provider(jest.fn())
+
+describe('Notifications', () => {
+  let alertMock = jest.fn()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    global.fetch = jest.fn()
+    window.alert = alertMock
+  })
+
+  describe('requestNotificationPermission', () => {
+    let requestPermissionMock = jest.fn()
+
+    beforeEach(() => {
+      globalThis.Notification = {
+        requestPermission: requestPermissionMock,
+        permission: 'default',
+      } as unknown as jest.Mocked<typeof Notification>
+    })
+
+    it('should return true and not request permission again if already granted', async () => {
+      globalThis.Notification = {
+        requestPermission: requestPermissionMock,
+        permission: 'granted',
+      } as unknown as jest.Mocked<typeof Notification>
+
+      const result = await logic.requestNotificationPermission()
+
+      expect(requestPermissionMock).not.toHaveBeenCalled()
+      expect(result).toBe(true)
+    })
+
+    it('should return false if permission is denied', async () => {
+      requestPermissionMock.mockResolvedValue('denied')
+
+      const result = await logic.requestNotificationPermission()
+
+      expect(requestPermissionMock).toHaveBeenCalledTimes(1)
+      expect(result).toBe(false)
+    })
+
+    it('should return false if permission request throw', async () => {
+      requestPermissionMock.mockImplementation(Promise.reject)
+
+      const result = await logic.requestNotificationPermission()
+
+      expect(requestPermissionMock).toHaveBeenCalledTimes(1)
+      expect(result).toBe(false)
+    })
+
+    it('should return true if permission are granted', async () => {
+      requestPermissionMock.mockResolvedValue('granted')
+
+      const result = await logic.requestNotificationPermission()
+
+      expect(requestPermissionMock).toHaveBeenCalledTimes(1)
+      expect(result).toBe(true)
+    })
+  })
+
+  describe('createRegisterSafePayload', () => {
+    it('should return the current registrations if the safe is already registered', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      const payload = await logic.createRegisterSafePayload(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+        currentRegistration,
+      )
+
+      expect(payload).toStrictEqual(currentRegistration)
+    })
+
+    describe('should return a registration payload if the safe is not already registered', () => {
+      it('should just be the current Safe if none is registered on the current chain', async () => {
+        const token = crypto.randomUUID()
+        const signature = hexZeroPad('0xDEAD', 65)
+
+        jest.spyOn(firebase, 'getToken').mockImplementation(() => Promise.resolve(token))
+        jest.spyOn(mockProvider, 'getSigner').mockImplementation(
+          () =>
+            ({
+              signMessage: jest.fn().mockResolvedValue(signature),
+            } as unknown as JsonRpcSigner),
+        )
+
+        const safeAddress = hexZeroPad('0x1', 20)
+        const chainId = '1'
+
+        const payload = await logic.createRegisterSafePayload(
+          { chainId, address: { value: safeAddress } } as SafeInfo,
+          mockProvider,
+        )
+
+        expect(payload).toStrictEqual({
+          uuid: expect.any(String),
+          cloudMessagingToken: token,
+          buildNumber: '0',
+          bundle: 'https://app.safe.global',
+          deviceType: logic.DeviceType.WEB,
+          version: packageJson.version,
+          timestamp: expect.any(String),
+          safeRegistrations: [
+            {
+              chainId,
+              safes: [safeAddress],
+              signatures: [signature],
+            },
+          ],
+        })
+      })
+
+      it('should append the Safe if one is already registered on the chain', async () => {
+        const token = crypto.randomUUID()
+        const signature = hexZeroPad('0xDEAD', 65)
+
+        jest.spyOn(firebase, 'getToken').mockImplementation(() => Promise.resolve(token))
+        jest.spyOn(mockProvider, 'getSigner').mockImplementation(
+          () =>
+            ({
+              signMessage: jest.fn().mockResolvedValue(signature),
+            } as unknown as JsonRpcSigner),
+        )
+
+        const safeAddress = hexZeroPad('0x1', 20)
+        const chainId = '1'
+
+        const currentRegistration = {
+          uuid: crypto.randomUUID(),
+          cloudMessagingToken: token,
+          buildNumber: '0',
+          bundle: 'https://app.safe.global',
+          deviceType: logic.DeviceType.WEB,
+          version: packageJson.version,
+          timestamp: expect.any(String),
+          safeRegistrations: [
+            {
+              chainId,
+              safes: [hexZeroPad('0x2', 20)],
+              signatures: [signature],
+            },
+          ],
+        }
+
+        const payload = await logic.createRegisterSafePayload(
+          { chainId, address: { value: safeAddress } } as SafeInfo,
+          mockProvider,
+          currentRegistration,
+        )
+
+        expect(payload?.timestamp).not.toBe(currentRegistration.timestamp)
+        expect(payload?.safeRegistrations[0].signatures).not.toBe(currentRegistration?.safeRegistrations[0].signatures)
+
+        expect(payload).toStrictEqual({
+          uuid: currentRegistration.uuid, // Same UUID
+          cloudMessagingToken: expect.any(String),
+          buildNumber: '0',
+          bundle: 'https://app.safe.global',
+          deviceType: logic.DeviceType.WEB,
+          version: packageJson.version,
+          timestamp: expect.any(String),
+          safeRegistrations: [
+            {
+              chainId,
+              safes: [hexZeroPad('0x2', 20), safeAddress],
+              signatures: [expect.any(String)],
+            },
+          ],
+        })
+      })
+    })
+  })
+
+  describe('registerSafe', () => {
+    it('should return undefined if no registration exists and the registration failed', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      jest
+        .spyOn(logic, 'createRegisterSafePayload')
+        .mockImplementation(() => Promise.resolve({} as logic.RegisterDeviceDto))
+
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+          status: 69420, // Failed
+          ok: true,
+        })
+      })
+
+      const registration = await logic.registerSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to register Safe')
+
+      expect(registration).toBe(undefined)
+    })
+
+    it('should return the current registration if one exists and the registration failed', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      jest
+        .spyOn(logic, 'createRegisterSafePayload')
+        .mockImplementation(() => Promise.resolve({} as logic.RegisterDeviceDto))
+
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+          status: 69420, // Failed
+          ok: true,
+        })
+      })
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      const registration = await logic.registerSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+        currentRegistration,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to register Safe')
+
+      expect(registration).toBe(currentRegistration)
+    })
+
+    it('should return undefined if no registration exists and the registration threw', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      jest
+        .spyOn(logic, 'createRegisterSafePayload')
+        .mockImplementation(() => Promise.resolve({} as logic.RegisterDeviceDto))
+
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject()
+      })
+
+      const registration = await logic.registerSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to register Safe')
+
+      expect(registration).toBe(undefined)
+    })
+
+    it('should return the current registration if one exists and the registration threw', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      jest
+        .spyOn(logic, 'createRegisterSafePayload')
+        .mockImplementation(() => Promise.resolve({} as logic.RegisterDeviceDto))
+
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject()
+      })
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      const registration = await logic.registerSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+        currentRegistration,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to register Safe')
+
+      expect(registration).toBe(currentRegistration)
+    })
+
+    it('should return the registration payload if the registration succeeded', async () => {
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      const registrationPayload: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      jest.spyOn(logic, 'createRegisterSafePayload').mockImplementation(() => Promise.resolve(registrationPayload))
+
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+          status: 200,
+          ok: true,
+        })
+      })
+
+      const registration = await logic.registerSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        mockProvider,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).not.toHaveBeenCalled()
+
+      expect(registration).not.toBe(registrationPayload)
+    })
+  })
+
+  describe('unregisterSafe', () => {
+    it('should return the current registration if the unregistration was unsuccessful', async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+          status: 69420, // Failed
+          ok: true,
+        })
+      })
+
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      const updatedRegistration = await logic._unregisterSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        currentRegistration,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to unregister Safe')
+
+      expect(updatedRegistration).toEqual(currentRegistration)
+    })
+
+    it('should return the current registration if the unregistration threw', async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject()
+      })
+
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+        ],
+      }
+
+      const updatedRegistration = await logic._unregisterSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        currentRegistration,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).toHaveBeenCalledTimes(1)
+      expect(alertMock).toHaveBeenCalledWith('Unable to unregister Safe')
+
+      expect(updatedRegistration).toEqual(currentRegistration)
+    })
+
+    it('should return the updated registration if the registration succeeded', async () => {
+      global.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve({}),
+          status: 200,
+          ok: true,
+        })
+      })
+
+      const safeAddress = hexZeroPad('0x1', 20)
+      const chainId = '1'
+
+      const currentRegistration: logic.RegisterDeviceDto = {
+        uuid: crypto.randomUUID(),
+        cloudMessagingToken: crypto.randomUUID(),
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: crypto.randomUUID(),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [safeAddress, hexZeroPad('0x2', 20)],
+            signatures: [hexZeroPad('0xDEAD', 65)],
+          },
+          {
+            chainId: '5',
+            safes: [safeAddress], // Same address Safe on a different chain
+            signatures: [hexZeroPad('0xBEEF', 65)],
+          },
+        ],
+      }
+
+      const updatedRegistration = await logic._unregisterSafe(
+        { chainId, address: { value: safeAddress } } as SafeInfo,
+        currentRegistration,
+      )
+
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      expect(alertMock).not.toHaveBeenCalled()
+
+      expect(updatedRegistration?.timestamp).not.toBe(currentRegistration.timestamp)
+
+      expect(updatedRegistration).toEqual({
+        uuid: currentRegistration.uuid, // Same UUID
+        cloudMessagingToken: currentRegistration.cloudMessagingToken, // Same token
+        buildNumber: '0',
+        bundle: 'https://app.safe.global',
+        deviceType: logic.DeviceType.WEB,
+        version: packageJson.version,
+        timestamp: expect.any(String),
+        safeRegistrations: [
+          {
+            chainId,
+            safes: [hexZeroPad('0x2', 20)],
+            signatures: [],
+          },
+          {
+            chainId: '5',
+            safes: [safeAddress], // Same address Safe on a different chain
+            signatures: [hexZeroPad('0xBEEF', 65)],
+          },
+        ],
+      })
+    })
+  })
+})
---
+++ src/components/settings/Notifications/logic.ts
@@ -0,0 +1,200 @@
+import { keccak256, toUtf8Bytes } from 'ethers/lib/utils'
+import { getToken, getMessaging } from 'firebase/messaging'
+import type { SafeInfo } from '@safe-global/safe-gateway-typescript-sdk'
+import type { Web3Provider } from '@ethersproject/providers'
+
+import packageJson from '../../../../package.json'
+import { FIREBASE_MESSAGING_SW_PATH, FIREBASE_VAPID_KEY, GATEWAY_URL_STAGING } from '@/config/constants'
+
+export const requestNotificationPermission = async (): Promise<boolean> => {
+  if (Notification.permission === 'granted') {
+    return true
+  }
+
+  let permission: NotificationPermission | undefined
+
+  try {
+    permission = await Notification.requestPermission()
+  } catch (e) {
+    console.error('Error requesting notification permission', e)
+  }
+
+  const isGranted = permission === 'granted'
+
+  if (!isGranted) {
+    alert('You must allow notifications to register your device.')
+  }
+
+  return isGranted
+}
+
+export const enum DeviceType {
+  WEB = 'WEB',
+}
+
+export type RegisterDeviceDto = {
+  uuid: string
+  cloudMessagingToken: string
+  buildNumber: string
+  bundle: string
+  deviceType: DeviceType
+  version: string
+  timestamp: string
+  safeRegistrations: Array<{
+    chainId: string
+    safes: Array<string>
+    signatures: Array<string>
+  }>
+}
+
+const getTimestampWithoutMilliseconds = () => {
+  return Math.floor(new Date().getTime() / 1000).toString()
+}
+
+export const createRegisterSafePayload = async (
+  safe: SafeInfo,
+  web3: Web3Provider,
+  currentRegistration?: RegisterDeviceDto,
+): Promise<RegisterDeviceDto | undefined> => {
+  const MESSAGE_PREFIX = 'gnosis-safe'
+
+  const currentChainSafeRegistrations = currentRegistration?.safeRegistrations.find(
+    (registration) => registration.chainId === safe.chainId,
+  )?.safes
+
+  const safeAddress = safe.address.value
+
+  // Safe is already registered
+  if (currentChainSafeRegistrations?.includes(safeAddress)) {
+    return currentRegistration
+  }
+
+  const swRegistration = await navigator.serviceWorker.getRegistration(FIREBASE_MESSAGING_SW_PATH)
+
+  // Get Firebase token
+  const messaging = getMessaging()
+  const token = await getToken(messaging, {
+    vapidKey: FIREBASE_VAPID_KEY,
+    serviceWorkerRegistration: swRegistration,
+  })
+
+  // If uuid is not provided a new device will be created.
+  // If a uuid for an existing Safe is provided the FirebaseDevice will be updated with all the new data provided.
+  // Safes provided on the request are always added and never removed/replaced
+  // Signature must sign `keccack256('gnosis-safe{timestamp-epoch}{uuid}{cloud_messaging_token}{safes_sorted}':
+  //   - `{timestamp-epoch}` must be an integer (no milliseconds)
+  //   - `{safes_sorted}` must be checksummed safe addresses sorted and joined with no spaces
+
+  // @see https://github.com/safe-global/safe-transaction-service/blob/3644c08ac4b01b6a1c862567bc1d1c81b1a8c21f/safe_transaction_service/notifications/views.py#L19-L24
+
+  const timestamp = getTimestampWithoutMilliseconds()
+  const uuid = currentRegistration?.uuid ?? self.crypto.randomUUID()
+
+  const safesToRegister = currentChainSafeRegistrations
+    ? [...currentChainSafeRegistrations, safeAddress]
+    : [safeAddress]
+
+  const message = MESSAGE_PREFIX + timestamp + uuid + token + safesToRegister.join('')
+  const hashedMessage = keccak256(toUtf8Bytes(message))
+
+  const signature = await web3.getSigner().signMessage(hashedMessage)
+
+  return {
+    uuid,
+    cloudMessagingToken: token,
+    buildNumber: '0', // Required value, but does not exist on web
+    bundle: location.origin,
+    deviceType: DeviceType.WEB,
+    version: packageJson.version,
+    timestamp,
+    safeRegistrations: [
+      {
+        chainId: safe.chainId,
+        safes: safesToRegister,
+        signatures: [signature],
+      },
+    ],
+  }
+}
+
+export const registerSafe = async (
+  safe: SafeInfo,
+  web3: Web3Provider,
+  currentRegistration?: RegisterDeviceDto,
+): Promise<RegisterDeviceDto | undefined> => {
+  const SAFE_REGISTRATION_ENDPOINT = `${GATEWAY_URL_STAGING}/v1/register/notifications`
+
+  let didRegister = false
+
+  let payload: RegisterDeviceDto | undefined
+
+  try {
+    payload = await createRegisterSafePayload(safe, web3, currentRegistration)
+
+    const response = await fetch(SAFE_REGISTRATION_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+
+    // Gateway will return 200 if the device was registered successfully
+    // @see https://github.com/safe-global/safe-client-gateway-nest/blob/27b6b3846b4ecbf938cdf5d0595ca464c10e556b/src/routes/notifications/notifications.service.ts#L29
+    didRegister = response.ok && response.status === 200
+  } catch (e) {
+    console.error('Error registering Safe', e)
+  }
+
+  if (!didRegister) {
+    alert('Unable to register Safe')
+    return currentRegistration
+  }
+
+  return payload
+}
+
+export const _unregisterSafe = async (
+  safe: SafeInfo,
+  currentRegistration: RegisterDeviceDto,
+): Promise<RegisterDeviceDto> => {
+  const SAFE_UNREGISTRATION_ENDPOINT = `${GATEWAY_URL_STAGING}/v1/chains/${safe.chainId}/notifications/devices/${currentRegistration.uuid}/safes/${safe.address.value}`
+
+  let didUnregister = false
+
+  try {
+    const response = await fetch(SAFE_UNREGISTRATION_ENDPOINT, {
+      method: 'DELETE',
+    })
+
+    didUnregister = response.ok && response.status === 200
+  } catch (e) {
+    console.error('Error unregistering Safe', e)
+  }
+
+  if (!didUnregister) {
+    alert('Unable to unregister Safe')
+    return currentRegistration
+  }
+
+  // Remove deleted Safe from registration and clear signatures
+  const updatedSafeRegistrations = currentRegistration.safeRegistrations.map((registration) => {
+    if (registration.chainId !== safe.chainId) {
+      return registration
+    }
+
+    const updatedSafes = registration.safes.filter((safeAddress) => safeAddress !== safe.address.value)
+
+    return {
+      ...registration,
+      safes: updatedSafes,
+      signatures: [],
+    }
+  })
+
+  return {
+    ...currentRegistration,
+    timestamp: getTimestampWithoutMilliseconds(),
+    safeRegistrations: updatedSafeRegistrations,
+  }
+}
