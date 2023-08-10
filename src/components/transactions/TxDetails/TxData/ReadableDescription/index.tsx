15db3c570fb0c9990dbe73b6524a59e0a8aeb1e5
Usame Algan <usame@safe.global>
2023-08-10 12:09:30 +0200

    Add some spacing

--- src/components/transactions/TxDetails/TxData/ReadableDescription/index.tsx
+++ src/components/transactions/TxDetails/TxData/ReadableDescription/index.tsx
@@ -1,11 +1,12 @@
 import { type TransactionData } from '@safe-global/safe-gateway-typescript-sdk'
+import css from './styles.module.css'
 
 const ReadableDescription = ({ txData }: { txData?: TransactionData }) => {
   // @ts-ignore TODO: Update gateway-sdk
   if (!txData?.readableDescription) return null
 
   // @ts-ignore TODO: Update gateway-sdk
-  return <div>{txData.readableDescription}</div>
+  return <div className={css.wrapper}>{txData.readableDescription}</div>
 }
 
 export default ReadableDescription
