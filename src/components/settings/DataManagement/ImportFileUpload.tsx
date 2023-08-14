ba62ea13f9ece33e038c43d6cf2b503115242938
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-08-14 15:05:57 +0200

    Feat: import/export in the Safe List sidebar (#2299)
    * Feat: import/export in the Safe List sidebar
    
    * Adjust style

--- src/components/settings/DataManagement/ImportFileUpload.tsx
+++ src/components/settings/DataManagement/ImportFileUpload.tsx
@@ -52,7 +52,7 @@ export const ImportFileUpload = ({
 
   return (
     <>
-      <Typography>Import {'Safe{Wallet}'} data by clicking or dragging a file below.</Typography>
+      <Typography>Import {'Safe{Wallet}'} data by uploading a file in the area below.</Typography>
 
       <FileUpload
         fileType={FileTypes.JSON}
@@ -74,7 +74,7 @@ export const ImportFileUpload = ({
             mr: 0.5,
           }}
         />
-        Only JSON files exported from a {'Safe{Wallet}'} can be imported.
+        Only JSON files exported from the {'Safe{Wallet}'} can be imported.
       </Typography>
     </>
   )
