ada4501b823c8379bb76913d569c25075708c03b
iamacook <aaron@safe.global>
2023-08-16 13:29:21 +0200

    fix: test

--- src/hooks/__tests__/useSafeTokenAllocation.test.ts
+++ src/hooks/__tests__/useSafeTokenAllocation.test.ts
@@ -69,6 +69,9 @@ describe('useSafeTokenAllocation', () => {
 
   beforeEach(() => {
     jest.resetAllMocks()
+    // Clear memoization cache
+    _getRedeemDeadline.cache.clear?.()
+
     jest.spyOn(useSafeInfoHook, 'default').mockImplementation(
       () =>
         ({
