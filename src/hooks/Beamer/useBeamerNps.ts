4a7ff25fb3f2fc3cf29795622ea43c46de9b9df5
iamacook <aaron@safe.global>
2023-08-29 17:24:46 +0200

    refactor: unsubscribe if shown

--- src/hooks/Beamer/useBeamerNps.ts
+++ src/hooks/Beamer/useBeamerNps.ts
@@ -1,30 +1,31 @@
 import { useEffect } from 'react'
 
-import { BEAMER_ID } from '@/config/constants'
 import { TxEvent, txSubscribe } from '@/services/tx/txEvents'
-
-const COOKIE_NAME = `_BEAMER_NPS_LAST_SHOWN_${BEAMER_ID}`
+import { useAppSelector } from '@/store'
+import { selectCookies, CookieType } from '@/store/cookiesSlice'
+import { shouldShowBeamerNps } from '@/services/beamer'
 
 export const useBeamerNps = (): void => {
+  const cookies = useAppSelector(selectCookies)
+  const isBeamerEnabled = cookies[CookieType.UPDATES]
+
   useEffect(() => {
-    if (typeof window === 'undefined' || !window.Beamer) {
+    if (typeof window === 'undefined' || !isBeamerEnabled) {
       return
     }
 
     const unsubscribe = txSubscribe(TxEvent.PROPOSED, () => {
-      // Beamer advise using their '/nps/check' endpoint to see if the NPS should be shown
-      // As we need to check this more than the request limit, we instead check the cookie
-      // @see https://www.getbeamer.com/api
-      const lastShown = window.Beamer.getCookie(COOKIE_NAME)
-
-      // We "force" the NPS banner as we have it globally disabled in Beamer to prevent it
-      // randomly showing on pages that we don't want it to
-      // Note: this is not documented but confirmed by Beamer support
-      if (!lastShown) {
-        window.Beamer.forceShowNPS()
+      // Cannot check at the top of effect as Beamer may not have loaded yet
+      if (shouldShowBeamerNps()) {
+        // We "force" the NPS banner as we have it globally disabled in Beamer to prevent it
+        // randomly showing on pages that we don't want it to
+        // Note: this is not documented but confirmed by Beamer support
+        window.Beamer?.forceShowNPS()
       }
+
+      unsubscribe()
     })
 
     return unsubscribe
-  }, [])
+  }, [isBeamerEnabled])
 }
