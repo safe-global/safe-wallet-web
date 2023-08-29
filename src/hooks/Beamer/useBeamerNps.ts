22f7e30dd3590f4ddb68ddf67c59608c295745a3
iamacook <aaron@safe.global>
2023-08-29 12:47:00 +0200

    fix: add clarification comments

--- src/hooks/Beamer/useBeamerNps.ts
+++ src/hooks/Beamer/useBeamerNps.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 import { BEAMER_ID } from '@/config/constants'
 
-const LS_KEY = `_BEAMER_NPS_LAST_SHOWN_${BEAMER_ID}`
+const COOKIE_NAME = `_BEAMER_NPS_LAST_SHOWN_${BEAMER_ID}`
 
 export const useBeamerNps = (): void => {
   useEffect(() => {
@@ -10,8 +10,14 @@ export const useBeamerNps = (): void => {
       return
     }
 
-    const lastShown = window.Beamer.getCookie(LS_KEY)
+    // Beamer advise using their '/nps/check' endpoint to see if the NPS should be shown
+    // As we need to check this more than the request limit, we instead check the cookie
+    // @see https://www.getbeamer.com/api
+    const lastShown = window.Beamer.getCookie(COOKIE_NAME)
 
+    // We "force" the NPS banner as we have it globally disabled in Beamer to prevent it
+    // randomly showing on pages that we don't want it to
+    // Note: this is not documented but confirmed by Beamer support
     if (!lastShown) {
       window.Beamer.forceShowNPS()
     }
