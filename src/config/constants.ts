f272cc2f1f3e296c4a5d3ff42372429529ea13cf
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-09-04 16:25:01 +0100

    Update default value for Relay Service URL (staging)
    Updates the default value set for `SAFE_RELAY_SERVICE_URL_STAGING` from `https://safe-client-nest.staging.5afe.dev/v1/relay` to `https://safe-client.staging.5afe.dev/v1/relay`

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -64,7 +64,7 @@ export enum SafeAppsTag {
 export const SAFE_RELAY_SERVICE_URL_PRODUCTION =
   process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_PRODUCTION || 'https://safe-client-nest.safe.global/v1/relay'
 export const SAFE_RELAY_SERVICE_URL_STAGING =
-  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_STAGING || 'https://safe-client-nest.staging.5afe.dev/v1/relay'
+  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_STAGING || 'https://safe-client.staging.5afe.dev/v1/relay'
 
 // Help Center
 export const HELP_CENTER_URL = 'https://help.safe.global'
