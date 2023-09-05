b04ba7eb5b9aa8ff60ff8ab1f5739b79475360a4
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2023-09-05 13:16:31 +0100

    Fix: update default value for Relay Service URL (#2488)
    * Update default value for Relay Service URL (staging)
    
    Updates the default value set for `SAFE_RELAY_SERVICE_URL_STAGING` from `https://safe-client-nest.staging.5afe.dev/v1/relay` to `https://safe-client.staging.5afe.dev/v1/relay`
    
    * Update default value for Relay Service URL (production)
    
    Updates the default value set for `NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_PRODUCTION` from `https://safe-client-nest.safe.global/v1/relay` to `https://safe-client.safe.global/v1/relay`

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -62,9 +62,9 @@ export enum SafeAppsTag {
 
 // Safe Gelato relay service
 export const SAFE_RELAY_SERVICE_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_PRODUCTION || 'https://safe-client-nest.safe.global/v1/relay'
+  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_PRODUCTION || 'https://safe-client.safe.global/v1/relay'
 export const SAFE_RELAY_SERVICE_URL_STAGING =
-  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_STAGING || 'https://safe-client-nest.staging.5afe.dev/v1/relay'
+  process.env.NEXT_PUBLIC_SAFE_RELAY_SERVICE_URL_STAGING || 'https://safe-client.staging.5afe.dev/v1/relay'
 
 // Help Center
 export const HELP_CENTER_URL = 'https://help.safe.global'
