c76a400338d5d409d844130d46e8808da54549ed
iamacook <aaron@safe.global>
2023-09-21 11:20:19 +0200

    fix: forward ref

--- src/components/walletconnect/HeaderWidget/Icon.tsx
+++ src/components/walletconnect/HeaderWidget/Icon.tsx
@@ -1,16 +1,19 @@
 import { Button, SvgIcon, Typography } from '@mui/material'
 import WalletConnectIcon from '@/public/images/common/walletconnect.svg'
+import { forwardRef } from 'react'
 
 type IconProps = {
   onClick: () => void
   ref: React.RefObject<HTMLButtonElement>
 }
 
-const Icon = (props: IconProps) => (
-  <Button onClick={props.onClick} ref={props.ref}>
+const Icon = forwardRef<HTMLButtonElement, IconProps>((props, ref) => (
+  <Button onClick={props.onClick} ref={ref}>
     <SvgIcon component={WalletConnectIcon} inheritViewBox />
     <Typography ml={1}>WalletConnect</Typography>
   </Button>
-)
+))
+
+Icon.displayName = 'Icon'
 
 export default Icon
