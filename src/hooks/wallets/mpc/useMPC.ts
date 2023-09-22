cf04f22fa94e45c17abfce5b8d20c2f2545986f1
schmanu <manu@safe.global>
2023-09-22 13:39:03 +0200

    test: mpc wallet session rehydration, chain switching, onboard connection

--- src/hooks/wallets/mpc/useMPC.ts
+++ src/hooks/wallets/mpc/useMPC.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import ExternalStore from '@/services/ExternalStore'
-import { Web3AuthMPCCoreKit, WEB3AUTH_NETWORK, COREKIT_STATUS } from '@web3auth/mpc-core-kit'
+import { Web3AuthMPCCoreKit, WEB3AUTH_NETWORK } from '@web3auth/mpc-core-kit'
 import { CHAIN_NAMESPACES } from '@web3auth/base'
 
 import { WEB3_AUTH_CLIENT_ID } from '@/config/constants'
@@ -29,6 +29,14 @@ export const useInitMPC = () => {
       tickerName: chain.nativeCurrency.name,
     }
 
+    const currentInstance = getStore()
+    let previousChainChangedListeners: Function[] = []
+    if (currentInstance?.provider) {
+      // We are already connected. We copy onboards event listener for the chainChanged event to propagate a potentially new chainId
+      const oldProvider = currentInstance.provider
+      previousChainChangedListeners = oldProvider.listeners('chainChanged')
+    }
+
     const web3AuthCoreKit = new Web3AuthMPCCoreKit({
       web3AuthClientId: WEB3_AUTH_CLIENT_ID,
       // Available networks are "sapphire_devnet", "sapphire_mainnet"
@@ -43,10 +51,8 @@ export const useInitMPC = () => {
       .init()
       .then(() => {
         setStore(web3AuthCoreKit)
-
         // If rehydration was successful, connect to onboard
-        if (web3AuthCoreKit.status === COREKIT_STATUS.INITIALIZED) {
-          console.log('Logged in', web3AuthCoreKit)
+        if (web3AuthCoreKit.provider) {
           // await mpcCoreKit.enableMFA({})
           const connectedWallet = getConnectedWallet(onboard.state.get().wallets)
           if (!connectedWallet) {
@@ -57,19 +63,15 @@ export const useInitMPC = () => {
               },
             }).catch((reason) => console.error('Error connecting to MPC module:', reason))
           } else {
+            const newProvider = web3AuthCoreKit.provider
+
             // To propagate the changedChain we disconnect and connect
-            onboard
-              .disconnectWallet({
-                label: ONBOARD_MPC_MODULE_LABEL,
-              })
-              .then(() =>
-                connectWallet(onboard, {
-                  autoSelect: {
-                    label: ONBOARD_MPC_MODULE_LABEL,
-                    disableModals: true,
-                  },
-                }),
+            if (previousChainChangedListeners.length > 0 && newProvider) {
+              previousChainChangedListeners.forEach((previousListener) =>
+                newProvider.addListener('chainChanged', (...args: []) => previousListener(...args)),
               )
+              newProvider.emit('chainChanged', `0x${Number(chainConfig.chainId).toString(16)}`)
+            }
           }
         }
       })
@@ -78,5 +80,7 @@ export const useInitMPC = () => {
 }
 
 export const getMPCCoreKitInstance = getStore
+
+export const setMPCCoreKitInstance = setStore
 
 export default useStore
