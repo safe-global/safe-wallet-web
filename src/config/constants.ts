89bcb821966bcff2b715e0e43b4b5ae920dad6ff
Aaron Cook <aaron@safe.global>
2023-09-25 10:31:23 +0200

    fix: boolean env vars (#2532)
    * fix: boolean env vars
    
    * fix: remove fallback

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import chains from './chains'
 
-export const IS_PRODUCTION = !!process.env.NEXT_PUBLIC_IS_PRODUCTION
+export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
 export const GATEWAY_URL_PRODUCTION =
@@ -91,7 +91,7 @@ export const DISCORD_URL = 'https://chat.safe.global'
 export const TWITTER_URL = 'https://twitter.com/safe'
 
 // Legal
-export const IS_OFFICIAL_HOST = process.env.NEXT_PUBLIC_IS_OFFICIAL_HOST || false
+export const IS_OFFICIAL_HOST = process.env.NEXT_PUBLIC_IS_OFFICIAL_HOST === 'true'
 
 // Risk mitigation (Redefine)
 export const REDEFINE_SIMULATION_URL = 'https://dashboard.redefine.net/reports/'
