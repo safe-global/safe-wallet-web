d18d622bd0ad0ddabeabc229bfdf894329d9aede
katspaugh <katspaugh@gmail.com>
2023-09-26 09:01:01 +0200

    Text edits

--- src/components/common/PairingDetails/PairingDeprecationWarning.tsx
+++ src/components/common/PairingDetails/PairingDeprecationWarning.tsx
@@ -3,8 +3,8 @@ import { Alert } from '@mui/material'
 const PairingDeprecationWarning = (): React.ReactElement => {
   return (
     <Alert severity="warning" sx={{ mb: 4 }}>
-      We&apos;re sorry to share that the Safe mobile pairing feature will be <b>discontinued</b> on 15th November 2023.
-      Please plan a migration to a different wallet as soon as possible.
+      The {'Safe{Wallet}'} web-mobile pairing feature will be discontinued from 15th November 2023. Please migrate to a
+      different signer wallet before this date.
     </Alert>
   )
 }
