d435add526287d4e4d2791e4ff9d122205e6a28b
katspaugh <katspaugh@gmail.com>
2023-09-29 15:03:21 +0200

    Only map

--- src/utils/mad-props.tsx
+++ src/utils/mad-props.tsx
@@ -1,26 +1,21 @@
 import type { ComponentType, FC } from 'react'
 import React, { memo } from 'react'
 
-type SinglePropHook<T> = () => T
 type HookMap<P> = {
-  [K in keyof P]?: SinglePropHook<P[K]>
+  [K in keyof P]?: () => P[K]
 }
 
 const madProps = <P extends Record<string, unknown>, H extends keyof P>(
   Component: ComponentType<P>,
-  useHook: SinglePropHook<Pick<P, H>> | HookMap<Pick<P, H>>,
+  useHook: HookMap<Pick<P, H>>,
 ): FC<Omit<P, H>> => {
   const MadComponent = (externalProps: Omit<P, H>) => {
     let newProps: P = { ...externalProps } as P
 
-    if (typeof useHook === 'function') {
-      newProps = { ...newProps, ...useHook() } as P
-    } else {
-      for (const key in useHook) {
-        const hook = useHook[key]
-        if (hook !== undefined) {
-          newProps[key as H] = hook()
-        }
+    for (const key in useHook) {
+      const hook = useHook[key]
+      if (hook !== undefined) {
+        newProps[key as H] = hook()
       }
     }
 
