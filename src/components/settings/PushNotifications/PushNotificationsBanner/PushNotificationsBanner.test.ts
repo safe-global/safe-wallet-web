4bc563c4f4ea99d55e74b31169a6ba8e36aae2d4
Aaron Cook <aaron@safe.global>
2023-10-04 18:05:06 +0200

    fix: enable notifications per chain (#2584)
    * fix: enable notifications per chain
    
    * fix: adjust text
    
    * fix: "Safes" -> "Safe Accounts"
    
    * fix: remove Safes on unsupported chains

--- src/components/settings/PushNotifications/PushNotificationsBanner/PushNotificationsBanner.test.ts
+++ src/components/settings/PushNotifications/PushNotificationsBanner/PushNotificationsBanner.test.ts
@@ -1,39 +1,28 @@
 import { _getSafesToRegister } from '.'
-import type { AddedSafesState } from '@/store/addedSafesSlice'
+import type { AddedSafesOnChain } from '@/store/addedSafesSlice'
 import type { PushNotificationPreferences } from '@/services/push-notifications/preferences'
 
 describe('PushNotificationsBanner', () => {
   describe('getSafesToRegister', () => {
     it('should return all added safes if no preferences exist', () => {
-      const addedSafes = {
-        '1': {
-          '0x123': {},
-          '0x456': {},
-        },
-        '4': {
-          '0x789': {},
-        },
-      } as unknown as AddedSafesState
+      const addedSafesOnChain = {
+        '0x123': {},
+        '0x456': {},
+      } as unknown as AddedSafesOnChain
       const allPreferences = undefined
 
-      const result = _getSafesToRegister(addedSafes, allPreferences)
+      const result = _getSafesToRegister('1', addedSafesOnChain, allPreferences)
 
       expect(result).toEqual({
         '1': ['0x123', '0x456'],
-        '4': ['0x789'],
       })
     })
 
     it('should return only newly added safes if preferences exist', () => {
-      const addedSafes = {
-        '1': {
-          '0x123': {},
-          '0x456': {},
-        },
-        '4': {
-          '0x789': {},
-        },
-      } as unknown as AddedSafesState
+      const addedSafesOnChain = {
+        '0x123': {},
+        '0x456': {},
+      } as unknown as AddedSafesOnChain
       const allPreferences = {
         '1:0x123': {
           safeAddress: '0x123',
@@ -45,7 +34,7 @@ describe('PushNotificationsBanner', () => {
         },
       } as unknown as PushNotificationPreferences
 
-      const result = _getSafesToRegister(addedSafes, allPreferences)
+      const result = _getSafesToRegister('1', addedSafesOnChain, allPreferences)
 
       expect(result).toEqual({
         '1': ['0x456'],
@@ -53,15 +42,10 @@ describe('PushNotificationsBanner', () => {
     })
 
     it('should return all added safes if no preferences match', () => {
-      const addedSafes = {
-        '1': {
-          '0x123': {},
-          '0x456': {},
-        },
-        '4': {
-          '0x789': {},
-        },
-      } as unknown as AddedSafesState
+      const addedSafesOnChain = {
+        '0x123': {},
+        '0x456': {},
+      } as unknown as AddedSafesOnChain
       const allPreferences = {
         '1:0x111': {
           safeAddress: '0x111',
@@ -73,11 +57,10 @@ describe('PushNotificationsBanner', () => {
         },
       } as unknown as PushNotificationPreferences
 
-      const result = _getSafesToRegister(addedSafes, allPreferences)
+      const result = _getSafesToRegister('1', addedSafesOnChain, allPreferences)
 
       expect(result).toEqual({
         '1': ['0x123', '0x456'],
-        '4': ['0x789'],
       })
     })
   })
