1a17c5072b3fd2a9243afd8ba132ad261ad266c5
Aaron Cook <aaron@safe.global>
2023-10-12 18:43:59 +0200

    fix: change colour of icon + add error badge (#2627)

--- src/components/walletconnect/SessionManager/Header.tsx
+++ src/components/walletconnect/SessionManager/Header.tsx
@@ -2,18 +2,17 @@ import { SvgIcon, Typography } from '@mui/material'
 import type { ReactElement } from 'react'
 
 import WalletConnect from '@/public/images/common/walletconnect.svg'
+import Alert from '@/public/images/notifications/alert.svg'
+
+import css from './styles.module.css'
 
 export const WalletConnectHeader = ({ error }: { error?: boolean }): ReactElement => {
   return (
     <>
-      <SvgIcon
-        component={WalletConnect}
-        inheritViewBox
-        sx={{
-          color: error ? '#FF5F72' : '#3A99FB',
-          width: '40px',
-        }}
-      />
+      <div>
+        <SvgIcon component={WalletConnect} inheritViewBox className={css.icon} />
+        {error && <SvgIcon component={Alert} inheritViewBox className={css.errorBadge} fontSize="small" />}
+      </div>
 
       <Typography variant="h5" mt={2} mb={0.5}>
         Connect dApp to {`Safe{Wallet}`}
