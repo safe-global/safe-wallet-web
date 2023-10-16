53984fb25be45e078699cc4bdae5ed4a238993c8
Usame Algan <usame@safe.global>
2023-10-16 16:05:10 +0200

    feat: Implement new form layout

--- src/components/settings/SignerAccountMFA/PasswordForm.tsx
+++ src/components/settings/SignerAccountMFA/PasswordForm.tsx
@@ -1,9 +1,24 @@
 import { SecurityQuestionRecovery } from '@/hooks/wallets/mpc/recovery/SecurityQuestionRecovery'
-import { Typography, TextField, Button, Box } from '@mui/material'
+import {
+  Typography,
+  Button,
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Grid,
+  FormControl,
+  SvgIcon,
+  Divider,
+} from '@mui/material'
 import { type Web3AuthMPCCoreKit } from '@web3auth/mpc-core-kit'
 import { useState, useMemo } from 'react'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { enableMFA } from './helper'
+import CheckIcon from '@/public/images/common/check-filled.svg'
+import LockWarningIcon from '@/public/images/common/lock-warning.svg'
+import PasswordInput from '@/components/settings/SignerAccountMFA/PasswordInput'
+import css from './styles.module.css'
 
 enum PasswordFieldNames {
   oldPassword = 'oldPassword',
@@ -27,7 +42,7 @@ export const PasswordForm = ({ mpcCoreKit }: { mpcCoreKit: Web3AuthMPCCoreKit })
     },
   })
 
-  const { register, formState, getValues, handleSubmit } = formMethods
+  const { formState, getValues, handleSubmit } = formMethods
 
   const [enablingMFA, setEnablingMFA] = useState(false)
 
@@ -46,61 +61,100 @@ export const PasswordForm = ({ mpcCoreKit }: { mpcCoreKit: Web3AuthMPCCoreKit })
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box display="flex" flexDirection="column" gap={3} alignItems="baseline">
-        {isPasswordSet ? (
-          <Typography>You already have a recovery password setup.</Typography>
-        ) : (
-          <Typography>You have no password setup. We suggest adding one to secure your Account.</Typography>
-        )}
-        {isPasswordSet && (
-          <TextField
-            placeholder="Old password"
-            label="Old password"
-            type="password"
-            error={!!formState.errors[PasswordFieldNames.oldPassword]}
-            helperText={formState.errors[PasswordFieldNames.oldPassword]?.message}
-            {...register(PasswordFieldNames.oldPassword, {
-              required: true,
-            })}
-          />
-        )}
-        <TextField
-          placeholder="New password"
-          label="New password"
-          type="password"
-          error={!!formState.errors[PasswordFieldNames.newPassword]}
-          helperText={formState.errors[PasswordFieldNames.newPassword]?.message}
-          {...register(PasswordFieldNames.newPassword, {
-            required: true,
-            minLength: 6,
-          })}
-        />
-        <TextField
-          placeholder="Confirm new password"
-          label="Confirm new password"
-          type="password"
-          error={!!formState.errors[PasswordFieldNames.confirmPassword]}
-          helperText={formState.errors[PasswordFieldNames.confirmPassword]?.message}
-          {...register(PasswordFieldNames.confirmPassword, {
-            required: true,
-            validate: (value: string) => {
-              const currentNewPW = getValues(PasswordFieldNames.newPassword)
-              if (value !== currentNewPW) {
-                return 'Passwords do not match'
-              }
-            },
-          })}
-        />
+    <FormProvider {...formMethods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box display="flex" flexDirection="column" gap={3} alignItems="baseline">
+          {isPasswordSet ? (
+            <Typography>You already have a recovery password setup.</Typography>
+          ) : (
+            <Typography>You have no password setup. We suggest adding one to secure your Account.</Typography>
+          )}
+          <Accordion expanded={isPasswordSet}>
+            <AccordionSummary>
+              <Box display="flex" alignItems="center" gap={1}>
+                <SvgIcon component={CheckIcon} sx={{ color: isPasswordSet ? 'success.main' : 'border.light' }} />
+                <Typography fontWeight="bold">Password</Typography>
+              </Box>
+            </AccordionSummary>
+            <AccordionDetails sx={{ p: 0 }}>
+              <Grid container>
+                <Grid item container xs={12} md={7} gap={3} p={3}>
+                  <Typography>
+                    Your password will be used to access your social key and serve as a recovery factor.
+                  </Typography>
+                  {isPasswordSet && (
+                    <>
+                      <FormControl fullWidth>
+                        <PasswordInput
+                          name={PasswordFieldNames.oldPassword}
+                          placeholder="Old password"
+                          label="Old password"
+                          helperText={formState.errors[PasswordFieldNames.oldPassword]?.message}
+                          required
+                        />
+                      </FormControl>
+                      <Divider sx={{ width: 1 }} />
+                    </>
+                  )}
 
-        <Button
-          sx={{ justifySelf: 'flex-start' }}
-          disabled={!formMethods.formState.isValid || enablingMFA}
-          type="submit"
-        >
-          Change
-        </Button>
-      </Box>
-    </form>
+                  <FormControl fullWidth>
+                    <PasswordInput
+                      name={PasswordFieldNames.newPassword}
+                      placeholder="New password"
+                      label="New password"
+                      helperText={formState.errors[PasswordFieldNames.newPassword]?.message}
+                      required
+                    />
+                  </FormControl>
+
+                  <FormControl fullWidth>
+                    <PasswordInput
+                      name={PasswordFieldNames.confirmPassword}
+                      placeholder="Confirm new password"
+                      label="Confirm new password"
+                      helperText={formState.errors[PasswordFieldNames.confirmPassword]?.message}
+                      validate={(value: string) => {
+                        const currentNewPW = getValues(PasswordFieldNames.newPassword)
+                        if (value !== currentNewPW) {
+                          return 'Passwords do not match'
+                        }
+                      }}
+                      required
+                    />
+                  </FormControl>
+
+                  <Button
+                    sx={{ justifySelf: 'flex-end', marginLeft: 'auto', fontSize: '14px' }}
+                    disabled={!formMethods.formState.isValid || enablingMFA}
+                    type="submit"
+                    variant="contained"
+                  >
+                    {isPasswordSet ? 'Change' : 'Create'} Password
+                  </Button>
+                </Grid>
+                <Grid item xs={12} md={5} p={3} sx={{ borderLeft: '1px solid #DCDEE0' }}>
+                  <Box>
+                    <LockWarningIcon />
+                    <Typography variant="subtitle1" fontWeight="bold">
+                      You won&apos;t be able to restore this password
+                    </Typography>
+                    <ol className={css.list}>
+                      <Typography component="li" variant="body2">
+                        You will have to input this password if you need to recover access to Safe in another browser or
+                        on another device.
+                      </Typography>
+                      <Typography component="li" variant="body2">
+                        We suggest to use a password manager or to write the password down on a piece of paper and
+                        secure it.
+                      </Typography>
+                    </ol>
+                  </Box>
+                </Grid>
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+      </form>
+    </FormProvider>
   )
 }
