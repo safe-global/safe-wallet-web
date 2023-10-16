34acfb956f365c464d623eb16cfbbdb3ec2c640f
iamacook <aaron@safe.global>
2023-10-16 14:57:21 +0200

    refactor: move notification logic to own file

--- src/services/safe-wallet-provider/useSafeWalletProvider.tsx
+++ src/services/safe-wallet-provider/useSafeWalletProvider.tsx
@@ -16,41 +16,7 @@ import { getTransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
 import { getAddress } from 'ethers/lib/utils'
 import { AppRoutes } from '@/config/routes'
 import useChains from '@/hooks/useChains'
-
-const showNotification = (title: string, options?: NotificationOptions) => {
-  if (Notification.permission !== 'granted' || document.hasFocus()) {
-    return
-  }
-
-  const notification = new Notification(title, {
-    icon: '/images/safe-logo-green.png',
-    ...options,
-  })
-
-  notification.onclick = () => {
-    window.focus()
-    notification.close()
-  }
-
-  setTimeout(() => {
-    notification.close()
-  }, 5_000)
-}
-
-const NotificationMessages: Record<string, (appInfo: AppInfo) => { title: string; options: NotificationOptions }> = {
-  SIGNATURE_REQUEST: (appInfo: AppInfo) => ({
-    title: 'Signature request',
-    options: {
-      body: `${appInfo.name} wants you to sign a message. Open the Safe{Wallet} to continue.`,
-    },
-  }),
-  TRANSACTION_REQUEST: (appInfo: AppInfo) => ({
-    title: 'Transaction request',
-    options: {
-      body: `${appInfo.name} wants to submit a transaction. Open the Safe{Wallet} to continue.`,
-    },
-  }),
-}
+import { NotificationMessages, showNotification } from './notifications'
 
 export const _useTxFlowApi = (chainId: string, safeAddress: string): WalletSDK | undefined => {
   const { setTxFlow } = useContext(TxModalContext)
