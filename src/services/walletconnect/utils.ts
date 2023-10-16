3df9e1232a32a7d9af978548a8424e78dc9e0f1d
iamacook <aaron@safe.global>
2023-10-16 14:52:15 +0200

    fix: disable approval of unsupported sessions

--- src/services/walletconnect/utils.ts
+++ src/services/walletconnect/utils.ts
@@ -1,3 +1,6 @@
+import type { ChainInfo } from '@safe-global/safe-apps-sdk'
+import type { ProposalTypes } from '@walletconnect/types'
+
 import { EIP155 } from './constants'
 
 export const getEip155ChainId = (chainId: string): string => {
@@ -6,4 +9,18 @@ export const getEip155ChainId = (chainId: string): string => {
 
 export const stripEip155Prefix = (eip155Address: string): string => {
   return eip155Address.split(':').pop() ?? ''
+}
+
+export const getSupportedChainIds = (configs: Array<ChainInfo>, params: ProposalTypes.Struct): Array<string> => {
+  const { requiredNamespaces, optionalNamespaces } = params
+
+  const requiredChains = requiredNamespaces[EIP155]?.chains ?? []
+  const optionalChains = optionalNamespaces[EIP155]?.chains ?? []
+
+  return configs
+    .filter((chain) => {
+      const eipChainId = getEip155ChainId(chain.chainId)
+      return requiredChains.includes(eipChainId) || optionalChains.includes(eipChainId)
+    })
+    .map((chain) => chain.chainId)
 }
