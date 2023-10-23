b22bd801f96eac7a60fa48210ebcfef3b1bd3384
Usame Algan <usame@safe.global>
2023-10-23 11:12:48 +0200

    refactor: Reuse PasswordInput

--- src/components/common/ConnectWallet/MPCWalletProvider.tsx
+++ src/components/common/ConnectWallet/MPCWalletProvider.tsx
@@ -1,10 +1,11 @@
-import { useMPCWallet, MPCWalletState, type MPCWalletHook } from '@/hooks/wallets/mpc/useMPCWallet'
+import { type MPCWalletHook, MPCWalletState, useMPCWallet } from '@/hooks/wallets/mpc/useMPCWallet'
 import { createContext, type ReactElement } from 'react'
+import { COREKIT_STATUS } from '@web3auth/mpc-core-kit'
 
 export const MpcWalletContext = createContext<MPCWalletHook>({
   walletState: MPCWalletState.NOT_INITIALIZED,
   setWalletState: () => {},
-  triggerLogin: () => Promise.resolve(false),
+  triggerLogin: () => Promise.resolve(COREKIT_STATUS.NOT_INITIALIZED),
   resetAccount: () => Promise.resolve(),
   upsertPasswordBackup: () => Promise.resolve(),
   recoverFactorWithPassword: () => Promise.resolve(false),
--- src/components/common/ConnectWallet/PasswordRecovery.tsx
+++ src/components/common/ConnectWallet/PasswordRecovery.tsx
@@ -1,9 +1,6 @@
 import { MPC_WALLET_EVENTS } from '@/services/analytics/events/mpcWallet'
-import { VisibilityOff, Visibility } from '@mui/icons-material'
 import {
   Typography,
-  TextField,
-  IconButton,
   FormControlLabel,
   Checkbox,
   Button,
@@ -11,10 +8,12 @@ import {
   Divider,
   Grid,
   LinearProgress,
+  FormControl,
 } from '@mui/material'
 import { useState } from 'react'
 import Track from '../Track'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
+import PasswordInput from '@/components/settings/SignerAccountMFA/PasswordInput'
 
 type PasswordFormData = {
   password: string
@@ -27,7 +26,6 @@ export const PasswordRecovery = ({
   recoverFactorWithPassword: (password: string, storeDeviceFactor: boolean) => Promise<void>
   onSuccess: (() => void) | undefined
 }) => {
-  const [showPassword, setShowPassword] = useState(false)
   const [storeDeviceFactor, setStoreDeviceFactor] = useState(false)
 
   const formMethods = useForm<PasswordFormData>({
@@ -37,7 +35,7 @@ export const PasswordRecovery = ({
     },
   })
 
-  const { handleSubmit, register, formState, setError } = formMethods
+  const { handleSubmit, formState, setError } = formMethods
 
   const onSubmit = async (data: PasswordFormData) => {
     try {
@@ -51,68 +49,58 @@ export const PasswordRecovery = ({
   const isDisabled = formState.isSubmitting
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container justifyContent="center" alignItems="center">
-        <Grid item xs={12} md={5} p={2}>
-          <Typography variant="h2" fontWeight="bold" mb={3}>
-            Verify your account
-          </Typography>
-          <Box bgcolor="background.paper" borderRadius={1}>
-            <LinearProgress
-              color="secondary"
-              sx={{ borderTopLeftRadius: '6px', borderTopRightRadius: '6px', opacity: isDisabled ? 1 : 0 }}
-            />
-            <Box p={4}>
-              <Typography variant="h6" fontWeight="bold" mb={0.5}>
-                Enter security password
-              </Typography>
-              <Typography>
-                This browser is not registered with your Account yet. Please enter your recovery password to restore
-                access to this Account.
-              </Typography>
-            </Box>
-            <Divider />
-            <Box p={4} display="flex" flexDirection="column" alignItems="baseline" gap={1}>
-              {/* TODO: Reuse PasswordInput here */}
-              <TextField
-                fullWidth
-                label="Recovery password"
-                type={showPassword ? 'text' : 'password'}
-                error={!!formState.errors['password']}
-                helperText={formState.errors['password']?.message}
-                disabled={isDisabled}
-                InputProps={{
-                  endAdornment: (
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword((prev) => !prev)}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  ),
-                }}
-                {...register('password', {
-                  required: true,
-                })}
+    <FormProvider {...formMethods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={12} md={5} p={2}>
+            <Typography variant="h2" fontWeight="bold" mb={3}>
+              Verify your account
+            </Typography>
+            <Box bgcolor="background.paper" borderRadius={1}>
+              <LinearProgress
+                color="secondary"
+                sx={{ borderTopLeftRadius: '6px', borderTopRightRadius: '6px', opacity: isDisabled ? 1 : 0 }}
               />
-              <FormControlLabel
-                disabled={isDisabled}
-                control={<Checkbox checked={storeDeviceFactor} onClick={() => setStoreDeviceFactor((prev) => !prev)} />}
-                label="Do not ask again on this device"
-              />
+              <Box p={4}>
+                <Typography variant="h6" fontWeight="bold" mb={0.5}>
+                  Enter security password
+                </Typography>
+                <Typography>
+                  This browser is not registered with your Account yet. Please enter your recovery password to restore
+                  access to this Account.
+                </Typography>
+              </Box>
+              <Divider />
+              <Box p={4} display="flex" flexDirection="column" alignItems="baseline" gap={1}>
+                <FormControl fullWidth>
+                  <PasswordInput
+                    name="password"
+                    label="Recovery password"
+                    helperText={formState.errors['password']?.message}
+                    disabled={isDisabled}
+                    required
+                  />
+                </FormControl>
+                <FormControlLabel
+                  disabled={isDisabled}
+                  control={
+                    <Checkbox checked={storeDeviceFactor} onClick={() => setStoreDeviceFactor((prev) => !prev)} />
+                  }
+                  label="Do not ask again on this device"
+                />
+              </Box>
+              <Divider />
+              <Box p={4} display="flex" justifyContent="flex-end">
+                <Track {...MPC_WALLET_EVENTS.RECOVER_PASSWORD}>
+                  <Button variant="contained" type="submit" disabled={isDisabled}>
+                    Submit
+                  </Button>
+                </Track>
+              </Box>
             </Box>
-            <Divider />
-            <Box p={4} display="flex" justifyContent="flex-end">
-              <Track {...MPC_WALLET_EVENTS.RECOVER_PASSWORD}>
-                <Button variant="contained" type="submit" disabled={isDisabled}>
-                  Submit
-                </Button>
-              </Track>
-            </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </form>
+      </form>
+    </FormProvider>
   )
 }
