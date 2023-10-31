030dc63dc759d13c6f7b05ff617555fa49677421
katspaugh <katspaugh@gmail.com>
2023-10-31 14:50:07 +0100

    PR suggestion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,15 @@ import { AppRoutes } from '@/config/routes'
 
 const IndexPage: NextPage = () => {
   const router = useRouter()
-  const { chain } = router.query
 
   useEffect(() => {
     if (router.pathname === AppRoutes.index) {
-      router.replace(chain ? `${AppRoutes.welcome}?chain=${chain}` : AppRoutes.welcome)
+      router.replace({
+        pathname: AppRoutes.welcome,
+        query: router.query,
+      })
     }
-  }, [router, chain])
+  }, [router])
 
   return <></>
 }
