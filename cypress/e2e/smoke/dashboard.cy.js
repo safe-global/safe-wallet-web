481e6a37927dd5b5f0c641e2e2752896dfbb5aac
Michael <30682308+mike10ca@users.noreply.github.com>
2023-11-01 13:14:19 +0100

    Tests: Update skipped tests (#2723)
    * Update skipped tests
    * Fix permissions tests
    * Fix NFTS tests

--- cypress/e2e/smoke/dashboard.cy.js
+++ cypress/e2e/smoke/dashboard.cy.js
@@ -3,27 +3,27 @@ import * as dashboard from '../pages/dashboard.pages'
 import * as main from '../pages/main.page'
 
 describe('Dashboard tests', () => {
-  before(() => {
+  beforeEach(() => {
     cy.clearLocalStorage()
     cy.visit(constants.BALANCE_URL + constants.SEPOLIA_TEST_SAFE_5)
-    main.acceptCookies()
+    main.acceptCookies(2)
     main.clickOnSideMenuItem(constants.mainSideMenuOptions.home)
     dashboard.verifyConnectTransactStrIsVisible()
   })
 
-  it.skip('Verify the overview widget is displayed [C56107]', () => {
+  it('Verify the overview widget is displayed [C56107]', () => {
     dashboard.verifyOverviewWidgetData()
   })
 
-  it.skip('Verify the transaction queue widget is displayed [C56108]', () => {
+  it('Verify the transaction queue widget is displayed [C56108]', () => {
     dashboard.verifyTxQueueWidget()
   })
 
-  it.skip('Verify the featured Safe Apps are displayed [C56109]', () => {
+  it('Verify the featured Safe Apps are displayed [C56109]', () => {
     dashboard.verifyFeaturedAppsSection()
   })
 
-  it.skip('Verify the Safe Apps Section is displayed [C56110]', () => {
+  it('Verify the Safe Apps Section is displayed [C56110]', () => {
     dashboard.verifySafeAppsSection()
   })
 })
