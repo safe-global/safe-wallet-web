bfae37888bf893bdc5e675cddcd31e8e47fe1b5a
katspaugh <katspaugh@gmail.com>
2023-11-02 09:32:57 +0100

    Fix: new Safe logo for onboard/walletconnect

--- src/services/onboard.ts
+++ src/services/onboard.ts
@@ -50,9 +50,8 @@ export const createOnboard = (
 
     appMetadata: {
       name: 'Safe{Wallet}',
-      // Both heights need be set to correctly size the image in the connecting screen/modal
-      icon: 'https://app.safe.global/images/safe-logo-green.png',
-      description: 'Please select a wallet to connect to Safe{Wallet}',
+      icon: location.origin + '/images/logo-round.svg',
+      description: 'Safe{Wallet} – smart contract wallet for Ethereum (ex-Gnosis Safe multisig)',
       recommendedInjectedWallets: getRecommendedInjectedWallets(),
     },
 
