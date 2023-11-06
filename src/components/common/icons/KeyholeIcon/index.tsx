7aea373d298cbe40ed6a793193ee83db03378299
Usame Algan <usame@safe.global>
2023-11-06 14:28:38 +0100

    fix: Adjust connection center design

--- src/components/common/icons/KeyholeIcon/index.tsx
+++ src/components/common/icons/KeyholeIcon/index.tsx
@@ -1,9 +1,32 @@
-import Keyhole from '@/components/common/icons/KeyholeIcon/keyhole.svg'
+import css from '@/components/common/icons/CircularIcon/styles.module.css'
+import LockIcon from '@/public/images/common/lock.svg'
+import { Badge, SvgIcon } from '@mui/material'
 
-import CircularIcon from '../CircularIcon'
-
-const KeyholeIcon = ({ size = 40 }: { size?: number }) => {
-  return <CircularIcon icon={Keyhole} badgeColor="error" size={size} />
+const KeyholeIcon = ({ size = 28 }: { size?: number }) => {
+  return (
+    <Badge
+      color="error"
+      overlap="circular"
+      variant="dot"
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      className={css.badge}
+    >
+      <SvgIcon
+        component={LockIcon}
+        inheritViewBox
+        sx={{
+          height: size,
+          width: size,
+          '& path': {
+            fill: ({ palette }) => palette.border.main,
+          },
+        }}
+      />
+    </Badge>
+  )
 }
 
 export default KeyholeIcon
