4b66c3767872af4a1ab274b978dd6d040c10cd49
schmanu <manu@safe.global>
2023-11-07 14:10:56 +0100

    fix: remove console.log

--- src/service-workers/firebase-messaging/firebase-messaging-sw.ts
+++ src/service-workers/firebase-messaging/firebase-messaging-sw.ts
@@ -45,7 +45,6 @@ export function firebaseMessagingSw() {
   const messaging = getMessaging(app)
 
   onBackgroundMessage(messaging, async (payload) => {
-    console.log('onBackgroundMessage', payload)
     const shouldShow = await shouldShowServiceWorkerPushNotification(payload)
 
     if (!shouldShow) {
