ced3211d6d5552e7c9087a476d6cd9f0f0db82ec
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-11-13 16:39:07 +0100

    Fix: revert relaying message (#2799)

--- src/components/tx/SponsoredBy/index.tsx
+++ src/components/tx/SponsoredBy/index.tsx
@@ -11,72 +11,43 @@ export const SPONSOR_LOGOS = {
   [chains.gor]: '/images/common/token-placeholder.svg',
 }
 
-const SponsoredBy = ({
-  relays,
-  tooltip,
-  shouldRelay,
-}: {
-  relays?: RelayResponse
-  tooltip?: string
-  shouldRelay: boolean
-}) => {
+const SponsoredBy = ({ relays, tooltip }: { relays: RelayResponse; tooltip?: string }) => {
   const chain = useCurrentChain()
 
   return (
     <Box className={css.sponsoredBy}>
       <SvgIcon component={GasStationIcon} inheritViewBox className={css.icon} />
-
-      {shouldRelay ? (
-        <div>
-          <Stack direction="row" spacing={0.5} alignItems="center">
-            <Typography variant="body2" fontWeight={700} letterSpacing="0.1px">
-              Sponsored by
-            </Typography>
-
-            <img src={SPONSOR_LOGOS[chain?.chainId || '']} alt={chain?.chainName} className={css.logo} />
-            <Typography variant="body2" fontWeight={700} letterSpacing="0.1px">
-              {chain?.chainName}
-            </Typography>
-
-            {tooltip ? (
-              <Tooltip title={tooltip} placement="top" arrow>
-                <span style={{ display: 'flex' }}>
-                  <SvgIcon
-                    component={InfoIcon}
-                    inheritViewBox
-                    color="info"
-                    fontSize="small"
-                    sx={{ verticalAlign: 'middle', color: '#B2B5B2' }}
-                  />
-                </span>
-              </Tooltip>
-            ) : null}
-          </Stack>
-
-          <Typography variant="body2" color="primary.light">
-            Transactions per hour:{' '}
-            <Box component="span" sx={{ fontWeight: '700', color: 'text.primary' }}>
-              {relays?.remaining ?? 0} of {relays?.limit ?? 0}
-            </Box>
-            {relays && !relays.remaining && (
-              <Box component="span" sx={{ color: 'error.main' }}>
-                {' '}
-                &mdash; will reset in the next hour
-              </Box>
-            )}
-          </Typography>
-        </div>
-      ) : (
-        <div>
+      <div>
+        <Stack direction="row" spacing={0.5} alignItems="center">
           <Typography variant="body2" fontWeight={700} letterSpacing="0.1px">
-            Pay gas from the connected wallet
+            Sponsored by
           </Typography>
+          <img src={SPONSOR_LOGOS[chain?.chainId || '']} alt={chain?.chainName} className={css.logo} />
+          <Typography variant="body2" fontWeight={700} letterSpacing="0.1px">
+            {chain?.chainName}
+          </Typography>
+          {tooltip ? (
+            <Tooltip title={tooltip} placement="top" arrow>
+              <span style={{ display: 'flex' }}>
+                <SvgIcon
+                  component={InfoIcon}
+                  inheritViewBox
+                  color="info"
+                  fontSize="small"
+                  sx={{ verticalAlign: 'middle', color: '#B2B5B2' }}
+                />
+              </span>
+            </Tooltip>
+          ) : null}
+        </Stack>
 
-          <Typography variant="body2" color="primary.light">
-            Please make sure your wallet has sufficient funds.
-          </Typography>
-        </div>
-      )}
+        <Typography variant="body2" color="primary.light">
+          Transactions per hour:{' '}
+          <Box component="span" sx={{ fontWeight: '700', color: 'text.primary' }}>
+            {relays.remaining} of {relays.limit}
+          </Box>
+        </Typography>
+      </div>
     </Box>
   )
 }
