b03a51d46b0c986fe877aa946dafcef94d3fc569
katspaugh <katspaugh@gmail.com>
2023-11-21 10:48:18 +0100

    Fix: remove experimental address emoji

--- src/pages/settings/appearance.tsx
+++ src/pages/settings/appearance.tsx
@@ -4,18 +4,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import { useAppDispatch, useAppSelector } from '@/store'
-import {
-  selectSettings,
-  setCopyShortName,
-  setDarkMode,
-  setShowShortName,
-  setAddressEmojis,
-} from '@/store/settingsSlice'
+import { selectSettings, setCopyShortName, setDarkMode, setShowShortName } from '@/store/settingsSlice'
 import SettingsHeader from '@/components/settings/SettingsHeader'
 import { trackEvent, SETTINGS_EVENTS } from '@/services/analytics'
 import { useDarkMode } from '@/hooks/useDarkMode'
 import ExternalLink from '@/components/common/ExternalLink'
-import EmojiPreview from '@/components/settings/EmojiPreview'
 
 const Appearance: NextPage = () => {
   const dispatch = useAppDispatch()
@@ -23,12 +16,11 @@ const Appearance: NextPage = () => {
   const isDarkMode = useDarkMode()
 
   const handleToggle = (
-    action: typeof setCopyShortName | typeof setDarkMode | typeof setShowShortName | typeof setAddressEmojis,
+    action: typeof setCopyShortName | typeof setDarkMode | typeof setShowShortName,
     event:
       | typeof SETTINGS_EVENTS.APPEARANCE.PREPEND_PREFIXES
       | typeof SETTINGS_EVENTS.APPEARANCE.COPY_PREFIXES
-      | typeof SETTINGS_EVENTS.APPEARANCE.DARK_MODE
-      | typeof SETTINGS_EVENTS.APPEARANCE.ADDRESS_EMOJIS,
+      | typeof SETTINGS_EVENTS.APPEARANCE.DARK_MODE,
   ) => {
     return (_: ChangeEvent<HTMLInputElement>, checked: boolean) => {
       dispatch(action(checked))
@@ -103,27 +95,6 @@ const Appearance: NextPage = () => {
                 }
                 label="Dark mode"
               />
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={3} mt={2}>
-            <Grid item lg={4} xs={12}>
-              <Typography variant="h4" fontWeight="bold">
-                Experimental
-              </Typography>
-            </Grid>
-
-            <Grid item xs>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={settings.addressEmojis}
-                    onChange={handleToggle(setAddressEmojis, SETTINGS_EVENTS.APPEARANCE.ADDRESS_EMOJIS)}
-                  />
-                }
-                label="Address emoji"
-              />
-              <EmojiPreview />
             </Grid>
           </Grid>
         </Paper>
