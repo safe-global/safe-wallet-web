64cb6a35ad7c330cac2a6712a3e7c80566026808
mike10ca <michael@safe.global>
2023-11-27 18:29:55 +0100

    Fix eslint

--- cypress/e2e/pages/create_tx.pages.js
+++ cypress/e2e/pages/create_tx.pages.js
@@ -1,221 +1,196 @@
-import * as constants from "../../support/constants";
+import * as constants from '../../support/constants'
 
-const newTransactionBtnStr = "New transaction";
-const recepientInput = 'input[name="recipient"]';
-const sendTokensBtnStr = "Send tokens";
-const tokenAddressInput = 'input[name="tokenAddress"]';
-const amountInput = 'input[name="amount"]';
-const nonceInput = 'input[name="nonce"]';
-const gasLimitInput = '[name="gasLimit"]';
-const rotateLeftIcon = '[data-testid="RotateLeftIcon"]';
-const transactionItemExpandable = 'div[id^="transfer"]';
-const expandItemIcon = '[data-testid="ExpandMoreIcon"]';
+const newTransactionBtnStr = 'New transaction'
+const recepientInput = 'input[name="recipient"]'
+const sendTokensBtnStr = 'Send tokens'
+const tokenAddressInput = 'input[name="tokenAddress"]'
+const amountInput = 'input[name="amount"]'
+const nonceInput = 'input[name="nonce"]'
+const gasLimitInput = '[name="gasLimit"]'
+const rotateLeftIcon = '[data-testid="RotateLeftIcon"]'
+const transactionItemExpandable = 'div[id^="transfer"]'
+const expandItemIcon = '[data-testid="ExpandMoreIcon"]'
 
-const viewTransactionBtn = "View transaction";
-const transactionDetailsTitle = "Transaction details";
-const QueueLabel = "needs to be executed first";
-const TransactionSummary = "Send ";
+const viewTransactionBtn = 'View transaction'
+const transactionDetailsTitle = 'Transaction details'
+const QueueLabel = 'needs to be executed first'
+const TransactionSummary = 'Send '
 
-const maxAmountBtnStr = "Max";
-const nextBtnStr = "Next";
-const nativeTokenTransferStr = "Native token transfer";
-const yesStr = "Yes, ";
-const estimatedFeeStr = "Estimated fee";
-const executeStr = "Execute";
-const transactionsPerHrStr = "Transactions per hour";
-const transactionsPerHr5Of5Str = "5 of 5";
-const editBtnStr = "Edit";
-const executionParamsStr = "Execution parameters";
-const noLaterStr = "No, later";
-const signBtnStr = "Sign";
-const expandAllBtnStr = "Expand all";
-const collapseAllBtnStr = "Collapse all";
+const maxAmountBtnStr = 'Max'
+const nextBtnStr = 'Next'
+const nativeTokenTransferStr = 'Native token transfer'
+const yesStr = 'Yes, '
+const estimatedFeeStr = 'Estimated fee'
+const executeStr = 'Execute'
+const transactionsPerHrStr = 'Transactions per hour'
+const transactionsPerHr5Of5Str = '5 of 5'
+const editBtnStr = 'Edit'
+const executionParamsStr = 'Execution parameters'
+const noLaterStr = 'No, later'
+const signBtnStr = 'Sign'
+const expandAllBtnStr = 'Expand all'
+const collapseAllBtnStr = 'Collapse all'
 
 export function clickOnNewtransactionBtn() {
   // Assert that "New transaction" button is visible
   cy.contains(newTransactionBtnStr, {
     timeout: 60_000, // `lastWallet` takes a while initialize in CI
   })
-    .should("be.visible")
-    .and("not.be.disabled");
+    .should('be.visible')
+    .and('not.be.disabled')
 
   // Open the new transaction modal
-  cy.contains(newTransactionBtnStr).click();
-  cy.contains("h1", newTransactionBtnStr).should("be.visible");
+  cy.contains(newTransactionBtnStr).click()
+  cy.contains('h1', newTransactionBtnStr).should('be.visible')
 }
 
 export function typeRecipientAddress(address) {
-  cy.get(recepientInput).type(address).should("have.value", address);
+  cy.get(recepientInput).type(address).should('have.value', address)
 }
 export function clickOnSendTokensBtn() {
-  cy.contains(sendTokensBtnStr).click();
+  cy.contains(sendTokensBtnStr).click()
 }
 
 export function clickOnTokenselectorAndSelectSepoliaEth() {
-  cy.get(tokenAddressInput).prev().click();
-  cy.get('ul[role="listbox"]')
-    .contains(constants.tokenNames.sepoliaEther)
-    .click();
+  cy.get(tokenAddressInput).prev().click()
+  cy.get('ul[role="listbox"]').contains(constants.tokenNames.sepoliaEther).click()
 }
 
 export function setMaxAmount() {
-  cy.contains(maxAmountBtnStr).click();
+  cy.contains(maxAmountBtnStr).click()
 }
 
 export function verifyMaxAmount(token, tokenAbbreviation) {
   cy.get(tokenAddressInput)
     .prev()
-    .find("p")
+    .find('p')
     .contains(token)
     .next()
     .then((element) => {
-      const maxBalance = parseFloat(
-        element.text().replace(tokenAbbreviation, "").trim()
-      );
+      const maxBalance = parseFloat(element.text().replace(tokenAbbreviation, '').trim())
       cy.get(amountInput).should(($input) => {
-        const actualValue = parseFloat($input.val());
-        expect(actualValue).to.be.closeTo(maxBalance, 0.1);
-      });
-      console.log(maxBalance);
-    });
+        const actualValue = parseFloat($input.val())
+        expect(actualValue).to.be.closeTo(maxBalance, 0.1)
+      })
+      console.log(maxBalance)
+    })
 }
 
 export function setSendValue(value) {
-  cy.get(amountInput).clear().type(value);
+  cy.get(amountInput).clear().type(value)
 }
 
 export function clickOnNextBtn() {
-  cy.contains(nextBtnStr).click();
+  cy.contains(nextBtnStr).click()
 }
 
 export function verifySubmitBtnIsEnabled() {
-  cy.get('button[type="submit"]').should("not.be.disabled");
+  cy.get('button[type="submit"]').should('not.be.disabled')
 }
 
 export function verifyNativeTokenTransfer() {
-  cy.contains(nativeTokenTransferStr).should("be.visible");
+  cy.contains(nativeTokenTransferStr).should('be.visible')
 }
 
 export function changeNonce(value) {
-  cy.get(nonceInput).clear().type(value, { force: true }).blur();
+  cy.get(nonceInput).clear().type(value, { force: true }).blur()
 }
 
 export function verifyConfirmTransactionData() {
-  cy.contains(yesStr).should("exist").click();
-  cy.contains(estimatedFeeStr).should("exist");
+  cy.contains(yesStr).should('exist').click()
+  cy.contains(estimatedFeeStr).should('exist')
 
   // Asserting the sponsored info is present
-  cy.contains(executeStr).scrollIntoView().should("be.visible");
+  cy.contains(executeStr).scrollIntoView().should('be.visible')
 
-  cy.get("span").contains(estimatedFeeStr);
+  cy.get('span').contains(estimatedFeeStr)
 }
 
 export function openExecutionParamsModal() {
-  cy.contains(estimatedFeeStr).click();
-  cy.contains(editBtnStr).click();
+  cy.contains(estimatedFeeStr).click()
+  cy.contains(editBtnStr).click()
 }
 
 export function verifyAndSubmitExecutionParams() {
-  cy.contains(executionParamsStr).parents("form").as("Paramsform");
+  cy.contains(executionParamsStr).parents('form').as('Paramsform')
 
-  const arrayNames = [
-    "Wallet nonce",
-    "Max priority fee (Gwei)",
-    "Max fee (Gwei)",
-    "Gas limit",
-  ];
+  const arrayNames = ['Wallet nonce', 'Max priority fee (Gwei)', 'Max fee (Gwei)', 'Gas limit']
   arrayNames.forEach((element) => {
-    cy.get("@Paramsform")
-      .find("label")
-      .contains(`${element}`)
-      .next()
-      .find("input")
-      .should("not.be.disabled");
-  });
+    cy.get('@Paramsform').find('label').contains(`${element}`).next().find('input').should('not.be.disabled')
+  })
 
-  cy.get("@Paramsform")
-    .find(gasLimitInput)
-    .clear()
-    .type("300000")
-    .invoke("prop", "value")
-    .should("equal", "300000");
-  cy.get("@Paramsform")
-    .find(gasLimitInput)
-    .parent("div")
-    .find(rotateLeftIcon)
-    .click();
-  cy.get("@Paramsform").submit();
+  cy.get('@Paramsform').find(gasLimitInput).clear().type('300000').invoke('prop', 'value').should('equal', '300000')
+  cy.get('@Paramsform').find(gasLimitInput).parent('div').find(rotateLeftIcon).click()
+  cy.get('@Paramsform').submit()
 }
 
 export function clickOnNoLaterOption() {
   // Asserts the execute checkbox is uncheckable (???)
-  cy.contains(noLaterStr).click();
+  cy.contains(noLaterStr).click()
 }
 
 export function clickOnSignTransactionBtn() {
-  cy.contains(signBtnStr).click();
+  cy.contains(signBtnStr).click()
 }
 
 export function waitForProposeRequest() {
-  cy.intercept("POST", constants.proposeEndpoint).as("ProposeTx");
-  cy.wait("@ProposeTx");
+  cy.intercept('POST', constants.proposeEndpoint).as('ProposeTx')
+  cy.wait('@ProposeTx')
 }
 
 export function clickViewTransaction() {
-  cy.contains(viewTransactionBtn).click();
+  cy.contains(viewTransactionBtn).click()
 }
 
 export function verifySingleTxPage() {
-  cy.get("h3").contains(transactionDetailsTitle).should("be.visible");
+  cy.get('h3').contains(transactionDetailsTitle).should('be.visible')
 }
 
 export function verifyQueueLabel() {
-  cy.contains(QueueLabel).should("be.visible");
+  cy.contains(QueueLabel).should('be.visible')
 }
 
 export function verifyTransactionSummary(sendValue) {
-  cy.contains(
-    TransactionSummary + `${sendValue} ${constants.tokenAbbreviation.sep}`
-  ).should("exist");
+  cy.contains(TransactionSummary + `${sendValue} ${constants.tokenAbbreviation.sep}`).should('exist')
 }
 
 export function verifyDateExists(date) {
-  cy.contains("div", date).should("exist");
+  cy.contains('div', date).should('exist')
 }
 
 export function verifyImageAltTxt(index, text) {
-  cy.get("img").eq(index).should("have.attr", "alt", text).should("be.visible");
+  cy.get('img').eq(index).should('have.attr', 'alt', text).should('be.visible')
 }
 
 export function verifyStatus(status) {
-  cy.contains("div", status).should("exist");
+  cy.contains('div', status).should('exist')
 }
 
 export function verifyTransactionStrExists(str) {
-  cy.contains(str).should("exist");
+  cy.contains(str).should('exist')
 }
 
 export function verifyTransactionStrNotVible(str) {
-  cy.contains(str).should("not.be.visible");
+  cy.contains(str).should('not.be.visible')
 }
 
 export function clickOnTransactionExpandableItem(name, actions) {
-  cy.contains("div", name)
+  cy.contains('div', name)
     .next()
     .click()
     .within(() => {
-      actions();
-    });
+      actions()
+    })
 }
 
 export function clickOnExpandAllBtn() {
-  cy.contains(expandAllBtnStr).click();
+  cy.contains(expandAllBtnStr).click()
 }
 
 export function clickOnCollapseAllBtn() {
-  cy.contains(collapseAllBtnStr).click();
+  cy.contains(collapseAllBtnStr).click()
 }
 
 export function clickOnExpandIcon() {
-  cy.get(expandItemIcon).click();
+  cy.get(expandItemIcon).click()
 }
--- cypress/e2e/pages/main.page.js
+++ cypress/e2e/pages/main.page.js
@@ -1,155 +1,146 @@
-import * as constants from "../../support/constants";
+import * as constants from '../../support/constants'
 
-const acceptSelection = "Accept selection";
-const executeStr = "Execute";
-export const modalDialogCloseBtn = '[data-testid="modal-dialog-close-btn"]';
+const acceptSelection = 'Accept selection'
+const executeStr = 'Execute'
+export const modalDialogCloseBtn = '[data-testid="modal-dialog-close-btn"]'
 
 export function clickOnExecuteBtn() {
-  cy.get("button").contains(executeStr).click();
+  cy.get('button').contains(executeStr).click()
 }
 export function clickOnSideMenuItem(item) {
-  cy.get("p").contains(item).click();
+  cy.get('p').contains(item).click()
 }
 
 export function waitForTrnsactionHistoryToComplete() {
-  cy.intercept("GET", constants.transactionHistoryEndpoint).as("History");
-  cy.wait("@History");
+  cy.intercept('GET', constants.transactionHistoryEndpoint).as('History')
+  cy.wait('@History')
 }
 
 export function waitForSafeListRequestToComplete() {
-  cy.intercept("GET", constants.safeListEndpoint).as("Safes");
-  cy.wait("@Safes");
+  cy.intercept('GET', constants.safeListEndpoint).as('Safes')
+  cy.wait('@Safes')
 }
 
 export function acceptCookies(index = 0) {
-  cy.wait(1000);
+  cy.wait(1000)
 
-  cy.findAllByText("Got it!")
-    .should("have.length.at.least", index)
-    .each(($el) => $el.click());
+  cy.findAllByText('Got it!')
+    .should('have.length.at.least', index)
+    .each(($el) => $el.click())
 
-  cy.get("button")
+  cy.get('button')
     .contains(acceptSelection)
     .should(() => {})
     .then(($button) => {
       if (!$button.length) {
-        return;
+        return
       }
-      cy.wrap($button).click();
-      cy.contains(acceptSelection).should("not.exist");
-      cy.wait(500);
-    });
+      cy.wrap($button).click()
+      cy.contains(acceptSelection).should('not.exist')
+      cy.wait(500)
+    })
 }
 
 export function verifyGoerliWalletHeader() {
-  cy.contains(constants.goerlyE2EWallet);
+  cy.contains(constants.goerlyE2EWallet)
 }
 
 export function verifyHomeSafeUrl(safe) {
-  cy.location("href", { timeout: 10000 }).should(
-    "include",
-    constants.homeUrl + safe
-  );
+  cy.location('href', { timeout: 10000 }).should('include', constants.homeUrl + safe)
 }
 
 export function checkTextsExistWithinElement(element, texts) {
   texts.forEach((text) => {
-    cy.wrap(element).findByText(text).should("exist");
-  });
+    cy.wrap(element).findByText(text).should('exist')
+  })
 }
 
 export function verifyCheckboxeState(element, index, state) {
-  cy.get(element).eq(index).should(state);
+  cy.get(element).eq(index).should(state)
 }
 
 export function verifyInputValue(selector, value) {
-  cy.get(selector).invoke("val").should("include", value);
+  cy.get(selector).invoke('val').should('include', value)
 }
 
 export function generateRandomString(length) {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZz0123456789";
-  let result = "";
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZz0123456789'
+  let result = ''
 
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
   }
 
-  return result;
+  return result
 }
 
 export function verifyElementsCount(element, count) {
-  cy.get(element).should("have.length", count);
+  cy.get(element).should('have.length', count)
 }
 
 export function verifyValuesDoNotExist(element, values) {
   values.forEach((value) => {
-    cy.get(element).should("not.contain", value);
-  });
+    cy.get(element).should('not.contain', value)
+  })
 }
 
 export function verifyValuesExist(element, values) {
   values.forEach((value) => {
-    cy.get(element).should("contain", value);
-  });
+    cy.get(element).should('contain', value)
+  })
 }
 
 export function verifyElementsExist(elements) {
   elements.forEach((element) => {
-    cy.get(element).should("exist");
-  });
+    cy.get(element).should('exist')
+  })
 }
 
 export function getTextToArray(selector, textArray) {
   cy.get(selector).each(($element) => {
-    textArray.push($element.text());
-  });
+    textArray.push($element.text())
+  })
 }
 
 export function extractDigitsToArray(selector, digitsArray) {
   cy.get(selector).each(($element) => {
-    const text = $element.text();
-    const digits = text.match(/\d+\.\d+|\d+\b/g);
+    const text = $element.text()
+    const digits = text.match(/\d+\.\d+|\d+\b/g)
     if (digits) {
-      digitsArray.push(...digits);
+      digitsArray.push(...digits)
     }
-  });
+  })
 }
 
-export function isItemInLocalstorage(
-  key,
-  expectedValue,
-  maxAttempts = 10,
-  delay = 100
-) {
+export function isItemInLocalstorage(key, expectedValue, maxAttempts = 10, delay = 100) {
   return new Promise((resolve, reject) => {
-    let attempts = 0;
+    let attempts = 0
 
     const isItemInLocalstorage = () => {
-      attempts++;
-      const storedValue = JSON.parse(window.localStorage.getItem(key));
-      const keyEqualsValue =
-        JSON.stringify(expectedValue) === JSON.stringify(storedValue);
+      attempts++
+      const storedValue = JSON.parse(window.localStorage.getItem(key))
+      const keyEqualsValue = JSON.stringify(expectedValue) === JSON.stringify(storedValue)
       if (keyEqualsValue) {
-        cy.log("Item retrieved from local storage successfully");
-        resolve();
+        cy.log('Item retrieved from local storage successfully')
+        resolve()
       } else if (attempts < maxAttempts) {
-        setTimeout(isItemInLocalstorage, delay);
+        setTimeout(isItemInLocalstorage, delay)
       } else {
-        cy.log("Error: Key does not equal the expected value in local storage");
-        reject(error);
+        cy.log('Error: Key does not equal the expected value in local storage')
+        reject(error)
       }
-    };
-    isItemInLocalstorage();
-  });
+    }
+    isItemInLocalstorage()
+  })
 }
 
 export function addToLocalStorage(key, jsonValue) {
   return new Promise((resolve, reject) => {
     try {
-      window.localStorage.setItem(key, JSON.stringify(jsonValue));
-      resolve("Item added to local storage successfully");
+      window.localStorage.setItem(key, JSON.stringify(jsonValue))
+      resolve('Item added to local storage successfully')
     } catch (error) {
-      reject("Error adding item to local storage: " + error);
+      reject('Error adding item to local storage: ' + error)
     }
-  });
+  })
 }
