cf33e76c3820cf0548459db72ada48cc3c20c3c6
mike10ca <michael@safe.global>
2023-11-27 18:07:23 +0100

    Use local storage to avoid extra test steps

--- cypress/e2e/smoke/address_book.cy.js
+++ cypress/e2e/smoke/address_book.cy.js
@@ -1,53 +1,44 @@
-import "cypress-file-upload";
-import * as constants from "../../support/constants";
-import * as addressBook from "../../e2e/pages/address_book.page";
-import * as main from "../../e2e/pages/main.page";
-import * as ls from "../../support/localstorage_data.js";
+import 'cypress-file-upload'
+import * as constants from '../../support/constants'
+import * as addressBook from '../../e2e/pages/address_book.page'
+import * as main from '../../e2e/pages/main.page'
+import * as ls from '../../support/localstorage_data.js'
 
-const NAME = "Owner1";
-const EDITED_NAME = "Edited Owner1";
+const NAME = 'Owner1'
+const EDITED_NAME = 'Edited Owner1'
 
-describe("[SMOKE] Address book tests", () => {
+describe('[SMOKE] Address book tests', () => {
   beforeEach(() => {
-    cy.clearLocalStorage();
-    cy.visit(constants.addressBookUrl + constants.SEPOLIA_TEST_SAFE_1);
-    main.waitForTrnsactionHistoryToComplete();
-    main.acceptCookies();
-  });
+    cy.clearLocalStorage()
+    cy.visit(constants.addressBookUrl + constants.SEPOLIA_TEST_SAFE_1)
+    main.waitForTrnsactionHistoryToComplete()
+    main.acceptCookies()
+  })
 
-  it("[SMOKE] Verify entry can be added", () => {
-    addressBook.clickOnCreateEntryBtn();
-    addressBook.addEntry(NAME, constants.RECIPIENT_ADDRESS);
-  });
+  it('[SMOKE] Verify entry can be added', () => {
+    addressBook.clickOnCreateEntryBtn()
+    addressBook.addEntry(NAME, constants.RECIPIENT_ADDRESS)
+  })
 
-  it("[SMOKE] Verify entry can be deleted", () => {
+  it('[SMOKE] Verify entry can be deleted', () => {
     main
-      .addToLocalStorage(
-        constants.localStorageKeys.SAFE_v2__addressBook,
-        ls.addressBookData.sepoliaAddress1
-      )
+      .addToLocalStorage(constants.localStorageKeys.SAFE_v2__addressBook, ls.addressBookData.sepoliaAddress1)
       .then(() => {
         main
-          .isItemInLocalstorage(
-            constants.localStorageKeys.SAFE_v2__addressBook,
-            ls.addressBookData.sepoliaAddress1
-          )
+          .isItemInLocalstorage(constants.localStorageKeys.SAFE_v2__addressBook, ls.addressBookData.sepoliaAddress1)
           .then(() => {
-            cy.reload();
-            addressBook.clickDeleteEntryButton();
-            addressBook.clickDeleteEntryModalDeleteButton();
-            addressBook.verifyEditedNameNotExists(EDITED_NAME);
-          });
-      });
-  });
+            cy.reload()
+            addressBook.clickDeleteEntryButton()
+            addressBook.clickDeleteEntryModalDeleteButton()
+            addressBook.verifyEditedNameNotExists(EDITED_NAME)
+          })
+      })
+  })
 
-  it("[SMOKE] Verify csv file can be imported", () => {
-    addressBook.clickOnImportFileBtn();
-    addressBook.importFile();
-    addressBook.verifyImportModalIsClosed();
-    addressBook.verifyDataImported(
-      constants.SEPOLIA_CSV_ENTRY.name,
-      constants.SEPOLIA_CSV_ENTRY.address
-    );
-  });
-});
+  it('[SMOKE] Verify csv file can be imported', () => {
+    addressBook.clickOnImportFileBtn()
+    addressBook.importFile()
+    addressBook.verifyImportModalIsClosed()
+    addressBook.verifyDataImported(constants.SEPOLIA_CSV_ENTRY.name, constants.SEPOLIA_CSV_ENTRY.address)
+  })
+})
--- cypress/e2e/smoke/batch_tx.cy.js
+++ cypress/e2e/smoke/batch_tx.cy.js
@@ -1,93 +1,59 @@
-import * as batch from "../pages/batches.pages";
-import * as constants from "../../support/constants";
-import * as main from "../../e2e/pages/main.page";
-import * as owner from "../../e2e/pages/owners.pages.js";
-import * as ls from "../../support/localstorage_data.js";
+import * as batch from '../pages/batches.pages'
+import * as constants from '../../support/constants'
+import * as main from '../../e2e/pages/main.page'
+import * as owner from '../../e2e/pages/owners.pages.js'
+import * as ls from '../../support/localstorage_data.js'
 
-const currentNonce = 3;
-const funds_first_tx = "0.001";
-const funds_second_tx = "0.002";
+const currentNonce = 3
+const funds_first_tx = '0.001'
+const funds_second_tx = '0.002'
 
-describe("[SMOKE] Batch transaction tests", () => {
+describe('[SMOKE] Batch transaction tests', () => {
   beforeEach(() => {
-    cy.clearLocalStorage();
-    cy.visit(constants.BALANCE_URL + constants.SEPOLIA_TEST_SAFE_5);
-    owner.waitForConnectionStatus();
-    main.acceptCookies();
-  });
+    cy.clearLocalStorage()
+    cy.visit(constants.BALANCE_URL + constants.SEPOLIA_TEST_SAFE_5)
+    owner.waitForConnectionStatus()
+    main.acceptCookies()
+  })
 
-  it("[SMOKE] Verify empty batch list can be opened", () => {
-    batch.openBatchtransactionsModal();
-    batch.openNewTransactionModal();
-  });
+  it('[SMOKE] Verify empty batch list can be opened', () => {
+    batch.openBatchtransactionsModal()
+    batch.openNewTransactionModal()
+  })
 
-  it("[SMOKE] Verify a transaction can be added to the batch", () => {
-    batch.addNewTransactionToBatch(constants.EOA, currentNonce, funds_first_tx);
-    cy.contains(batch.transactionAddedToBatchStr).should("be.visible");
-    batch.verifyBatchIconCount(1);
-    batch.clickOnBatchCounter();
-    batch.verifyAmountTransactionsInBatch(1);
-  });
+  it('[SMOKE] Verify a transaction can be added to the batch', () => {
+    batch.addNewTransactionToBatch(constants.EOA, currentNonce, funds_first_tx)
+    cy.contains(batch.transactionAddedToBatchStr).should('be.visible')
+    batch.verifyBatchIconCount(1)
+    batch.clickOnBatchCounter()
+    batch.verifyAmountTransactionsInBatch(1)
+  })
 
-  it("[SMOKE] Verify the batch can be confirmed and related transactions exist in the form", () => {
-    main
-      .addToLocalStorage(
-        constants.localStorageKeys.SAFE_v2__batch,
-        ls.batchData.entry0
-      )
-      .then(() => {
-        main
-          .isItemInLocalstorage(
-            constants.localStorageKeys.SAFE_v2__batch,
-            ls.batchData.entry0
-          )
-          .then(() => {
-            cy.reload();
-            batch.clickOnBatchCounter();
-            batch.clickOnConfirmBatchBtn();
-            batch.verifyBatchTransactionsCount(2);
-            batch.clickOnBatchCounter();
-            cy.contains(funds_first_tx).parents("ul").as("TransactionList");
-            cy.get("@TransactionList")
-              .find("li")
-              .eq(0)
-              .find("span")
-              .eq(0)
-              .contains(funds_first_tx);
-            cy.get("@TransactionList")
-              .find("li")
-              .eq(1)
-              .find("span")
-              .eq(0)
-              .contains(funds_first_tx);
-          });
-      });
-  });
+  it('[SMOKE] Verify the batch can be confirmed and related transactions exist in the form', () => {
+    main.addToLocalStorage(constants.localStorageKeys.SAFE_v2__batch, ls.batchData.entry0).then(() => {
+      main.isItemInLocalstorage(constants.localStorageKeys.SAFE_v2__batch, ls.batchData.entry0).then(() => {
+        cy.reload()
+        batch.clickOnBatchCounter()
+        batch.clickOnConfirmBatchBtn()
+        batch.verifyBatchTransactionsCount(2)
+        batch.clickOnBatchCounter()
+        cy.contains(funds_first_tx).parents('ul').as('TransactionList')
+        cy.get('@TransactionList').find('li').eq(0).find('span').eq(0).contains(funds_first_tx)
+        cy.get('@TransactionList').find('li').eq(1).find('span').eq(0).contains(funds_first_tx)
+      })
+    })
+  })
 
-  it("[SMOKE] Verify a transaction can be removed from the batch", () => {
-    main
-      .addToLocalStorage(
-        constants.localStorageKeys.SAFE_v2__batch,
-        ls.batchData.entry0
-      )
-      .then(() => {
-        main
-          .isItemInLocalstorage(
-            constants.localStorageKeys.SAFE_v2__batch,
-            ls.batchData.entry0
-          )
-          .then(() => {
-            cy.reload();
-            batch.clickOnBatchCounter();
-            cy.contains(batch.batchedTransactionsStr)
-              .should("be.visible")
-              .parents("aside")
-              .find("ul > li")
-              .as("BatchList");
-            cy.get("@BatchList").find(batch.deleteTransactionbtn).eq(0).click();
-            cy.get("@BatchList").should("have.length", 1);
-            cy.get("@BatchList").contains(funds_second_tx).should("not.exist");
-          });
-      });
-  });
-});
+  it('[SMOKE] Verify a transaction can be removed from the batch', () => {
+    main.addToLocalStorage(constants.localStorageKeys.SAFE_v2__batch, ls.batchData.entry0).then(() => {
+      main.isItemInLocalstorage(constants.localStorageKeys.SAFE_v2__batch, ls.batchData.entry0).then(() => {
+        cy.reload()
+        batch.clickOnBatchCounter()
+        cy.contains(batch.batchedTransactionsStr).should('be.visible').parents('aside').find('ul > li').as('BatchList')
+        cy.get('@BatchList').find(batch.deleteTransactionbtn).eq(0).click()
+        cy.get('@BatchList').should('have.length', 1)
+        cy.get('@BatchList').contains(funds_second_tx).should('not.exist')
+      })
+    })
+  })
+})
