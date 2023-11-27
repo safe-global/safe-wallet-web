a8a0c7d195c5703e5fd30c7f976b607092496e2b
iamacook <aaron@safe.global>
2023-11-27 09:49:44 +0100

    fix: don't filter expired recovery transactions

--- src/hooks/useRecoveryQueue.ts
+++ src/hooks/useRecoveryQueue.ts
@@ -1,18 +1,10 @@
 import { selectRecoveryQueues } from '@/services/recovery/selectors'
-import { useClock } from './useClock'
 import { useRecovery } from '@/components/recovery/RecoveryContext'
 import type { RecoveryQueueItem } from '@/services/recovery/recovery-state'
 
 export function useRecoveryQueue(): Array<RecoveryQueueItem> {
   const [recovery] = useRecovery()
   const queue = recovery && selectRecoveryQueues(recovery)
-  const clock = useClock()
 
-  if (!queue) {
-    return []
-  }
-
-  return queue.filter(({ expiresAt }) => {
-    return expiresAt ? expiresAt.gt(clock) : true
-  })
+  return queue ?? []
 }
