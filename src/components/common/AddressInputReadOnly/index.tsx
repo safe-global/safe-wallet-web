40c3cc347db6ae5046426ae88028b61eba34d49f
katspaugh <381895+katspaugh@users.noreply.github.com>
2023-12-04 07:57:58 +0100

    Fix: disable copying address in AddressBookInput (#2924)
    * Fix: disable copying address in AddressBookInput
    
    * removing copy from readOnly inputs
    
    ---------
    
    Co-authored-by: Franco Venica <francovenica@gmail.com>

--- src/components/common/AddressInputReadOnly/index.tsx
+++ src/components/common/AddressInputReadOnly/index.tsx
@@ -19,7 +19,7 @@ const AddressInputReadOnly = ({ label, address }: { label: string; address: stri
           startAdornment={
             <InputAdornment position="start">
               <Typography variant="body2" component="div">
-                <EthHashInfo address={address} shortAddress={false} />
+                <EthHashInfo address={address} shortAddress={false} copyAddress={false} />
               </Typography>
             </InputAdornment>
           }
