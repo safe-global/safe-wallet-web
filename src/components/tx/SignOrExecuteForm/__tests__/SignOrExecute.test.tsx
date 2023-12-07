f0ecb89ff116b0ab2d184e913b1cf15c99ca727f
Usame Algan <usame@safe.global>
2023-12-07 16:58:18 +0100

    test: Add initial SignOrExecuteForm test

--- src/components/tx/SignOrExecuteForm/__tests__/SignOrExecute.test.tsx
+++ src/components/tx/SignOrExecuteForm/__tests__/SignOrExecute.test.tsx
@@ -1,8 +1,19 @@
-import SignOrExecuteForm from '@/components/tx/SignOrExecuteForm'
+import { SignOrExecuteForm } from '@/components/tx/SignOrExecuteForm'
+import { safeTxBuilder } from '@/tests/builders/safeTx'
 import { render } from '@/tests/test-utils'
 
 describe('SignOrExecute', () => {
-  it('renders without crashing', () => {
-    const result = render(<SignOrExecuteForm onSubmit={jest.fn()} />)
+  it('should display a safeTxError', () => {
+    const { getByText } = render(
+      <SignOrExecuteForm
+        onSubmit={jest.fn()}
+        safeTxError={new Error('Safe transaction error')}
+        safeTx={safeTxBuilder().build()}
+      />,
+    )
+
+    expect(
+      getByText('This transaction will most likely fail. To save gas costs, avoid confirming the transaction.'),
+    ).toBeInTheDocument()
   })
 })
