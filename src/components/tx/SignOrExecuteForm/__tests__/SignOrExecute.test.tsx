501a35801ea41b48e2459d9514533cfaa44c3d76
Usame Algan <usame@safe.global>
2023-12-08 12:23:43 +0100

    test: Add more SignOrExecuteForm tests

--- src/components/tx/SignOrExecuteForm/__tests__/SignOrExecute.test.tsx
+++ src/components/tx/SignOrExecuteForm/__tests__/SignOrExecute.test.tsx
@@ -1,4 +1,5 @@
 import { SignOrExecuteForm } from '@/components/tx/SignOrExecuteForm'
+import * as hooks from '@/components/tx/SignOrExecuteForm/hooks'
 import { safeTxBuilder } from '@/tests/builders/safeTx'
 import { render } from '@/tests/test-utils'
 
@@ -16,4 +17,44 @@ describe('SignOrExecute', () => {
       getByText('This transaction will most likely fail. To save gas costs, avoid confirming the transaction.'),
     ).toBeInTheDocument()
   })
+
+  describe('Existing transaction', () => {
+    it('should display radio options to sign or execute if both are possible', () => {
+      jest.spyOn(hooks, 'useValidateNonce').mockReturnValue(true)
+
+      const { getByText } = render(
+        <SignOrExecuteForm
+          safeTx={safeTxBuilder().build()}
+          onSubmit={jest.fn()}
+          safeTxError={undefined}
+          isExecutable={true}
+        />,
+      )
+
+      expect(getByText('Would you like to execute the transaction immediately?')).toBeInTheDocument()
+    })
+  })
+
+  describe('New transaction', () => {
+    it('should display radio options to sign or execute if both are possible', () => {
+      jest.spyOn(hooks, 'useValidateNonce').mockReturnValue(true)
+      jest.spyOn(hooks, 'useImmediatelyExecutable').mockReturnValue(true)
+
+      const { getByText } = render(
+        <SignOrExecuteForm
+          safeTx={safeTxBuilder().build()}
+          onSubmit={jest.fn()}
+          safeTxError={undefined}
+          txId={undefined}
+          isExecutable={true}
+        />,
+      )
+
+      expect(getByText('Would you like to execute the transaction immediately?')).toBeInTheDocument()
+    })
+  })
+
+  it.todo('should not display radio options if execution is the only option')
+  it.todo('should display an execution title if that option is selected')
+  it.todo('should display a sign title if that option is selected')
 })
