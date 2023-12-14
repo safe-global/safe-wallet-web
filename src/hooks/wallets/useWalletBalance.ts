f82d825c55f39446799bf54ab6d2259d6b93c990
Manuel Gellfart <manu@safe.global>
2023-12-14 12:10:49 +0100

    fix: poll wallet balance (#3011)

--- src/hooks/wallets/useWalletBalance.ts
+++ src/hooks/wallets/useWalletBalance.ts
@@ -2,18 +2,32 @@ import useAsync, { type AsyncResult } from '../useAsync'
 import useWallet from './useWallet'
 import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
 import { type BigNumber } from 'ethers'
+import useIntervalCounter from '../useIntervalCounter'
+import { POLLING_INTERVAL } from '@/config/constants'
+import { useEffect } from 'react'
 
 const useWalletBalance = (): AsyncResult<BigNumber | undefined> => {
   const web3ReadOnly = useWeb3ReadOnly()
   const wallet = useWallet()
+  const [pollingInterval, resetPolling] = useIntervalCounter(POLLING_INTERVAL)
 
-  return useAsync<BigNumber | undefined>(() => {
-    if (!wallet || !web3ReadOnly) {
-      return undefined
-    }
+  // Reset polling if new wallet connects
+  useEffect(() => {
+    resetPolling()
+  }, [resetPolling, wallet, web3ReadOnly])
 
-    return web3ReadOnly.getBalance(wallet.address, 'latest')
-  }, [wallet, web3ReadOnly])
+  return useAsync<BigNumber | undefined>(
+    () => {
+      if (!wallet || !web3ReadOnly) {
+        return undefined
+      }
+
+      return web3ReadOnly.getBalance(wallet.address, 'latest')
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [wallet, web3ReadOnly, pollingInterval],
+    false,
+  )
 }
 
 export default useWalletBalance
