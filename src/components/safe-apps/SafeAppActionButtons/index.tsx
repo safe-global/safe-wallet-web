5daee5059a143680b23b44e4ce883404fac58fa1
katspaugh <katspaugh@gmail.com>
2023-12-20 09:56:52 +0100

    Fix: [Safe Apps] align the share button

--- src/components/safe-apps/SafeAppActionButtons/index.tsx
+++ src/components/safe-apps/SafeAppActionButtons/index.tsx
@@ -37,7 +37,7 @@ const SafeAppActionButtons = ({
   }
 
   return (
-    <Box display="flex" gap={1.5} alignItems="center">
+    <Box display="flex" gap={1} alignItems="center">
       {/* Open the preview drawer */}
       {openPreviewDrawer && (
         <IconButton
@@ -58,7 +58,9 @@ const SafeAppActionButtons = ({
         onCopy={handleCopyShareSafeAppUrl}
         text={shareSafeAppUrl}
       >
-        <SvgIcon component={ShareIcon} inheritViewBox color="border" fontSize="small" />
+        <IconButton size="small">
+          <SvgIcon component={ShareIcon} inheritViewBox color="border" fontSize="small" />
+        </IconButton>
       </CopyButton>
 
       {/* Bookmark Safe App button */}
