973a325e68520118bf2a228c99f9bf37b2937605
schmanu <manu@safe.global>
2024-01-02 17:54:51 +0100

    fix: show fieldErrors in readonly input

--- src/components/common/AddressBookInput/index.tsx
+++ src/components/common/AddressBookInput/index.tsx
@@ -1,6 +1,6 @@
 import AddressInputReadOnly from '@/components/common/AddressInputReadOnly'
 import { type ReactElement, useState, useMemo } from 'react'
-import { useFormContext, useWatch } from 'react-hook-form'
+import { get, useFormContext, useWatch } from 'react-hook-form'
 import { Box, SvgIcon, Typography } from '@mui/material'
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete'
 import useAddressBook from '@/hooks/useAddressBook'
@@ -20,7 +20,7 @@ const abFilterOptions = createFilterOptions({
  */
 const AddressBookInput = ({ name, canAdd, ...props }: AddressInputProps & { canAdd?: boolean }): ReactElement => {
   const addressBook = useAddressBook()
-  const { setValue, control } = useFormContext()
+  const { setValue, control, formState } = useFormContext()
   const addressValue = useWatch({ name, control })
   const [open, setOpen] = useState(false)
   const [openAddressBook, setOpenAddressBook] = useState<boolean>(false)
@@ -46,11 +46,14 @@ const AddressBookInput = ({ name, canAdd, ...props }: AddressInputProps & { canA
     : undefined
 
   if (addressBook[addressValue]) {
+    const fieldError = get(formState.errors, name)
+
     return (
       <Box data-testid="address-book-recipient" onClick={() => setValue(name, '')}>
         <AddressInputReadOnly
           address={addressValue}
-          label={typeof props.label === 'string' ? props.label : 'Sending to'}
+          label={fieldError?.message || (typeof props.label === 'string' ? props.label : 'Sending to')}
+          error={!!fieldError}
         />
       </Box>
     )
--- src/components/common/AddressInputReadOnly/index.tsx
+++ src/components/common/AddressInputReadOnly/index.tsx
@@ -1,4 +1,4 @@
-import { useState, type ReactElement } from 'react'
+import { type ReactNode, useState, type ReactElement } from 'react'
 import { IconButton, InputAdornment, InputLabel, OutlinedInput, SvgIcon, Typography } from '@mui/material'
 import EthHashInfo from '@/components/common/EthHashInfo'
 import SaveAddressIcon from '@/public/images/common/save-address.svg'
@@ -6,16 +6,27 @@ import css from './styles.module.css'
 import EntryDialog from '@/components/address-book/EntryDialog'
 import useAddressBook from '@/hooks/useAddressBook'
 
-const AddressInputReadOnly = ({ label, address }: { label: string; address: string }): ReactElement => {
+const AddressInputReadOnly = ({
+  label,
+  address,
+  error,
+}: {
+  label: ReactNode
+  address: string
+  error?: boolean
+}): ReactElement => {
   const addressBook = useAddressBook()
   const [open, setOpen] = useState(false)
 
   return (
     <>
       <div className={css.wrapper}>
-        <InputLabel shrink>{label}</InputLabel>
+        <InputLabel shrink error={error}>
+          {label}
+        </InputLabel>
         <OutlinedInput
           className={css.input}
+          error={error}
           startAdornment={
             <InputAdornment position="start">
               <Typography variant="body2" component="div">
