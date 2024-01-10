c95045860be663e39010bed44cd186356e6e8dd8
katspaugh <katspaugh@gmail.com>
2024-01-10 14:39:14 +0100

    Adjust getTransactionQueue signature

--- src/hooks/useTxNotifications.ts
+++ src/hooks/useTxNotifications.ts
@@ -97,8 +97,8 @@ const useTxNotifications = (): void => {
             link: txId
               ? getTxLink(txId, chain, safeAddress)
               : txHash
-                ? getExplorerLink(txHash, chain.blockExplorerUriTemplate)
-                : undefined,
+              ? getExplorerLink(txHash, chain.blockExplorerUriTemplate)
+              : undefined,
           }),
         )
       }),
