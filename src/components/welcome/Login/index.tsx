6af74af714ca82d228fbd910ebaca915e4be6a71
katspaugh <katspaugh@gmail.com>
2024-01-22 14:33:59 +0100

    Load owned safes for allchains

--- src/components/welcome/Login/index.tsx
+++ src/components/welcome/Login/index.tsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import { Box, Grid, SvgIcon, Typography } from '@mui/material'
+import React, { useMemo } from 'react'
 import SafeListItem from '@/components/sidebar/SafeListItem'
+import useAllOwnedSafes from '@/hooks/useOwnedSafes'
+import { Button } from '@mui/material'
+
+const maxSafes = 5
 
 const Login = () => {
+  const [safes] = useAllOwnedSafes(maxSafes)
+
+  const safesToShow = useMemo(() => {
+    return safes.slice(0, maxSafes)
+  }, [safes])
+
   return (
     <>
-      <SafeListItem
-        key={'sep:0x47676ba668Cf7025C1a53B8329608310FCC80583'}
-        address={'sep:0x47676ba668Cf7025C1a53B8329608310FCC80583'}
-        chainId={'11155111'}
-        href={''}
-        shouldScrollToSafe={false}
-        isAdded
-        isWelcomePage={false}
-      />
-      <SafeListItem
-        key={'sep:0x47676ba668Cf7025C1a53B8329608310FCC80583'}
-        address={'sep:0x47676ba668Cf7025C1a53B8329608310FCC80583'}
-        chainId={'11155111'}
-        href={''}
-        shouldScrollToSafe={false}
-        isAdded
-        isWelcomePage={false}
-      />
+      {safesToShow.map(({ safeAddress, chainId }) => (
+        <SafeListItem
+          key={chainId + safeAddress}
+          address={safeAddress}
+          chainId={chainId}
+          href={''}
+          shouldScrollToSafe={false}
+          isAdded
+          isWelcomePage={false}
+        />
+      ))}
+
+      <Button variant="contained" color="primary" href="/safes/new" sx={{ mt: 2 }}>
+        Show more
+      </Button>
     </>
   )
 }
--- src/hooks/useOwnedSafes.ts
+++ src/hooks/useOwnedSafes.ts
@@ -1,37 +1,82 @@
-import { useEffect } from 'react'
-import { getOwnedSafes, type OwnedSafes } from '@safe-global/safe-gateway-typescript-sdk'
+import { useEffect, useMemo, useState } from 'react'
+import { getOwnedSafes } from '@safe-global/safe-gateway-typescript-sdk'
 
 import useWallet from '@/hooks/wallets/useWallet'
-import { Errors, logError } from '@/services/exceptions'
-import useChainId from './useChainId'
-import useAsync from '@/hooks/useAsync'
+import useChainId from '@/hooks/useChainId'
 import useChains from '@/hooks/useChains'
 
-const CACHE_KEY = 'ownedSafes'
-
-type OwnedSafesCache = {
-  [walletAddress: string]: {
-    [chainId: string]: OwnedSafes['safes']
-  }
+const getWalletSafes = async (walletAddress?: string, chainId?: string) => {
+  if (!walletAddress || !chainId) return
+  return getOwnedSafes(chainId, walletAddress)
 }
 
-const useOwnedSafes = (chainId: string) => {
+type OwnedSafe = {
+  chainId: string
+  safeAddress: string
+}
+
+/**
+ * Fetch all safes owned by the current wallet up to a certain limit.
+ * The hook loads safes sequentially from all chains, updating its state as it goes.
+ */
+const useAllOwnedSafes = (safesToFetch: number): [OwnedSafe[], Error | undefined, boolean] => {
   const { address: walletAddress } = useWallet() || {}
-
-  return useAsync(() => {
-    if (!walletAddress || !chainId) return
-
-    return getOwnedSafes(chainId, walletAddress)
-  }, [chainId, walletAddress])
-}
-
-const useAllOwnedSafes = (safesToFetch: number) => {
-
+  const currentChainId = useChainId()
   const { configs } = useChains()
-  const currentChainId = useChainId();
 
-  const ownedSafesOnCurrentChain = useOwnedSafes(currentChainId);
+  const chainIds = useMemo(() => {
+    const allChainIds = configs.map((config) => config.chainId).filter((chainId) => chainId !== currentChainId)
+    return [currentChainId, ...allChainIds]
+  }, [configs, currentChainId])
 
+  const [allSafes, setAllSafes] = useState<OwnedSafe[]>([])
+  const [error, setError] = useState<Error>()
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    let current = true
+    const allSafes: OwnedSafe[] = []
+
+    const load = async (index: number) => {
+      if (!current) return
+
+      const chainId = chainIds[index]
+      let chainSafes
+      try {
+        chainSafes = await getWalletSafes(walletAddress, chainId)
+      } catch (error) {
+        setError(error as Error)
+      }
+
+      if (!current) return
+
+      const ownedSafesOnChain = (chainSafes?.safes || []).map((safeAddress) => ({
+        safeAddress,
+        chainId,
+      }))
+
+      const newAllSafes = [...allSafes, ...ownedSafesOnChain]
+
+      setAllSafes(newAllSafes)
+
+      if (safesToFetch > newAllSafes.length && index < chainIds.length - 1) {
+        load(index + 1)
+      } else {
+        setLoading(false)
+      }
+    }
+
+    if (safesToFetch > 0) {
+      setLoading(true)
+      load(0)
+    }
+
+    return () => {
+      current = false
+    }
+  }, [walletAddress, chainIds])
+
+  return [allSafes, error, loading]
 }
 
-export default useOwnedSafes
+export default useAllOwnedSafes
