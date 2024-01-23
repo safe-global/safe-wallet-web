ea70df148a5857cf97a5b39fc39ac1ee182d93a0
Manuel Gellfart <manu@safe.global>
2024-01-23 15:42:48 +0100

    fix: ignore trusted flags on networks without token list (#3137)

--- src/components/transactions/TrustedToggle/index.tsx
+++ src/components/transactions/TrustedToggle/index.tsx
@@ -1,11 +1,17 @@
+import { useHasFeature } from '@/hooks/useChains'
 import { useAppDispatch, useAppSelector } from '@/store'
 import { selectSettings, setshowOnlyTrustedTransactions } from '@/store/settingsSlice'
+import { FEATURES } from '@/utils/chains'
 import madProps from '@/utils/mad-props'
 import _TrustedToggleButton from './TrustedToggleButton'
 
 const useOnlyTrusted = () => {
   const userSettings = useAppSelector(selectSettings)
   return userSettings.showOnlyTrustedTransactions || false
+}
+
+const useHasDefaultTokenList = () => {
+  return useHasFeature(FEATURES.DEFAULT_TOKENLIST)
 }
 
 const useSetOnlyTrusted = () => {
@@ -18,6 +24,7 @@ const useSetOnlyTrusted = () => {
 const TrustedToggle = madProps(_TrustedToggleButton, {
   onlyTrusted: useOnlyTrusted,
   setOnlyTrusted: useSetOnlyTrusted,
+  hasDefaultTokenlist: useHasDefaultTokenList,
 })
 
 export default TrustedToggle
