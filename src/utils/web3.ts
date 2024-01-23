59788d910de37d92a0a17e59206b0825691f5234
Daniel Dimitrov <daniel@compojoom.com>
2024-01-23 11:39:02 +0100

    fix: message signing
    ethers v6 converts the chainId to a hex value: https://github.com/ethers-io/ethers.js/blob/50b74b8806ef2064f2764b09f89c7ac75fda3a3c/src.ts/hash/typed-data.ts#L75
    Our SDK expects a number, that's why we convert the hex value from ethers v6 to number.
    If this gets fixed here: https://github.com/safe-global/safe-eth-py/issues/748
    we can remove this workaround

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -12,5 +12,16 @@ export const hashTypedData = (typedData: EIP712TypedData): string => {
 
 export const normalizeTypedData = (typedData: EIP712TypedData): EIP712Normalized => {
   const { EIP712Domain: _, ...types } = typedData.types
-  return TypedDataEncoder.getPayload(typedData.domain as TypedDataDomain, types, typedData.message)
+  const payload = TypedDataEncoder.getPayload(typedData.domain as TypedDataDomain, types, typedData.message)
+
+  // ethers v6 converts the chainId to a hex value:
+  // https://github.com/ethers-io/ethers.js/blob/50b74b8806ef2064f2764b09f89c7ac75fda3a3c/src.ts/hash/typed-data.ts#L75
+  // Our SDK expects a number, that's why we convert it here
+  // If this gets fixed here: https://github.com/safe-global/safe-eth-py/issues/748
+  // we can remove this workaround
+  if (payload.domain.chainId) {
+    payload.domain.chainId = Number(BigInt(payload.domain.chainId))
+  }
+
+  return payload
 }
