90dba24547f89c653525f22a6782ad20b92befa0
mike10ca <michael@safe.global>
2024-01-30 13:19:36 +0100

    tests: skip drain account tests - wip

--- cypress/e2e/safe-apps/drain_account.spec.cy.js
+++ cypress/e2e/safe-apps/drain_account.spec.cy.js
@@ -6,10 +6,10 @@ import * as safeapps from '../pages/safeapps.pages'
 describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
   const appUrl = constants.drainAccount_url
   const iframeSelector = `iframe[id="iframe-${appUrl}"]`
-  const visitUrl = `/apps/open?safe=${constants.GOERLI_SAFE_APPS_SAFE}&appUrl=${encodeURIComponent(appUrl)}`
+  const visitUrl = `/apps/open?safe=${constants.SEPOLIA_TEST_SAFE_5}&appUrl=${encodeURIComponent(appUrl)}`
 
   beforeEach(() => {
-    cy.intercept(`**//v1/chains/5/safes/${constants.GOERLI_SAFE_APPS_SAFE.substring(4)}/balances/**`, {
+    cy.intercept(`**//v1/chains/5/safes/${constants.SEPOLIA_TEST_SAFE_5.substring(4)}/balances/**`, {
       fixture: 'balances.json',
     })
 
@@ -19,7 +19,7 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     safeapps.clickOnContinueBtn()
   })
 
-  it('Verify drain can be created', () => {
+  it.skip('Verify drain can be created', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.recipientStr).type(constants.SAFE_APP_ADDRESS_2)
       getBody().findAllByText(safeapps.transferEverythingStr).click()
@@ -28,7 +28,7 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     cy.findByRole('button', { name: safeapps.testNativeTransfer2 })
   })
 
-  it('Verify partial drain can be created', () => {
+  it.skip('Verify partial drain can be created', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.selectAllRowsChbxStr).click()
       getBody().findAllByLabelText(safeapps.selectRowChbxStr).eq(1).click()
@@ -40,7 +40,7 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     cy.findByRole('button', { name: safeapps.testNativeTransfer1 })
   })
 
-  it('Verify a drain can be created when a ENS is specified', () => {
+  it.skip('Verify a drain can be created when a ENS is specified', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.recipientStr).type('goerli-test-safe.eth').wait(2000)
       getBody().findAllByText(safeapps.transferEverythingStr).click()
@@ -61,14 +61,14 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     })
   })
 
-  it('Verify a drain cannot be created with no recipient selected', () => {
+  it.skip('Verify a drain cannot be created with no recipient selected', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findAllByText(safeapps.transferEverythingStr).click()
       getBody().findByText(safeapps.validRecipientAddressStr)
     })
   })
 
-  it('Verify a drain cannot be created with invalid recipient selected', () => {
+  it.skip('Verify a drain cannot be created with invalid recipient selected', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.recipientStr).type(constants.SAFE_APP_ADDRESS_2.substring(1))
       getBody().findAllByText(safeapps.transferEverythingStr).click()
@@ -76,7 +76,7 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     })
   })
 
-  it('Verify a drain cannot be created when no assets are selected', () => {
+  it.skip('Verify a drain cannot be created when no assets are selected', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.selectAllRowsChbxStr).click()
       getBody().findByLabelText(safeapps.recipientStr).type(constants.SAFE_APP_ADDRESS_2)
@@ -84,7 +84,7 @@ describe('Drain Account tests', { defaultCommandTimeout: 12000 }, () => {
     })
   })
 
-  it('Verify a drain cannot be created when no assets and recipient are selected', () => {
+  it.skip('Verify a drain cannot be created when no assets and recipient are selected', () => {
     cy.enter(iframeSelector).then((getBody) => {
       getBody().findByLabelText(safeapps.selectAllRowsChbxStr).click()
       getBody().findAllByText(safeapps.noTokensSelectedStr).should('be.visible')
