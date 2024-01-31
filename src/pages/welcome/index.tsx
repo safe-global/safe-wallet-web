4b7d6da6a4da662718c28218e866151eba405678
James Mealy <james@safe.global>
2024-01-31 12:20:54 +0100

    show existing welcome page if no wallet is connected

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,15 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import NewSafe from '@/components/welcome/NewSafe'
+import useWallet from '@/hooks/wallets/useWallet'
+import SafeList from '@/components/common/SafeList'
+import { DataWidget } from '@/components/welcome/SafeListDrawer/DataWidget'
 
 const Welcome: NextPage = () => {
+  const wallet = useWallet()
   return (
     <>
       <Head>
         <title>{'Safe{Wallet} – Welcome'}</title>
       </Head>
 
-      <NewSafe />
+      {wallet ? (
+        <>
+          <SafeList />
+          <DataWidget />
+        </>
+      ) : (
+        <NewSafe />
+      )}
     </>
   )
 }
