25cfa7f4c2384bda93fed9ff38610cb50f8571f8
Usame Algan <usame@safe.global>
2024-02-06 12:35:41 +0100

    fix: Allow relaying when deploying a safe

--- src/features/counterfactual/ActivateAccount.tsx
+++ src/features/counterfactual/ActivateAccount.tsx
@@ -1,22 +1,31 @@
 import ChainIndicator from '@/components/common/ChainIndicator'
 import EthHashInfo from '@/components/common/EthHashInfo'
-import { createNewSafe } from '@/components/new-safe/create/logic'
+import { createNewSafe, relaySafeCreation } from '@/components/new-safe/create/logic'
+import NetworkWarning from '@/components/new-safe/create/NetworkWarning'
 import { NetworkFee } from '@/components/new-safe/create/steps/ReviewStep'
 import css from '@/components/new-safe/create/steps/ReviewStep/styles.module.css'
 import ReviewRow from '@/components/new-safe/ReviewRow'
 import { TxModalContext } from '@/components/tx-flow'
 import TxCard from '@/components/tx-flow/common/TxCard'
 import ErrorMessage from '@/components/tx/ErrorMessage'
+import { ExecutionMethod, ExecutionMethodSelector } from '@/components/tx/ExecutionMethodSelector'
 import useDeployGasLimit from '@/features/counterfactual/hooks/useDeployGasLimit'
 import { removeUndeployedSafe, selectUndeployedSafe } from '@/features/counterfactual/store/undeployedSafeSlice'
 import useChainId from '@/hooks/useChainId'
 import { useCurrentChain } from '@/hooks/useChains'
 import useGasPrice, { getTotalFeeFormatted } from '@/hooks/useGasPrice'
+import useIsWrongChain from '@/hooks/useIsWrongChain'
+import { useLeastRemainingRelays } from '@/hooks/useRemainingRelays'
 import useSafeInfo from '@/hooks/useSafeInfo'
+import useWalletCanPay from '@/hooks/useWalletCanPay'
+import useWallet from '@/hooks/wallets/useWallet'
 import { useWeb3 } from '@/hooks/wallets/web3'
 import { asError } from '@/services/exceptions/utils'
+import { isSocialLoginWallet } from '@/services/mpc/SocialLoginModule'
+import { waitForCreateSafeTx } from '@/services/tx/txMonitor'
 import { useAppDispatch, useAppSelector } from '@/store'
 import { hasFeature } from '@/utils/chains'
+import { hasRemainingRelays } from '@/utils/relaying'
 import { Alert, Box, Button, CircularProgress, Divider, Grid, Typography } from '@mui/material'
 import type { DeploySafeProps } from '@safe-global/protocol-kit'
 import { FEATURES } from '@safe-global/safe-gateway-typescript-sdk'
@@ -42,14 +51,17 @@ const useActivateAccount = () => {
     : { gasPrice: maxFeePerGas?.toString(), gasLimit: gasLimit?.toString() }
 
   const totalFee = getTotalFeeFormatted(maxFeePerGas, maxPriorityFeePerGas, gasLimit, chain)
+  const walletCanPay = useWalletCanPay({ gasLimit, maxFeePerGas, maxPriorityFeePerGas })
 
-  return { options, totalFee }
+  return { options, totalFee, walletCanPay }
 }
 
 const ActivateAccountFlow = () => {
   const [isSubmittable, setIsSubmittable] = useState<boolean>(true)
   const [submitError, setSubmitError] = useState<Error | undefined>()
+  const [executionMethod, setExecutionMethod] = useState(ExecutionMethod.RELAY)
 
+  const isWrongChain = useIsWrongChain()
   const chain = useCurrentChain()
   const chainId = useChainId()
   const { safeAddress } = useSafeInfo()
@@ -57,22 +69,43 @@ const ActivateAccountFlow = () => {
   const undeployedSafe = useAppSelector((state) => selectUndeployedSafe(state, chainId, safeAddress))
   const { setTxFlow } = useContext(TxModalContext)
   const dispatch = useAppDispatch()
-  const { options, totalFee } = useActivateAccount()
+  const wallet = useWallet()
+  const { options, totalFee, walletCanPay } = useActivateAccount()
+
+  const ownerAddresses = undeployedSafe?.safeAccountConfig.owners || []
+  const [minRelays] = useLeastRemainingRelays(ownerAddresses)
+
+  // Every owner has remaining relays and relay method is selected
+  const canRelay = hasRemainingRelays(minRelays)
+  const willRelay = canRelay && executionMethod === ExecutionMethod.RELAY
 
   if (!undeployedSafe) return null
 
   const createSafe = async () => {
-    if (!provider) return
+    if (!provider || !chain) return
 
     setIsSubmittable(false)
     setSubmitError(undefined)
 
     try {
-      await createNewSafe(provider, {
-        safeAccountConfig: undeployedSafe.safeAccountConfig,
-        saltNonce: undeployedSafe.safeDeploymentConfig?.saltNonce,
-        options,
-      })
+      if (willRelay) {
+        const taskId = await relaySafeCreation(
+          chain,
+          undeployedSafe.safeAccountConfig.owners,
+          undeployedSafe.safeAccountConfig.threshold,
+          Number(undeployedSafe.safeDeploymentConfig?.saltNonce!),
+        )
+
+        waitForCreateSafeTx(taskId, (status) => {
+          console.log(status)
+        })
+      } else {
+        await createNewSafe(provider, {
+          safeAccountConfig: undeployedSafe.safeAccountConfig,
+          saltNonce: undeployedSafe.safeDeploymentConfig?.saltNonce,
+          options,
+        })
+      }
       dispatch(removeUndeployedSafe({ chainId, address: safeAddress }))
     } catch (_err) {
       const err = asError(_err)
@@ -85,6 +118,7 @@ const ActivateAccountFlow = () => {
   }
 
   const submitDisabled = !isSubmittable
+  const isSocialLogin = isSocialLoginWallet(wallet?.label)
 
   return (
     <TxLayout title="Activate account" hideNonce>
@@ -127,28 +161,54 @@ const ActivateAccountFlow = () => {
           />
         </Grid>
 
-        <Divider sx={{ mx: -3, my: 2 }} />
+        <Divider sx={{ mx: -3, mt: 2, mb: 1 }} />
+        <Box display="flex" flexDirection="column" gap={3}>
+          {canRelay && !isSocialLogin && (
+            <Grid container spacing={3}>
+              <ReviewRow
+                name="Execution method"
+                value={
+                  <ExecutionMethodSelector
+                    executionMethod={executionMethod}
+                    setExecutionMethod={setExecutionMethod}
+                    relays={minRelays}
+                  />
+                }
+              />
+            </Grid>
+          )}
 
-        <Grid container spacing={3}>
-          <ReviewRow
-            name="Est. network fee"
-            value={
-              <>
-                <NetworkFee totalFee={totalFee} willRelay={false} chain={chain} />
+          <Grid data-testid="network-fee-section" container spacing={3}>
+            <ReviewRow
+              name="Est. network fee"
+              value={
+                <>
+                  <NetworkFee totalFee={totalFee} willRelay={willRelay} chain={chain} />
 
-                <Typography variant="body2" color="text.secondary" mt={1}>
-                  You will have to confirm a transaction with your connected wallet.
-                </Typography>
-              </>
-            }
-          />
-        </Grid>
+                  {!willRelay && !isSocialLogin && (
+                    <Typography variant="body2" color="text.secondary" mt={1}>
+                      You will have to confirm a transaction with your connected wallet.
+                    </Typography>
+                  )}
+                </>
+              }
+            />
+          </Grid>
 
-        {submitError && (
-          <Box mt={1}>
-            <ErrorMessage error={submitError}>Error submitting the transaction. Please try again.</ErrorMessage>
-          </Box>
-        )}
+          {submitError && (
+            <Box mt={1}>
+              <ErrorMessage error={submitError}>Error submitting the transaction. Please try again.</ErrorMessage>
+            </Box>
+          )}
+
+          {isWrongChain && <NetworkWarning />}
+
+          {!walletCanPay && !willRelay && (
+            <ErrorMessage>
+              Your connected wallet doesn&apos;t have enough funds to execute this transaction
+            </ErrorMessage>
+          )}
+        </Box>
 
         <Divider sx={{ mx: -3, mt: 2, mb: 1 }} />
 
