686592816ea06dd0ea260b91c1e3efebffe19a95
James Mealy <james@safe.global>
2024-02-07 13:59:51 +0000

    use endpoint to fetch all owned safes

--- src/hooks/useSafes.ts
+++ src/hooks/useSafes.ts
@@ -1,10 +1,11 @@
 import { useMemo } from 'react'
-import type { ChainInfo, AddressEx } from '@safe-global/safe-gateway-typescript-sdk'
-
+import type { ChainInfo, AddressEx, AllOwnedSafes } from '@safe-global/safe-gateway-typescript-sdk'
+import { getAllOwnedSafes } from '@safe-global/safe-gateway-typescript-sdk'
 import useChainId from '@/hooks/useChainId'
 import useChains from '@/hooks/useChains'
 import { useAppSelector } from '@/store'
 import { selectAllAddedSafes } from '@/store/addedSafesSlice'
+import useAsync from './useAsync'
 
 type SafeListItemDetails = {
   chain: ChainInfo
@@ -18,6 +19,13 @@ const sortChainsByCurrentChain = (chains: ChainInfo[], currentChainId: string): 
   const currentChain = chains.find(({ chainId }) => chainId === currentChainId)
   const otherChains = chains.filter(({ chainId }) => chainId !== currentChainId)
   return currentChain ? [currentChain, ...otherChains] : chains
+}
+
+export const useOwnedSafes = (walletAddress: string): [AllOwnedSafes | undefined, Error | undefined, boolean] => {
+  const [allOwnedSafes, error, loading] = useAsync(() => {
+    return getAllOwnedSafes(walletAddress)
+  }, [walletAddress])
+  return [allOwnedSafes, error, loading]
 }
 
 export const useWatchedSafes = (): SafeListItemDetails[] => {
