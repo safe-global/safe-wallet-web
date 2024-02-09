65ad80cdc9c7e2373a1e915bdc3fbd64db51e167
Daniel Dimitrov <daniel@compojoom.com>
2024-02-09 11:42:39 +0100

    refactor usage of useAppCommunicator

--- src/components/safe-apps/AppFrame/index.tsx
+++ src/components/safe-apps/AppFrame/index.tsx
@@ -1,28 +1,15 @@
-import { useContext, useState } from 'react'
 import type { ReactElement } from 'react'
 import { useMemo } from 'react'
 import { useCallback, useEffect } from 'react'
 import { CircularProgress, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import { getBalances, getTransactionDetails, getSafeMessage } from '@safe-global/safe-gateway-typescript-sdk'
-import type {
-  AddressBookItem,
-  BaseTransaction,
-  EIP712TypedData,
-  RequestId,
-  SafeSettings,
-  SendTransactionRequestParams,
-} from '@safe-global/safe-apps-sdk'
-import { Methods } from '@safe-global/safe-apps-sdk'
+import type { RequestId } from '@safe-global/safe-apps-sdk'
 
 import { trackSafeAppOpenCount } from '@/services/safe-apps/track-app-usage-count'
-import { TxEvent, txSubscribe } from '@/services/tx/txEvents'
 import { SAFE_APPS_EVENTS, trackSafeAppEvent } from '@/services/analytics'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import { useSafeAppFromBackend } from '@/hooks/safe-apps/useSafeAppFromBackend'
-import useChainId from '@/hooks/useChainId'
-import useAddressBook from '@/hooks/useAddressBook'
 import { useSafePermissions } from '@/hooks/safe-apps/permissions'
 import { useCurrentChain } from '@/hooks/useChains'
 import { isSameUrl } from '@/utils/url'
@@ -31,24 +18,14 @@ import { gtmTrackPageview } from '@/services/analytics/gtm'
 import useThirdPartyCookies from './useThirdPartyCookies'
 import useAnalyticsFromSafeApp from './useFromAppAnalytics'
 import useAppIsLoading from './useAppIsLoading'
-import useAppCommunicator, { CommunicatorMessages } from './useAppCommunicator'
 import { ThirdPartyCookiesWarning } from './ThirdPartyCookiesWarning'
 import TransactionQueueBar, { TRANSACTION_BAR_HEIGHT } from './TransactionQueueBar'
-import { safeMsgSubscribe, SafeMsgEvent } from '@/services/safe-messages/safeMsgEvents'
-import { useAppSelector } from '@/store'
-import { selectSafeMessages } from '@/store/safeMessagesSlice'
-import { isSafeMessageListItem } from '@/utils/safe-message-guards'
-import { isOffchainEIP1271Supported } from '@/utils/safe-messages'
 import PermissionsPrompt from '@/components/safe-apps/PermissionsPrompt'
 import { PermissionStatus, type SafeAppDataWithPermissions } from '@/components/safe-apps/types'
 
 import css from './styles.module.css'
 import SafeAppIframe from './SafeAppIframe'
-import useGetSafeInfo from './useGetSafeInfo'
-import { hasFeature, FEATURES } from '@/utils/chains'
-import { selectTokenList, selectOnChainSigning, TOKEN_LISTS } from '@/store/settingsSlice'
-import { TxModalContext } from '@/components/tx-flow'
-import { SafeAppsTxFlow, SignMessageFlow, SignMessageOnChainFlow } from '@/components/tx-flow/flows'
+import { useCustomAppCommunicator } from '@/hooks/safe-apps/useCustomAppCommunicator'
 
 const UNKNOWN_APP_NAME = 'Unknown Safe App'
 
@@ -59,18 +36,7 @@ type AppFrameProps = {
 }
 
 const AppFrame = ({ appUrl, allowedFeaturesList, safeAppFromManifest }: AppFrameProps): ReactElement => {
-  const chainId = useChainId()
-  // We use offChainSigning by default
-  const [settings, setSettings] = useState<SafeSettings>({
-    offChainSigning: true,
-  })
-  const [currentRequestId, setCurrentRequestId] = useState<RequestId | undefined>()
-  const safeMessages = useAppSelector(selectSafeMessages)
   const { safe, safeLoaded, safeAddress } = useSafeInfo()
-  const tokenlist = useAppSelector(selectTokenList)
-  const onChainSigning = useAppSelector(selectOnChainSigning)
-
-  const addressBook = useAddressBook()
   const chain = useCurrentChain()
   const router = useRouter()
   const {
@@ -85,130 +51,10 @@ const AppFrame = ({ appUrl, allowedFeaturesList, safeAppFromManifest }: AppFrame
   const { thirdPartyCookiesDisabled, setThirdPartyCookiesDisabled } = useThirdPartyCookies()
   const { iframeRef, appIsLoading, isLoadingSlow, setAppIsLoading } = useAppIsLoading()
   useAnalyticsFromSafeApp(iframeRef)
-  const { getPermissions, hasPermission, permissionsRequest, setPermissionsRequest, confirmPermissionRequest } =
-    useSafePermissions()
+  const { permissionsRequest, setPermissionsRequest, confirmPermissionRequest } = useSafePermissions()
   const appName = useMemo(() => (remoteApp ? remoteApp.name : appUrl), [appUrl, remoteApp])
-  const { setTxFlow } = useContext(TxModalContext)
 
-  const onTxFlowClose = () => {
-    setCurrentRequestId((prevId) => {
-      if (prevId) {
-        communicator?.send(CommunicatorMessages.REJECT_TRANSACTION_MESSAGE, prevId, true)
-        trackSafeAppEvent(SAFE_APPS_EVENTS.PROPOSE_TRANSACTION_REJECTED, appName)
-      }
-      return undefined
-    })
-  }
-
-  const communicator = useAppCommunicator(iframeRef, remoteApp || safeAppFromManifest, chain, {
-    onConfirmTransactions: (txs: BaseTransaction[], requestId: RequestId, params?: SendTransactionRequestParams) => {
-      const data = {
-        app: safeAppFromManifest,
-        appId: remoteApp ? String(remoteApp.id) : undefined,
-        requestId: requestId,
-        txs: txs,
-        params: params,
-      }
-
-      setCurrentRequestId(requestId)
-      setTxFlow(<SafeAppsTxFlow data={data} />, onTxFlowClose)
-    },
-    onSignMessage: (
-      message: string | EIP712TypedData,
-      requestId: string,
-      method: Methods.signMessage | Methods.signTypedMessage,
-      sdkVersion: string,
-    ) => {
-      const isOffChainSigningSupported = isOffchainEIP1271Supported(safe, chain, sdkVersion)
-      const signOffChain = isOffChainSigningSupported && !onChainSigning && !!settings.offChainSigning
-
-      setCurrentRequestId(requestId)
-
-      if (signOffChain) {
-        setTxFlow(
-          <SignMessageFlow
-            logoUri={safeAppFromManifest?.iconUrl || ''}
-            name={safeAppFromManifest?.name || ''}
-            message={message}
-            safeAppId={remoteApp?.id}
-            requestId={requestId}
-          />,
-          onTxFlowClose,
-        )
-      } else {
-        setTxFlow(
-          <SignMessageOnChainFlow
-            props={{
-              app: safeAppFromManifest,
-              requestId,
-              message,
-              method,
-            }}
-          />,
-        )
-      }
-    },
-    onGetPermissions: getPermissions,
-    onSetPermissions: setPermissionsRequest,
-    onRequestAddressBook: (origin: string): AddressBookItem[] => {
-      if (hasPermission(origin, Methods.requestAddressBook)) {
-        return Object.entries(addressBook).map(([address, name]) => ({ address, name, chainId }))
-      }
-
-      return []
-    },
-    onGetTxBySafeTxHash: (safeTxHash) => getTransactionDetails(chainId, safeTxHash),
-    onGetEnvironmentInfo: () => ({
-      origin: document.location.origin,
-    }),
-    onGetSafeInfo: useGetSafeInfo(),
-    onGetSafeBalances: (currency) => {
-      const isDefaultTokenlistSupported = chain && hasFeature(chain, FEATURES.DEFAULT_TOKENLIST)
-      return getBalances(chainId, safeAddress, currency, {
-        exclude_spam: true,
-        trusted: isDefaultTokenlistSupported && TOKEN_LISTS.TRUSTED === tokenlist,
-      })
-    },
-    onGetChainInfo: () => {
-      if (!chain) return
-
-      const { nativeCurrency, chainName, chainId, shortName, blockExplorerUriTemplate } = chain
-
-      return {
-        chainName,
-        chainId,
-        shortName,
-        nativeCurrency,
-        blockExplorerUriTemplate,
-      }
-    },
-    onSetSafeSettings: (safeSettings: SafeSettings) => {
-      const newSettings: SafeSettings = {
-        ...settings,
-        offChainSigning: !!safeSettings.offChainSigning,
-      }
-
-      setSettings(newSettings)
-
-      return newSettings
-    },
-    onGetOffChainSignature: async (messageHash: string) => {
-      const safeMessage = safeMessages.data?.results
-        ?.filter(isSafeMessageListItem)
-        ?.find((item) => item.messageHash === messageHash)
-
-      if (safeMessage) {
-        return safeMessage.preparedSignature
-      }
-
-      try {
-        const { preparedSignature } = await getSafeMessage(chainId, messageHash)
-        return preparedSignature
-      } catch {
-        return ''
-      }
-    },
-  })
+  const communicator = useCustomAppCommunicator(iframeRef, remoteApp || safeAppFromManifest, chain)
 
   const onAcceptPermissionRequest = (_origin: string, requestId: RequestId) => {
     const permissions = confirmPermissionRequest(PermissionStatus.GRANTED)
@@ -250,27 +96,6 @@ const AppFrame = ({ appUrl, allowedFeaturesList, safeAppFromManifest }: AppFrame
       )
     }
   }, [appIsLoading, isBackendAppsLoading, appName])
-
-  useEffect(() => {
-    const unsubscribe = txSubscribe(TxEvent.SAFE_APPS_REQUEST, async ({ safeAppRequestId, safeTxHash }) => {
-      if (safeAppRequestId && currentRequestId === safeAppRequestId) {
-        trackSafeAppEvent(SAFE_APPS_EVENTS.PROPOSE_TRANSACTION, appName)
-        communicator?.send({ safeTxHash }, safeAppRequestId)
-      }
-    })
-
-    return unsubscribe
-  }, [appName, chainId, communicator, currentRequestId])
-
-  useEffect(() => {
-    const unsubscribe = safeMsgSubscribe(SafeMsgEvent.SIGNATURE_PREPARED, ({ messageHash, requestId, signature }) => {
-      if (requestId && currentRequestId === requestId) {
-        communicator?.send({ messageHash, signature }, requestId)
-      }
-    })
-
-    return unsubscribe
-  }, [communicator, currentRequestId])
 
   if (!safeLoaded) {
     return <div />
--- src/components/swap/CowWidgetCommunicator.tsx
+++ src/components/swap/CowWidgetCommunicator.tsx
@@ -1,44 +1,17 @@
 import { type CowSwapWidgetParams, TradeType, CowSwapWidget } from '@cowprotocol/widget-react'
-import { useState, useEffect, useContext, type MutableRefObject } from 'react'
+import { useState, useEffect, type MutableRefObject } from 'react'
 import { Container, Grid, useTheme } from '@mui/material'
 import useChainId from '@/hooks/useChainId'
 import { useRef } from 'react'
 import { Box } from '@mui/material'
-import useAppCommunicator, { CommunicatorMessages } from '@/components/safe-apps/AppFrame/useAppCommunicator'
-import {
-  type AddressBookItem,
-  type BaseTransaction,
-  type EIP712TypedData,
-  Methods,
-  type RequestId,
-  type SafeSettings,
-  type SendTransactionRequestParams,
-} from '@safe-global/safe-apps-sdk'
-import { SafeAppsTxFlow, SignMessageFlow, SignMessageOnChainFlow } from '@/components/tx-flow/flows'
-import { isOffchainEIP1271Supported } from '@/utils/safe-messages'
-import { getBalances, getSafeMessage, getTransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
-import useGetSafeInfo from '@/components/safe-apps/AppFrame/useGetSafeInfo'
-import { FEATURES, hasFeature } from '@/utils/chains'
-import { isSafeMessageListItem } from '@/utils/safe-message-guards'
 import {
   SafeAppAccessPolicyTypes,
   type SafeAppData,
   SafeAppFeatures,
 } from '@safe-global/safe-gateway-typescript-sdk/dist/types/safe-apps'
 import { useCurrentChain } from '@/hooks/useChains'
-import { TxModalContext } from '@/components/tx-flow'
-import { selectTokenList, selectOnChainSigning, TOKEN_LISTS } from '@/store/settingsSlice'
-import { useAppSelector } from '@/store'
-import useAddressBook from '@/hooks/useAddressBook'
-import useTransactionQueueBarState from '@/components/safe-apps/AppFrame/useTransactionQueueBarState'
-import useSafeInfo from '@/hooks/useSafeInfo'
-import { selectSafeMessages } from '@/store/safeMessagesSlice'
-import { useSafePermissions } from '@/hooks/safe-apps/permissions'
 import { useDarkMode } from '@/hooks/useDarkMode'
-import { type SafeAppsTxParams } from '../tx-flow/flows/SafeAppsTx'
-import { trackSafeAppEvent, SAFE_APPS_EVENTS } from '@/services/analytics'
-import { safeMsgSubscribe, SafeMsgEvent } from '@/services/safe-messages/safeMsgEvents'
-import { txSubscribe, TxEvent } from '@/services/tx/txEvents'
+import { useCustomAppCommunicator } from '@/hooks/safe-apps/useCustomAppCommunicator'
 
 const supportedChains = [1, 100, 11155111]
 
@@ -63,13 +36,7 @@ const appData: SafeAppData = {
   socialProfiles: [],
 }
 export const CowWidgetCommunicator = ({ sell }: Params) => {
-  console.log('sell', sell)
-  const { safe, safeLoaded, safeAddress } = useSafeInfo()
-
   const chainId = useChainId()
-  const [currentRequestId, setCurrentRequestId] = useState<RequestId | undefined>()
-  const safeMessages = useAppSelector(selectSafeMessages)
-
   const { palette } = useTheme()
   const darkMode = useDarkMode()
 
@@ -119,36 +86,6 @@ export const CowWidgetCommunicator = ({ sell }: Params) => {
 
   const chain = useCurrentChain()
 
-  const addressBook = useAddressBook()
-
-  const {
-    expanded: queueBarExpanded,
-    dismissedByUser: queueBarDismissed,
-    setExpanded,
-    dismissQueueBar,
-    transactions,
-  } = useTransactionQueueBarState()
-
-  const { getPermissions, hasPermission, permissionsRequest, setPermissionsRequest, confirmPermissionRequest } =
-    useSafePermissions()
-  const [settings, setSettings] = useState<SafeSettings>({
-    offChainSigning: true,
-  })
-  const tokenlist = useAppSelector(selectTokenList)
-  const onChainSigning = useAppSelector(selectOnChainSigning)
-
-  const { setTxFlow } = useContext(TxModalContext)
-
-  const onTxFlowClose = () => {
-    setCurrentRequestId((prevId) => {
-      if (prevId) {
-        communicator?.send(CommunicatorMessages.REJECT_TRANSACTION_MESSAGE, prevId, true)
-        // trackSafeAppEvent(SAFE_APPS_EVENTS.PROPOSE_TRANSACTION_REJECTED, appName)
-      }
-      return undefined
-    })
-  }
-
   const iframeRef: MutableRefObject<HTMLIFrameElement | null | undefined> = useRef<HTMLIFrameElement | null>()
 
   useEffect(() => {
@@ -158,136 +95,7 @@ export const CowWidgetCommunicator = ({ sell }: Params) => {
     }
   }, [params])
 
-  const communicator = useAppCommunicator(iframeRef, appData, chain, {
-    onConfirmTransactions: (txs: BaseTransaction[], requestId: RequestId, params?: SendTransactionRequestParams) => {
-      const data: SafeAppsTxParams = {
-        app: appData,
-        appId: String(appData.id),
-        requestId: requestId,
-        txs: txs,
-        params: params,
-      }
-
-      setCurrentRequestId(requestId)
-      setTxFlow(<SafeAppsTxFlow data={data} />, onTxFlowClose)
-    },
-    onSignMessage: (
-      message: string | EIP712TypedData,
-      requestId: string,
-      method: Methods.signMessage | Methods.signTypedMessage,
-      sdkVersion: string,
-    ) => {
-      const isOffChainSigningSupported = isOffchainEIP1271Supported(safe, chain, sdkVersion)
-      const signOffChain = isOffChainSigningSupported && !onChainSigning && !!settings.offChainSigning
-
-      setCurrentRequestId(requestId)
-
-      if (signOffChain) {
-        setTxFlow(
-          <SignMessageFlow
-            logoUri={appData?.iconUrl || ''}
-            name={appData?.name || ''}
-            message={message}
-            safeAppId={appData?.id}
-            requestId={requestId}
-          />,
-          onTxFlowClose,
-        )
-      } else {
-        setTxFlow(
-          <SignMessageOnChainFlow
-            props={{
-              app: appData,
-              requestId,
-              message,
-              method,
-            }}
-          />,
-        )
-      }
-    },
-    onGetPermissions: getPermissions,
-    onSetPermissions: setPermissionsRequest,
-    onRequestAddressBook: (origin: string): AddressBookItem[] => {
-      if (hasPermission(origin, Methods.requestAddressBook)) {
-        return Object.entries(addressBook).map(([address, name]) => ({ address, name, chainId }))
-      }
-
-      return []
-    },
-    onGetTxBySafeTxHash: (safeTxHash) => getTransactionDetails(chainId, safeTxHash),
-    onGetEnvironmentInfo: () => ({
-      origin: document.location.origin,
-    }),
-    onGetSafeInfo: useGetSafeInfo(),
-    onGetSafeBalances: (currency) => {
-      const isDefaultTokenlistSupported = chain && hasFeature(chain, FEATURES.DEFAULT_TOKENLIST)
-      return getBalances(chainId, safeAddress, currency, {
-        exclude_spam: true,
-        trusted: isDefaultTokenlistSupported && TOKEN_LISTS.TRUSTED === tokenlist,
-      })
-    },
-    onGetChainInfo: () => {
-      if (!chain) return
-
-      const { nativeCurrency, chainName, chainId, shortName, blockExplorerUriTemplate } = chain
-
-      return {
-        chainName,
-        chainId,
-        shortName,
-        nativeCurrency,
-        blockExplorerUriTemplate,
-      }
-    },
-    onSetSafeSettings: (safeSettings: SafeSettings) => {
-      const newSettings: SafeSettings = {
-        ...settings,
-        offChainSigning: !!safeSettings.offChainSigning,
-      }
-
-      setSettings(newSettings)
-
-      return newSettings
-    },
-    onGetOffChainSignature: async (messageHash: string) => {
-      const safeMessage = safeMessages.data?.results
-        ?.filter(isSafeMessageListItem)
-        ?.find((item) => item.messageHash === messageHash)
-
-      if (safeMessage) {
-        return safeMessage.preparedSignature
-      }
-
-      try {
-        const { preparedSignature } = await getSafeMessage(chainId, messageHash)
-        return preparedSignature
-      } catch {
-        return ''
-      }
-    },
-  })
-
-  useEffect(() => {
-    const unsubscribe = txSubscribe(TxEvent.SAFE_APPS_REQUEST, async ({ safeAppRequestId, safeTxHash }) => {
-      if (safeAppRequestId && currentRequestId === safeAppRequestId) {
-        trackSafeAppEvent(SAFE_APPS_EVENTS.PROPOSE_TRANSACTION, appData.name)
-        communicator?.send({ safeTxHash }, safeAppRequestId)
-      }
-    })
-
-    return unsubscribe
-  }, [chainId, communicator, currentRequestId])
-
-  useEffect(() => {
-    const unsubscribe = safeMsgSubscribe(SafeMsgEvent.SIGNATURE_PREPARED, ({ messageHash, requestId, signature }) => {
-      if (requestId && currentRequestId === requestId) {
-        communicator?.send({ messageHash, signature }, requestId)
-      }
-    })
-
-    return unsubscribe
-  }, [communicator, currentRequestId])
+  useCustomAppCommunicator(iframeRef, appData, chain)
 
   if (!params) {
     return null
