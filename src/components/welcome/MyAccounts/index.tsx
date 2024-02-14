f05bbfe46f5b2076a49d959f944e0de972f8d3e4
James Mealy <james@safe.global>
2024-02-14 11:49:38 +0100

    add watchlist to my accounts page

--- src/components/welcome/MyAccounts/index.tsx
+++ src/components/welcome/MyAccounts/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react'
-import { Button, Box, Paper, Typography } from '@mui/material'
+import { Button, Box, Paper, Typography, Link, SvgIcon } from '@mui/material'
 import madProps from '@/utils/mad-props'
 import AccountItem from './AccountItem'
 import CreateButton from './CreateButton'
@@ -8,6 +8,9 @@ import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS } from '@/services/analytics'
 import { DataWidget } from '@/components/welcome/SafeListDrawer/DataWidget'
 import css from './styles.module.css'
+import { VisibilityOutlined } from '@mui/icons-material'
+import AddIcon from '@/public/images/common/add.svg'
+import { AppRoutes } from '@/config/routes'
 
 type AccountsListProps = {
   safes: SafeItems
@@ -32,21 +35,23 @@ const AccountsList = ({ safes, onLinkClick }: AccountsListProps) => {
   }
 
   return (
-    <Box display="flex" justifyContent="center" className={css.container}>
-      <Box m={2} className={css.myAccounts}>
+    <Box className={css.container}>
+      <Box className={css.myAccounts}>
         <Box className={css.header}>
           <Typography variant="h1" fontWeight={700} className={css.title}>
-            My Safe accounts
+            Safe accounts
+          </Typography>
+          <CreateButton />
+        </Box>
+
+        <Paper className={css.safeList}>
+          <Typography variant="h5" fontWeight={700} mb={2}>
+            My accounts
             <Typography component="span" color="text.secondary" fontSize="inherit" fontWeight="normal">
               {' '}
               ({safes.length})
             </Typography>
           </Typography>
-
-          <CreateButton />
-        </Box>
-
-        <Paper className={css.safeList}>
           {shownSafes.length ? (
             shownSafes.map((item) => (
               <AccountItem onLinkClick={onLinkClick} {...item} key={item.chainId + item.address} />
@@ -56,7 +61,43 @@ const AccountsList = ({ safes, onLinkClick }: AccountsListProps) => {
               You don&apos;t have any Safe Accounts yet
             </Typography>
           )}
+          {safes.length > shownSafes.length && (
+            <Box display="flex" justifyContent="center">
+              <Track {...OVERVIEW_EVENTS.SHOW_MORE_SAFES}>
+                <Button onClick={onShowMore}>Show more</Button>
+              </Track>
+            </Box>
+          )}
+        </Paper>
 
+        <Paper className={css.safeList}>
+          <div className={css.watchlistHeader}>
+            <Typography variant="h5" display="inline" fontWeight={700} className={css.watchlistTitle}>
+              <VisibilityOutlined sx={{ verticalAlign: 'middle', marginRight: '10px' }} fontSize="small" />
+              Watchlist
+            </Typography>
+            <Track {...OVERVIEW_EVENTS.ADD_SAFE}>
+              <Link href={AppRoutes.newSafe.load}>
+                <Button
+                  disableElevation
+                  size="small"
+                  onClick={onLinkClick}
+                  startIcon={<SvgIcon component={AddIcon} inheritViewBox fontSize="small" />}
+                >
+                  Add
+                </Button>
+              </Link>
+            </Track>
+          </div>{' '}
+          {shownSafes.length ? (
+            shownSafes.map((item) => (
+              <AccountItem onLinkClick={onLinkClick} {...item} key={item.chainId + item.address} />
+            ))
+          ) : (
+            <Typography variant="body2" color="primary.light" textAlign="center" my={3}>
+              You don&apos;t have any Safe Accounts yet
+            </Typography>
+          )}
           {safes.length > shownSafes.length && (
             <Box display="flex" justifyContent="center">
               <Track {...OVERVIEW_EVENTS.SHOW_MORE_SAFES}>
