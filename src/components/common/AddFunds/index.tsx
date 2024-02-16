0fc7d5e43594022c567a4ba2dd4e20139e779af6
Usame Algan <usame@safe.global>
2024-02-16 10:42:13 +0100

    fix: Revert changes to AddFunds widget

--- src/components/common/AddFunds/index.tsx
+++ src/components/common/AddFunds/index.tsx
@@ -1,4 +1,3 @@
-import useBalances from '@/hooks/useBalances'
 import { Box, FormControlLabel, Grid, Paper, Switch, Typography } from '@mui/material'
 import EthHashInfo from '@/components/common/EthHashInfo'
 import QRCode from '@/components/common/QRCode'
@@ -9,17 +8,12 @@ import { selectSettings, setQrShortName } from '@/store/settingsSlice'
 import BuyCryptoButton from '@/components/common/BuyCryptoButton'
 
 const AddFundsCTA = () => {
-  const { balances } = useBalances()
   const safeAddress = useSafeAddress()
   const chain = useCurrentChain()
   const dispatch = useAppDispatch()
   const settings = useAppSelector(selectSettings)
   const qrPrefix = settings.shortName.qr ? `${chain?.shortName}:` : ''
   const qrCode = `${qrPrefix}${safeAddress}`
-
-  const hasNoAssets = balances.items.length === 1 && balances.items[0].balance === '0'
-
-  if (!hasNoAssets) return null
 
   return (
     <Paper>
@@ -39,34 +33,22 @@ const AddFundsCTA = () => {
           />
         </Grid>
 
-        <Grid item container xs={12} md={8} gap={1} flexDirection="column">
+        <Grid item container xs={12} md={6} gap={2} flexDirection="column">
           <Typography variant="h3" fontWeight="bold">
             Add funds to get started
           </Typography>
 
-          <Typography mb={2}>
+          <Typography>
             Add funds directly from your bank account or copy your address to send tokens from a different account.
           </Typography>
 
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={8}>
-              <Typography mb={1}>Send assets to your address:</Typography>
-              <Box bgcolor="background.main" p={2} borderRadius="6px" alignSelf="flex-start" fontSize="14px">
-                <EthHashInfo
-                  address={safeAddress}
-                  showName={false}
-                  shortAddress={false}
-                  showCopyButton
-                  hasExplorer
-                  avatarSize={24}
-                />
-              </Box>
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <Typography mb={1}>Buy crypto with fiat:</Typography>
-              <BuyCryptoButton />
-            </Grid>
-          </Grid>
+          <Box bgcolor="background.main" p={2} borderRadius="6px" alignSelf="flex-start" fontSize="14px">
+            <EthHashInfo address={safeAddress} shortAddress={false} showCopyButton hasExplorer avatarSize={24} />
+          </Box>
+
+          <Box alignSelf="flex-start">
+            <BuyCryptoButton />
+          </Box>
         </Grid>
       </Grid>
     </Paper>
