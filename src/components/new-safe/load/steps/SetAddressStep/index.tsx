4862ae85abdb4919f51ce463fe4b6d2a14508814
mike10ca <michael@safe.global>
2024-02-20 16:06:26 +0100

    tests: fix formating

--- src/components/new-safe/load/steps/SetAddressStep/index.tsx
+++ src/components/new-safe/load/steps/SetAddressStep/index.tsx
@@ -135,10 +135,10 @@ const SetAddressStep = ({ data, onSubmit, onBack }: StepRenderProps<LoadSafeForm
             </Grid>
           </Grid>
 
-          <AddressInput 
-            data-testid="address-section" 
-            label="Safe Account" 
-            validate={validateSafeAddress} 
+          <AddressInput
+            data-testid="address-section"
+            label="Safe Account"
+            validate={validateSafeAddress}
             name={Field.address} />
 
           <Typography mt={4}>
