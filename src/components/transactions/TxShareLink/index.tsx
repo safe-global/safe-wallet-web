009272b7db05e1e145a797b6eec41d8e03e0be00
Daniel <daniel0ar@hotmail.com>
2024-02-20 13:56:35 -0500

    fix: Modify TxShareLink extract location.origin

--- src/components/transactions/TxShareLink/index.tsx
+++ src/components/transactions/TxShareLink/index.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react'
+import { useEffect, useState } from 'react'
 import { IconButton, Link, SvgIcon } from '@mui/material'
 import ShareIcon from '@/public/images/common/share.svg'
 import { AppRoutes } from '@/config/routes'
@@ -8,15 +9,26 @@ import { TX_LIST_EVENTS } from '@/services/analytics/events/txList'
 import React from 'react'
 import CopyTooltip from '@/components/common/CopyTooltip'
 
+const useOrigin = () => {
+  const [origin, setOrigin] = useState('')
+
+  useEffect(() => {
+    if (typeof location !== 'undefined') {
+      setOrigin(location.origin)
+    }
+  }, [])
+  return origin
+}
+
 const TxShareLink = ({ id }: { id: string }): ReactElement => {
   const router = useRouter()
   const { safe = '' } = router.query
   const href = `${AppRoutes.transactions.tx}?safe=${safe}&id=${id}`
-  const tx_url = location.origin + href
+  const txUrl = useOrigin() + href
 
   return (
     <Track {...TX_LIST_EVENTS.COPY_DEEPLINK}>
-      <CopyTooltip text={tx_url}>
+      <CopyTooltip text={txUrl} initialToolTipText="Copy the transaction URL">
         <IconButton data-testid="share-btn" component={Link} aria-label="Share">
           <SvgIcon component={ShareIcon} inheritViewBox fontSize="small" color="border" />
         </IconButton>
