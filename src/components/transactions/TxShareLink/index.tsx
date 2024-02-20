0b82a788fc192dc9e183c275590b3d760a988b2b
Daniel <daniel0ar@hotmail.com>
2024-02-20 11:54:45 -0500

    Revert "fix: Add export tooltip and move copy action"
    This reverts commit 9a6ed048bc06693790da78f8b2ce6779bcbc4432.

--- src/components/transactions/TxShareLink/index.tsx
+++ src/components/transactions/TxShareLink/index.tsx
@@ -1,26 +1,45 @@
-import type { ReactElement } from 'react'
+import type { ReactElement, MouseEvent } from 'react'
 import { IconButton, Link, SvgIcon } from '@mui/material'
 import ShareIcon from '@/public/images/common/share.svg'
 import { AppRoutes } from '@/config/routes'
 import { useRouter } from 'next/router'
 import Track from '@/components/common/Track'
 import { TX_LIST_EVENTS } from '@/services/analytics/events/txList'
-import React from 'react'
-import ExportTooltip from '@/components/common/ExportTooltip'
+import React, { useState } from 'react'
 
 const TxShareLink = ({ id }: { id: string }): ReactElement => {
+  const [isCopyEnabled, setIsCopyEnabled] = useState(true)
+
   const router = useRouter()
   const { safe = '' } = router.query
   const href = `${AppRoutes.transactions.tx}?safe=${safe}&id=${id}`
-  const tx_url = location.origin + href
+
+  const onClick = (e: MouseEvent) => {
+    if (!e.ctrlKey && !e.metaKey) {
+      e.preventDefault()
+    }
+
+    try {
+      // copy href to clipboard
+      navigator.clipboard.writeText(location.origin + href)
+    } catch (error) {
+      console.error(error)
+      setIsCopyEnabled(false)
+    }
+  }
 
   return (
     <Track {...TX_LIST_EVENTS.COPY_DEEPLINK}>
-      <ExportTooltip text={tx_url}>
-        <IconButton data-testid="share-btn" component={Link} aria-label="Share">
-          <SvgIcon component={ShareIcon} inheritViewBox fontSize="small" color="border" />
-        </IconButton>
-      </ExportTooltip>
+      <IconButton
+        data-testid="share-btn"
+        component={Link}
+        aria-label="Share"
+        href={href}
+        onClick={onClick}
+        disabled={!isCopyEnabled}
+      >
+        <SvgIcon component={ShareIcon} inheritViewBox fontSize="small" color="border" />
+      </IconButton>
     </Track>
   )
 }
