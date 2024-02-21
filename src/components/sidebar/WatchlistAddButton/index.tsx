4a49f71ecada4805155821393a096acd3913d468
James Mealy <james@safe.global>
2024-02-21 16:09:58 +0100

    Show remove option when safe is already added to the watchlist

--- src/components/sidebar/WatchlistAddButton/index.tsx
+++ src/components/sidebar/WatchlistAddButton/index.tsx
@@ -1,14 +1,24 @@
-import Button from '@mui/material/Button'
 import { OVERVIEW_EVENTS, trackEvent } from '@/services/analytics'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '@/config/routes'
 import { useCurrentChain } from '@/hooks/useChains'
 import useSafeAddress from '@/hooks/useSafeAddress'
+import { Button, SvgIcon } from '@mui/material'
+import SafeListRemoveDialog from '../SafeListRemoveDialog'
+import { useAppSelector } from '@/store'
+import { selectAddedSafes } from '@/store/addedSafesSlice'
+import { useState } from 'react'
+import { VisibilityOutlined } from '@mui/icons-material'
+import DeleteIcon from '@/public/images/common/delete.svg'
 
 const WatchlistAddButton = () => {
+  const [open, setOpen] = useState(false)
   const router = useRouter()
   const chain = useCurrentChain()
   const address = useSafeAddress()
+  const chainId = chain?.chainId || ''
+  const addedSafes = useAppSelector((state) => selectAddedSafes(state, chainId))
+  const isInWatchlist = !!addedSafes?.[address]
 
   const onClick = () => {
     trackEvent({ ...OVERVIEW_EVENTS.ADD_SAFE })
@@ -23,17 +33,39 @@ const WatchlistAddButton = () => {
   }
 
   return (
-    <Button
-      data-testid="add-watchlist-btn"
-      onClick={onClick}
-      variant="contained"
-      size="small"
-      fullWidth
-      disableElevation
-      sx={{ py: 1.3 }}
-    >
-      Add to watchlist
-    </Button>
+    <>
+      {!isInWatchlist ? (
+        <Button
+          data-testid="add-watchlist-btn"
+          onClick={onClick}
+          variant="outlined"
+          size="small"
+          fullWidth
+          disableElevation
+          sx={{ py: 1.3 }}
+          startIcon={<VisibilityOutlined sx={{ verticalAlign: 'middle', marginRight: 1 }} />}
+        >
+          Add to watchlist
+        </Button>
+      ) : (
+        <Button
+          data-testid="add-watchlist-btn"
+          onClick={() => setOpen(true)}
+          variant="outlined"
+          size="small"
+          fullWidth
+          disableElevation
+          sx={{ py: 1.3, px: 1 }}
+          startIcon={<SvgIcon component={DeleteIcon} inheritViewBox fontSize="small" />}
+        >
+          Remove from watchlist
+        </Button>
+      )}
+
+      {open && chainId && (
+        <SafeListRemoveDialog handleClose={() => setOpen(false)} address={address} chainId={chainId} />
+      )}
+    </>
   )
 }
 
