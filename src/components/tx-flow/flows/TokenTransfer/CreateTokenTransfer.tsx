85855007d8a975189b58dfb032d4a676cb582664
Daniel Dimitrov <daniel@compojoom.com>
2024-03-04 16:45:17 +0100

    fix - token icon not refreshing
    When navigating to a token that doesn’t have an icon we were failing to update the icon when the user navigate to a token that has one.

--- src/components/tx-flow/flows/TokenTransfer/CreateTokenTransfer.tsx
+++ src/components/tx-flow/flows/TokenTransfer/CreateTokenTransfer.tsx
@@ -21,7 +21,7 @@ import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 
 export const AutocompleteItem = (item: { tokenInfo: TokenInfo; balance: string }): ReactElement => (
   <Grid container alignItems="center" gap={1}>
-    <TokenIcon logoUri={item.tokenInfo.logoUri} tokenSymbol={item.tokenInfo.symbol} />
+    <TokenIcon logoUri={item.tokenInfo.logoUri} key={item.tokenInfo.address} tokenSymbol={item.tokenInfo.symbol} />
 
     <Grid item xs>
       <Typography variant="body2">{item.tokenInfo.name}</Typography>
