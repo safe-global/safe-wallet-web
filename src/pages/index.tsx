c838db1a2c0dd08bd20fe42394d574714736ce8e
James Mealy <james@safe.global>
2024-03-04 09:20:25 +0100

    Feat: if there are local safes redirect from index route `/` to accounts page  (#3376)
    * index route redirects to login page if there are safes stored locally
    
    * fix: update unit tests for WalletLogin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,25 @@ import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '@/config/routes'
+import { useHasSafes } from '@/components/welcome/MyAccounts/useAllSafes'
 
 const IndexPage: NextPage = () => {
   const router = useRouter()
   const { chain } = router.query
+  const { hasSafes } = useHasSafes()
 
   useEffect(() => {
     if (!router.isReady || router.pathname !== AppRoutes.index) {
       return
     }
 
+    const pathname = hasSafes ? AppRoutes.welcome.accounts : AppRoutes.welcome.index
+
     router.replace({
-      pathname: AppRoutes.welcome.index,
+      pathname,
       query: chain ? { chain } : undefined,
     })
-  }, [router, chain])
+  }, [router, chain, hasSafes])
 
   return <></>
 }
