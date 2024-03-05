6a388b2e9f45c99b0242d0e9c6781e1e3a71be7b
James Mealy <james@safe.global>
2024-03-05 16:18:08 +0100

    fix: track events only on first visit to accounts page or on reloading the app

--- src/components/welcome/MyAccounts/useTrackedSafesCount.ts
+++ src/components/welcome/MyAccounts/useTrackedSafesCount.ts
@@ -1,11 +1,12 @@
 import { AppRoutes } from '@/config/routes'
 import { OVERVIEW_EVENTS, trackEvent } from '@/services/analytics'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import type { SafeItems } from './useAllSafes'
 
+let isTracked = false
+
 const useTrackSafesCount = (ownedSafes: SafeItems | undefined, watchlistSafes: SafeItems | undefined) => {
-  const [isTracked, setIsTracked] = useState(false)
   const router = useRouter()
   const isLoginPage = router.pathname === AppRoutes.welcome.accounts
 
@@ -13,9 +14,9 @@ const useTrackSafesCount = (ownedSafes: SafeItems | undefined, watchlistSafes: S
     if (watchlistSafes && ownedSafes && isLoginPage && !isTracked) {
       trackEvent({ ...OVERVIEW_EVENTS.TOTAL_SAFES_OWNED, label: ownedSafes.length })
       trackEvent({ ...OVERVIEW_EVENTS.TOTAL_SAFES_WATCHLIST, label: watchlistSafes.length })
-      setIsTracked(true)
+      isTracked = true
     }
-  }, [isLoginPage, isTracked, ownedSafes, watchlistSafes])
+  }, [isLoginPage, ownedSafes, watchlistSafes])
 }
 
 export default useTrackSafesCount
