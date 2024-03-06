eefd5586bb89c4326051e56872e77364faf0ca12
James Mealy <james@safe.global>
2024-03-06 15:23:20 +0100

    fix: incorrect redirect after adding and then removing a safe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '@/config/routes'
+import { isEmpty } from 'lodash'
 
 const ADDED_SAFES_KEY = 'SAFE_v2__addedSafes'
 
@@ -13,10 +14,10 @@ const IndexPage: NextPage = () => {
     if (!router.isReady || router.pathname !== AppRoutes.index) {
       return
     }
-
     // read directly from localstorage so we have value on first render
-    const localSafes = localStorage.getItem(ADDED_SAFES_KEY)
-    const pathname = localSafes ? AppRoutes.welcome.accounts : AppRoutes.welcome.index
+    const addedSafes = localStorage.getItem(ADDED_SAFES_KEY)
+    const hasAddedSafes = addedSafes !== null && !isEmpty(JSON.parse(addedSafes))
+    const pathname = hasAddedSafes ? AppRoutes.welcome.accounts : AppRoutes.welcome.index
 
     router.replace({
       pathname,
