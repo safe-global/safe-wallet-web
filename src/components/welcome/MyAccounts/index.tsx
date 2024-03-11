91125372c17ce2dc6f37c9870dcf395a6c02c868
James Mealy <james@safe.global>
2024-03-11 15:17:59 +0100

    remove unecessary safelist component

--- src/components/welcome/MyAccounts/index.tsx
+++ src/components/welcome/MyAccounts/index.tsx
@@ -5,12 +5,12 @@ import useAllSafes, { type SafeItems } from './useAllSafes'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS, OVERVIEW_LABELS } from '@/services/analytics'
 import css from './styles.module.css'
-import SafeList from './SafeList'
 import { AppRoutes } from '@/config/routes'
-import ConnectWalletButton from '@/components/common/ConnectWallet/ConnectWalletButton'
 import useWallet from '@/hooks/wallets/useWallet'
 import { useRouter } from 'next/router'
 import classNames from 'classnames'
+import AccountItem from './AccountItem'
+import ConnectWalletButton from '@/components/common/ConnectWallet/ConnectWalletButton'
 
 const NO_SAFES_MESSAGE = "You don't have any Safe Accounts yet"
 
@@ -52,28 +52,28 @@ const AccountsList = ({ safes, onLinkClick }: AccountsListProps) => {
           </Track>
           <Track {...OVERVIEW_EVENTS.CREATE_NEW_SAFE} label={trackingLabel}>
             <CreateButton compact={true} />
-            {/* <CreateButton compact={!isLoginPage || isMobile} /> */}
           </Track>
         </Box>
 
-        <SafeList
-          safes={safes || []}
-          onLinkClick={onLinkClick}
-          noSafesMessage={
-            wallet ? (
-              NO_SAFES_MESSAGE
-            ) : (
-              <>
-                <Box mb={2}>Connect a wallet to view your Safe Accounts or to create a new one</Box>
-                <Track {...OVERVIEW_EVENTS.OPEN_ONBOARD} label={trackingLabel}>
-                  <ConnectWalletButton text="Connect a wallet" contained={false} />
-                </Track>
-              </>
-            )
-          }
-        />
+        <div className={css.safeList}>
+          {safes?.length ? (
+            safes.map((item) => <AccountItem onLinkClick={onLinkClick} {...item} key={item.chainId + item.address} />)
+          ) : (
+            <Typography variant="body2" color="text.secondary" textAlign="center" py={3} mx="auto" width={250}>
+              {wallet ? (
+                NO_SAFES_MESSAGE
+              ) : (
+                <>
+                  <Box mb={2}>Connect a wallet to view your Safe Accounts or to create a new one</Box>
+                  <Track {...OVERVIEW_EVENTS.OPEN_ONBOARD} label={trackingLabel}>
+                    <ConnectWalletButton text="Connect a wallet" contained={false} />
+                  </Track>
+                </>
+              )}
+            </Typography>
+          )}
+        </div>
       </Box>
-      {/* <DataWidget /> */}
     </Box>
   )
 }
