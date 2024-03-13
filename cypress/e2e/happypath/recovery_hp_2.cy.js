74b64b9b4e72b5ee903e017b5a9b321ca9a69106
mike10ca <michael@safe.global>
2024-03-13 11:26:14 +0100

    tests: add api call and waiters

--- cypress/e2e/happypath/recovery_hp_2.cy.js
+++ cypress/e2e/happypath/recovery_hp_2.cy.js
@@ -1,28 +1,32 @@
 import * as constants from '../../support/constants'
 import * as main from '../pages/main.page'
-import * as owner from '../pages/owners.pages'
+import * as ownerP from '../pages/owners.pages'
 import * as recovery from '../pages/recovery.pages'
-import * as addressbook from '../pages/address_book.page'
 
 describe('Recovery happy path tests 2', () => {
   beforeEach(() => {
-    cy.visit(constants.setupUrl + constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2)
+    cy.visit(constants.homeUrl + constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2)
     cy.clearLocalStorage()
     main.acceptCookies()
   })
 
   // Check that recoverer can start and complete the process if not cancelled by the owner
   it('Recovery setup happy path 2', { defaultCommandTimeout: 300000 }, () => {
-    owner.waitForConnectionStatus()
-    recovery.postponeRecovery()
+    main.fetchSafeData(constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2.substring(4)).then((response) => {
+      expect(response.status).to.eq(200)
+      console.log(response.body)
+      expect(response.body).to.have.property('owners')
 
-    main.getElementText(addressbook.tableContainer).then((text) => {
+      const owners = response.body.owners
+
       let owner = constants.SPENDING_LIMIT_ADDRESS_2
-      if (text.includes(constants.SPENDING_LIMIT_ADDRESS_2)) {
+      if (owners.includes(constants.SPENDING_LIMIT_ADDRESS_2)) {
         owner = constants.SEPOLIA_OWNER_2
       }
 
-      cy.visit(constants.homeUrl + constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2)
+      ownerP.waitForConnectionStatus()
+      recovery.postponeRecovery()
+
       recovery.clickOnStartRecoveryBtn()
       recovery.enterOwnerAddress(owner)
       recovery.clickOnNextBtn()
@@ -34,11 +38,11 @@ describe('Recovery happy path tests 2', () => {
       recovery.clickOnRecoveryExecuteBtn()
       cy.wait(1000)
       recovery.verifyTxNotInQueue()
-      cy.visit(constants.setupUrl + constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2)
-      main.verifyElementsCount(addressbook.tableContainer, 1)
-      cy.wait(15000)
-      main.getElementText(addressbook.tableContainer).then((text) => {
-        expect(text).to.contain(owner)
+      cy.wait(1000)
+
+      main.fetchSafeData(constants.SEPOLIA_TEST_SAFE_24_RECOVERY_2.substring(4)).then((response) => {
+        const owners = response.body.owners
+        expect(owners).to.include(owner)
       })
     })
   })
