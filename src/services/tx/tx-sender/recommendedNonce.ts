1dc5e3ea1aa0e37b2126b4c270b5e8be9f82323a
Manuel Gellfart <manu@safe.global>
2024-03-27 09:48:31 +0100

    fix: safeTxGas for rejection txs (#3452)
    The protocol-kit estimates the safeTxGas correctly but our SafeTxProvider overrides it with it's own estimation.
    Therefore this logic is removed with this commit so we only rely on the safeTxGas from the core-sdk.

--- src/services/tx/tx-sender/recommendedNonce.ts
+++ src/services/tx/tx-sender/recommendedNonce.ts
@@ -1,44 +1,5 @@
-import {
-  Operation,
-  postSafeGasEstimation,
-  getNonces as fetchNonces,
-  type SafeTransactionEstimation,
-} from '@safe-global/safe-gateway-typescript-sdk'
-import type { MetaTransactionData, SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types'
-import { isLegacyVersion } from '@/hooks/coreSDK/safeCoreSDK'
+import { getNonces as fetchNonces } from '@safe-global/safe-gateway-typescript-sdk'
 import { Errors, logError } from '@/services/exceptions'
-
-const fetchRecommendedParams = async (
-  chainId: string,
-  safeAddress: string,
-  txParams: MetaTransactionData,
-): Promise<SafeTransactionEstimation> => {
-  return postSafeGasEstimation(chainId, safeAddress, {
-    to: txParams.to,
-    value: txParams.value,
-    data: txParams.data,
-    operation: (txParams.operation as unknown as Operation) || Operation.CALL,
-  })
-}
-
-export const getSafeTxGas = async (
-  chainId: string,
-  safeAddress: string,
-  safeVersion: string,
-  safeTxData: SafeTransactionDataPartial,
-): Promise<string | undefined> => {
-  const isSafeTxGasRequired = isLegacyVersion(safeVersion)
-
-  // For 1.3.0+ Safes safeTxGas is not required
-  if (!isSafeTxGasRequired) return '0'
-
-  try {
-    const estimation = await fetchRecommendedParams(chainId, safeAddress, safeTxData)
-    return estimation.safeTxGas
-  } catch (e) {
-    logError(Errors._616, e)
-  }
-}
 
 export const getNonces = async (chainId: string, safeAddress: string) => {
   try {
