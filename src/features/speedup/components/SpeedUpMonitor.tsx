98f6656833719a8a0d55a18185631cde728469f7
Manuel Gellfart <manu@safe.global>
2024-04-09 14:33:54 +0200

    analytics: add event handlers for speedup tx feature (#3535)

--- src/features/speedup/components/SpeedUpMonitor.tsx
+++ src/features/speedup/components/SpeedUpMonitor.tsx
@@ -2,12 +2,14 @@ import { Alert, AlertTitle, Box, Button, SvgIcon, Typography } from '@mui/materi
 import { SpeedUpModal } from '@/features/speedup/components/SpeedUpModal'
 import Rocket from '@/public/images/common/rocket.svg'
 import { useCounter } from '@/components/common/Notifications/useCounter'
+import type { MouseEventHandler } from 'react'
 import { useState } from 'react'
 import type { PendingProcessingTx } from '@/store/pendingTxsSlice'
 import useAsync from '@/hooks/useAsync'
 import { isSmartContract, useWeb3ReadOnly } from '@/hooks/wallets/web3'
 import useWallet from '@/hooks/wallets/useWallet'
 import { isSpeedableTx } from '@/features/speedup/utils/IsSpeedableTx'
+import { MODALS_EVENTS, trackEvent } from '@/services/analytics'
 import { useHasFeature } from '@/hooks/useChains'
 import { FEATURES } from '@/utils/chains'
 
@@ -39,6 +41,12 @@ export const SpeedUpMonitor = ({ txId, pendingTx, modalTrigger = 'alertBox' }: S
     return null
   }
 
+  const onOpen: MouseEventHandler = (e) => {
+    e.stopPropagation()
+    setOpenSpeedUpModal(true)
+    trackEvent(MODALS_EVENTS.OPEN_SPEED_UP_MODAL)
+  }
+
   return (
     <>
       <Box>
@@ -56,14 +64,7 @@ export const SpeedUpMonitor = ({ txId, pendingTx, modalTrigger = 'alertBox' }: S
           <Alert
             severity="warning"
             icon={<SvgIcon component={Rocket} />}
-            action={
-              <Button
-                onClick={(e) => {
-                  e.stopPropagation()
-                  setOpenSpeedUpModal(true)
-                }}
-              >{`Speed up >`}</Button>
-            }
+            action={<Button onClick={onOpen}>{`Speed up >`}</Button>}
           >
             <AlertTitle>
               <Typography textAlign="left">Taking too long?</Typography>
@@ -71,15 +72,7 @@ export const SpeedUpMonitor = ({ txId, pendingTx, modalTrigger = 'alertBox' }: S
             Try to speed up with better gas parameters.
           </Alert>
         ) : (
-          <Button
-            variant="outlined"
-            size="small"
-            sx={{ py: 0.6 }}
-            onClick={(e) => {
-              e.stopPropagation()
-              setOpenSpeedUpModal(true)
-            }}
-          >
+          <Button variant="outlined" size="small" sx={{ py: 0.6 }} onClick={onOpen}>
             Speed up
           </Button>
         )}
