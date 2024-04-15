ee80eb63aba8e70b5f654c7b01e359c4ab094038
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2024-04-15 14:33:53 +0200

    feat: Add OrderId component for swaps (#3560)

---
+++ src/features/swap/components/OrderId/index.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import OrderId from './index'
+import { Paper } from '@mui/material'
+
+const meta = {
+  component: OrderId,
+  parameters: {
+    componentSubtitle: 'Renders a CowSwap order id with an external link and a copy button',
+  },
+
+  decorators: [
+    (Story) => {
+      return (
+        <Paper sx={{ padding: 2 }}>
+          <Story />
+        </Paper>
+      )
+    },
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof OrderId>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    orderId:
+      '0x1282e32a3a69aeb5a65fcdec0ae40fe16b398d54609bc9a3c8be3eb57d1a0fd07a9af6ef9197041a5841e84cb27873bebd3486e2661510ab',
+  },
+}
