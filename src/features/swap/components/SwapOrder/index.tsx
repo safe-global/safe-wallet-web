6bfe939cfb8b39bc0ed54732fa50904af975a43e
Daniel Dimitrov <daniel@compojoom.com>
2024-04-15 12:22:37 +0200

    feat: storybook for sellOrder

--- src/features/swap/components/SwapOrder/index.tsx
+++ src/features/swap/components/SwapOrder/index.tsx
@@ -1,12 +1,13 @@
 import OrderId from '@/features/swap/components/OrderId'
 import StatusLabel from '@/features/swap/components/StatusLabel'
-import { formatTimeInWords } from '@/utils/date'
+import { formatDateTime, formatTimeInWords } from '@/utils/date'
 import type { ReactElement } from 'react'
 import Image from 'next/image'
 import { type SwapOrder as SwapOrderType, type TransactionData } from '@safe-global/safe-gateway-typescript-sdk'
 import { DataRow } from '@/components/common/Table/DataRow'
 import { DataTable } from '@/components/common/Table/DataTable'
 import { HexEncodedData } from '@/components/transactions/HexEncodedData'
+import { compareAsc } from 'date-fns'
 import css from './styles.module.css'
 
 type SwapOrderProps = {
@@ -14,7 +15,7 @@ type SwapOrderProps = {
   txInfo?: SwapOrderType
 }
 
-const SellOrder = ({ order }: { order: SwapOrderType }) => {
+export const SellOrder = ({ order }: { order: SwapOrderType }) => {
   const {
     buyToken,
     sellToken,
@@ -26,6 +27,9 @@ const SellOrder = ({ order }: { order: SwapOrderType }) => {
     surplusLabel,
     explorerUrl,
   } = order
+
+  const expires = new Date(expiresTimestamp * 1000)
+  const now = new Date()
 
   return (
     <DataTable
@@ -48,18 +52,31 @@ const SellOrder = ({ order }: { order: SwapOrderType }) => {
             </div>
           </div>
         </DataRow>,
-        <DataRow key="Execution price" title="Execution price">
-          {executionPriceLabel}
-        </DataRow>,
-        <DataRow key="Limit price" title="Limit price">
-          {limitPriceLabel}
-        </DataRow>,
-        <DataRow key="Surplus" title="Surplus">
-          {surplusLabel}
-        </DataRow>,
-        <DataRow key="Expiry" title="Expiry">
-          {formatTimeInWords(expiresTimestamp * 1000)}
-        </DataRow>,
+        status === 'fulfilled' ? (
+          <DataRow key="Execution price" title="Execution price">
+            {executionPriceLabel}
+          </DataRow>
+        ) : (
+          <DataRow key="Limit price" title="Limit price">
+            {limitPriceLabel}
+          </DataRow>
+        ),
+        status === 'fulfilled' ? (
+          <DataRow key="Surplus" title="Surplus">
+            {surplusLabel}
+          </DataRow>
+        ) : (
+          <></>
+        ),
+        compareAsc(now, expires) !== 1 ? (
+          <DataRow key="Expiry" title="Expiry">
+            {formatTimeInWords(expiresTimestamp * 1000)} ({formatDateTime(expiresTimestamp * 1000)})
+          </DataRow>
+        ) : (
+          <DataRow key="Expiry" title="Expiry">
+            {formatDateTime(expiresTimestamp * 1000)}
+          </DataRow>
+        ),
         <DataRow key="Order ID" title="Order ID">
           <OrderId orderId={orderUid} href={explorerUrl} />
         </DataRow>,
