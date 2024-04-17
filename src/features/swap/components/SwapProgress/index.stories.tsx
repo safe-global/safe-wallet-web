e339259e17a1749114bbddfba91934b80333d23d
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2024-04-17 16:11:51 +0200

    feat: Add SwapProgress component (#3572)
    * feat: Add SwapProgress component
    
    * fix: Add NotFilled state to storybook

---
+++ src/features/swap/components/SwapProgress/index.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import SwapProgress from './index'
+import { Paper } from '@mui/material'
+
+const meta = {
+  component: SwapProgress,
+  parameters: {
+    componentSubtitle: 'Renders a linear progress bar with % of a token sold',
+  },
+
+  decorators: [
+    (Story) => {
+      return (
+        <Paper sx={{ padding: 2 }}>
+          <Story />
+        </Paper>
+      )
+    },
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof SwapProgress>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Filled: Story = {
+  args: {
+    progress: '100.00',
+    tokenAmount: '1',
+    tokenSymbol: 'ETH',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/VyA38zUPbJ2zflzCIYR6Nu/Swap?node-id=5974%3A14487&mode=dev',
+    },
+  },
+}
+
+export const PartiallyFilled: Story = {
+  args: {
+    progress: '40.00',
+    tokenAmount: '1',
+    tokenSymbol: 'ETH',
+  },
+}
+
+export const NotFilled: Story = {
+  args: {
+    progress: '0.00',
+    tokenAmount: '1',
+    tokenSymbol: 'ETH',
+  },
+}
