afe8a41248017461fbbe17c9709b1204c067e6a8
Usame Algan <usame@safe.global>
2024-04-17 16:31:33 +0200

    fix: Show different label for sell orders

--- src/hooks/useMnemonicName/index.ts
+++ src/hooks/useMnemonicName/index.ts
@@ -5,7 +5,7 @@ import { animalsDict, adjectivesDict } from './dict'
 const animals: string[] = animalsDict.trim().split(/\s+/)
 const adjectives: string[] = adjectivesDict.trim().split(/\s+/)
 
-const capitalize = (word: string) => (word.length > 0 ? `${word.charAt(0).toUpperCase()}${word.slice(1)}` : word)
+export const capitalize = (word: string) => (word.length > 0 ? `${word.charAt(0).toUpperCase()}${word.slice(1)}` : word)
 
 const getRandomItem = <T>(arr: T[]): T => {
   return arr[Math.floor(arr.length * Math.random())]
