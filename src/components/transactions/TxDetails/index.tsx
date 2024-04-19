c2a84178da028793624b9f95574361f5a79f056e
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2024-04-19 11:31:03 +0200

    fix: Disable Execute button for expired swaps (#3583)
    * fix: Disable Execute button for expired swaps
    
    * fix: Add Cancelled label for cancelled swaps
    
    * refactor: Add helper methods for expired and cancelled swaps

--- src/components/transactions/TxDetails/index.tsx
+++ src/components/transactions/TxDetails/index.tsx
@@ -1,7 +1,7 @@
 import React, { type ReactElement } from 'react'
 import type { TransactionDetails, TransactionSummary } from '@safe-global/safe-gateway-typescript-sdk'
 import { getTransactionDetails, Operation } from '@safe-global/safe-gateway-typescript-sdk'
-import { Box, CircularProgress } from '@mui/material'
+import { Box, CircularProgress, Typography } from '@mui/material'
 
 import TxSigners from '@/components/transactions/TxSigners'
 import Summary from '@/components/transactions/TxDetails/Summary'
@@ -10,6 +10,7 @@ import useChainId from '@/hooks/useChainId'
 import useAsync from '@/hooks/useAsync'
 import {
   isAwaitingExecution,
+  isExpiredSwap,
   isModuleExecutionInfo,
   isMultiSendTxInfo,
   isMultisigDetailedExecutionInfo,
@@ -62,6 +63,8 @@ const TxDetailsBlock = ({ txSummary, txDetails }: TxDetailsProps): ReactElement 
     proposer = txDetails.detailedExecutionInfo.proposer?.value
     safeTxHash = txDetails.detailedExecutionInfo.safeTxHash
   }
+
+  const expiredSwap = isExpiredSwap(txSummary.txInfo)
 
   return (
     <>
@@ -129,6 +132,12 @@ const TxDetailsBlock = ({ txSummary, txDetails }: TxDetailsProps): ReactElement 
               {awaitingExecution ? <ExecuteTxButton txSummary={txSummary} /> : <SignTxButton txSummary={txSummary} />}
               <RejectTxButton txSummary={txSummary} safeTxHash={safeTxHash} proposer={proposer} />
             </Box>
+          )}
+
+          {expiredSwap && (
+            <Typography color="text.secondary" mt={2}>
+              This order has expired. Reject this transaction and try again.
+            </Typography>
           )}
         </div>
       )}
