e6c395d80bf67519ffc4f17bf02d0e67103770fa
Usame Algan <usame@safe.global>
2024-04-22 14:45:08 +0200

    fix: Include Filled percentage for swap limit orders

--- src/features/swap/components/SwapOrder/index.tsx
+++ src/features/swap/components/SwapOrder/index.tsx
@@ -1,6 +1,7 @@
 import TokenIcon from '@/components/common/TokenIcon'
 import OrderId from '@/features/swap/components/OrderId'
 import StatusLabel from '@/features/swap/components/StatusLabel'
+import SwapProgress from '@/features/swap/components/SwapProgress'
 import { capitalize } from '@/hooks/useMnemonicName'
 import { formatDateTime, formatTimeInWords } from '@/utils/date'
 import Stack from '@mui/material/Stack'
@@ -31,10 +32,10 @@ export const SellOrder = ({ order }: { order: SwapOrderType }) => {
     buyToken,
     sellAmount,
     buyAmount,
-    executedSellAmount,
-    executedBuyAmount,
+    class: swapClass,
     explorerUrl,
   } = order
+
   const executionPrice = getExecutionPrice(order)
   const limitPrice = getLimitPrice(order)
   const surplusPrice = getSurplusPrice(order)
@@ -97,6 +98,13 @@ export const SellOrder = ({ order }: { order: SwapOrderType }) => {
               {formatDateTime(validUntil * 1000)}
             </DataRow>
           )
+        ) : (
+          <></>
+        ),
+        swapClass === 'limit' ? (
+          <DataRow title="Filled" key="Filled">
+            <SwapProgress order={order} />
+          </DataRow>
         ) : (
           <></>
         ),
