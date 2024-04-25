d0b241af5e984a4acbf95692af3571b721abaeab
James Mealy <james@safe.global>
2024-04-25 13:23:35 +0200

    feat: create blocked address component in storybook

---
+++ src/features/swap/components/BlockedAddress/index.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import BlockedAddress from './index'
+
+const meta = {
+  component: BlockedAddress,
+  parameters: {
+    componentSubtitle: 'Renders a Status label with icon and text for a swap order',
+  },
+
+  decorators: [
+    (Story) => {
+      return <Story />
+    },
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof BlockedAddress>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    address: '0xD3a484faEa53313eF85b5916C9302a3E304ae622',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/VyA38zUPbJ2zflzCIYR6Nu/Swap?node-id=6167%3A14371&mode=dev',
+    },
+  },
+}
---
+++ src/features/swap/components/BlockedAddress/index.tsx
@@ -0,0 +1,51 @@
+import type { ReactElement } from 'react'
+import { Box, Button, Divider, Paper, Stack, SvgIcon, Typography, useMediaQuery, useTheme } from '@mui/material'
+import InfoIcon from '@/public/images/notifications/info.svg'
+import css from './styles.module.css'
+import { shortenAddress } from '@/utils/formatters'
+
+type Props = {
+  address: string
+}
+
+export const BlockedAddress = (props: Props): ReactElement => {
+  const { address } = props
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const displayAddress = isMobile ? shortenAddress(address) : address
+
+  const handleDismissButton = () => {}
+
+  return (
+    <Paper className={css.container}>
+      <Stack
+        padding="var(--space-3)"
+        gap={2}
+        display="flex"
+        alignItems="center"
+        borderBottom="1px solid var(--color-border-Light, #DCDEE0);"
+      >
+        <Typography color="var(--color-text-Secondary, #A1A3A7)">{displayAddress}</Typography>
+
+        <Box className={css.iconCircle}>
+          <SvgIcon component={InfoIcon} inheritViewBox fontSize="medium" />
+        </Box>
+        <Typography variant="h3" fontWeight={700}>
+          Blocked Address
+        </Typography>
+        <Typography variant="body2">
+          This signer address is blocked by the Safe interface, due to being associated with the blocked activities by
+          the U.S. Department of Treasury in the Specially Designated Nationals (SDN) list.{' '}
+        </Typography>
+        <Divider />
+      </Stack>
+      <Box display="flex" justifyContent="center" pt={3} pb={2}>
+        <Button variant="contained" onClick={handleDismissButton}>
+          Got it
+        </Button>
+      </Box>
+    </Paper>
+  )
+}
+
+export default BlockedAddress
