481b49262322d86e9e4194c758b035a25dfb1905
James Mealy <james@safe.global>
2024-04-25 16:10:06 +0200

    Dismiss button leads to dashboard

--- src/features/swap/components/BlockedAddress/index.tsx
+++ src/features/swap/components/BlockedAddress/index.tsx
@@ -1,8 +1,11 @@
 import type { ReactElement } from 'react'
 import { Box, Button, Divider, Paper, Stack, SvgIcon, Typography, useMediaQuery, useTheme } from '@mui/material'
+import Link from 'next/link'
 import InfoIcon from '@/public/images/notifications/info.svg'
 import css from './styles.module.css'
 import { shortenAddress } from '@/utils/formatters'
+import { useRouter } from 'next/router'
+import { AppRoutes } from '@/config/routes'
 
 type Props = {
   address: string
@@ -13,8 +16,12 @@ export const BlockedAddress = (props: Props): ReactElement => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
   const displayAddress = isMobile ? shortenAddress(address) : address
+  const router = useRouter()
 
-  const handleDismissButton = () => {}
+  const handleDismissButton = () => {
+    router.push(AppRoutes.home)
+  }
+
   return (
     <div className={css.container}>
       <Paper sx={{ maxWidth: '500px' }}>
@@ -34,9 +41,9 @@ export const BlockedAddress = (props: Props): ReactElement => {
           <Divider />
         </Stack>
         <Box display="flex" justifyContent="center" pt={3} pb={2}>
-          <Button variant="contained" onClick={handleDismissButton}>
-            Got it
-          </Button>
+          <Link href={{ pathname: AppRoutes.home, query: router.query }}>
+            <Button variant="contained">Got it</Button>
+          </Link>
         </Box>
       </Paper>
     </div>
