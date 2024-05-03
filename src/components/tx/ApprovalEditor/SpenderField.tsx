3ecb731e6e504a9e6aaebea7e3251fd89c354b10
schmanu <manu@safe.global>
2024-05-03 13:15:16 +0200

    fix: shortAddress for small screens only

--- src/components/tx/ApprovalEditor/SpenderField.tsx
+++ src/components/tx/ApprovalEditor/SpenderField.tsx
@@ -1,13 +1,16 @@
 import EthHashInfo from '@/components/common/EthHashInfo'
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
 
 import css from './styles.module.css'
 import useAsync from '@/hooks/useAsync'
 import useChainId from '@/hooks/useChainId'
 import { getContract } from '@safe-global/safe-gateway-typescript-sdk'
+
 export const SpenderField = ({ address }: { address: string }) => {
   const chainId = useChainId()
   const [spendingContract] = useAsync(() => getContract(chainId, address), [chainId, address])
+  const { breakpoints } = useTheme()
+  const isSmallScreen = useMediaQuery(breakpoints.down('md'))
 
   return (
     <Stack direction="row" justifyContent="space-between" alignItems="center" gap={2} className={css.approvalField}>
@@ -15,12 +18,12 @@ export const SpenderField = ({ address }: { address: string }) => {
         Spender
       </Typography>
       <div>
-        {/* TODO: set to name only once merged with swap changes */}
         <EthHashInfo
           avatarSize={24}
           address={address}
           name={spendingContract?.displayName || spendingContract?.name}
           customAvatar={spendingContract?.logoUri}
+          shortAddress={isSmallScreen}
           hasExplorer
         />
       </div>
