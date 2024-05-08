33debace9a229eaa4d0e34cf156db7f094d4ad51
katspaugh <katspaugh@gmail.com>
2024-05-08 16:19:32 +0200

    ENS resolver adjustment

--- src/hooks/useAddressResolver.ts
+++ src/hooks/useAddressResolver.ts
@@ -1,36 +1,33 @@
+import useAddressBook from '@/hooks/useAddressBook'
 import { useWeb3ReadOnly } from '@/hooks/wallets/web3'
 import { lookupAddress } from '@/services/ens'
-import { FEATURES, hasFeature } from '@/utils/chains'
+import { FEATURES } from '@/utils/chains'
 import { useMemo } from 'react'
 import useAsync from '@/hooks/useAsync'
 import useDebounce from './useDebounce'
-import { useChain } from './useChains'
-import useChainId from './useChainId'
-import useAllAddressBooks from '@/hooks/useAllAddressBooks'
+import { useHasFeature } from './useChains'
 
-export const useAddressResolver = (address?: string, chainId?: string) => {
+export const useAddressResolver = (address: string) => {
+  const addressBook = useAddressBook()
   const ethersProvider = useWeb3ReadOnly()
   const debouncedValue = useDebounce(address, 200)
-  const currentChainId = useChainId()
-  const chain = useChain(chainId ?? currentChainId)
-  const isDomainLookupEnabled = chain && hasFeature(chain, FEATURES.DOMAIN_LOOKUP)
-  const addressBook = useAllAddressBooks()[chainId ?? currentChainId] || {}
-  const shouldResolve = isDomainLookupEnabled && !!ethersProvider && !!address && !addressBook[address]
+  const addressBookName = addressBook[address]
+  const isDomainLookupEnabled = useHasFeature(FEATURES.DOMAIN_LOOKUP)
+  const shouldResolve = !addressBookName && isDomainLookupEnabled && !!ethersProvider && !!debouncedValue
 
   const [ens, _, isResolving] = useAsync<string | undefined>(() => {
     if (!shouldResolve) return
-    return lookupAddress(ethersProvider, debouncedValue || '')
+    return lookupAddress(ethersProvider, debouncedValue)
   }, [ethersProvider, debouncedValue, shouldResolve])
 
   const resolving = shouldResolve && isResolving
-  const name = addressBook[address || '']
 
   return useMemo(
     () => ({
-      name,
       ens,
+      name: addressBookName,
       resolving,
     }),
-    [name, ens, resolving],
+    [ens, addressBookName, resolving],
   )
 }
