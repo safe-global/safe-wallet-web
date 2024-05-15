9dd7c524f22f4e22f30ef41fff44cad1d4dad9d3
Usame Algan <usame@safe.global>
2024-05-15 09:38:31 +0200

    fix: Hide signed transaction from untrusted pending queue

--- src/components/transactions/TxList/index.tsx
+++ src/components/transactions/TxList/index.tsx
@@ -1,10 +1,13 @@
+import GroupedTxListItems from '@/components/transactions/GroupedTxListItems'
+import { useHasPendingTxs } from '@/hooks/usePendingTxs'
+import { isLabelListItem } from '@/utils/transaction-guards'
+import { groupConflictingTxs } from '@/utils/tx-list'
+import { Box } from '@mui/material'
+import type { LabelValue, TransactionListPage } from '@safe-global/safe-gateway-typescript-sdk'
+import { TransactionListItemType } from '@safe-global/safe-gateway-typescript-sdk'
 import type { ReactElement, ReactNode } from 'react'
 import { useMemo } from 'react'
-import { Box } from '@mui/material'
-import type { TransactionListPage } from '@safe-global/safe-gateway-typescript-sdk'
 import TxListItem from '../TxListItem'
-import GroupedTxListItems from '@/components/transactions/GroupedTxListItems'
-import { groupConflictingTxs } from '@/utils/tx-list'
 import css from './styles.module.css'
 
 type TxListProps = {
@@ -15,8 +18,24 @@ export const TxListGrid = ({ children }: { children: ReactNode }): ReactElement 
   return <Box className={css.container}>{children}</Box>
 }
 
+const adjustPendingLabel = (items: TransactionListPage['results']) => {
+  const result = [...items]
+
+  if (isLabelListItem(result[0])) {
+    result[0] = {
+      label: 'Pending' as LabelValue,
+      type: TransactionListItemType.LABEL,
+    }
+  }
+
+  return result
+}
+
 const TxList = ({ items }: TxListProps): ReactElement => {
-  const groupedItems = useMemo(() => groupConflictingTxs(items), [items])
+  const hasPending = useHasPendingTxs()
+
+  const pendingItems = useMemo(() => (hasPending ? adjustPendingLabel(items) : items), [items, hasPending])
+  const groupedItems = useMemo(() => groupConflictingTxs(pendingItems), [pendingItems])
 
   const transactions = groupedItems.map((item, index) => {
     if (Array.isArray(item)) {
