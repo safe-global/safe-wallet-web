c453216e92d7669f5aeb7565ad8d3fe3ad3f0c6e
katspaugh <katspaugh@gmail.com>
2024-05-17 11:46:31 +0200

    Adjust types

--- src/components/welcome/MyAccounts/useSafeOverviews.ts
+++ src/components/welcome/MyAccounts/useSafeOverviews.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { useTokenListSetting } from '@/hooks/loadables/useLoadBalances'
-import useAsync from '@/hooks/useAsync'
+import useAsync, { type AsyncResult } from '@/hooks/useAsync'
 import useWallet from '@/hooks/wallets/useWallet'
 import { useAppSelector } from '@/store'
 import { selectCurrency } from '@/store/settingsSlice'
@@ -16,23 +16,23 @@ type SafeParams = {
 // EIP155 address format
 const makeSafeId = ({ chainId, address }: SafeParams) => `${chainId}:${address}` as `${number}:0x${string}`
 
-function useSafeOverviews(safes: Array<SafeParams>) {
+function useSafeOverviews(safes: Array<SafeParams>): AsyncResult<SafeOverview[]> {
   const excludeSpam = useTokenListSetting() || false
   const currency = useAppSelector(selectCurrency)
   const wallet = useWallet()
   const walletAddress = wallet?.address
-  const safesStrings = useMemo(() => safes.map(makeSafeId), [safes])
+  const safesIds = useMemo(() => safes.map(makeSafeId), [safes])
 
   const [data, error, isLoading] = useAsync(async () => {
-    return await getSafeOverviews(safesStrings, {
+    return await getSafeOverviews(safesIds, {
       trusted: true,
       exclude_spam: excludeSpam,
       currency,
       wallet_address: walletAddress,
     })
-  }, [safesStrings, excludeSpam, currency, walletAddress])
+  }, [safesIds, excludeSpam, currency, walletAddress])
 
-  const cacheKey = safesStrings.join()
+  const cacheKey = safesIds.join()
   const result = data ?? _cache[cacheKey]
 
   // Cache until the next page load
