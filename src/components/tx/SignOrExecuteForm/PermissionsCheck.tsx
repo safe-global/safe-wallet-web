a5670d562847e0564603657a865388ce579f03b0
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-05-27 16:11:49 +0200

    implement static permission checking

--- src/components/tx/SignOrExecuteForm/PermissionsCheck.tsx
+++ src/components/tx/SignOrExecuteForm/PermissionsCheck.tsx
@@ -1,5 +1,14 @@
-import { useContext, useState } from 'react'
-import { type ChainId, chains, fetchRolesMod } from 'zodiac-roles-deployments'
+import { useContext, useMemo, useState } from 'react'
+import {
+  type ChainId,
+  chains,
+  fetchRolesMod,
+  Clearance,
+  type RoleSummary,
+  ExecutionOptions,
+  Status,
+} from 'zodiac-roles-deployments'
+import { type MetaTransactionData } from '@safe-global/safe-core-sdk-types'
 
 import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 import { Box, Button, CardActions, CircularProgress, Divider, Typography } from '@mui/material'
@@ -11,21 +20,17 @@ import { TX_EVENTS } from '@/services/analytics/events/transactions'
 import { trackEvent } from '@/services/analytics'
 import useChainId from '@/hooks/useChainId'
 import useSafeInfo from '@/hooks/useSafeInfo'
-import useWallet from '@/hooks/wallets/useWallet'
 import useAsync from '@/hooks/useAsync'
 import WalletRejectionError from './WalletRejectionError'
 import ErrorMessage from '../ErrorMessage'
+import useWallet from '@/hooks/wallets/useWallet'
 
 const PermissionsCheck: React.FC<{}> = ({}) => {
   const chainId = useChainId()
   const { safeTx, safeTxError } = useContext(SafeTxContext)
   const [isRejectedByUser, setIsRejectedByUser] = useState<boolean>(false)
 
-  const rolesMods = useRolesMods()
-
-  //   if (rolesMods.length === 0) {
-  //     return null
-  //   }
+  const roles = useRoles(safeTx?.data)
 
   const isPending = false
   const isDisabled = isPending
@@ -90,16 +95,14 @@ const ROLES_V2_SUPPORTED_CHAINS = Object.keys(chains)
  */
 const useRolesMods = () => {
   const { safe } = useSafeInfo()
-  const wallet = useWallet()
 
   const [data] = useAsync(async () => {
-    if (!wallet) return []
     if (!ROLES_V2_SUPPORTED_CHAINS.includes(safe.chainId)) return []
 
     const safeModules = safe.modules || []
     const rolesMods = await Promise.all(
       safeModules.map((address) =>
-        fetchRolesMod({ address: address.value, chainId: parseInt(safe.chainId) as ChainId }),
+        fetchRolesMod({ address: address.value as `0x${string}`, chainId: parseInt(safe.chainId) as ChainId }),
       ),
     )
 
@@ -110,7 +113,86 @@ const useRolesMods = () => {
         mod.avatar === safe.address.value.toLowerCase() &&
         mod.roles.length > 0,
     )
-  }, [safe, wallet])
+  }, [safe])
 
-  return data || []
+  return data
+}
+
+/**
+ * Returns a list of roles mod address + role key assigned to the connected wallet.
+ * For each role, checks if the role allows the given meta transaction and returns the status.
+ */
+const useRoles = (metaTx?: MetaTransactionData) => {
+  const rolesMods = useRolesMods()
+  const wallet = useWallet()
+  const walletAddress = wallet?.address.toLowerCase() as undefined | `0x${string}`
+
+  // find all roles assigned to the connected wallet, statically check if they allow the given meta transaction
+  const potentialRoles = useMemo(() => {
+    const result: {
+      modAddress: `0x${string}`
+      roleKey: `0x${string}`
+      status: Status | null
+    }[] = []
+
+    if (walletAddress && rolesMods) {
+      for (const rolesMod of rolesMods) {
+        for (const role of rolesMod.roles) {
+          if (role.members.includes(walletAddress)) {
+            potentialRoles.push({
+              modAddress: rolesMod.address,
+              roleKey: role.key,
+              status: metaTx ? checkTransaction(role, metaTx) : null,
+            })
+          }
+        }
+      }
+    }
+
+    return result
+  }, [rolesMods, walletAddress, metaTx])
+
+  // TODO if the static check is inconclusive (status: null), evaluate the condition through a test call
+
+  return potentialRoles
+}
+
+/**
+ * Returns the status of the permissions check, `null` if it depends on the condition evaluation.
+ */
+const checkTransaction = (role: RoleSummary, metaTx: MetaTransactionData): Status | null => {
+  const target = role.targets.find((t) => t.address === metaTx.to.toLowerCase())
+  if (!target) return Status.TargetAddressNotAllowed
+
+  if (target.clearance === Clearance.Target) {
+    // all calls to the target are allowed
+    return checkExecutionOptions(target.executionOptions, metaTx)
+  }
+
+  if (target.clearance === Clearance.Function) {
+    // check if the function is allowed
+    const selector = metaTx.data.slice(0, 10) as `0x${string}`
+    const func = target.functions.find((f) => f.selector === selector)
+    if (func) {
+      const execOptionsStatus = checkExecutionOptions(func.executionOptions, metaTx)
+      if (execOptionsStatus !== Status.Ok) return execOptionsStatus
+      return func.wildcarded ? Status.Ok : null // wildcarded means there's no condition set
+    }
+  }
+
+  return Status.FunctionNotAllowed
+}
+
+const checkExecutionOptions = (execOptions: ExecutionOptions, metaTx: MetaTransactionData): Status => {
+  const isSend = BigInt(metaTx.value || '0') === BigInt(0)
+  const isDelegateCall = metaTx.operation === 1
+
+  if (isSend && execOptions !== ExecutionOptions.Send && execOptions !== ExecutionOptions.Both) {
+    return Status.SendNotAllowed
+  }
+  if (isDelegateCall && execOptions !== ExecutionOptions.DelegateCall && execOptions !== ExecutionOptions.Both) {
+    return Status.DelegateCallNotAllowed
+  }
+
+  return Status.Ok
 }
