939f47bcdedb697024f3dc52b8dd1259039bfc23
Usame Algan <usame@safe.global>
2024-05-31 10:52:28 +0200

    fix: optimize widget for mobile, adjust badges and chips style

--- src/components/common/Chip/index.tsx
+++ src/components/common/Chip/index.tsx
@@ -4,5 +4,5 @@ import type { ReactElement } from 'react'
 import React from 'react'
 
 export function Chip(props: ChipProps): ReactElement {
-  return <MuiChip label="New" color="success" {...props} />
+  return <MuiChip label="New" color="success" size="small" sx={{ fontSize: '11px', fontWeight: 'bold' }} {...props} />
 }
