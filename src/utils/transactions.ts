edce3a3b89ec7990e3bf65a5dc321d273c41df49
James Mealy <james@safe.global>
2024-06-07 14:07:38 +0100

    lint errors

--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -87,6 +87,7 @@ export const makeTxFromDetails = (txDetails: TransactionDetails): Transaction =>
       txInfo: txDetails.txInfo,
       executionInfo,
       safeAppInfo: txDetails?.safeAppInfo,
+      txHash: txDetails?.txHash || null,
     },
     conflictType: ConflictType.NONE,
   }
