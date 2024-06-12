b978126a2d718583e2c2f30a2bd8b57345f5a7bc
katspaugh <katspaugh@gmail.com>
2024-06-12 09:13:27 +0200

    Fix: add loading skeletons in the Account List

--- src/components/welcome/MyAccounts/AccountItem.tsx
+++ src/components/welcome/MyAccounts/AccountItem.tsx
@@ -2,7 +2,7 @@ import { LoopIcon } from '@/features/counterfactual/CounterfactualStatusButton'
 import { selectUndeployedSafe } from '@/features/counterfactual/store/undeployedSafesSlice'
 import type { ChainInfo, SafeOverview } from '@safe-global/safe-gateway-typescript-sdk'
 import { useCallback, useMemo } from 'react'
-import { ListItemButton, Box, Typography, Chip } from '@mui/material'
+import { ListItemButton, Box, Typography, Chip, Skeleton } from '@mui/material'
 import Link from 'next/link'
 import SafeIcon from '@/components/common/SafeIcon'
 import Track from '@/components/common/Track'
@@ -110,7 +110,7 @@ const AccountItem = ({ onLinkClick, safeItem, safeOverview }: AccountItemProps) 
           </Typography>
 
           <Typography variant="body2" fontWeight="bold" textAlign="right" pr={5}>
-            {safeOverview?.fiatTotal && <FiatValue value={safeOverview.fiatTotal} />}
+            {safeOverview ? <FiatValue value={safeOverview.fiatTotal} /> : <Skeleton variant="text" />}
           </Typography>
 
           <ChainIndicator chainId={chainId} responsive />
