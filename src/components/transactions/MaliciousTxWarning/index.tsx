508b17cc58bbaa8d01eb11a9530a55f06329436b
James Mealy <james@safe.global>
2024-06-21 09:34:51 +0100

    remove unused css

--- src/components/transactions/MaliciousTxWarning/index.tsx
+++ src/components/transactions/MaliciousTxWarning/index.tsx
@@ -4,12 +4,12 @@ import WarningIcon from '@/public/images/notifications/warning.svg'
 const MaliciousTxWarning = ({ withTooltip = true }: { withTooltip?: boolean }) => {
   return withTooltip ? (
     <Tooltip title="This token is unfamiliar and may pose risks when interacting with it or involved addresses">
-      <Box lineHeight="16px" sx={{ opacity: 1 }}>
+      <Box lineHeight="16px">
         <SvgIcon component={WarningIcon} fontSize="small" inheritViewBox color="warning" />
       </Box>
     </Tooltip>
   ) : (
-    <Box lineHeight="16px" sx={{ opacity: 1 }}>
+    <Box lineHeight="16px">
       <SvgIcon component={WarningIcon} fontSize="small" inheritViewBox color="warning" />
     </Box>
   )
