748e8cf86aaca1c2eb3dc3c3742394bcc06f0475
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-06-25 11:22:46 +0200

    Update index.tsx

--- src/components/common/MetaTags/index.tsx
+++ src/components/common/MetaTags/index.tsx
@@ -3,7 +3,7 @@ import { ContentSecurityPolicy, StrictTransportSecurity } from '@/config/securit
 import lightPalette from '@/components/theme/lightPalette'
 import darkPalette from '@/components/theme/darkPalette'
 
-const descriptionText = 'Safe{Wallet} is the most trusted smart account wallet on Ethereum. Over $100B secured.'
+const descriptionText = 'Safe{Wallet} is the most trusted smart account wallet on Ethereum with over $100B secured.'
 const titleText = 'Safe{Wallet}'
 
 const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
