e7f029183a168be9aca20e94847d97eb20a37422
Usame Algan <usame@safe.global>
2024-06-25 13:03:09 +0200

    fix: Extract functions into gateway-sdk

--- src/services/siwe/index.ts
+++ src/services/siwe/index.ts
@@ -1,19 +1,12 @@
-import { cgwDebugStorage } from '@/components/sidebar/DebugToggle'
-import { GATEWAY_URL_PRODUCTION, GATEWAY_URL_STAGING, IS_PRODUCTION } from '@/config/constants'
+import { getAuthNonce, verifyAuth } from '@safe-global/safe-gateway-typescript-sdk'
 import type { BrowserProvider } from 'ethers'
-
-const GATEWAY_URL = IS_PRODUCTION || cgwDebugStorage.get() ? GATEWAY_URL_PRODUCTION : GATEWAY_URL_STAGING
 
 /**
  * Prompt the user to sign in with their wallet and set an access_token cookie
  * @param provider
  */
 async function signInWithEthereum(provider: BrowserProvider) {
-  const { nonce } = await fetch(`${GATEWAY_URL}/v1/auth/nonce`, {
-    method: 'GET',
-    // Essential to 'include' credentials on all auth requests
-    credentials: 'include',
-  }).then((res) => res.json())
+  const { nonce } = await getAuthNonce()
 
   const [network, signer] = await Promise.all([provider.getNetwork(), provider.getSigner()])
 
@@ -42,17 +35,7 @@ Issued At: ${message.issuedAt.toISOString()}`
 
   const signature = await signer.signMessage(signableMessage)
 
-  return fetch(`${GATEWAY_URL}/v1/auth/verify`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: signableMessage,
-      signature,
-    }),
-  })
+  return verifyAuth({ message: signableMessage, signature })
 }
 
 export default signInWithEthereum
