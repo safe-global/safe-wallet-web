34da84f6ebcfb88e4b3340f34b0c18eb04a37227
James Mealy <james@safe.global>
2024-06-26 15:34:56 +0100

    Feat: Show warning for address poisoning transactions (#3851)
    * Flag imitation transactions and add warning component
    
    * add imitation to Erc20Transfer type
    
    * make txddetails full width when there is  no execution info to display
    
    * Make warning symbold full opacity, remove tooltip for imitation txs
    
    * handle poisoned addresses
    
    * hide share
    
    * temporary cast for changes to SDK type
    
    * restore share link
    
    * remove unused css and rename imitation prop
    
    * remove learn more link from imitation tx warning
    
    * reuse malicious transaction warning component.
    
    * fix: missing tooltip for untrusted txs
    
    * update gateway sdks
    
    * add mock fields for updated gateway types
    
    * remove temporary cast for Erc20Transfer type from gateway sdk
    
    * remove unused css
    
    * change hide untrusted button to hide suspicious toggle switch
    
    * fix: unit tests

--- src/components/transactions/MaliciousTxWarning/index.tsx
+++ src/components/transactions/MaliciousTxWarning/index.tsx
@@ -1,19 +1,18 @@
 import { Tooltip, SvgIcon, Box } from '@mui/material'
 import WarningIcon from '@/public/images/notifications/warning.svg'
 
-const UntrustedTxWarning = () => {
-  return (
+const MaliciousTxWarning = ({ withTooltip = true }: { withTooltip?: boolean }) => {
+  return withTooltip ? (
     <Tooltip title="This token is unfamiliar and may pose risks when interacting with it or involved addresses">
-      <Box
-        lineHeight="16px"
-        sx={{
-          opacity: 1,
-        }}
-      >
+      <Box lineHeight="16px">
         <SvgIcon component={WarningIcon} fontSize="small" inheritViewBox color="warning" />
       </Box>
     </Tooltip>
+  ) : (
+    <Box lineHeight="16px">
+      <SvgIcon component={WarningIcon} fontSize="small" inheritViewBox color="warning" />
+    </Box>
   )
 }
 
-export default UntrustedTxWarning
+export default MaliciousTxWarning
