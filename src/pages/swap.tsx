d4ecf5881f125839e0736535511875172f6a43fa
James Mealy <james@safe.global>
2024-07-02 17:41:32 +0200

    redirect to 403 page from the swaps page if country is blocked

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -2,10 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import SwapWidget from '@/features/swap'
+import { GeoblockingContext } from '@/components/common/GeoblockingProvider'
+import { useContext } from 'react'
+import { AppRoutes } from '@/config/routes'
 
 const Swap: NextPage = () => {
   const router = useRouter()
+  const isBlockedCountry = useContext(GeoblockingContext)
   const { token, amount } = router.query
+
+  if (isBlockedCountry) {
+    router.replace(AppRoutes['403'])
+  }
 
   let sell = undefined
   if (token && amount) {
