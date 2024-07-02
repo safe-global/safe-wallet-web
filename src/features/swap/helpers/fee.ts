88eba22d189f2e364c1ada7e2ac544bd24758ff4
Daniel Dimitrov <daniel@compojoom.com>
2024-07-02 16:38:30 +0200

    fix: check for stablecoin was not working
    Fixed a case-mismatch between the addresses

--- src/features/swap/helpers/fee.ts
+++ src/features/swap/helpers/fee.ts
@@ -19,6 +19,14 @@ const FEE_TIERS = {
   TIER_2: 1_000_000, // 100k - 1m
 }
 
+const getLowerCaseStableCoinAddresses = () => {
+  const lowerCaseStableCoinAddresses = Object.keys(stableCoinAddresses).reduce((result, key) => {
+    result[key.toLowerCase()] = stableCoinAddresses[key]
+    return result
+  }, {} as typeof stableCoinAddresses)
+
+  return lowerCaseStableCoinAddresses
+}
 /**
  * Function to calculate the fee % in bps to apply for a trade.
  * The fee % should be applied based on the fiat value of the buy or sell token.
@@ -27,8 +35,8 @@ const FEE_TIERS = {
  */
 export const calculateFeePercentageInBps = (orderParams: OnTradeParamsPayload) => {
   const { sellToken, buyToken, buyTokenFiatAmount, sellTokenFiatAmount, orderKind } = orderParams
-
-  const isStableCoin = stableCoinAddresses[sellToken?.address] && stableCoinAddresses[buyToken?.address]
+  const stableCoins = getLowerCaseStableCoinAddresses()
+  const isStableCoin = stableCoins[sellToken?.address?.toLowerCase()] && stableCoins[buyToken?.address.toLowerCase()]
 
   const fiatAmount = Number(orderKind == 'sell' ? sellTokenFiatAmount : buyTokenFiatAmount) || 0
 
