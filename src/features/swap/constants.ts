d59c61ea9f12655053d9f9a7e83b915a4c64a188
Daniel Dimitrov <daniel@compojoom.com>
2024-07-02 13:21:35 +0200

    chore: change fee recipient

--- src/features/swap/constants.ts
+++ src/features/swap/constants.ts
@@ -3,4 +3,4 @@ export const SWAP_ORDER_TITLE = 'Swap order'
 export const LIMIT_ORDER_TITLE = 'Limit order'
 export const TWAP_ORDER_TITLE = 'TWAP order'
 
-export const SWAP_FEE_RECIPIENT = '0x1697AD99A16474F16d8D7C05320fb6777c7AE850'
+export const SWAP_FEE_RECIPIENT = '0x63695Eee2c3141BDE314C5a6f89B98E62808d716'
