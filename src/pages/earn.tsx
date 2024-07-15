888ae5e0ee1b73357b825919f1dc6573422e56ec
LeTamanoir <martin.saldinger@gmail.com>
2024-07-15 20:30:08 +0200

    add earn page

---
+++ src/pages/earn.tsx
@@ -0,0 +1,39 @@
+import AppFrame from '@/components/safe-apps/AppFrame'
+import { SafeAppAccessPolicyTypes, SafeAppFeatures, type SafeAppData } from '@safe-global/safe-gateway-typescript-sdk'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useMemo } from 'react'
+
+const Earn: NextPage = () => {
+  const appData: SafeAppData = useMemo(
+    () => ({
+      id: 1,
+      url: 'https://widget.devnet.kiln.fi/d6309182-cbad-11ec-9d64-0242ac120003/overview',
+      name: 'Earn',
+      iconUrl: './images/common/earn.svg',
+      description: 'Safe Apps',
+      chainIds: ['17000', '11155111', '1', '42161', '137', '56', '8453', '10'],
+      accessControl: { type: SafeAppAccessPolicyTypes.NoRestrictions },
+      tags: ['safe-apps'],
+      features: [SafeAppFeatures.BATCHED_TRANSACTIONS],
+      socialProfiles: [],
+    }),
+    [],
+  )
+
+  return (
+    <>
+      <Head>
+        <title>{'Safe{Wallet} – Earn'}</title>
+      </Head>
+
+      <AppFrame
+        allowedFeaturesList="clipboard-read; clipboard-write"
+        appUrl={appData.url}
+        safeAppFromManifest={{ ...appData, safeAppsPermissions: [] }}
+      />
+    </>
+  )
+}
+
+export default Earn
