f6b49703401125a4182bce1c0185070018e58fde
Daniel Dimitrov <daniel@compojoom.com>
2024-07-31 10:05:24 +0200

    fix: wrong widget url

--- src/pages/stake.tsx
+++ src/pages/stake.tsx
@@ -15,7 +15,7 @@ const Swap: NextPage = () => {
   const appData: SafeAppData = useMemo(
     () => ({
       id: 321412441,
-      url: 'https://widget.devnet.kiln.fi/d6309182-cbad-11ec-9d64-0242ac120003/overview',
+      url: 'https://widget.devnet.kiln.fi/safe-widget/earn',
       name: 'Stake',
       iconUrl: './images/common/earn.svg',
       description: 'Safe Apps',
