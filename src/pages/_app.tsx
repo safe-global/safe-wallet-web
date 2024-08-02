419090fc1ad00c69e805bec762f6e1bfcc902fe0
Daniel Dimitrov <daniel@compojoom.com>
2024-08-02 15:05:16 +0200

    console.log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -79,7 +79,7 @@ export const AppProviders = ({ children }: { children: ReactNode | ReactNode[] }
   const isDarkMode = useDarkMode()
   const themeMode = isDarkMode ? 'dark' : 'light'
 
-  console.log('test')
+  console.log('test1')
   return (
     <SafeThemeProvider mode={themeMode}>
       {(safeTheme: Theme) => (
