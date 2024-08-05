9e0e2ea397625c94b4aa1adf19cad1c1460997f0
katspaugh <katspaugh@gmail.com>
2024-08-05 15:09:35 +0200

    Restore isSafeSrc

--- src/components/safe-apps/SafeAppIconCard/index.test.tsx
+++ src/components/safe-apps/SafeAppIconCard/index.test.tsx
@@ -3,14 +3,31 @@ import SafeAppIconCard from '.'
 
 describe('SafeAppIconCard', () => {
   it('should render an icon', () => {
+    const src = 'https://safe-transaction-assets.safe.global/safe_apps/160/icon.png'
     const { queryByAltText } = render(
-      <SafeAppIconCard src="/icon.png" fallback="/fallback.png" height={100} width={100} alt="test" />,
+      <SafeAppIconCard src={src} fallback="/fallback.png" height={100} width={100} alt="test" />,
     )
 
     const img = queryByAltText('test')
     expect(img).toBeInTheDocument()
-    expect(img).toHaveAttribute('src', '/icon.png')
+    expect(img).toHaveAttribute('src', src)
     expect(img).toHaveAttribute('height', '100')
     expect(img).toHaveAttribute('width', '100')
+    expect(img).not.toHaveAttribute('crossorigin')
+  })
+
+  it('should render an icon from an unknown domain', () => {
+    const src = 'https://unknown.com/icon.png'
+
+    const { queryByAltText } = render(
+      <SafeAppIconCard src={src} fallback="/fallback.png" height={200} width={200} alt="test" />,
+    )
+
+    const img = queryByAltText('test')
+    expect(img).toBeInTheDocument()
+    expect(img).toHaveAttribute('src', src)
+    expect(img).toHaveAttribute('height', '200')
+    expect(img).toHaveAttribute('width', '200')
+    expect(img).toHaveAttribute('crossorigin', 'anonymous')
   })
 })
--- src/components/safe-apps/SafeAppIconCard/index.tsx
+++ src/components/safe-apps/SafeAppIconCard/index.tsx
@@ -3,6 +3,22 @@ import { type ReactElement, memo } from 'react'
 
 const APP_LOGO_FALLBACK_IMAGE = `/images/apps/app-placeholder.svg`
 
+export const _isSafeSrc = (src: string) => {
+  const allowedHosts = ['.safe.global', '.5afe.dev']
+  const isRelative = src.startsWith('/')
+
+  let hostname = ''
+  if (!isRelative) {
+    try {
+      hostname = new URL(src).hostname
+    } catch (e) {
+      return false
+    }
+  }
+
+  return isRelative || allowedHosts.some((host) => hostname.endsWith(host))
+}
+
 const SafeAppIconCard = ({
   src,
   alt,
@@ -16,7 +32,16 @@ const SafeAppIconCard = ({
   height?: number
   fallback?: string
 }): ReactElement => {
-  return <ImageFallback src={src} alt={alt} width={width} height={height} fallbackSrc={fallback} />
+  return (
+    <ImageFallback
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      fallbackSrc={fallback}
+      crossOrigin={_isSafeSrc(src) ? undefined : 'anonymous'}
+    />
+  )
 }
 
 export default memo(SafeAppIconCard)
