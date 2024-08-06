8c39108350ac8ff8af9b9e95badd4dfe9cf7f14f
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-08-06 07:58:32 +0200

    Feat: total asset value cents in grey (#4035)
    * Feat: total asset value cents in grey
    
    * Adjust for currency-first locales
    
    * Inherit font weight

--- src/components/common/FiatValue/index.tsx
+++ src/components/common/FiatValue/index.tsx
@@ -1,13 +1,21 @@
 import type { CSSProperties, ReactElement } from 'react'
 import { useMemo } from 'react'
-import { Tooltip } from '@mui/material'
+import { Tooltip, Typography } from '@mui/material'
 import { useAppSelector } from '@/store'
 import { selectCurrency } from '@/store/settingsSlice'
 import { formatCurrency, formatCurrencyPrecise } from '@/utils/formatNumber'
 
 const style = { whiteSpace: 'nowrap' } as CSSProperties
 
-const FiatValue = ({ value, maxLength }: { value: string | number; maxLength?: number }): ReactElement => {
+const FiatValue = ({
+  value,
+  maxLength,
+  precise,
+}: {
+  value: string | number
+  maxLength?: number
+  precise?: boolean
+}): ReactElement => {
   const currency = useAppSelector(selectCurrency)
 
   const fiat = useMemo(() => {
@@ -18,10 +26,27 @@ const FiatValue = ({ value, maxLength }: { value: string | number; maxLength?: n
     return formatCurrencyPrecise(value, currency)
   }, [value, currency])
 
+  const [whole, decimals, endCurrency] = useMemo(() => {
+    const match = preciseFiat.match(/(.+)(\D\d+)(\D+)?$/)
+    return match ? match.slice(1) : ['', preciseFiat, '', '']
+  }, [preciseFiat])
+
   return (
-    <Tooltip title={preciseFiat}>
+    <Tooltip title={precise ? undefined : preciseFiat}>
       <span suppressHydrationWarning style={style}>
-        {fiat}
+        {precise ? (
+          <>
+            {whole}
+            {decimals && (
+              <Typography component="span" color="text.secondary" fontSize="inherit" fontWeight="inherit">
+                {decimals}
+              </Typography>
+            )}
+            {endCurrency}
+          </>
+        ) : (
+          fiat
+        )}
       </span>
     </Tooltip>
   )
