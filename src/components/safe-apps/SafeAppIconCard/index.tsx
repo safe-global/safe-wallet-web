a96d505b7c130ef73432818d5dab65297be13c0d
katspaugh <katspaugh@gmail.com>
2024-08-12 11:57:58 +0200

    Fallback for external hosts

--- src/components/safe-apps/SafeAppIconCard/index.tsx
+++ src/components/safe-apps/SafeAppIconCard/index.tsx
@@ -2,9 +2,9 @@ import ImageFallback from '@/components/common/ImageFallback'
 import { type ReactElement, memo } from 'react'
 
 const APP_LOGO_FALLBACK_IMAGE = `/images/apps/app-placeholder.svg`
+const ALLOWED_HOSTS = ['.safe.global', '.5afe.dev']
 
 export const _isSafeSrc = (src: string) => {
-  const allowedHosts = ['.safe.global', '.5afe.dev']
   const isRelative = src.startsWith('/')
 
   let hostname = ''
@@ -16,7 +16,7 @@ export const _isSafeSrc = (src: string) => {
     }
   }
 
-  return isRelative || allowedHosts.some((host) => hostname.endsWith(host))
+  return isRelative || ALLOWED_HOSTS.some((host) => hostname.endsWith(host))
 }
 
 const SafeAppIconCard = ({
@@ -34,12 +34,11 @@ const SafeAppIconCard = ({
 }): ReactElement => {
   return (
     <ImageFallback
-      src={src}
+      src={_isSafeSrc(src) ? src : fallback}
       alt={alt}
       width={width}
       height={height}
       fallbackSrc={fallback}
-      crossOrigin={_isSafeSrc(src) ? undefined : 'anonymous'}
     />
   )
 }
