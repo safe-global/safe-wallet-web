69a73bcb53faf68d1e29eaad0dcfbfdf197f258c
katspaugh <katspaugh@gmail.com>
2024-08-12 12:08:56 +0200

    Always add crossOrigin="anonymous"

--- src/components/safe-apps/SafeAppIconCard/index.test.tsx
+++ src/components/safe-apps/SafeAppIconCard/index.test.tsx
@@ -13,21 +13,6 @@ describe('SafeAppIconCard', () => {
     expect(img).toHaveAttribute('src', src)
     expect(img).toHaveAttribute('height', '100')
     expect(img).toHaveAttribute('width', '100')
-    expect(img).not.toHaveAttribute('crossorigin')
-  })
-
-  it('should render an icon from an unknown domain', () => {
-    const src = 'https://unknown.com/icon.png'
-
-    const { queryByAltText } = render(
-      <SafeAppIconCard src={src} fallback="/fallback.png" height={200} width={200} alt="test" />,
-    )
-
-    const img = queryByAltText('test')
-    expect(img).toBeInTheDocument()
-    expect(img).toHaveAttribute('src', src)
-    expect(img).toHaveAttribute('height', '200')
-    expect(img).toHaveAttribute('width', '200')
-    expect(img).toHaveAttribute('crossorigin', 'anonymous')
+    expect(img).toHaveAttribute('crossorigin')
   })
 })
--- src/components/safe-apps/SafeAppIconCard/index.tsx
+++ src/components/safe-apps/SafeAppIconCard/index.tsx
@@ -1,23 +1,6 @@
 import ImageFallback from '@/components/common/ImageFallback'
-import { type ReactElement, memo } from 'react'
 
 const APP_LOGO_FALLBACK_IMAGE = `/images/apps/app-placeholder.svg`
-const ALLOWED_HOSTS = ['.safe.global', '.5afe.dev']
-
-export const _isSafeSrc = (src: string) => {
-  const isRelative = src.startsWith('/')
-
-  let hostname = ''
-  if (!isRelative) {
-    try {
-      hostname = new URL(src).hostname
-    } catch (e) {
-      return false
-    }
-  }
-
-  return isRelative || ALLOWED_HOSTS.some((host) => hostname.endsWith(host))
-}
 
 const SafeAppIconCard = ({
   src,
@@ -31,16 +14,6 @@ const SafeAppIconCard = ({
   width?: number
   height?: number
   fallback?: string
-}): ReactElement => {
-  return (
-    <ImageFallback
-      src={_isSafeSrc(src) ? src : fallback}
-      alt={alt}
-      width={width}
-      height={height}
-      fallbackSrc={fallback}
-    />
-  )
-}
+}) => <ImageFallback src={src} alt={alt} width={width} height={height} fallbackSrc={fallback} crossOrigin="anonymous" />
 
-export default memo(SafeAppIconCard)
+export default SafeAppIconCard
