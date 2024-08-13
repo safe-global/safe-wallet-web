3488f8210a0396518dc9c1f86a84969685af4483
schmanu <manu@safe.global>
2024-08-13 16:49:38 +0200

    fix: remove sep5 logic and fix tests

--- src/components/common/SafeTokenWidget/index.tsx
+++ src/components/common/SafeTokenWidget/index.tsx
@@ -1,7 +1,7 @@
 import { IS_PRODUCTION, SAFE_TOKEN_ADDRESSES, SAFE_LOCKING_ADDRESS } from '@/config/constants'
 import { AppRoutes } from '@/config/routes'
 import useChainId from '@/hooks/useChainId'
-import useSafeTokenAllocation, { useSafeVotingPower, type Vesting } from '@/hooks/useSafeTokenAllocation'
+import useSafeTokenAllocation, { useSafeVotingPower } from '@/hooks/useSafeTokenAllocation'
 import { OVERVIEW_EVENTS } from '@/services/analytics'
 import { formatVisualAmount } from '@/utils/formatters'
 import { Box, ButtonBase, Divider, Skeleton, SvgIcon, Tooltip, Typography } from '@mui/material'
@@ -11,12 +11,10 @@ import Track from '../Track'
 import SafeTokenIcon from '@/public/images/common/safe-token.svg'
 import SafePassStar from '@/public/images/common/safe-pass-star.svg'
 import css from './styles.module.css'
-import classnames from 'classnames'
-import { useGetOwnGlobalCampaignRankQuery } from '@/store/safePass'
 import useSafeAddress from '@/hooks/useSafeAddress'
 import { skipToken } from '@reduxjs/toolkit/query/react'
-import { formatAmount } from '@/utils/formatNumber'
 import { useDarkMode } from '@/hooks/useDarkMode'
+import { useGetOwnGlobalCampaignRankQuery } from '@/store/safePass'
 
 const TOKEN_DECIMALS = 18
 
@@ -31,16 +29,6 @@ export const getSafeTokenAddress = (chainId: string): string | undefined => {
 
 export const getSafeLockingAddress = (chainId: string): string | undefined => {
   return SAFE_LOCKING_ADDRESS[chainId]
-}
-
-const canRedeemSep5Airdrop = (allocation?: Vesting[]): boolean => {
-  const sep5Allocation = allocation?.find(({ tag }) => tag === 'user_v2')
-
-  if (!sep5Allocation) {
-    return false
-  }
-
-  return !sep5Allocation.isRedeemed && !sep5Allocation.isExpired
 }
 
 const GOVERNANCE_APP_URL = IS_PRODUCTION ? 'https://community.safe.global' : 'https://safe-dao-governance.dev.5afe.dev'
@@ -69,7 +57,6 @@ const SafeTokenWidget = () => {
     query: { safe: query?.get('safe'), appUrl: GOVERNANCE_APP_URL },
   }
 
-  const canRedeemSep5 = canRedeemSep5Airdrop(allocationData)
   const flooredSafeBalance = formatVisualAmount(allocation || BigInt(0), TOKEN_DECIMALS, 2)
 
   return (
@@ -78,11 +65,7 @@ const SafeTokenWidget = () => {
         <span>
           <Track {...OVERVIEW_EVENTS.SAFE_TOKEN_WIDGET}>
             <Link href={url} passHref legacyBehavior>
-              <ButtonBase
-                aria-describedby="safe-token-widget"
-                className={classnames(css.tokenButton, { [css.sep5]: canRedeemSep5 })}
-                disabled={url === undefined}
-              >
+              <ButtonBase aria-describedby="safe-token-widget" className={css.tokenButton} disabled={url === undefined}>
                 <SafeTokenIcon width={24} height={24} />
                 <Typography
                   component="div"
@@ -116,7 +99,7 @@ const SafeTokenWidget = () => {
                   {ownGlobalRankLoading ? (
                     <Skeleton width="16px" animation="wave" />
                   ) : (
-                    formatAmount(ownGlobalRank?.totalBoostedPoints ?? 0, 0)
+                    ownGlobalRank?.totalBoostedPoints ?? 0
                   )}
                 </Typography>
               </ButtonBase>
