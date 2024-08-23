2389668ec525854f558d18207b8f29abd01b44ef
katspaugh <katspaugh@gmail.com>
2024-08-23 14:37:23 +0200

    Format values

--- src/features/stake/components/StakingOrderConfirmationView/index.tsx
+++ src/features/stake/components/StakingOrderConfirmationView/index.tsx
@@ -1,20 +1,37 @@
 import FieldsGrid from '@/components/tx/FieldsGrid'
+import { Typography } from '@mui/material'
 import type { NativeStakingDepositConfirmationView } from '@safe-global/safe-gateway-typescript-sdk'
+import { formatDuration, intervalToDuration } from 'date-fns'
 
 type StakingOrderConfirmationViewProps = {
   order: NativeStakingDepositConfirmationView
   contractAddress: string
 }
 
+const formatSeconds = (seconds: number) => {
+  const duration = intervalToDuration({ start: 0, end: seconds * 1000 })
+  return formatDuration(duration, {
+    format: ['hours', 'minutes'],
+  })
+}
+
 const StakingOrderConfirmationView = ({ order }: StakingOrderConfirmationViewProps) => {
+  /* https://docs.api.kiln.fi/reference/getethnetworkstats */
   return (
     <>
-      <FieldsGrid title="Estimated entry time">{order.estimatedEntryTime}</FieldsGrid>
-      <FieldsGrid title="Estimated exit time">{order.estimatedExitTime}</FieldsGrid>
-      <FieldsGrid title="Estimated withdrawal time">{order.estimatedWithdrawalTime}</FieldsGrid>
-      <FieldsGrid title="Fee">{order.fee}</FieldsGrid>
-      <FieldsGrid title="Monthly NRR">{order.monthlyNrr}</FieldsGrid>
-      <FieldsGrid title="Annual NRR">{order.annualNrr}</FieldsGrid>
+      <FieldsGrid title="Time to active *">{formatSeconds(order.estimatedEntryTime)}</FieldsGrid>
+      <FieldsGrid title="Time to exit">{formatSeconds(order.estimatedExitTime)}</FieldsGrid>
+      <FieldsGrid title="Time to withdraw">{formatSeconds(order.estimatedWithdrawalTime)}</FieldsGrid>
+      <FieldsGrid title="Fee">{order.fee}%</FieldsGrid>
+      <FieldsGrid title="Annual NRR">{order.annualNrr.toFixed(3)}%</FieldsGrid>
+
+      <Typography variant="body2" color="text.secondary" display="flex">
+        <span>*&nbsp;</span>
+        <span>
+          Estimated time to active is the time it takes for the staking order to be activated and start earning rewards
+          after executing this transaction.
+        </span>
+      </Typography>
     </>
   )
 }
