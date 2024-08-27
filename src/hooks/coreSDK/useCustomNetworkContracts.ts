f8426613a9574d69ee1055481a7b6d58fd58ae0e
James Mealy <james@safe.global>
2024-08-27 17:25:56 +0200

    fix: uncomment unit test abd remove isContractNetworkConfig as a function

--- src/hooks/coreSDK/useCustomNetworkContracts.ts
+++ src/hooks/coreSDK/useCustomNetworkContracts.ts
@@ -1,26 +1,27 @@
-import type { ChainInfo } from '@safe-global/safe-gateway-typescript-sdk'
 import { useCurrentChain } from '../useChains'
 import type { ContractNetworkConfig, ContractNetworksConfig } from '@safe-global/protocol-kit'
 import { IS_OFFICIAL_HOST, IS_PRODUCTION } from '@/config/constants'
-
-const isContractNetworkConfig = (obj: Omit<ChainInfo['contractAddresses'], 'safeWebAuthnSignerFactoryAddress'>) => {
-  return Object.values(obj).every((value) => !!value)
-}
 
 /**
  * Get deployed safe contract addresses from the config service.
  * To be used when the addresses are not available from safe-deployments
  */
-export const useCustomNetworkContracts = () => {
-  const currentChain = useCurrentChain()
-  if (!currentChain || (IS_PRODUCTION && IS_OFFICIAL_HOST)) return
 
-  // Exclude safeWebAuthnSignerFactoryAddress as it is not yet supported.
-  const { safeWebAuthnSignerFactoryAddress, ...contractAddresses } = currentChain.contractAddresses
+export const useCustomNetworkContracts =
+  IS_PRODUCTION && IS_OFFICIAL_HOST
+    ? () => {
+        const currentChain = useCurrentChain()
+        if (!currentChain) return
 
-  if (!isContractNetworkConfig(contractAddresses)) return
-  return contractAddresses as ContractNetworkConfig
-}
+        // Exclude safeWebAuthnSignerFactoryAddress as it is not yet supported.
+        const { safeWebAuthnSignerFactoryAddress, ...contractAddresses } = currentChain.contractAddresses
+
+        const customContractsDefined = Object.values(contractAddresses).every((value) => !!value)
+        if (!customContractsDefined) return
+
+        return contractAddresses as ContractNetworkConfig
+      }
+    : () => undefined
 
 export const useCustomNetworksContracts = () => {
   const customNetworkContracts = useCustomNetworkContracts()
