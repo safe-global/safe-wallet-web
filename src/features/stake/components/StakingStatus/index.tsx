9b864c67196ea48a83a0b643a89026794c75d0e4
katspaugh <katspaugh@gmail.com>
2024-08-30 16:22:57 +0200

    Capitalize status labels

--- src/features/stake/components/StakingStatus/index.tsx
+++ src/features/stake/components/StakingStatus/index.tsx
@@ -10,39 +10,51 @@ const ColorIcons: Record<
   | {
       color: TxStatusChipProps['color']
       icon?: React.ComponentType
+      text: string
     }
   | undefined
 > = {
   [NativeStakingStatus.AWAITING_ENTRY]: {
     color: 'info',
     icon: TimelapseIcon,
+    text: 'Activating',
   },
   [NativeStakingStatus.REQUESTED_EXIT]: {
     color: 'info',
     icon: TimelapseIcon,
+    text: 'Requested exit',
   },
   [NativeStakingStatus.SIGNATURE_NEEDED]: {
     color: 'warning',
     icon: SignatureIcon,
+    text: 'Signature needed',
   },
   [NativeStakingStatus.AWAITING_EXECUTION]: {
     color: 'warning',
     icon: TimelapseIcon,
+    text: 'Awaiting execution',
   },
   [NativeStakingStatus.VALIDATION_STARTED]: {
     color: 'success',
     icon: CheckIcon,
+    text: 'Validating',
   },
   [NativeStakingStatus.WITHDRAWN]: {
     color: 'success',
     icon: CheckIcon,
+    text: 'Withdrawn',
   },
   [NativeStakingStatus.UNKNOWN]: undefined,
 }
 
+const capitalizedStatus = (status: string) =>
+  status
+    .toLowerCase()
+    .replace(/_/g, ' ')
+    .replace(/^\w/g, (l) => l.toUpperCase())
+
 const StakingStatus = ({ status }: { status: NativeStakingStatus }) => {
   const config = ColorIcons[status]
-  const text = status.toLowerCase().replace(/_/g, ' ')
 
   return (
     <TxStatusChip color={config?.color}>
@@ -53,7 +65,7 @@ const StakingStatus = ({ status }: { status: NativeStakingStatus }) => {
           viewBox={config.icon === SignatureIcon ? '0 0 16 16' : undefined}
         />
       )}
-      {text}
+      {config?.text || capitalizedStatus(status)}
     </TxStatusChip>
   )
 }
