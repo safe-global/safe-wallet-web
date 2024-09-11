1fb379cdaef7218dc353c791124ed535ebd3468c
James Mealy <james@safe.global>
2024-09-11 13:37:47 +0200

    feat: use current chain name in warning message

--- src/features/multichain/components/ChangeOwnerSetupWarning/ChangeOwnerSetupWarning.tsx
+++ src/features/multichain/components/ChangeOwnerSetupWarning/ChangeOwnerSetupWarning.tsx
@@ -1,17 +1,18 @@
 import { Box } from '@mui/material'
 import { useIsMultichainSafe } from '../../hooks/useIsMultichainSafe'
 import ErrorMessage from '@/components/tx/ErrorMessage'
+import { useCurrentChain } from '@/hooks/useChains'
 
 export const ChangeSignerSetupWarning = () => {
   const isMultichainSafe = useIsMultichainSafe()
+  const currentChain = useCurrentChain()
 
   if (!isMultichainSafe) return
 
   return (
     <Box mt={1} mb={1}>
       <ErrorMessage level="warning">
-        Signers are not consistent across networks on this account. Changing signers will only affect the account on the
-        selected network.
+        {`Signers are not consistent across networks on this account. Changing signers will only affect the account on ${currentChain}`}
       </ErrorMessage>
     </Box>
   )
