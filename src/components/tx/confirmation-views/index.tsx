25cd39e0d88f27da7de5e203c1d5dd1066fdece4
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-09-11 15:41:15 +0200

    chore: refactor the confirmation view compponent

--- src/components/tx/confirmation-views/index.tsx
+++ src/components/tx/confirmation-views/index.tsx
@@ -1,29 +1,37 @@
-import { isSettingsChangeTxInfo } from '@/utils/transaction-guards'
-import SettingsChange from './SettingsChange'
-import { TransactionInfoType, type TransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
+import { type TransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
 import DecodedTx from '../DecodedTx'
 import ConfirmationOrder from '../ConfirmationOrder'
 import useDecodeTx from '@/hooks/useDecodeTx'
 import type { SafeTransaction } from '@safe-global/safe-core-sdk-types'
+import { isCustomTxInfo, isGenericConfirmation } from '@/utils/transaction-guards'
+import { getConfirmationViewComponent } from './utils'
+import { useMemo } from 'react'
 
 type ConfirmationViewProps = {
   txDetails: TransactionDetails
   safeTx?: SafeTransaction
-}
-
-const NarrowViews: Record<any, any> = {
-  [TransactionInfoType.SETTINGS_CHANGE]: SettingsChange,
+  txId?: string
+  isBatch?: boolean
+  isApproval?: boolean
 }
 
 const ConfirmationView = (props: ConfirmationViewProps) => {
   const { txInfo } = props.txDetails
   const [decodedData] = useDecodeTx(props.safeTx)
 
-  const Component = NarrowViews[txInfo.type]
+  const ConfirmationViewComponent = useMemo(
+    () =>
+      getConfirmationViewComponent(txInfo.type, {
+        txDetails: props.txDetails,
+        txInfo,
+      }),
+    [props.txDetails, txInfo],
+  )
+  const showTxDetails = props.txId && props.txDetails && !isCustomTxInfo(props.txDetails.txInfo)
 
   return (
     <>
-      {!!Component && <Component txDetails={props.txDetails} txInfo={txInfo} />}
+      {ConfirmationViewComponent}
 
       {decodedData && <ConfirmationOrder decodedData={decodedData} toAddress={props.safeTx?.data.to ?? ''} />}
 
@@ -31,8 +39,10 @@ const ConfirmationView = (props: ConfirmationViewProps) => {
         tx={props.safeTx}
         txDetails={props.txDetails}
         decodedData={decodedData}
-        showMultisend
-        showMethodCall={!Component}
+        showMultisend={!props.isBatch}
+        showMethodCall={
+          !ConfirmationViewComponent && !showTxDetails && !props.isApproval && isGenericConfirmation(decodedData)
+        }
       />
     </>
   )
