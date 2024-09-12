10bea21b1603b01de0159c8158318dbd3ab1f5e5
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-09-12 17:35:23 +0200

    feat: create storybook for SettingsChange component

--- src/components/tx/confirmation-views/SettingsChange/SettingsChange.test.tsx
+++ src/components/tx/confirmation-views/SettingsChange/SettingsChange.test.tsx
@@ -1,36 +1,7 @@
 import { render } from '@/tests/test-utils'
-import { SettingsInfoType, TransactionDetails, TransactionInfoType } from '@safe-global/safe-gateway-typescript-sdk'
+import { SettingsInfoType, TransactionDetails } from '@safe-global/safe-gateway-typescript-sdk'
 import SettingsChange from './index'
-
-const ownerAddress = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
-const txInfo: TransactionDetails['txInfo'] = {
-  type: TransactionInfoType.SETTINGS_CHANGE,
-  humanDescription: 'Add new owner 0xd8dA...6045 with threshold 1',
-  dataDecoded: {
-    method: 'addOwnerWithThreshold',
-    parameters: [
-      {
-        name: 'owner',
-        type: 'address',
-        value: ownerAddress,
-      },
-      {
-        name: '_threshold',
-        type: 'uint256',
-        value: '1',
-      },
-    ],
-  },
-  settingsInfo: {
-    type: SettingsInfoType.ADD_OWNER,
-    owner: {
-      value: ownerAddress,
-      name: 'Nevinha',
-      logoUri: 'http://something.com',
-    },
-    threshold: 1,
-  },
-}
+import { ownerAddress, txInfo } from './utils'
 
 describe('SettingsChange', () => {
   it('should display the SettingsChange component with owner details', () => {
