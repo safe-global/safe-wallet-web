73694b41164689387b1bb8274f9a00a3cdd72fc1
Daniel Dimitrov <daniel@compojoom.com>
2024-09-17 11:12:20 +0200

    feat: add stake button on assets page
    fix: add area-label to stake button

--- src/features/stake/components/StakingWidget/index.tsx
+++ src/features/stake/components/StakingWidget/index.tsx
@@ -1,38 +1,19 @@
 import { useMemo } from 'react'
-import { useDarkMode } from '@/hooks/useDarkMode'
 import AppFrame from '@/components/safe-apps/AppFrame'
 import { getEmptySafeApp } from '@/components/safe-apps/utils'
-import useChainId from '@/hooks/useChainId'
-import useChains from '@/hooks/useChains'
+import { useGetStakeWidgetUrl } from '@/features/stake/hooks/useGetStakeWidgetUrl'
+import { widgetAppData } from '@/features/stake/constants'
 
-const WIDGET_PRODUCTION_URL = 'https://safe.widget.kiln.fi/earn'
-const WIDGET_TESTNET_URL = 'https://safe.widget.testnet.kiln.fi/earn'
-const widgetAppData = {
-  url: WIDGET_PRODUCTION_URL,
-  name: 'Stake',
-  iconUrl: '/images/common/stake.svg',
-  chainIds: ['17000', '11155111', '1', '42161', '137', '56', '8453', '10'],
-}
-
-const StakingWidget = () => {
-  const isDarkMode = useDarkMode()
-  let url = widgetAppData.url
-  const currentChainId = useChainId()
-  const { configs } = useChains()
-  const testChains = useMemo(() => configs.filter((chain) => chain.isTestnet), [configs])
-
-  // if currentChainId is in testChains, then set the url to the testnet version
-  if (testChains.some((chain) => chain.chainId === currentChainId)) {
-    url = WIDGET_TESTNET_URL
-  }
+const StakingWidget = ({ asset }: { asset?: string }) => {
+  const url = useGetStakeWidgetUrl(asset)
 
   const appData = useMemo(
     () => ({
       ...getEmptySafeApp(),
       ...widgetAppData,
-      url: url + `?theme=${isDarkMode ? 'dark' : 'light'}`,
+      url,
     }),
-    [isDarkMode, url],
+    [url],
   )
 
   return (
