1f48e847d0a25baaae2233d119b467586ad656ab
Daniel Dimitrov <daniel@compojoom.com>
2024-09-18 08:55:52 +0200

    feat: add title and link to staking
    .

--- src/features/stake/components/StakeDashboardWidget/index.tsx
+++ src/features/stake/components/StakeDashboardWidget/index.tsx
@@ -3,9 +3,22 @@ import AppFrame from '@/components/safe-apps/AppFrame'
 import { getEmptySafeApp } from '@/components/safe-apps/utils'
 import { useGetStakeWidgetUrl } from '@/features/stake/hooks/useGetStakeWidgetUrl'
 import { widgetAppData } from '@/features/stake/constants'
+import css from '@/components/dashboard/PendingTxs/styles.module.css'
+import { Typography } from '@mui/material'
+import { ViewAllLink } from '@/components/dashboard/styled'
+import { AppRoutes } from '@/config/routes'
+import { useRouter } from 'next/router'
 
 const StakingDashboardWidget = () => {
   const url = useGetStakeWidgetUrl('', 'overview') + '&interactive=false&navigation=none&widget_width=full'
+  const router = useRouter()
+  const stakeUrl = useMemo(
+    () => ({
+      pathname: AppRoutes.stake,
+      query: { safe: router.query.safe },
+    }),
+    [router.query.safe],
+  )
 
   const appData = useMemo(
     () => ({
@@ -17,12 +30,21 @@ const StakingDashboardWidget = () => {
   )
 
   return (
-    <AppFrame
-      appUrl={appData.url}
-      allowedFeaturesList="clipboard-read; clipboard-write"
-      safeAppFromManifest={appData}
-      isNativeEmbed
-    />
+    <>
+      <div className={css.title}>
+        <Typography component="h2" variant="subtitle1" fontWeight={700} mb={2}>
+          Stake
+        </Typography>
+
+        <ViewAllLink url={stakeUrl} text="Go to staking" />
+      </div>
+      <AppFrame
+        appUrl={appData.url}
+        allowedFeaturesList="clipboard-read; clipboard-write"
+        safeAppFromManifest={appData}
+        isNativeEmbed
+      />
+    </>
   )
 }
 
