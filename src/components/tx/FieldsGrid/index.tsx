d769925870476ffb71f799e845a129da000cea31
Daniel Dimitrov <daniel.d@safe.global>
2024-09-27 10:21:41 +0200

    Fix(staking): display stake statuses & exit & claim "values"  (#4249)
    * fix: wrong stake statuses
    
    * fix: small UI discrepancies with the figma designs
    
    * fix: change to format duration to milliseconds
    
    * fix: incorrect status displayed
    
    * refactor: validator exit
    
    * chore: update @safe-global/safe-gateway-typescript-sdk

--- src/components/tx/FieldsGrid/index.tsx
+++ src/components/tx/FieldsGrid/index.tsx
@@ -1,19 +1,19 @@
 import { type ReactNode } from 'react'
 import { Grid, Typography } from '@mui/material'
 
-const minWidth = { xl: '25%', lg: '100px' }
+const minWidth = { xl: '25%', lg: '200px' }
 const wrap = { flexWrap: { xl: 'nowrap' } }
 
 const FieldsGrid = ({ title, children }: { title: string | ReactNode; children: ReactNode }) => {
   return (
     <Grid container alignItems="center" gap={1} sx={wrap}>
-      <Grid item minWidth={minWidth}>
+      <Grid item minWidth={minWidth} data-testid="tx-row-title">
         <Typography color="primary.light" noWrap>
           {title}
         </Typography>
       </Grid>
 
-      <Grid item xs>
+      <Grid item xs data-testid="tx-data-row">
         {children}
       </Grid>
     </Grid>
