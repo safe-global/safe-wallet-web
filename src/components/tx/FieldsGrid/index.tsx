44ff39147b1aea27712cf9509b0381fed69bd548
Daniel Dimitrov <daniel@compojoom.com>
2024-09-30 11:29:03 +0200

    fix: too wide label

--- src/components/tx/FieldsGrid/index.tsx
+++ src/components/tx/FieldsGrid/index.tsx
@@ -1,16 +1,15 @@
 import { type ReactNode } from 'react'
 import { Grid, Typography } from '@mui/material'
 
+const width = { xl: '25%', lg: '200px', xs: 'auto' }
 const minWidth = { xl: '25%', lg: '200px' }
 const wrap = { flexWrap: { xl: 'nowrap' } }
 
 const FieldsGrid = ({ title, children }: { title: string | ReactNode; children: ReactNode }) => {
   return (
     <Grid container alignItems="center" gap={1} sx={wrap}>
-      <Grid item minWidth={minWidth} data-testid="tx-row-title">
-        <Typography color="primary.light" noWrap>
-          {title}
-        </Typography>
+      <Grid item data-testid="tx-row-title" width={width} minWidth={minWidth} style={{ wordBreak: 'break-word' }}>
+        <Typography color="primary.light">{title}</Typography>
       </Grid>
 
       <Grid item xs data-testid="tx-data-row">
