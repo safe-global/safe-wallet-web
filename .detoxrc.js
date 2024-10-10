42c2e6284310775ca858cc9f29127d930955af04
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-10-10 12:09:05 +0200

    feat(detox-android): Setup detox for android tests

--- .detoxrc.js
+++ .detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86'
+        avdName: 'Pixel_8_API_35'
       }
     }
   },
