64f5b1f548dfb75efbedf9bfd1cd87a4d714b994
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-10-11 12:23:42 +0200

    feat(setup-emotion): add emotion into the project for custom styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import ParallaxScrollView from "@/src/components/ParallaxScrollView";
+import { Button, Text } from "react-native-paper";
+import styled from "@emotion/native";
+
+const StyledText = styled(Text)`
+  color: ${({ theme }) => theme.colors?.primary};
+`;
 
 export default function HomeScreen() {
   return (
@@ -16,42 +19,15 @@ export default function HomeScreen() {
         />
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText testID="welcome-title" type="title">
+      <View style={styles.titleContainer}>
+        <Text variant="titleLarge" testID="welcome-title">
           Welcome!
-        </ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+        </Text>
+
+        <Button icon="camera">Press me</Button>
+      </View>
+
+      <StyledText>Here is my styled component</StyledText>
     </ParallaxScrollView>
   );
 }
