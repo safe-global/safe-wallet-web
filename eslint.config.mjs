10a26fd63b6612a4020776b839051b0ca7b58b35
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-10-11 16:23:20 +0200

    feat(prettier): setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,  
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
   {
     settings: {
-    react: {
-        version: "detect", // Automatically detect the react version
+      react: {
+        version: 'detect', // Automatically detect the react version
+      },
     },
-  },
 
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/no-unescaped-entities": "off",
-      "@typescript-eslint/no-require-imports": "off"
-    }
-  }
-];
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+]
