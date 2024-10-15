b6b24897fcdd73b72b80ba52fea00160c66b2bb3
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-10-15 10:07:59 +0200

    feat(setup-redux): add redux and other necessary dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,8 @@
-import { StorybookConfig } from "@storybook/react-native";
+import { StorybookConfig } from '@storybook/react-native'
 
 const main: StorybookConfig = {
-  stories: ["../components/**/*.stories.?(ts|tsx|js|jsx)"],
-  addons: [
-    "@storybook/addon-ondevice-controls",
-    "@storybook/addon-ondevice-actions",
-  ],
-};
+  stories: ['../src/components/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+}
 
-export default main;
+export default main
