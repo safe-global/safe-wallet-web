88708f20f0a71de5c86dc9cbef303be94d1ff1fd
James Mealy <james@safe.global>
2024-10-15 09:28:46 +0200

    fix: mock cookie banner state in unit tests

--- src/features/targetedOutreach/hooks/__tests__/useShowOutreachPopup.test.ts
+++ src/features/targetedOutreach/hooks/__tests__/useShowOutreachPopup.test.ts
@@ -2,23 +2,27 @@ import { renderHook } from '@testing-library/react'
 import { useShowOutreachPopup } from '../useShowOutreachPopup'
 import * as useIsSafeOwner from '@/hooks/useIsSafeOwner'
 import * as useSafeAddress from '@/hooks/useSafeAddress'
+import * as store from '@/store'
 import { faker } from '@faker-js/faker'
 
 jest.mock('@/hooks/useIsSafeOwner')
 jest.mock('@/hooks/useSafeAddress')
+jest.mock('@/store')
 
 describe('useShowOutreachPopup', () => {
-  it('should return false if the safe is not on the targeted list', async () => {
-    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
-    jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce(faker.finance.ethereumAddress())
+  it('should return false if the connected wallet is not a signer of a targeted safe', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(false)
+    jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const { result } = renderHook(() => useShowOutreachPopup({}))
     expect(result.current).toEqual(false)
   })
 
-  it('should return false if the connected wallet is not a signer of a targeted safe', async () => {
-    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(false)
+  it('should return false when the cookie banner is open', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: true })
 
     const { result } = renderHook(() => useShowOutreachPopup({}))
     expect(result.current).toEqual(false)
@@ -27,6 +31,7 @@ describe('useShowOutreachPopup', () => {
   it('should return true for signers of targeted safes', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const { result } = renderHook(() => useShowOutreachPopup({}))
     expect(result.current).toEqual(true)
@@ -35,6 +40,7 @@ describe('useShowOutreachPopup', () => {
   it('should return false if a targeted user has previously closed the popup', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const outreachPopupLocalStorage = {
       isClosed: true,
@@ -49,6 +55,7 @@ describe('useShowOutreachPopup', () => {
   it('should return false if the user has chosen ask me later and the minimum time threshold has not elapsed', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const now = Date.now()
     const firstTimestamp = Date.now() - 24 * 60 * 60 * 1000 // 1 day
@@ -69,6 +76,7 @@ describe('useShowOutreachPopup', () => {
   it('should return true if the user has chosen ask me later and the maximum time has elapsed', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const now = Date.now()
     const firstTimestamp = Date.now() - 11 * 24 * 60 * 60 * 1000 // 11 days
@@ -89,6 +97,7 @@ describe('useShowOutreachPopup', () => {
   it('should return true if a frequent user has chosen ask me later and is between the min and max delays', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const now = Date.now()
     const firstTimestamp = Date.now() - 3 * 24 * 60 * 60 * 1000 // 3 days
@@ -109,6 +118,7 @@ describe('useShowOutreachPopup', () => {
   it('should return false if a non frequent user has chosen ask me later and is between the min and max delays', async () => {
     jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
     jest.spyOn(useSafeAddress, 'default').mockReturnValueOnce('0x5Cd167cd2D246B19834726904FA3247362182f6F')
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
 
     const now = Date.now()
     const firstTimestamp = Date.now() - 3 * 24 * 60 * 60 * 1000 // 3 days
