af1e917929af663538fdac9c11a0796979adab9b
James Mealy <james@safe.global>
2024-10-16 17:21:07 +0200

    feat: use light theme also in dark mode

--- src/features/targetedOutreach/components/OutreachPopup/index.tsx
+++ src/features/targetedOutreach/components/OutreachPopup/index.tsx
@@ -1,19 +1,17 @@
 import { useEffect, type ReactElement } from 'react'
-import classnames from 'classnames'
-import { Avatar, Box, Button, Chip, IconButton, Link, Paper, Stack, Typography } from '@mui/material'
+import { Avatar, Box, Button, Chip, IconButton, Link, Paper, Stack, ThemeProvider, Typography } from '@mui/material'
 import { Close } from '@mui/icons-material'
-
+import type { Theme } from '@mui/material/styles'
 import { useAppDispatch, useAppSelector } from '@/store'
-
 import css from './styles.module.css'
 import { closeOutreachBanner, openOutreachBanner, selectOutreachBanner } from '@/store/popupSlice'
 import useLocalStorage from '@/services/local-storage/useLocalStorage'
 import { useShowOutreachPopup } from '@/features/targetedOutreach/hooks/useShowOutreachPopup'
 import { getUpdatedUserActivity } from '@/features/targetedOutreach/utils/getUpdatedUserActivity'
 import { ACTIVE_OUTREACH, OUTREACH_LS_KEY } from '@/features/targetedOutreach/constants'
-import { useDarkMode } from '@/hooks/useDarkMode'
 import Track from '@/components/common/Track'
 import { OUTREACH_EVENTS } from '@/services/analytics/events/outreach'
+import SafeThemeProvider from '@/components/theme/SafeThemeProvider'
 
 export type OutreachPopupState = {
   isClosed?: boolean
@@ -26,7 +24,6 @@ const OutreachPopup = (): ReactElement | null => {
   const outreachPopup = useAppSelector(selectOutreachBanner)
   const [outreachPopupState, setOutreachPopupState] = useLocalStorage<OutreachPopupState>(OUTREACH_LS_KEY)
   const shouldOpen = useShowOutreachPopup(outreachPopupState)
-  const isDarkMode = useDarkMode()
 
   const handleClose = () => {
     setOutreachPopupState({ isClosed: true, askAgainLater: false })
@@ -61,59 +58,65 @@ const OutreachPopup = (): ReactElement | null => {
   if (!outreachPopup.open) return null
 
   return (
-    <Box className={css.popup}>
-      <Paper className={classnames(css.container, { [css.gradient]: !isDarkMode })}>
-        <Stack gap={2}>
-          <Box display="flex">
-            <Avatar alt="Clem Bihorel" src="/images/common/outreach-popup-avatar.png" />
-            <Box ml={1}>
-              <Typography variant="body2">Clem Bihorel</Typography>
-              <Typography variant="body2" color="primary.light">
-                Product Lead
-              </Typography>
-            </Box>
-          </Box>
-          <Box>
-            <Chip
-              size="small"
-              sx={{ backgroundColor: 'text.primary', color: 'background.paper', mt: '-2px' }}
-              label={
-                <Typography fontWeight={700} variant="overline">
-                  EARN REWARDS
+    <SafeThemeProvider mode="light">
+      {(safeTheme: Theme) => (
+        <ThemeProvider theme={safeTheme}>
+          <Box className={css.popup}>
+            <Paper className={css.container}>
+              <Stack gap={2}>
+                <Box display="flex">
+                  <Avatar alt="Clem Bihorel" src="/images/common/outreach-popup-avatar.png" />
+                  <Box ml={1}>
+                    <Typography variant="body2">Clem Bihorel</Typography>
+                    <Typography variant="body2" color="primary.light">
+                      Product Lead
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <Chip
+                    size="small"
+                    sx={{ backgroundColor: 'text.primary', color: 'background.paper', mt: '-2px' }}
+                    label={
+                      <Typography fontWeight={700} variant="overline">
+                        EARN REWARDS
+                      </Typography>
+                    }
+                  />
+                </Box>
+                <Typography variant="h4" fontWeight={700}>
+                  You&apos;re invited!
                 </Typography>
-              }
-            />
+                <Typography>
+                  As one of our top users, we&apos;d love to hear your feedback on how we can enhance Safe. Share your
+                  contact info, and we&apos;ll reach out for a short interview.
+                </Typography>
+                <Track {...OUTREACH_EVENTS.OPEN_SURVEY}>
+                  <Link rel="noreferrer noopener" target="_blank" href={ACTIVE_OUTREACH.url}>
+                    <Button fullWidth variant="contained">
+                      Get Involved
+                    </Button>
+                  </Link>
+                </Track>
+                <Track {...OUTREACH_EVENTS.ASK_AGAIN_LATER}>
+                  <Button fullWidth variant="text" onClick={handleAskAgainLater}>
+                    Ask me later
+                  </Button>
+                </Track>
+                <Typography variant="body2" color="primary.light" mx="auto">
+                  It&apos;ll only take 2 minutes.
+                </Typography>
+                <Track {...OUTREACH_EVENTS.CLOSE_POPUP}>
+                  <IconButton className={css.close} aria-label="close" onClick={handleClose}>
+                    <Close />
+                  </IconButton>
+                </Track>
+              </Stack>
+            </Paper>
           </Box>
-          <Typography variant="h4" fontWeight={700}>
-            You&apos;re invited!
-          </Typography>
-          <Typography>
-            As one of our top users, we&apos;d love to hear your feedback on how we can enhance Safe. Share your contact
-            info, and we&apos;ll reach out for a short interview.
-          </Typography>
-          <Track {...OUTREACH_EVENTS.OPEN_SURVEY}>
-            <Link rel="noreferrer noopener" target="_blank" href={ACTIVE_OUTREACH.url}>
-              <Button fullWidth variant="contained">
-                Get Involved
-              </Button>
-            </Link>
-          </Track>
-          <Track {...OUTREACH_EVENTS.ASK_AGAIN_LATER}>
-            <Button fullWidth variant="text" onClick={handleAskAgainLater}>
-              Ask me later
-            </Button>
-          </Track>
-          <Typography variant="body2" color="primary.light" mx="auto">
-            It&apos;ll only take 2 minutes.
-          </Typography>
-          <Track {...OUTREACH_EVENTS.CLOSE_POPUP}>
-            <IconButton className={css.close} aria-label="close" onClick={handleClose}>
-              <Close />
-            </IconButton>
-          </Track>
-        </Stack>
-      </Paper>
-    </Box>
+        </ThemeProvider>
+      )}
+    </SafeThemeProvider>
   )
 }
 export default OutreachPopup
