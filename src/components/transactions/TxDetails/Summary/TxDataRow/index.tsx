9e4b49fa51abf1c8ef1489f66cb3c66ad66f9e64
schmanu <manu@safe.global>
2024-10-18 17:06:20 +0200

    fix: shorten hashes

--- src/components/transactions/TxDetails/Summary/TxDataRow/index.tsx
+++ src/components/transactions/TxDetails/Summary/TxDataRow/index.tsx
@@ -29,7 +29,6 @@ export const generateDataRowValue = (
           customAvatar={customAvatar}
           showAvatar={!!customAvatar}
           hasExplorer={hasExplorer}
-          shortAddress={type === 'address'}
           showCopyButton
         />
       )
