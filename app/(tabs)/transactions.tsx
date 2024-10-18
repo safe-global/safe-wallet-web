1d29176a228e9f780f457a11faf14de0b1bb14ef
Daniel Dimitrov <daniel@compojoom.com>
2024-10-18 16:27:45 +0200

    feature: add tabs with proper Icon for navigation

--- app/(tabs)/transactions.tsx
+++ app/(tabs)/transactions.tsx
@@ -4,14 +4,14 @@ import { StyleSheet, View } from 'react-native'
 import ParallaxScrollView from '@/src/components/ParallaxScrollView'
 import { Text } from 'react-native-paper'
 
-export default function TabTwoScreen() {
+export default function TransactionsScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}
     >
       <View style={styles.titleContainer}>
-        <Text testID="explore-title">Explore</Text>
+        <Text testID="explore-title">Transactions</Text>
       </View>
     </ParallaxScrollView>
   )
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
         <Text variant="titleLarge">This screen doesn't exist.</Text>
-        <Link href="/" style={styles.link}>
+        <Link href="/home" style={styles.link}>
           <Text variant="titleLarge">Go to home screen!</Text>
         </Link>
       </View>
