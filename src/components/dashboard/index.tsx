8dc98de7dd0d0a30e238bf1d384c2ed57ee29b5f
Usame Algan <usame@safe.global>
2024-10-23 19:54:54 +0200

    fix: Remove swap and SAP banner

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -9,13 +9,9 @@ import Overview from '@/components/dashboard/Overview/Overview'
 import SafeAppsDashboardSection from '@/components/dashboard/SafeAppsDashboardSection/SafeAppsDashboardSection'
 import GovernanceSection from '@/components/dashboard/GovernanceSection/GovernanceSection'
 import { useIsRecoverySupported } from '@/features/recovery/hooks/useIsRecoverySupported'
-import ActivityRewardsSection from '@/components/dashboard/ActivityRewardsSection'
 import { useHasFeature } from '@/hooks/useChains'
 import { FEATURES } from '@/utils/chains'
 import css from './styles.module.css'
-import SwapWidget from '@/features/swap/components/SwapWidget'
-import useIsSwapFeatureEnabled from '@/features/swap/hooks/useIsSwapFeatureEnabled'
-import { useSafeTokenEnabled } from '@/hooks/useSafeTokenEnabled'
 import { InconsistentSignerSetupWarning } from '@/features/multichain/components/SignerSetupWarning/InconsistentSignerSetupWarning'
 
 const RecoveryHeader = dynamic(() => import('@/features/recovery/components/RecoveryHeader'))
@@ -23,9 +19,6 @@ const RecoveryHeader = dynamic(() => import('@/features/recovery/components/Reco
 const Dashboard = (): ReactElement => {
   const { safe } = useSafeInfo()
   const showSafeApps = useHasFeature(FEATURES.SAFE_APPS)
-  const isSafeTokenEnabled = useSafeTokenEnabled()
-  const isSwapFeatureEnabled = useIsSwapFeatureEnabled()
-  const isSAPBannerEnabled = useHasFeature(FEATURES.SAP_BANNER) && isSafeTokenEnabled
   const supportsRecovery = useIsRecoverySupported()
 
   return (
@@ -47,20 +40,6 @@ const Dashboard = (): ReactElement => {
 
         {safe.deployed && (
           <>
-            {isSwapFeatureEnabled && (
-              <Grid item xs={12} xl={isSAPBannerEnabled ? 6 : 12} className={css.hideIfEmpty}>
-                <SwapWidget />
-              </Grid>
-            )}
-
-            {isSAPBannerEnabled && (
-              <Grid item xs className={css.hideIfEmpty}>
-                <ActivityRewardsSection />
-              </Grid>
-            )}
-
-            <Grid item xs={12} />
-
             <Grid item xs={12} lg={6}>
               <AssetsWidget />
             </Grid>
