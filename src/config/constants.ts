f3c086054ed06e774d17998b146181ede67f235e
Clóvis Neto <clovisdasilvaneto@gmail.com>
2024-10-24 12:59:18 +0200

    chore: setup redux and rtk devtools support into the project

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,5 @@ import { Platform } from 'react-native'
 
 export const isProduction = process.env.NODE_ENV !== 'production'
 export const isAndroid = Platform.OS === 'android'
+export const isTestingEnv = process.env.NODE_ENV === 'test' || process.env.VITEST
 export const isStorybookEnv = Constants?.expoConfig?.extra?.storybookEnabled === 'true'
