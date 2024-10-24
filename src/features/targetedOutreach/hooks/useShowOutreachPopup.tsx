40464c9c8e0314054b3187dfcc2eb93f28241b4a
James Mealy <james@safe.global>
2024-10-24 12:35:56 +0200

    feat: only show banner if the safe is targeted for the current outreach and has not already clicked the CTA

--- src/features/targetedOutreach/hooks/useShowOutreachPopup.tsx
+++ src/features/targetedOutreach/hooks/useShowOutreachPopup.tsx
@@ -1,21 +1,35 @@
 import useIsSafeOwner from '@/hooks/useIsSafeOwner'
 import useSafeAddress from '@/hooks/useSafeAddress'
-import { MAX_ASK_AGAIN_DELAY } from '@/features/targetedOutreach/constants'
+import { ACTIVE_OUTREACH, MAX_ASK_AGAIN_DELAY } from '@/features/targetedOutreach/constants'
 import { useAppSelector } from '@/store'
 import { selectCookieBanner } from '@/store/popupSlice'
-
-const isTargetedSafeAddress = (safeAddress: string): boolean => {
-  // Todo: needs targeted safes list
-  return !!safeAddress
-}
+import { getSubmission } from '@safe-global/safe-client-gateway-sdk'
+import useChainId from '@/hooks/useChainId'
+import useWallet from '@/hooks/wallets/useWallet'
+import useAsync from '@/hooks/useAsync'
 
 export const useShowOutreachPopup = (isDismissed: boolean | undefined, askAgainLaterTimestamp: number | undefined) => {
   const cookiesPopup = useAppSelector(selectCookieBanner)
   const isSigner = useIsSafeOwner()
   const safeAddress = useSafeAddress()
-  const isTargetedSafe = isTargetedSafeAddress(safeAddress)
+  const currentChainId = useChainId()
+  const wallet = useWallet()
 
-  if (cookiesPopup?.open || isDismissed || !isSigner || !isTargetedSafe) return false
+  const [submission] = useAsync(() => {
+    if (wallet) {
+      return getSubmission({
+        params: {
+          path: { outreachId: ACTIVE_OUTREACH.id, chainId: currentChainId, safeAddress, signerAddress: wallet.address },
+        },
+      })
+    }
+  }, [currentChainId, safeAddress, wallet])
+
+  const isTargetedSafe = submission?.targetedSafeId && !submission.completionDate
+
+  if (cookiesPopup?.open || isDismissed || !isSigner || !isTargetedSafe) {
+    return false
+  }
 
   if (askAgainLaterTimestamp) {
     return Date.now() - askAgainLaterTimestamp > MAX_ASK_AGAIN_DELAY
