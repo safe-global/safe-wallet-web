4e130b98d2bfec5411d97ca1f9bb28cee540355d
Usame Algan <usame@safe.global>
2024-10-25 10:37:34 +0200

    fix: Handle update proposer in rtk query

--- src/store/api/gateway/proposers.ts
+++ src/store/api/gateway/proposers.ts
@@ -52,7 +52,17 @@ export const proposerEndpoints = (
     async onQueryStarted({ chainId, safeAddress, delegate, delegator, label }, { dispatch, queryFulfilled }) {
       const patchResult = dispatch(
         gatewayApi.util.updateQueryData('getProposers', { chainId, safeAddress }, (draft) => {
-          draft.results.push({ delegate, delegator, label, safe: safeAddress })
+          const existingProposer = draft.results.findIndex((proposer: Delegate) => proposer.delegate === delegate)
+
+          if (existingProposer !== -1) {
+            // Update the existing delegate's label
+            draft.results[existingProposer] = {
+              ...draft.results[existingProposer],
+              label,
+            }
+          } else {
+            draft.results.push({ delegate, delegator, label, safe: safeAddress })
+          }
         }),
       )
       try {
