e828002465520f65019f2c7a74b5855e1cf626fd
James Mealy <james@safe.global>
2024-10-30 13:56:34 +0100

    Feat: Targeted survey banner popup [SW-269] (#4338)
    * feat: add outreach popup
    
    * feat: add close and ask again later funcitonality
    
    * Feat: finish implementing design of outreach popup.
    
    * refactor: extract getUpdatedUserActivity to utils
    
    * fix: mobile popup styles
    
    * fix: do not show at the same time as cookie banner
    
    * fix: fix dark mode styles
    
    * Chore: add tracking to outreach banner
    
    * fix: mock cookie banner state in unit tests
    
    * tests: add unit tests for utils
    
    * fix: update import
    
    * feat: use light theme also in dark mode
    
    * feat: ask again after 24 hours or after current session
    
    * remove unused utils
    
    * feat: only show banner if the safe is targeted for the current outreach and has not already clicked the CTA
    
    * use faker for ethereum addresses in the tests
    
    * fix: cache getSubmission result to avoid multiple requests
    
    * fix: some non targeted safes showing banner

---
+++ src/features/targetedOutreach/hooks/__tests__/useShowOutreachPopup.test.ts
@@ -0,0 +1,120 @@
+import { renderHook } from '@testing-library/react'
+import useShowOutreachPopup from '../useShowOutreachPopup'
+import * as useIsSafeOwner from '@/hooks/useIsSafeOwner'
+import * as store from '@/store'
+import { HOUR_IN_MS } from '../../constants'
+import { faker } from '@faker-js/faker/.'
+
+jest.mock('@/hooks/useIsSafeOwner')
+jest.mock('@/store')
+
+beforeEach(() => {
+  jest.mock('@/hooks/useSafeAddress', () => ({
+    default: jest.fn(() => faker.finance.ethereumAddress()),
+  }))
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('useShowOutreachPopup', () => {
+  it('should return false when the cookie banner is open', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: true }) // mock cookie banner state
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: null,
+    }
+
+    const { result } = renderHook(() => useShowOutreachPopup(false, undefined, submission))
+    expect(result.current).toEqual(false)
+  })
+
+  it('should return false for targeted safes that are already marked as completed', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: faker.date.recent().getTime().toString(),
+    }
+    const { result } = renderHook(() => useShowOutreachPopup(false, undefined, submission))
+    expect(result.current).toEqual(false)
+  })
+
+  it('should return false for non targeted safes', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = undefined
+
+    const { result } = renderHook(() => useShowOutreachPopup(false, undefined, submission))
+    expect(result.current).toEqual(false)
+  })
+
+  it('should return true for signers of targeted safes', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: null,
+    }
+
+    const { result } = renderHook(() => useShowOutreachPopup(false, undefined, submission))
+    expect(result.current).toEqual(true)
+  })
+
+  it('should return false if a targeted user has previously closed the popup', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: null,
+    }
+
+    const { result } = renderHook(() => useShowOutreachPopup(true, undefined, submission))
+    expect(result.current).toEqual(false)
+  })
+
+  it('should return false if the user has chosen ask me later within the same session and before the maximum delay', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: null,
+    }
+
+    const { result } = renderHook(() => useShowOutreachPopup(false, Date.now() - HOUR_IN_MS * 2, submission))
+    expect(result.current).toEqual(false)
+  })
+
+  it('should return true if the user has chosen ask me later within the same session but after the maximum delay of 24 hours', async () => {
+    jest.spyOn(useIsSafeOwner, 'default').mockReturnValueOnce(true)
+    jest.spyOn(store, 'useAppSelector').mockReturnValueOnce({ open: false })
+
+    const submission = {
+      outreachId: 1,
+      targetedSafeId: 1,
+      signerAddress: faker.finance.ethereumAddress(),
+      completionDate: null,
+    }
+
+    const { result } = renderHook(() => useShowOutreachPopup(false, Date.now() - HOUR_IN_MS * 25, submission))
+    expect(result.current).toEqual(true)
+  })
+})
