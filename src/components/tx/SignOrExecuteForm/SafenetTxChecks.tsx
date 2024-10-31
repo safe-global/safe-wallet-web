3c77e38710a9549a852f4c07d16873d37d59312e
Greg Jeanmart <greg@jeanmart.me>
2024-10-31 16:03:40 +0100

    lint

--- src/components/tx/SignOrExecuteForm/SafenetTxChecks.tsx
+++ src/components/tx/SignOrExecuteForm/SafenetTxChecks.tsx
@@ -1,12 +1,11 @@
-import { type ReactElement, useContext } from 'react'
+import { type ReactElement } from 'react'
 import { SafenetTxSimulation } from '@/components/tx/security/safenet'
 import TxCard from '@/components/tx-flow/common/TxCard'
-import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
 import useIsSafenetEnabled from '@/hooks/useIsSafenetEnabled'
 import { Typography } from '@mui/material'
 import useChainId from '@/hooks/useChainId'
 import useSafeAddress from '@/hooks/useSafeAddress'
-import { SafeTransaction } from '@safe-global/safe-core-sdk-types'
+import type { SafeTransaction } from '@safe-global/safe-core-sdk-types'
 
 const SafenetTxChecks = ({ safeTx }: { safeTx?: SafeTransaction }): ReactElement | null => {
   const safe = useSafeAddress()
