f784a9fd9a81e1e9b0b37c6652dd8fc8802dbb7b
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-11-06 13:52:11 +0100

    Safenet assets bubble (#4493)

--- src/utils/safenet.ts
+++ src/utils/safenet.ts
@@ -1,12 +1,24 @@
 import type { SafenetBalanceEntity, SafenetConfigEntity } from '@/store/safenet'
-import { TokenType, type SafeBalanceResponse } from '@safe-global/safe-gateway-typescript-sdk'
+import type { TokenInfo } from '@safe-global/safe-gateway-typescript-sdk'
+import { TokenType } from '@safe-global/safe-gateway-typescript-sdk'
+
+export type SafeBalanceResponseWithSafenet = {
+  fiatTotal: string
+  items: Array<{
+    tokenInfo: TokenInfo
+    balance: string
+    fiatBalance: string
+    fiatConversion: string
+    safenetAssetFlag?: boolean
+  }>
+}
 
 const convertSafenetBalanceToSafeClientGatewayBalance = (
   safenetBalance: SafenetBalanceEntity,
   safenetConfig: SafenetConfigEntity,
   chainId: number,
-): SafeBalanceResponse => {
-  const balances: SafeBalanceResponse = {
+): SafeBalanceResponseWithSafenet => {
+  const balances: SafeBalanceResponseWithSafenet = {
     fiatTotal: safenetBalance.fiatTotal,
     items: [],
   }
@@ -25,12 +37,13 @@ const convertSafenetBalanceToSafeClientGatewayBalance = (
         address: tokenAddress,
         decimals,
         symbol: tokenName,
-        name: `${tokenName} (Safenet)`,
+        name: tokenName,
         logoUri: `https://assets.smold.app/api/token/${chainId}/${tokenAddress}/logo-128.png`,
       },
       balance,
       fiatBalance: ((parseInt(balance) * 1) / 10 ** decimals).toString(),
       fiatConversion: '1.00',
+      safenetAssetFlag: true,
     })
   }
 
