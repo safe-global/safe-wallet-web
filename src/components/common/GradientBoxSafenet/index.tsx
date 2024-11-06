29a92ef3879cc7c338a0919c54deb8511b40ff4a
Nicholas Rodrigues Lordello <nick@safe.global>
2024-11-06 11:00:20 +0100

    Refactor Sidebar Gradient Box
    This PR refactors the sidebar to also use a gradient border. It pushes
    the drawer button down to not cover Safe information.

--- src/components/common/GradientBoxSafenet/index.tsx
+++ src/components/common/GradientBoxSafenet/index.tsx
@@ -7,18 +7,21 @@ const GradientBoxSafenet = ({
   children,
   className,
   style,
+  variant = 'full',
 }: {
   heading?: string
   children?: ReactNode
   className?: string
   style?: CSSProperties
+  variant?: 'full' | 'bottom'
 }) => {
+  const br = 'calc(var(--space-1) - 1px)'
   return (
     <Box
       className={className}
       style={{
         background: 'linear-gradient(90deg, #32f970 0%, #eed509 100%)',
-        borderRadius: 'calc(var(--space-1) - 1px)',
+        borderRadius: variant === 'full' ? br : `0 0 ${br} ${br}`,
         display: 'flex',
         alignItems: 'stretch',
         flexDirection: 'column',
@@ -26,26 +29,28 @@ const GradientBoxSafenet = ({
         ...style,
       }}
     >
-      <Box
-        style={{
-          color: 'var(--color-static-main)',
-          padding: 'calc(var(--space-1) / 2) var(--space-1)',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <SvgIcon component={SafenetIcon} inheritViewBox fontSize="small" />
-        <Typography variant="h5" fontSize="small">
-          {heading ?? 'Powered by Safenet'}
-        </Typography>
-      </Box>
+      {variant === 'full' && (
+        <Box
+          style={{
+            color: 'var(--color-static-main)',
+            padding: 'calc(var(--space-1) / 2) var(--space-1)',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <SvgIcon component={SafenetIcon} inheritViewBox fontSize="small" />
+          <Typography variant="h5" fontSize="small">
+            {heading ?? 'Powered by Safenet'}
+          </Typography>
+        </Box>
+      )}
       <Box
         style={{
           background: 'var(--color-background-paper)',
           borderRadius: '0 0 var(--space-1) var(--space-1)',
-          margin: '1px',
+          margin: '0 1px 1px 1px',
         }}
       >
         {children}
