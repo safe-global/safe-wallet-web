6ee151ceb6981cf556e7040edde7589ccf227ffe
schmanu <manu@safe.global>
2024-11-07 13:42:57 +0100

    fix: Safe app readOnly state for nested ownership

--- src/components/safe-apps/AppFrame/useGetSafeInfo.ts
+++ src/components/safe-apps/AppFrame/useGetSafeInfo.ts
@@ -1,24 +1,26 @@
-import { useMemo } from 'react'
+import { useCallback } from 'react'
 import useChainId from '@/hooks/useChainId'
 import { useCurrentChain } from '@/hooks/useChains'
 import useIsSafeOwner from '@/hooks/useIsSafeOwner'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import { getLegacyChainName } from '../utils'
+import { useNestedSafeOwners } from '@/hooks/useNestedSafeOwners'
 
 const useGetSafeInfo = () => {
   const { safe, safeAddress } = useSafeInfo()
   const isOwner = useIsSafeOwner()
+  const nestedSafeOwners = useNestedSafeOwners()
   const chainId = useChainId()
   const chain = useCurrentChain()
   const chainName = chain?.chainName || ''
 
-  return useMemo(
-    () => () => ({
+  return useCallback(() => {
+    return {
       safeAddress,
       chainId: parseInt(chainId, 10),
       owners: safe.owners.map((owner) => owner.value),
       threshold: safe.threshold,
-      isReadOnly: !isOwner,
+      isReadOnly: !isOwner && (nestedSafeOwners == null || nestedSafeOwners.length === 0),
       nonce: safe.nonce,
       implementation: safe.implementation.value,
       modules: safe.modules ? safe.modules.map((module) => module.value) : null,
@@ -26,22 +28,22 @@ const useGetSafeInfo = () => {
       guard: safe.guard?.value || null,
       version: safe.version,
       network: getLegacyChainName(chainName || '', chainId).toUpperCase(),
-    }),
-    [
-      chainId,
-      chainName,
-      isOwner,
-      safeAddress,
-      safe.owners,
-      safe.threshold,
-      safe.nonce,
-      safe.implementation,
-      safe.modules,
-      safe.fallbackHandler,
-      safe.guard,
-      safe.version,
-    ],
-  )
+    }
+  }, [
+    safeAddress,
+    chainId,
+    safe.owners,
+    safe.threshold,
+    safe.nonce,
+    safe.implementation.value,
+    safe.modules,
+    safe.fallbackHandler,
+    safe.guard?.value,
+    safe.version,
+    isOwner,
+    nestedSafeOwners,
+    chainName,
+  ])
 }
 
 export default useGetSafeInfo
