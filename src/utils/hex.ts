3c9af70ee7c9db0fa1dd8ad7e8a9d908d3d5e9e8
Aaron Cook <aaron@safe.global>
2024-11-26 13:21:22 +0100

    fix: update `wallet_getCallsStatus` spec.

--- src/utils/hex.ts
+++ src/utils/hex.ts
@@ -1,3 +1,3 @@
 export const isEmptyHexData = (encodedData: string): boolean => encodedData !== '' && isNaN(parseInt(encodedData, 16))
 
-export const numberToHex = (value: number) => `0x${value.toString(16)}`
+export const numberToHex = (value: number | bigint): `0x${string}` => `0x${value.toString(16)}`
