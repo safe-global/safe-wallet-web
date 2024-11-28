efd35591516a103cf7925feca85f40e308d9464b
Daniel Dimitrov <daniel@compojoom.com>
2024-11-28 18:03:15 +0100

    fix: crash in production (#44)
    The endpoint was returning more results, but we were not going
    after the next page. Instead we were agian polling the first
    page and this ended up in 429.

--- src/store/gateway/cgwClient.ts
+++ src/store/gateway/cgwClient.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { GATEWAY_URL } from '@/src/config/constants'
 
 export const cgwClient = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://safe-client.safe.global/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: GATEWAY_URL }),
   endpoints: () => ({}),
 })
