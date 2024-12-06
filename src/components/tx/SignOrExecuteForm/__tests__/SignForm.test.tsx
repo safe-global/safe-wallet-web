6d50adf423e02c208e1f0112e109974bc436879b
Usame Algan <usame@safe.global>
2024-12-06 16:16:36 +0100

    fix: Add more SignForm tests

--- src/components/tx/SignOrExecuteForm/__tests__/SignForm.test.tsx
+++ src/components/tx/SignOrExecuteForm/__tests__/SignForm.test.tsx
@@ -227,4 +227,29 @@ describe('SignForm', () => {
     expect(button).toBeInTheDocument()
     expect(button).not.toBeDisabled()
   })
+
+  it('Hides the Add to batch button if there is an origin', () => {
+    const { queryByText } = render(
+      <SignForm
+        {...defaultProps}
+        safeTx={safeTransaction}
+        txSecurity={{ ...defaultSecurityContextValues, needsRiskConfirmation: true, isRiskConfirmed: true }}
+        origin="MockOrigin"
+      />,
+    )
+
+    const button = queryByText('Add to batch')
+
+    expect(button).not.toBeInTheDocument()
+  })
+
+  it('Shows the Add to batch button if there is no origin and it is a creation', () => {
+    const { getByText } = render(
+      <SignForm {...defaultProps} safeTx={safeTransaction} origin={undefined} isCreation={true} />,
+    )
+
+    const button = getByText('Add to batch')
+
+    expect(button).toBeInTheDocument()
+  })
 })
