26dd98d66c108871554ce6476b48534b6c1e0d53
Usame Algan <usame@safe.global>
2024-12-06 15:49:32 +0100

    fix: Show hexData if there are no parameters

--- src/components/transactions/TxDetails/TxData/DecodedData/index.test.tsx
+++ src/components/transactions/TxDetails/TxData/DecodedData/index.test.tsx
@@ -9,12 +9,12 @@ describe('DecodedData', () => {
   })
 
   it('shows an Interact with block if there is no txData but toInfo', () => {
-    const { getByText } = render(<DecodedData txData={undefined} toInfo={{ value: '0x123', name: 'Test' }} />)
+    const { getByText } = render(<DecodedData txData={undefined} toInfo={{ value: '0x123' }} />)
 
     expect(getByText('Interact with')).toBeInTheDocument()
   })
 
-  it('shows Hex encoded data if no method name exists or there are no parameters', () => {
+  it('shows Hex encoded data if there are no parameters', () => {
     const mockTxData = {
       to: {
         value: '0x874E2190e6B10f5173F00E27E6D5D9F90b7664C4',
@@ -30,8 +30,48 @@ describe('DecodedData', () => {
       trustedDelegateCallTarget: false,
     }
 
-    const { getByText } = render(<DecodedData txData={mockTxData} toInfo={{ value: '0x123', name: 'Test' }} />)
+    const { getByText } = render(<DecodedData txData={mockTxData} toInfo={{ value: '0x123' }} />)
 
+    expect(getByText('No parameters')).toBeInTheDocument()
+    expect(getByText('Data (hex-encoded)')).toBeInTheDocument()
+  })
+
+  it('does not show Hex encoded data if there is none', () => {
+    const mockTxData = {
+      to: {
+        value: '0x874E2190e6B10f5173F00E27E6D5D9F90b7664C4',
+      },
+      value: '0',
+      operation: 0,
+      dataDecoded: {
+        method: 'mint',
+        parameters: [],
+      },
+      hexData: '',
+      trustedDelegateCallTarget: false,
+    }
+
+    const { getByText, queryByText } = render(<DecodedData txData={mockTxData} toInfo={{ value: '0x123' }} />)
+
+    expect(getByText('No parameters')).toBeInTheDocument()
+    expect(queryByText('Data (hex-encoded)')).not.toBeInTheDocument()
+  })
+
+  it('only shows Hex encoded data if no decodedData exists', () => {
+    const mockTxData = {
+      to: {
+        value: '0x874E2190e6B10f5173F00E27E6D5D9F90b7664C4',
+      },
+      value: '0',
+      operation: 0,
+      hexData:
+        '0x895a74850000000000000000000000000000000000000000000004bb752b4d22ab390000000000000000000000000000000000000000000000000000000000000000000b00000000000000000000000000000001f76adba2311f154678f5e5605db5c9c2',
+      trustedDelegateCallTarget: false,
+    }
+
+    const { getByText, queryByText } = render(<DecodedData txData={mockTxData} toInfo={{ value: '0x123' }} />)
+
+    expect(queryByText('No parameters')).not.toBeInTheDocument()
     expect(getByText('Data (hex-encoded)')).toBeInTheDocument()
   })
 })
