1643fb84181b006d06a426894389294a1b609bbd
James Mealy <james@safe.global>
2024-12-09 16:29:13 +0100

    fix: prevent unecessary requests when wallet is undefined and retry with backoff when gateway returns 503

--- src/features/myAccounts/hooks/useAllOwnedSafes.ts
+++ src/features/myAccounts/hooks/useAllOwnedSafes.ts
@@ -4,14 +4,14 @@ import useLocalStorage from '@/services/local-storage/useLocalStorage'
 import { useEffect } from 'react'
 import { useGetAllOwnedSafesQuery } from '@/store/api/gateway'
 import { asError } from '@/services/exceptions/utils'
+import { skipToken } from '@reduxjs/toolkit/query'
 
 const CACHE_KEY = 'ownedSafesCache_'
 
 const useAllOwnedSafes = (address: string): AsyncResult<AllOwnedSafes> => {
   const [cache, setCache] = useLocalStorage<AllOwnedSafes>(CACHE_KEY + address)
 
-  const { data, error, isLoading } = useGetAllOwnedSafesQuery({ walletAddress: address })
-  // const { data, error, isLoading } = useGetAllOwnedSafesQuery(address === '' ? skipToken : { walletAddress: address })
+  const { data, error, isLoading } = useGetAllOwnedSafesQuery(address === '' ? skipToken : { walletAddress: address })
 
   useEffect(() => {
     if (data != undefined) {
