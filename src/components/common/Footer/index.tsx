97726262dc794470e095403b0af6f222b93f7cda
ajimeno04 <jimenobeltran11@gmail.com>
2024-12-11 08:37:16 +0100

    feat: update footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,13 +1,14 @@
-import type { ReactElement } from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import { SvgIcon, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
 import ExternalLink from '../ExternalLink'
 import MUILink from '@mui/material/Link'
-import { HELP_CENTER_URL, IS_DEV, IS_OFFICIAL_HOST } from '@/config/constants'
+import { HELP_CENTER_URL } from '@/config/constants'
 import darkPalette from '@/components/theme/darkPalette'
 import ProtofireLogo from '@/public/images/protofire-logo.svg'
+import Link from 'next/link'
 
 const footerPages = [
   AppRoutes.welcome.index,
@@ -18,42 +19,57 @@ const footerPages = [
   AppRoutes.terms,
   AppRoutes.licenses,
 ]
-
+const FooterLink = ({ children, href }: { children: ReactNode; href: string }): ReactElement => {
+  return href ? (
+    <Link href={href} passHref legacyBehavior>
+      <MUILink>{children}</MUILink>
+    </Link>
+  ) : (
+    <MUILink>{children}</MUILink>
+  )
+}
 const Footer = (): ReactElement | null => {
   const router = useRouter()
 
   if (!footerPages.some((path) => router.pathname.startsWith(path))) {
     return null
   }
+  const getHref = (path: string): string => {
+    return router.pathname === path ? '' : path
+  }
 
   return (
     <footer className={css.container}>
       <ul>
-        {IS_OFFICIAL_HOST || IS_DEV ? (
-          <>
-            <li>
-              <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
-                Help
-              </ExternalLink>
-            </li>
-            <li>
-              <Typography variant="caption">
-                Supported by{' '}
-                <SvgIcon
-                  component={ProtofireLogo}
-                  inheritViewBox
-                  fontSize="small"
-                  sx={{ verticalAlign: 'middle', mx: 0.5 }}
-                />
-                <MUILink href="https://protofire.io" sx={{ color: darkPalette.primary.main, textDecoration: 'none' }}>
-                  Protofire
-                </MUILink>
-              </Typography>
-            </li>
-          </>
-        ) : (
-          <li>{'This is an unofficial distribution of Safe{Wallet}'}</li>
-        )}
+        <li>
+          <FooterLink href={getHref(AppRoutes.terms)}>Terms</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.cookie)}>Cookie policy</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
+        </li>
+
+        <li>
+          <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
+            Help
+          </ExternalLink>
+        </li>
+        <li>
+          <Typography variant="caption">
+            Supported by{' '}
+            <SvgIcon
+              component={ProtofireLogo}
+              inheritViewBox
+              fontSize="small"
+              sx={{ verticalAlign: 'middle', mx: 0.5 }}
+            />
+            <MUILink href="https://protofire.io" sx={{ color: darkPalette.primary.main, textDecoration: 'none' }}>
+              Protofire
+            </MUILink>
+          </Typography>
+        </li>
       </ul>
     </footer>
   )
