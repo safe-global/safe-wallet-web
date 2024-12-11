b7a4ebb79f94d9878efe89feeecfe565ddc1e658
ajimeno04 <jimenobeltran11@gmail.com>
2024-12-11 08:41:06 +0100

    feat: fix lint

--- src/components/settings/ProposersList/index.tsx
+++ src/components/settings/ProposersList/index.tsx
@@ -13,8 +13,6 @@ import { SETTINGS_EVENTS } from '@/services/analytics'
 import { FEATURES } from '@/utils/chains'
 import { Box, Button, Grid, Paper, SvgIcon, Typography } from '@mui/material'
 import EthHashInfo from '@/components/common/EthHashInfo'
-import ExternalLink from '@/components/common/ExternalLink'
-import { HelpCenterArticle } from '@/config/constants'
 import React, { useMemo, useState } from 'react'
 
 const headCells = [
