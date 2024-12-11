996080c483fa0c5ea8f760148cd080dbe86d616c
Daniel Dimitrov <daniel@compojoom.com>
2024-12-11 14:21:01 +0100

    refactor: switch to use yarn v4
    fix: github action crashing
    
    .
    
    fix: failing yarn v4 install due to incorrect 3rd party dependency
    
    fix: remove Keystone pckg causing yarn v4 failure
    
    fix: resolve merge conflict
    
    fix: resolutions
    
    fix: failing tests

--- src/hooks/wallets/wallets.ts
+++ src/hooks/wallets/wallets.ts
@@ -3,7 +3,6 @@ import type { ChainInfo } from '@safe-global/safe-gateway-typescript-sdk'
 import type { InitOptions } from '@web3-onboard/core'
 import coinbaseModule from '@web3-onboard/coinbase'
 import injectedWalletModule from '@web3-onboard/injected-wallets'
-import keystoneModule from '@web3-onboard/keystone/dist/index'
 import ledgerModule from '@web3-onboard/ledger/dist/index'
 import trezorModule from '@web3-onboard/trezor'
 import walletConnect from '@web3-onboard/walletconnect'
@@ -44,7 +43,6 @@ const WALLET_MODULES: Partial<{ [_key in WALLET_KEYS]: (chain: ChainInfo) => Wal
   [WALLET_KEYS.COINBASE]: () => coinbaseModule({ darkMode: prefersDarkMode() }) as WalletInit,
   [WALLET_KEYS.LEDGER]: () => ledgerModule() as WalletInit,
   [WALLET_KEYS.TREZOR]: () => trezorModule({ appUrl: TREZOR_APP_URL, email: TREZOR_EMAIL }) as WalletInit,
-  [WALLET_KEYS.KEYSTONE]: () => keystoneModule() as WalletInit,
   [WALLET_KEYS.PK]: (chain) => pkModule(chain.chainId, chain.rpcUri) as WalletInit,
 }
 
