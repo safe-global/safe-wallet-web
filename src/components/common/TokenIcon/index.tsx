63e561cb23284442d87f3c3b225e77d09ecace5f
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-12-12 15:28:28 +0100

    Fix: crossorigin anonymous for token icons + fix token pair arrow

--- src/components/common/TokenIcon/index.tsx
+++ src/components/common/TokenIcon/index.tsx
@@ -21,6 +21,7 @@ const TokenIcon = ({
       alt={tokenSymbol}
       fallbackSrc={fallbackSrc || FALLBACK_ICON}
       height={size}
+      crossOrigin="anonymous"
       className={css.image}
     />
   )
