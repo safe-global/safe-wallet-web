f4866da005964f520fd6656a6cea8191156c0eb4
Usame Algan <usame@safe.global>
2024-12-13 12:01:37 +0100

    refactor: Extract useAllSafesGrouped logic and write tests

--- src/features/myAccounts/hooks/useAllSafesGrouped.ts
+++ src/features/myAccounts/hooks/useAllSafesGrouped.ts
@@ -16,18 +16,28 @@ export type AllSafesGrouped = {
   allMultiChainSafes: MultiChainSafeItem[] | undefined
 }
 
-const getMultiChainAccounts = (safes: SafeItems): MultiChainSafeItem[] => {
+export const _buildMultiChainSafeItem = (address: string, safes: SafeItems): MultiChainSafeItem => {
+  const isPinned = safes.some((safe) => safe.isPinned)
+  const lastVisited = safes.reduce((acc, safe) => Math.max(acc, safe.lastVisited || 0), 0)
+  const name = safes.find((safe) => safe.name !== undefined)?.name
+
+  return { address, safes, isPinned, lastVisited, name }
+}
+
+export const _getMultiChainAccounts = (safes: SafeItems): MultiChainSafeItem[] => {
   const groupedByAddress = groupBy(safes, (safe) => safe.address)
-  const multiChainSafeItems = Object.entries(groupedByAddress)
+
+  return Object.entries(groupedByAddress)
     .filter((entry) => entry[1].length > 1)
     .map((entry) => {
       const [address, safes] = entry
-      const isPinned = safes.some((safe) => safe.isPinned)
-      const lastVisited = safes.reduce((acc, safe) => Math.max(acc, safe.lastVisited || 0), 0)
-      const name = safes.find((safe) => safe.name !== undefined)?.name
-      return { address, safes, isPinned, lastVisited, name }
+
+      return _buildMultiChainSafeItem(address, safes)
     })
-  return multiChainSafeItems
+}
+
+export const _getSingleChainAccounts = (safes: SafeItems, allMultiChainSafes: MultiChainSafeItem[]) => {
+  return safes.filter((safe) => !allMultiChainSafes.some((multiSafe) => sameAddress(multiSafe.address, safe.address)))
 }
 
 export const useAllSafesGrouped = () => {
@@ -37,11 +47,9 @@ export const useAllSafesGrouped = () => {
     if (!allSafes) {
       return { allMultiChainSafes: undefined, allSingleSafes: undefined }
     }
-    // Extract all multichain Accounts and single Safes
-    const allMultiChainSafes = getMultiChainAccounts(allSafes)
-    const allSingleSafes = allSafes.filter(
-      (safe) => !allMultiChainSafes.some((multiSafe) => sameAddress(multiSafe.address, safe.address)),
-    )
+
+    const allMultiChainSafes = _getMultiChainAccounts(allSafes)
+    const allSingleSafes = _getSingleChainAccounts(allSafes, allMultiChainSafes)
 
     return {
       allMultiChainSafes,
