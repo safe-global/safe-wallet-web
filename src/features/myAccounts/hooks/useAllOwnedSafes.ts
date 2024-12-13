49dfe626947034252185b46a93699a16403446b4
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2024-12-13 11:22:38 +0100

    Refactor sidebar hooks [SW-647] (#4641)
    * fix: Remove cache in useAllOwnedSafes
    
    * fix: Write tests for useAllSafes
    
    * fix: Add more tests for useAllSafes
    
    * refactor: Extract useAllSafes logic into separate functions
    
    * refactor: Extract useHasSafes, remove sorting, iterate over all chains
    
    * fix: Write test cases for buildSafeItem and prepareAddresses
    
    * fix: Implement tests for _prepareAddresses and _buildSafeItem
    
    * fix: Check undeployed safe owners to determine readOnly status
    
    * fix: Remove condition
    
    * fix: Remove isOwnerOfAddedSafe check

--- src/features/myAccounts/hooks/useAllOwnedSafes.ts
+++ src/features/myAccounts/hooks/useAllOwnedSafes.ts
@@ -1,25 +1,13 @@
 import type { AllOwnedSafes } from '@safe-global/safe-gateway-typescript-sdk'
 import type { AsyncResult } from '@/hooks/useAsync'
-import useLocalStorage from '@/services/local-storage/useLocalStorage'
-import { useEffect } from 'react'
 import { useGetAllOwnedSafesQuery } from '@/store/api/gateway'
 import { asError } from '@/services/exceptions/utils'
 import { skipToken } from '@reduxjs/toolkit/query'
 
-const CACHE_KEY = 'ownedSafesCache_'
-
 const useAllOwnedSafes = (address: string): AsyncResult<AllOwnedSafes> => {
-  const [cache, setCache] = useLocalStorage<AllOwnedSafes>(CACHE_KEY + address)
-
   const { data, error, isLoading } = useGetAllOwnedSafesQuery(address === '' ? skipToken : { walletAddress: address })
 
-  useEffect(() => {
-    if (data != undefined) {
-      setCache(data)
-    }
-  }, [data, setCache])
-
-  return address ? [cache, asError(error), isLoading] : [{}, undefined, false]
+  return [address ? data : undefined, asError(error), isLoading]
 }
 
 export default useAllOwnedSafes
