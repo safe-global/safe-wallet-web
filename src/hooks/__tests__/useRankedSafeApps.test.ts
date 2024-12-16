d61021da823c77f46a27f43ffa24936a25fcaf70
Aaron Cook <aaron@safe.global>
2024-12-16 12:01:56 +0000

    fix: rank featured Safe Apps first on dashboard (#4644)
    * fix: show featured Safe Apps first on dashboard
    
    * fix: add test coverage
    
    * fix: only return featured/pinned Safe Apps
    
    * Remove unnecessary test

--- src/hooks/__tests__/useRankedSafeApps.test.ts
+++ src/hooks/__tests__/useRankedSafeApps.test.ts
@@ -17,7 +17,7 @@ describe('useRankedSafeApps', () => {
   })
 
   it('returns 5 safe apps at most', () => {
-    const mockSafeApp1 = getMockSafeApp({ id: 1 })
+    const mockSafeApp1 = getMockSafeApp({ id: 1, featured: true } as Partial<SafeAppData>)
     const mockSafeApp2 = getMockSafeApp({ id: 2 })
     const mockSafeApp3 = getMockSafeApp({ id: 3 })
     const mockSafeApp4 = getMockSafeApp({ id: 4 })
@@ -25,16 +25,16 @@ describe('useRankedSafeApps', () => {
     const mockSafeApp6 = getMockSafeApp({ id: 6 })
 
     const { result } = renderHook(() =>
-      useRankedSafeApps([mockSafeApp1, mockSafeApp2, mockSafeApp3, mockSafeApp4, mockSafeApp5, mockSafeApp6], []),
+      useRankedSafeApps([mockSafeApp1], [mockSafeApp2, mockSafeApp3, mockSafeApp4, mockSafeApp5, mockSafeApp6]),
     )
 
     expect(result.current.length).toEqual(5)
   })
 
-  it('returns pinned apps first', () => {
+  it('returns featured, then pinned apps', () => {
     const mockSafeApp1 = getMockSafeApp({ id: 1 })
-    const mockSafeApp2 = getMockSafeApp({ id: 2 })
-    const mockSafeApp3 = getMockSafeApp({ id: 3 })
+    const mockSafeApp2 = getMockSafeApp({ id: 2, featured: true } as Partial<SafeAppData>)
+    const mockSafeApp3 = getMockSafeApp({ id: 3, featured: true } as Partial<SafeAppData>)
     const mockSafeApp4 = getMockSafeApp({ id: 4 })
     const mockSafeApp5 = getMockSafeApp({ id: 5 })
 
@@ -48,7 +48,9 @@ describe('useRankedSafeApps', () => {
       ),
     )
 
-    expect(result.current[0]).toStrictEqual(mockPinnedApp1)
-    expect(result.current[1]).toStrictEqual(mockPinnedApp2)
+    expect(result.current[0]).toStrictEqual(mockSafeApp2)
+    expect(result.current[1]).toStrictEqual(mockSafeApp3)
+    expect(result.current[2]).toStrictEqual(mockPinnedApp1)
+    expect(result.current[3]).toStrictEqual(mockPinnedApp2)
   })
 })
