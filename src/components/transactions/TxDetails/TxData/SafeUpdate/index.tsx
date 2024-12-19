5f34605fc7eb175d0faa899259f151ddcc9c7768
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-12-19 10:41:13 +0100

    Don't show version in history

--- src/components/transactions/TxDetails/TxData/SafeUpdate/index.tsx
+++ src/components/transactions/TxDetails/TxData/SafeUpdate/index.tsx
@@ -1,12 +1,21 @@
-import { Stack } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import type { TransactionData } from '@safe-global/safe-gateway-typescript-sdk'
-import UpdateSafe from '@/components/tx/confirmation-views/UpdateSafe'
 import DecodedData from '../DecodedData'
 
 function SafeUpdate({ txData }: { txData?: TransactionData }) {
   return (
     <Stack mr={5} spacing={2}>
-      <UpdateSafe />
+      <Box
+        bgcolor="border.background"
+        p={2}
+        textAlign="center"
+        fontWeight={700}
+        fontSize={18}
+        borderRadius={1}
+        width="100%"
+      >
+        Safe version update
+      </Box>
 
       <DecodedData txData={txData} toInfo={txData?.to} />
     </Stack>
